3ca4dce814c17856f604cacfdb4f08d99eb5b534
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2020-06-09 12:37:45 +0200

    updated version

--- Installomator.sh
+++ Installomator.sh
@@ -8,8 +8,8 @@
 # inspired by the download scripts from William Smith and Sander Schram
 # with additional ideas and contribution from Isaac Ordonez, Mann consulting
 
-VERSION='0.2'
-VERSIONDATE='20200529'
+VERSION='0.3'
+VERSIONDATE='20200609'
 
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
