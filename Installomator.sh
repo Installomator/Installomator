8b1298a14829189e068606ebb1d192ca0ead5b17
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2020-06-10 09:53:38 +0200

    changed googlechromepkg downloadURL so it follows google's recommendation

--- Installomator.sh
+++ Installomator.sh
@@ -217,7 +217,11 @@ case $label in
     googlechromepkg)
         name="Google Chrome"
         type="pkg"
-        downloadURL="https://dl.google.com/chrome/mac/stable/gcem/GoogleChrome.pkg"
+        #
+        # Note: this url acknowledges that you accept the terms of service
+        # https://support.google.com/chrome/a/answer/9915669
+        #
+        downloadURL="https://dl.google.com/chrome/mac/stable/accept_tos%3Dhttps%253A%252F%252Fwww.google.com%252Fintl%252Fen_ph%252Fchrome%252Fterms%252F%26_and_accept_tos%3Dhttps%253A%252F%252Fpolicies.google.com%252Fterms/googlechrome.pkg"
         expectedTeamID="EQHXZ8M8AV"
         updateTool="/Library/Google/GoogleSoftwareUpdate/GoogleSoftwareUpdate.bundle/Contents/Resources/GoogleSoftwareUpdateAgent.app/Contents/MacOS/GoogleSoftwareUpdateAgent"
         updateToolArguments=( -runMode oneshot -userInitiated YES )
