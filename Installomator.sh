bbeccfce1b82932263564d47a5c676bdace65e24
kenchan0130 <tt.tanishi100@gmail.com>
2020-07-06 18:42:45 +0900

    Support BetterTouchTool install

--- Installomator.sh
+++ Installomator.sh
@@ -679,6 +679,12 @@ case $label in
         downloadURL="https://updates.cdn-apple.com/2019/cert/061-41823-20191025-5efc5a59-d7dc-46d3-9096-396bb8cb4a73/SwiftRuntimeForCommandLineTools.dmg"
         expectedTeamID="Software Update"
         ;;
+    bettertouchtool)
+        name="BetterTouchTool"
+        type="zip"
+        downloadURL="https://folivora.ai/releases/BetterTouchTool.zip"
+        expectedTeamID="DAFVSXZ82P"
+        ;;
 
 
 #    Note: Packages is signed but _not_ notarized, so spctl will reject it
