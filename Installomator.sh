7f264733a7cd33af296b6cf79ad7c59aacdcfe70
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2020-08-09 16:53:51 +0200

    added snagit2020

--- Installomator.sh
+++ Installomator.sh
@@ -784,6 +784,13 @@ camtasia)
     downloadURL=https://download.techsmith.com/camtasiamac/releases/Camtasia.dmg
     expectedTeamID="7TQL462TU8"
     ;;
+snagit2020)
+    # credit: Isaac Ordonez, Mann consulting (@mannconsulting)
+    name="Snagit 2020"
+    type="dmg"
+    downloadURL="https://download.techsmith.com/snagitmac/releases/Snagit.dmg"
+    expectedTeamID="7TQL462TU8"
+    ;;
 
 
 # MARK: add new labels above here
