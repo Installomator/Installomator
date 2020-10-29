287cfe79d7459a447ff384eaebf238bbe1eeecaf
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2020-10-29 09:53:30 +0100

    added ricohpsprinters

--- Installomator.sh
+++ Installomator.sh
@@ -1292,7 +1292,13 @@ bluejeans)
 	downloadURL=$(curl -fs "https://www.bluejeans.com/downloads" | xmllint --html --format - 2>/dev/null | grep -o "https://.*BlueJeansInstaller.dmg" | sed 's/dmg/pkg/g')
 	expectedTeamID="HE4P42JBGN"
 	;;
-
+ricohpsprinters)
+    # credit: Isaac Ordonez, Mann consulting (@mannconsulting)
+	name="Ricoh PS Printers"
+	type="pkgInDmg"
+	downloadURL=$(curl -fs https://support.ricoh.com//bb/html/dr_ut_e/rc3/model/mpc3004ex/mpc3004exen.htm | xmllint --html --format - 2>/dev/null | grep -m 1 -o "https://.*.dmg" | cut -d '"' -f 1)
+	expectedTeamID="5KACUT3YX8"
+	;;
 
 # MARK: add new labels above here
 
