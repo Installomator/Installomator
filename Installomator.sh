b72eb3188ae40f37a196bb53b8979f1b507e3872
Søren Theilgaard <soren@theilgaard.dk>
2021-01-06 15:12:07 +0100

    hazel label

--- Installomator.sh
+++ Installomator.sh
@@ -1748,6 +1748,14 @@ etrecheck)
     downloadURL="https://cdn.etrecheck.com/EtreCheckPro.zip"
     expectedTeamID="U87NE528LC"
     ;;
+hazel)
+    # credit: Søren Theilgaard (@theilgaard)
+    name="Hazel"
+    type="dmg"
+    downloadURL=$(curl -fsI https://www.noodlesoft.com/Products/Hazel/download | grep -i "^location" | awk '{print $2}' | tr -d '\r\n')
+    appNewVersion=$(curl -fsI https://www.noodlesoft.com/Products/Hazel/download | grep -i "^location" | awk '{print $2}' | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
+    expectedTeamID="86Z3GCJ4MF"
+    ;;
 
 
 # MARK: add new labels above here
