7d9ba19c17b41d5b540affd838be89c126d1be64
Søren Theilgaard <soren@theilgaard.dk>
2021-01-06 15:10:28 +0100

    devonthink label
    This is an app in a dmg in a zip. Demands handling of type appInDmgInZip in function installAppInDmgInZip.

--- Installomator.sh
+++ Installomator.sh
@@ -1748,6 +1748,14 @@ etrecheck)
     downloadURL="https://cdn.etrecheck.com/EtreCheckPro.zip"
     expectedTeamID="U87NE528LC"
     ;;
+devonthink) # It's a zipped dmg file, needs function installAppInDmgInZip
+    # credit: Søren Theilgaard (@theilgaard)
+    name="DEVONthink 3"
+    type="appInDmgInZip"
+    downloadURL=$( curl -fs https://www.devontechnologies.com/apps/devonthink | grep -i "download.devon" | tr '"' '\n' | tr "'" '\n' | grep -e '^https://' )
+    appNewVersion=$( echo ${downloadURL} | tr '/' '\n' | grep "[0-9]" | grep "[.]" | head -1 )
+    expectedTeamID="679S2QUWR8"
+    ;;
 
 
 # MARK: add new labels above here
