d1e6513234a06b47583136c5838098125b423b26
Søren Theilgaard <soren@theilgaard.dk>
2021-01-08 13:34:37 +0100

    with architecture in log

--- Installomator.sh
+++ Installomator.sh
@@ -1034,9 +1034,11 @@ brave)
     # credit: Søren Theilgaard @theilgaard
     name="Brave Browser"
     type="dmg"
-    if [[ $(arch) == "arm64" ]]; then
+    if [[ $(arch) != "i386" ]]; then
+        printlog "Architecture: arm64 (not i386)"
         downloadURL=$(curl -fsIL https://laptop-updates.brave.com/latest/osxarm64/release | grep -i "^location" | awk '{print $2}' | tr -d '\r\n')
-    elif [[ $(arch) == "i386" ]]; then
+    else
+        printlog "Architecture: i386"
         downloadURL=$(curl -fsIL https://laptop-updates.brave.com/latest/osx/release | grep -i "^location" | awk '{print $2}' | tr -d '\r\n')
     fi
     appNewVersion=$(curl --location --fail --silent "https://updates.bravesoftware.com/sparkle/Brave-Browser/stable/appcast.xml" | xpath '//rss/channel/item[last()]/enclosure/@sparkle:shortVersionString' 2>/dev/null  | cut -d '"' -f 2)
