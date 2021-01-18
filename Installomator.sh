266acb75290feeb0db2d044d042b3d84e5670b7f
Søren Theilgaard <soren@theilgaard.dk>
2021-01-18 23:42:21 +0100

    notion
    Label

--- Installomator.sh
+++ Installomator.sh
@@ -1748,6 +1748,18 @@ etrecheck)
     downloadURL="https://cdn.etrecheck.com/EtreCheckPro.zip"
     expectedTeamID="U87NE528LC"
     ;;
+notion)
+    # credit: Søren Theilgaard (@theilgaard)
+    name="Notion"
+    type="dmg"
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL="https://www.notion.so/desktop/apple-silicon/download"
+    elif [[ $(arch) == "i386" ]]; then
+        downloadURL="https://www.notion.so/desktop/mac/download"
+    fi
+    appNewVersion=$( curl -fsIL "${downloadURL}" | grep -i "^location" | awk '{print $2}' | tr -d '\r\n' | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g' )
+    expectedTeamID="LBQJ96FQ8D"
+    ;;
 
 
 # MARK: add new labels above here
