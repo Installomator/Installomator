72f68a52d6b74186fc037062b16fe3a9fa2a1cae
Kris Anderson <1562427+kris-anderson@users.noreply.github.com>
2021-02-09 19:10:44 -0800

    run editorconfig against sh and zsh scripts

--- buildCaseStatement.sh
+++ buildCaseStatement.sh
@@ -6,9 +6,8 @@ downloadURL=${1?:"need to provide a download URL"}
 
 # Note: this tool _very_ experimental and does not work in many cases
 
-
 # create temporary working directory
-tmpDir=$(dirname $0 )
+tmpDir=$(dirname $0)
 
 # change directory to temporary working directory
 echo "Changing directory to $tmpDir"
@@ -25,45 +24,45 @@ if ! curl --location --fail --silent "$downloadURL" --remote-header-name --remot
     exit 2
 fi
 
-archivePath=$(find $tmpDir -print )
+archivePath=$(find $tmpDir -print)
 archiveName=${archivePath##*/}
 name=${archiveName%.*}
 archiveExt=${archiveName##*.}
 identifier=$(echo $name | tr '[:upper:]' '[:lower:]')
 
 if [ "$archiveExt" = "pkg" ]; then
-    teamID=$(spctl -a -vv -t install "$archiveName" 2>&1 | awk '/origin=/ {print $NF }' | tr -d '()' )
+    teamID=$(spctl -a -vv -t install "$archiveName" 2>&1 | awk '/origin=/ {print $NF }' | tr -d '()')
 elif [ "$archiveExt" = "dmg" ]; then
     # mount the dmg
     echo "Mounting $archiveName"
-    if ! dmgmount=$(echo "Y"$'\n' | hdiutil attach "$archiveName" -nobrowse -readonly | tail -n 1 | cut -c 54- ); then
+    if ! dmgmount=$(echo "Y"$'\n' | hdiutil attach "$archiveName" -nobrowse -readonly | tail -n 1 | cut -c 54-); then
         echo "Error mounting $archiveName"
         exit 3
     fi
     echo "Mounted: $dmgmount"
     # check if app exists
-    
-    appPath=$(find "$dmgmount" -name "*.app" -maxdepth 1 -print )
+
+    appPath=$(find "$dmgmount" -name "*.app" -maxdepth 1 -print)
     appName=${appPath##*/}
 
     # verify with spctl
     echo "Verifying: $appPath"
-    if ! teamID=$(spctl -a -vv "$appPath" 2>&1 | awk '/origin=/ {print $NF }'  | tr -d '()' ); then
+    if ! teamID=$(spctl -a -vv "$appPath" 2>&1 | awk '/origin=/ {print $NF }' | tr -d '()'); then
         echo "Error verifying $appPath"
         exit 4
     fi
-    
+
     hdiutil detach "$dmgmount"
 elif [ "$archiveExt" = "zip" ] || [ "$archiveExt" = "tbz" ]; then
     # unzip the archive
     tar -xf "$archiveName"
-    
+
     # check if app exists
-    appPath=$(find "$tmpDir" -name "*.app" -maxdepth 2 -print )
+    appPath=$(find "$tmpDir" -name "*.app" -maxdepth 2 -print)
     appName=${appPath##*/}
     # verify with spctl
     echo "Verifying: $appPath"
-    if ! teamID=$(spctl -a -vv "$appPath" 2>&1 | awk '/origin=/ {print $NF }'  | tr -d '()' ); then
+    if ! teamID=$(spctl -a -vv "$appPath" 2>&1 | awk '/origin=/ {print $NF }' | tr -d '()'); then
         echo "Error verifying $appPath"
         exit 4
     fi
@@ -77,10 +76,10 @@ echo "        type=\"$archiveExt\""
 echo "        downloadURL=\"$downloadURL\""
 echo "        expectedTeamID=\"$teamID\""
 if [ -n "$appName" ] && [ "$appName" != "${name}.app" ]; then
-echo "        appName=\"$appName\""
+    echo "        appName=\"$appName\""
 fi
 echo "        ;;"
-echo 
+echo
 
 #if [ -e "${tmpDir}" ]; then
 #    #echo "deleting tmp dir"
