ace185ba3d280e7b7bbc6865a78fe23fb5cea682
Søren Theilgaard <soren@theilgaard.dk>
2021-03-31 11:35:51 +0200

    0.5

--- Installomator.sh
+++ Installomator.sh
@@ -775,8 +775,8 @@ longversion)
     downloadURL="https://app-updates.agilebits.com/download/OPM7"
     appNewVersion=$( curl -fsIL "${downloadURL}" | grep -i "^location" | awk '{print $2}' | sed -E 's/.*\/[0-9a-zA-Z]*-([0-9.]*)\..*/\1/g' )
     expectedTeamID="2BUA8C4S2C"
-    blockingProcesses=( "1Password Extension Helper" "1Password 7" )
-    forcefulQuit=YES
+    blockingProcesses=( "1Password Extension Helper" "1Password 7" "1Password (Safari)" "1PasswordNativeMessageHost" "1PasswordSafariAppExtension" )
+    #forcefulQuit=YES
     #Company="Agilebits"
     ;;
 8x8)
