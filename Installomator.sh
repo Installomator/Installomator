ba6d88709849427c123c03814db8a938371a897c
Adrian Bühler <48823479+midni9ht@users.noreply.github.com>
2021-05-07 12:10:14 +0200

    Updated 'webexteams' for Apple Silicon support
    'Webex Teams' changed its application name to just 'Webex'.
    Also there is an Apple Silicon version available now.
    Should solve #156

--- Installomator.sh
+++ Installomator.sh
@@ -2582,9 +2582,13 @@ webexmeetings)
     ;;
 webexteams)
     # credit: Erik Stam (@erikstam)
-    name="Webex Teams"
+    name="Webex"
     type="dmg"
-    downloadURL="https://binaries.webex.com/WebexTeamsDesktop-MACOS-Gold/WebexTeams.dmg"
+    if [[ $(arch) == arm64 ]]; then
+        downloadURL="https://binaries.webex.com/WebexDesktop-MACOS-Apple-Silicon-Gold/Webex.dmg"
+    elif [[ $(arch) == i386 ]]; then
+        downloadURL="https://binaries.webex.com/WebexTeamsDesktop-MACOS-Gold/Webex.dmg"
+    fi
     expectedTeamID="DE8Y96K9QP"
     ;;
 whatsapp)
