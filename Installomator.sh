ff72445dd46e22484fd6b48feec6df458bf7a819
Søren Theilgaard <soren@theilgaard.dk>
2021-05-20 16:14:46 +0200

    arq7

--- Installomator.sh
+++ Installomator.sh
@@ -1012,6 +1012,14 @@ aquaskk)
     #Company="Code First"
     #PatchSkip="YES"
     ;;
+arq7)
+    name="Arq7"
+    type="pkg"
+    packageID="com.haystacksoftware.Arq"
+    downloadURL="https://arqbackup.com/download/arqbackup/Arq7.pkg"
+    appNewVersion="$(curl -fs "https://arqbackup.com" | grep -io "version .*[0-9.]*.* for macOS" | cut -d ">" -f2 | cut -d "<" -f1)"
+    expectedTeamID="48ZCSDVL96"
+    ;;
 atom)
     name="Atom"
     type="zip"
