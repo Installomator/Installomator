3f16926faaba6558b95c0968bf66e2b15a0d1c8a
Søren Theilgaard <soren@theilgaard.dk>
2021-06-11 09:21:48 +0200

    label githubdesktop
    Now for arm and intel versions

--- Installomator.sh
+++ Installomator.sh
@@ -1522,7 +1522,11 @@ gimp)
 githubdesktop)
     name="GitHub Desktop"
     type="zip"
-    downloadURL="https://central.github.com/deployments/desktop/desktop/latest/darwin"
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL="https://central.github.com/deployments/desktop/desktop/latest/darwin-arm64"
+    elif [[ $(arch) == "i386" ]]; then
+        downloadURL="https://central.github.com/deployments/desktop/desktop/latest/darwin"
+    fi
     appNewVersion=$(curl -fsL https://central.github.com/deployments/desktop/desktop/changelog.json | awk -F '{' '/"version"/ { print $2 }' | sed -E 's/.*,\"version\":\"([0-9.]*)\".*/\1/g')
     expectedTeamID="VEKTX9H2N7"
     ;;
