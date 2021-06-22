d733bbdb0d06eceb7a418a9f1bb9d201b9e9a839
Tadayuki Onishi <tonishi@folio-sec.com>
2021-06-22 11:28:58 +0900

    Add Install Google Software Update

--- Installomator.sh
+++ Installomator.sh
@@ -1575,6 +1575,14 @@ googlejapaneseinput)
     blockingProcesses=( NONE )
     expectedTeamID="EQHXZ8M8AV"
     ;;
+googlesoftwareupdate)
+    name="Install Google Software Update"
+    type="pkgInDmg"
+    pkgName="Install Google Software Update.app/Contents/Resources/GSUInstall.pkg"
+    downloadURL="https://dl.google.com/mac/install/googlesoftwareupdate.dmg"
+    blockingProcesses=( NONE )
+    expectedTeamID="EQHXZ8M8AV"
+    ;;
 gotomeeting)
     # credit: @matins
     name="GoToMeeting"
