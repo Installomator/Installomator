5410739f99fec3395c36b42e9e1d28a11d6ddfe6
Søren Theilgaard <soren@theilgaard.dk>
2021-06-28 12:52:46 +0200

    anydesk

--- Installomator.sh
+++ Installomator.sh
@@ -950,6 +950,13 @@ androidfiletransfer)
     downloadURL="https://dl.google.com/dl/androidjumper/mtp/current/AndroidFileTransfer.dmg"
     expectedTeamID="EQHXZ8M8AV"
     ;;
+anydesk)
+    name="AnyDesk"
+    type="dmg"
+    downloadURL="https://download.anydesk.com/anydesk.dmg"
+    appNewVersion="$(curl -fs https://anydesk.com/da/downloads/mac-os | grep -i "d-block" | grep -E -o ">v[0-9.]* .*MB" | sed -E 's/.*v([0-9.]*) .*/\1/g')"
+    expectedTeamID="KU6W3B6JMZ"
+    ;;
 apparency)
     name="Apparency"
     type="dmg"
