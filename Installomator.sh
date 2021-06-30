d42ecaf82cc9b2ac1755a48eeb522f8ba5a01098
Søren Theilgaard <soren@theilgaard.dk>
2021-06-30 08:59:33 +0200

    favro

--- Installomator.sh
+++ Installomator.sh
@@ -1397,6 +1397,13 @@ fantastical)
     appNewVersion=$( curl -fsIL "${downloadURL}" | grep -i "^location" | awk '{print $2}' | sed -E 's/.*\/[a-zA-Z]*_([0-9.]*)\..*/\1/g' )
     expectedTeamID="85C27NK92C"
     ;;
+favro)
+    name="Favro"
+    type="dmg"
+    downloadURL="https://download.favro.com/FavroDesktop/macOS/x64/$(curl -fs https://download.favro.com/FavroDesktop/macOS/x64/Latest.html | cut -d ">" -f1 | cut -d "=" -f 4 | cut -d '"' -f1)"
+    appNewVersion="$(curl -fs https://download.favro.com/FavroDesktop/macOS/x64/Latest.html | cut -d ">" -f1 | cut -d "=" -f 4 | cut -d '"' -f1 | sed -E 's/.*-([0-9.]*)\.dmg/\1/g')"
+    expectedTeamID="PUA8Q354ZF"
+    ;;
 ferdi)
     # credit: Adrian Bühler (@midni9ht)
     name="Ferdi"
