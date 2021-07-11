d5c18bdfedf9c38f225bf46ab63f39be3c12a265
Gabe Marchan <darklink87@yahoo.com>
2021-07-11 13:04:16 -0500

    Added Audacity.

--- Installomator.sh
+++ Installomator.sh
@@ -1022,6 +1022,14 @@ atom)
     appNewVersion=$(versionFromGit atom atom)
     expectedTeamID="VEKTX9H2N7"
     ;;
+audacity)
+    # credit: Gabe Marchan (gabemarchan.com - @darklink87)
+    name="Audacity"
+    type="dmg"
+    downloadURL=$(downloadURLFromGit audacity audacity)
+    appNewVersion=$(versionFromGit audacity audacity)
+    expectedTeamID="T3N4JQ7YY6"
+    ;;
 autodmg)
     # credit: Mischa van der Bent (@mischavdbent)
     name="AutoDMG"
