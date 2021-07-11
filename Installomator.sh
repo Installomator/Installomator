b5d46339ac1793e68824d708d50e093d78752262
Gabe Marchan <darklink87@yahoo.com>
2021-07-11 13:08:11 -0500

    Added Balsamiq Wireframes.

--- Installomator.sh
+++ Installomator.sh
@@ -1092,6 +1092,13 @@ balenaetcher)
     appNewVersion=$(versionFromGit balena-io etcher )
     expectedTeamID="66H43P8FRG"
     ;;
+balsamiqwireframes)
+    # credit: Gabe Marchan (gabemarchan.com - @darklink87)
+    name="Balsamiq Wireframes"
+    type="dmg"
+    downloadURL=https://builds.balsamiq.com/bwd/$(curl -fs "https://builds.balsamiq.com" | awk -F "<Key>bwd/" "/dmg/ {print \$3}" | awk -F "</Key>" "{print \$1}" | sed "s/ /%20/g")
+    expectedTeamID="3DPKD72KQ7"
+    ;;
 basecamp3)
     #credit: @matins
     name="Basecamp 3"
