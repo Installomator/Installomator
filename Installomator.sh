da174ecf9220dd89f42bc9ee09cfe2c2009a8c82
Gabe Marchan <darklink87@yahoo.com>
2021-07-11 16:29:28 -0500

    Added OBS.

--- Installomator.sh
+++ Installomator.sh
@@ -2102,6 +2102,13 @@ nvivo)
     expectedTeamID="A66L57342X"
     blockingProcesses=( NVivo NVivoHelper )
     ;;
+obs)
+    # credit: Gabe Marchan (gabemarchan.com - @darklink87)
+    name="OBS"
+    type="dmg"
+    downloadURL=$(curl -fs "https://obsproject.com/download" | awk -F '"' "/dmg/ {print \$10}")
+    expectedTeamID="2MMRE5MTB8"
+    ;;
 obsidian)
     # credit: Søren Theilgaard (@theilgaard)
     name="Obsidian"
