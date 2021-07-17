fd51bb8fcf3a494efc7f11d3921199a53263b107
Greg R <60526422+imfromthebay@users.noreply.github.com>
2021-07-17 12:21:28 -0700

    Adding ScaleFT (Okta ASA)
    Adding support for the ScaleFT (Okta ASA) agent.

--- Installomator.sh
+++ Installomator.sh
@@ -2470,6 +2470,14 @@ santa)
     appNewVersion=$(versionFromGit google santa)
     expectedTeamID="EQHXZ8M8AV"
     ;;
+scaleft)
+    name="ScaleFT"
+    type="pkg"
+    downloadURL="https://dist.scaleft.com/client-tools/mac/latest/ScaleFT.pkg"
+    appNewVersion=$(curl -sf "https://dist.scaleft.com/client-tools/mac/" | awk '/dir/{i++}i==2' | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')
+    expectedTeamID="HV2G9Z3RP5"
+    blockingProcesses=( ScaleFT )
+    ;;
 screamingfrogseospider)
     name="Screaming Frog SEO Spider"
     type="dmg"
