feb8570a3ff24c20730cf4a8d59301d615205e55
Søren Theilgaard <soren@theilgaard.dk>
2021-07-18 21:07:31 +0200

    Update Installomator.sh

--- Installomator.sh
+++ Installomator.sh
@@ -1338,6 +1338,7 @@ defaultfolderx)
     name="Default Folder X"
     type="dmg"
     downloadURL=$(curl -fs "https://www.stclairsoft.com/cgi-bin/dl.cgi?DX" | awk -F '"' "/dmg/ {print \$4}" | head -2 | tail -1)
+    appNewVersion=$(echo "${downloadURL}" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
     expectedTeamID="7HK42V8R9D"
     ;;
 depnotify)
