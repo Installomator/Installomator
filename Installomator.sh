714b96979dee9629f7978043b93119642c7a5497
Søren Theilgaard <soren@theilgaard.dk>
2021-07-18 21:13:44 +0200

    alttab with version
    `appNewVersion`on AltTab.

--- Installomator.sh
+++ Installomator.sh
@@ -968,6 +968,7 @@ alttab)
     name="AltTab"
     type="zip"
     downloadURL=$(downloadURLFromGit lwouis alt-tab-macos)
+    appNewVersion=$(versionFromGit lwouis alt-tab-macos)
     expectedTeamID="QXD7GW8FHY"
     ;;
 amazonchime)
