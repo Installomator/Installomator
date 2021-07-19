ee1023e4357c77e04c1437fa65da22eef2a2652f
Adrian Bühler <48823479+adibue@users.noreply.github.com>
2021-07-19 10:30:16 +0200

    Got 'hyper' ready for Apple Silicon
    `hyper` now supports Apple Silicon

--- Installomator.sh
+++ Installomator.sh
@@ -1772,6 +1772,11 @@ hpeasystart)
 hyper)
     name="Hyper"
     type="dmg"
+    if [[ $(arch) == i386 ]]; then
+      archiveName="mac-x64.dmg"
+    elif [[ $(arch) == arm64 ]]; then
+      archiveName="mac-arm64.dmg"
+    fi
     downloadURL=$(downloadURLFromGit vercel hyper )
     appNewVersion=$(versionFromGit vercel hyper)
     expectedTeamID="JW6Y669B67"
