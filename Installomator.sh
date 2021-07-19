80d71b18fadfc8322246a145a1abb5aaed536111
Adrian Bühler <48823479+adibue@users.noreply.github.com>
2021-07-19 10:39:19 +0200

    Got 'ferdi' ready for Apple Silicon
    `ferdi` now supports Apple Silicon.

--- Installomator.sh
+++ Installomator.sh
@@ -1480,9 +1480,16 @@ favro)
     expectedTeamID="PUA8Q354ZF"
     ;;
 ferdi)
+    # credit: Adrian Bühler (@midni9ht)
     name="Ferdi"
-    type="dmg"
-    downloadURL=$(downloadURLFromGit getferdi ferdi )
+    type="zip"
+    if [[ $(arch) == i386 ]]; then
+      downloadURL=$(curl --silent --fail "https://api.github.com/repos/getferdi/ferdi/releases/latest" \
+      | awk -F '"' "/browser_download_url/ && /mac.zip/ && ! /blockmap/ && ! /arm64-mac/ && ! /AppImage/{ print \$4 }")
+    elif [[ $(arch) == arm64 ]]; then
+      downloadURL=$(downloadURLFromGit getferdi ferdi )
+      archiveName="arm64-mac.zip"
+    fi
     appNewVersion=$(versionFromGit getferdi ferdi )
     expectedTeamID="B6J9X9DWFL"
     ;;
