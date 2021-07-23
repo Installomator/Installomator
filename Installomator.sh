7cadeb93c69d7b8382e9b009c2c8c09676f1a93f
AP Orlebeke <aporlebeke@gmail.com>
2021-07-23 11:55:40 -0400

    Add JetBrains DataGrip

--- Installomator.sh
+++ Installomator.sh
@@ -1878,6 +1878,18 @@ jamfreenroller)
     #appNewVersion=$(versionFromGit jamf ReEnroller)
     expectedTeamID="PS2F6S478M"
     ;;
+jetbrainsdatagrip)
+    # credit: AP Orlebeke (@apizz)
+    name="DataGrip"
+    type="dmg"
+    appNewVersion=$(curl -fs "https://data.services.jetbrains.com/products/releases?code=DG&latest=true&type=release" | grep -o 'version*.*,' | cut -d '"' -f3)
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL=$(curl -fs "https://data.services.jetbrains.com/products/releases?code=DG&latest=true&type=release" | grep -o 'macM1*.*,' | cut -d '"' -f5)
+    elif [[ $(arch) == "i386" ]]; then
+        downloadURL=$(curl -fs "https://data.services.jetbrains.com/products/releases?code=DG&latest=true&type=release" | grep -o 'mac*.*,' | cut -d '"' -f5)
+    fi
+    expectedTeamID="2ZEFAR8TH3"
+    ;;
 jetbrainsintellijidea)
     # credit: Gabe Marchan (gabemarchan.com - @darklink87)
     name="IntelliJ IDEA"
