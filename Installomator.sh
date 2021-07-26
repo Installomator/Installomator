44fa032b8b7ce59e94585e01899c070961b9250e
Raptor399 <patrick.atoon@gmail.com>
2021-07-26 21:44:26 +0200

    Adding Box Tools
    From: https://support.box.com/hc/en-us/articles/360043695834-Large-Scale-Deployments-Box-Tools

--- Installomator.sh
+++ Installomator.sh
@@ -1201,6 +1201,12 @@ boxdrive)
     fi
     expectedTeamID="M683GB7CPW"
     ;;
+boxtools)
+    name="Box Tools"
+    type="pkg"
+    downloadURL="https://box-installers.s3.amazonaws.com/boxedit/mac/currentrelease/BoxToolsInstaller.pkg"
+    expectedTeamID="M683GB7CPW"
+    ;;
 brave)
     # credit: @securitygeneration
     name="Brave Browser"
