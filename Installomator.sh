4b403bd9437b76890888ab7b900d1c8320cd523a
Søren Theilgaard <soren@theilgaard.dk>
2021-08-03 14:31:52 +0200

    minor change to wireshark
    New `appNewVersion` and removal of credit-line.

--- Installomator.sh
+++ Installomator.sh
@@ -2933,11 +2933,10 @@ wickrpro)
     expectedTeamID="W8RC3R952A"
     ;;
 wireshark)
-    # credit: Oh4sh0 https://github.com/Oh4sh0
     name="Wireshark"
     type="dmg"
     downloadURL="https://1.as.dl.wireshark.org/osx/Wireshark%20Latest%20Intel%2064.dmg"
-    appNewVersion=$(curl -fs https://www.wireshark.org/download.html | grep "Stable Release" | grep -o "(.*.)" | cut -f2 | head -1 | awk -F '[()]' '{print $2}')
+    appNewVersion=$(curl -fs https://www.wireshark.org/download.html | grep -io "Stable Release.*[0-9.]*.*<" | head -1 | awk -F '[()]' '{print $2}')
     expectedTeamID="7Z6EMTD2C6"
     ;;
 wwdc)
