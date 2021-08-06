8754a6137b011dde5f144fd0b30cf2845c4c5bc5
AP Orlebeke <aporlebeke@gmail.com>
2021-08-06 11:35:48 -0400

    Add ScreenCloud Player

--- Installomator.sh
+++ Installomator.sh
@@ -2489,6 +2489,13 @@ screamingfrogseospider)
     downloadURL="https://download.screamingfrog.co.uk/products/seo-spider/ScreamingFrogSEOSpider-14.3.dmg"
     expectedTeamID="CAHEVC3HZC"
     ;;
+screencloudplayer)
+    # credit: AP Orlebeke (@apizz)
+    name="ScreenCloud Player"
+    type="dmg"
+    downloadURL=$(curl -sL "https://screencloud.com/download" | sed -n 's/^.*"url":"\([^"]*\)".*$/\1/p')
+    expectedTeamID="3C4F953K6P"
+    ;;
 screenflick)
     # credit: Gabe Marchan (gabemarchan.com - @darklink87)
     name="Screenflick"
