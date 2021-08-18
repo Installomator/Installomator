6ea06ce85e886bf17d0344ae8832867344e5e05d
Søren Theilgaard <soren@theilgaard.dk>
2021-08-18 15:29:20 +0200

    craftmanager

--- Installomator.sh
+++ Installomator.sh
@@ -1289,6 +1289,14 @@ cormorant)
     appNewVersion=$(curl -fs https://eclecticlight.co/downloads/ | grep zip | grep -o -E "$name [0-9.]*" | awk '{print $2}')
     expectedTeamID="QWY4LRW926"
     ;;
+craftmanager)
+    name="CraftManager"
+    type="zip"
+    #downloadURL="https://craft-assets.invisionapp.com/CraftManager/production/CraftManager.zip"
+    downloadURL="$(curl -fs https://craft-assets.invisionapp.com/CraftManager/production/appcast.xml | xpath '(//rss/channel/item/enclosure/@url)[1]' 2>/dev/null | head -1 | cut -d '"' -f 2)"
+    appNewVersion="$(curl -fs https://craft-assets.invisionapp.com/CraftManager/production/appcast.xml | xpath '(//rss/channel/item/enclosure/@sparkle:shortVersionString)[1]' 2>/dev/null | head -1 | cut -d '"' -f 2)"
+    expectedTeamID="VRXQSNCL5W"
+    ;;
 cryptomator)
     name="Cryptomator"
     type="dmg"
