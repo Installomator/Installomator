007ea8e5925bd45b12cbf5916765f4e7e7c5a2d0
Bilal Habib <github@pro4tlzz.com>
2021-08-24 15:03:00 +0100

    Update credits

--- fragments/labels/imazingprofileeditor.sh
+++ fragments/labels/imazingprofileeditor.sh
@@ -1,5 +1,5 @@
 imazingprofileeditor)
-    # Credit: Bilal Habib @Pro4TLZZZ
+    # Credit: Bilal Habib @Pro4TLZZ
     name="iMazing Profile Editor"
     type="dmg"
     downloadURL="https://downloads.imazing.com/mac/iMazing-Profile-Editor/iMazingProfileEditorMac.dmg"
