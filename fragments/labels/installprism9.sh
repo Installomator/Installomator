a456b23d28a90d537778caac7684d3537f83625c
Søren Theilgaard <soren@theilgaard.dk>
2021-08-27 14:41:55 +0200

    Labels

--- fragments/labels/installprism9.sh
+++
@@ -1,7 +0,0 @@
-installprism9)
-    name="InstallPrism9"
-    type="dmg"
-    downloadURL="https://cdn.graphpad.com/downloads/prism/9/InstallPrism9.dmg"
-    expectedTeamID="YQ2D36NS9M"
-    appName="Prism 9.app"
-    ;;
