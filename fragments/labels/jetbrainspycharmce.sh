1800b36ae188c70ab6048044af777a43130da954
Søren Theilgaard <soren@theilgaard.dk>
2021-08-27 11:17:41 +0200

    more label syncs

--- fragments/labels/jetbrainspycharmce.sh
+++ fragments/labels/jetbrainspycharmce.sh
@@ -2,11 +2,11 @@ jetbrainspycharmce|\
 pycharmce)
     name="PyCharm CE"
     type="dmg"
-    appNewVersion=$(curl -fs "https://data.services.jetbrains.com/products/releases?code=PCC&latest=true&type=release" | grep -o 'version*.*,' | cut -d '"' -f3)
     if [[ $(arch) == i386 ]]; then
-      downloadURL="https://download.jetbrains.com/product?code=PCC&latest&distribution=mac"
+        downloadURL="https://download.jetbrains.com/product?code=PCC&latest&distribution=mac"
     elif [[ $(arch) == arm64 ]]; then
-      downloadURL="https://download.jetbrains.com/product?code=PCC&latest&distribution=macM1"
+        downloadURL="https://download.jetbrains.com/product?code=PCC&latest&distribution=macM1"
     fi
+    appNewVersion=$( curl -fsIL "${downloadURL}" | grep -i "location" | tail -1 | sed -E 's/.*\/[a-zA-Z-]*-([0-9.]*).*[-.].*dmg/\1/g' )
     expectedTeamID="2ZEFAR8TH3"
     ;;
