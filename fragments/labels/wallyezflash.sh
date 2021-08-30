f7923edcb08b49e4239c82fbfdfaa94bf54dc713
AP Orlebeke <aporlebeke@gmail.com>
2021-08-30 00:02:35 -0400

    Create wallyezflash.sh

---
+++ fragments/labels/wallyezflash.sh
@@ -0,0 +1,8 @@
+wallyezflash)
+    # credit AP Orlebeke (@apizz)
+    name="Wally"
+    type="dmg"
+    downloadURL="https://configure.zsa.io/wally/osx"
+    expectedTeamID="V32BWKSNYH"
+    versionKey="CFBundleVersion"
+    ;;
