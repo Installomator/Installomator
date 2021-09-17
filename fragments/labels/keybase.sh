9f4aecfd652eb9f884ff216a46648d0429285539
Fleish <fleish@fleetstreetops.com>
2021-09-17 15:00:52 -0700

    Re-adding Keybase

---
+++ fragments/labels/keybase.sh
@@ -0,0 +1,7 @@
+keybase)
+    # credit: Todd Fleisher
+    name="Keybase"
+    type="dmg"
+    downloadURL=$(curl -s https://keybase.io/docs/the_app/install_macos | grep data-target | cut -d '"' -f2)
+    expectedTeamID="99229SGT5K"
+    ;;
