65b82cf20d9aadb846360da37c8b119efedcee2b
Søren Theilgaard <soren@theilgaard.dk>
2021-09-17 15:35:31 +0200

    rodeconnect

--- fragments/labels/rodeconnect.sh
+++ fragments/labels/rodeconnect.sh
@@ -1,7 +1,7 @@
 rodeconnect)
     name="RODE Connect"
     type="pkgInZip"
-    packageID="com.rodeconnect.installer"
+    #packageID="com.rodeconnect.installer" #Versioned wrong as 0 in 1.1.0 pkg
     downloadURL="https://cdn1.rode.com/rodeconnect_installer_mac.zip"
     appNewVersion="$(curl -fs https://rode.com/software/rode-connect | grep -i -o ">Current version .*<" | cut -d " " -f4)"
     expectedTeamID="Z9T72PWTJA"
