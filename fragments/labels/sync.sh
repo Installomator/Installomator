028fbd60d634f95446fc9284f72cc94622a8cfd6
Søren Theilgaard <soren@theilgaard.dk>
2021-09-21 15:45:42 +0200

    Update sync.sh

--- fragments/labels/sync.sh
+++ fragments/labels/sync.sh
@@ -1,7 +1,7 @@
 sync)
-    # credit: Todd Fleisher
     name="Sync"
     type="dmg"
     downloadURL="https://www.sync.com/download/apple/Sync.dmg"
+    appNewVersion="$(curl -fs "https://www.sync.com/blog/category/desktop/feed/" | xpath -e '(//channel/item/title)[1]' 2>/dev/null | sed -E 's/^.* ([0-9.]*) .*$/\1/g')"
     expectedTeamID="7QR39CMJ3W"
     ;;
