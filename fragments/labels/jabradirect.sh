2b92057edb5da36dca00076ffb26de4401afaf83
John Hutchison <john@randm.ltd>
2021-09-21 10:54:59 -0400

    Fix Jabra Direct

--- fragments/labels/jabradirect.sh
+++ fragments/labels/jabradirect.sh
@@ -1,6 +1,6 @@
 jabradirect)
     name="Jabra Direct"
-    type="dmg"
+    type="pkgInDmg"
     downloadURL="https://jabraxpressonlineprdstor.blob.core.windows.net/jdo/JabraDirectSetup.dmg"
     expectedTeamID="55LV32M29R"
     appNewVersion=$(curl -fs https://www.jabra.com/Support/release-notes/release-note-jabra-direct | grep -o "Jabra Direct macOS:*.*<" | head -1 | cut -d ":" -f2 | cut -d " " -f2 | cut -d "<" -f1)
