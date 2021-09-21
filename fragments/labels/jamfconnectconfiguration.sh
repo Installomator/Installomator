1aae395bfabdd465f5d555734c1c72f774ca1757
John Hutchison <john@randm.ltd>
2021-09-21 10:55:22 -0400

    New Labels
    New lablels for Jamf Connect Confifugration, Tag Editor, and ToM4AConverter

---
+++ fragments/labels/jamfconnectconfiguration.sh
@@ -0,0 +1,6 @@
+jamfconnectconfiguration)
+    name="Jamf Connect Configuration"
+    type="dmg"
+    downloadURL="https://files.jamfconnect.com/JamfConnect.dmg"
+    expectedTeamID="483DWKW443"
+    ;;
