f9b6533c102b4a01b40cb03ba44e9decf6650ffe
Jake <nicholsjacob35@gmail.com>
2021-10-06 15:59:06 -0400

    fix typo in name

--- fragments/labels/perimeter81.sh
+++ fragments/labels/perimeter81.sh
@@ -1,5 +1,5 @@
 perimeter81)
-    name="Perimeter81"
+    name="Perimeter 81"
     type="pkg"
     downloadURL="https://static.perimeter81.com/agents/mac/snapshot/latest/Perimeter81.pkg"
     appNewVersion="$(curl -fsIL "${downloadURL}" | grep -i ^x-amz-meta-version | sed -E 's/x-amz-meta-version: //')"
