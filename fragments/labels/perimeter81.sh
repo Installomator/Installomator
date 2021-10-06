cfef937feec9973fef932789d0e9834499e79952
Jake <nicholsjacob35@gmail.com>
2021-10-06 15:26:32 -0400

    update appNewVersion for Perimeter81

--- fragments/labels/perimeter81.sh
+++ fragments/labels/perimeter81.sh
@@ -2,6 +2,6 @@ perimeter81)
     name="Perimeter81"
     type="pkg"
     downloadURL="https://static.perimeter81.com/agents/mac/snapshot/latest/Perimeter81.pkg"
-    appNewVersion="curl -fsIL "${downloadURL}" | grep -i ^x-amz-meta-version | sed -E 's/x-amz-meta-version: //'"
+    appNewVersion="$(curl -fsIL "${downloadURL}" | grep -i ^x-amz-meta-version | sed -E 's/x-amz-meta-version: //')"
     expectedTeamID="924635PD62"
     ;;
