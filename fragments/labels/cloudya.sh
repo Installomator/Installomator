26ae2b31394d120a16a8cf227c09fab8f24862fa
Christian Hillebrand <github@webxass.de>
2021-10-07 18:41:36 +0200

    case-insensitive

--- fragments/labels/cloudya.sh
+++ fragments/labels/cloudya.sh
@@ -1,7 +1,7 @@
 cloudya)
     name="Cloudya"
     type="appInDmgInZip"
-    downloadURL="$(curl -fs https://www.nfon.com/de/service/downloads | grep -E -o "https://cdn.cloudya.com/Cloudya-[.0-9]+-mac.zip")"
-    appNewVersion="$(curl -fs https://www.nfon.com/de/service/downloads | grep -E -o "Cloudya Desktop App MAC [0-9.]*" | sed -E 's/.*Cloudya Desktop App MAC ([0-9.]*).*/\1/g')"
+    downloadURL="$(curl -fs https://www.nfon.com/de/service/downloads | grep -i -E -o "https://cdn.cloudya.com/Cloudya-[.0-9]+-mac.zip")"
+    appNewVersion="$(curl -fs https://www.nfon.com/de/service/downloads | grep -i -E -o "Cloudya Desktop App MAC [0-9.]*" | sed 's/^.*\ \([^ ]\{0,5\}\)$/\1/g')"
     expectedTeamID="X26F74J8TH"
     ;;
