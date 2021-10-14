05039ae82685287080b3680b3a347c0ac6b7b092
Søren Theilgaard <soren@theilgaard.dk>
2021-10-14 08:48:27 +0200

    zoomgov and yed

---
+++ fragments/labels/zoomgov.sh
@@ -0,0 +1,7 @@
+zoomgov)
+    name="zoom.us"
+    type="pkg"
+    downloadURL="https://www.zoomgov.com/client/latest/ZoomInstallerIT.pkg"
+    appNewVersion="$(curl -fsIL ${downloadURL} | grep -i ^location | cut -d "/" -f5)"
+    expectedTeamID="BJ4HAAB9B3"
+    ;;
