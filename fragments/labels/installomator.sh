d49508284f05e9eee8a96ef09cb76e607915a140
Søren Theilgaard <soren@theilgaard.dk>
2021-10-19 09:19:34 +0200

    installomator label

---
+++ fragments/labels/installomator.sh
@@ -0,0 +1,9 @@
+installomator)
+    name="Installomator"
+    type="pkg"
+    packageID="com.scriptingosx.Installomator"
+    downloadURL=$(downloadURLFromGit Installomator Installomator )
+    appNewVersion=$(versionFromGit Installomator Installomator )
+    expectedTeamID="JME5BW3F3R"
+    blockingProcesses=( NONE )
+    ;;
