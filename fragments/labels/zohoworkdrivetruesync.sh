2c9085c0c50144ef2ce250cc13622741128a0ba0
Søren Theilgaard <soren@theilgaard.dk>
2021-11-01 10:38:20 +0100

    Update zohoworkdrivetruesync.sh

--- fragments/labels/zohoworkdrivetruesync.sh
+++ fragments/labels/zohoworkdrivetruesync.sh
@@ -4,10 +4,8 @@ zohoworkdrivetruesync)
 # Privacy policy: https://www.zoho.com/privacy.html
     name="Zoho WorkDrive TrueSync"
     type="pkg"
-    if [[ $(arch) == "arm64" ]]; then
-        downloadURL="https://files-accl.zohopublic.com/public/tsbin/download/c12aff2fd2a830b382c3ff79cf57ff1d" # beta for M1
-    elif [[ $(arch) == "i386" ]]; then
-        downloadURL=$(curl -fs "https://www.zoho.com/workdrive/truesync.html" | tr '<' '\n' | grep -B3 "For Mac" | grep -o -m1 "https.*\"" | cut -d '"' -f1)
-    fi
+    #https://www.zoho.com/workdrive/truesync.html
+    #https://files-accl.zohopublic.com/public/tsbin/download/c488f53fb0fe339a8a3868a16d56ede6
+    downloadURL=$(curl -fs "https://www.zoho.com/workdrive/truesync.html" | tr '<' '\n' | grep -B3 "For Mac" | grep -o -m1 "https.*\"" | cut -d '"' -f1)
     expectedTeamID="TZ824L8Y37"
     ;;
