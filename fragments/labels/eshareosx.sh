76749bb518320af4ecf414a969b851ea6bbab764
Søren Theilgaard <soren@theilgaard.dk>
2021-11-05 20:03:50 +0100

    eshareosx label

---
+++ fragments/labels/eshareosx.sh
@@ -0,0 +1,9 @@
+eshareosx)
+    name="e-Share"
+    type="pkg"
+    packageID="com.ncryptedcloud.e-Share.pkg"
+    downloadURL=https://www.ncryptedcloud.com/static/downloads/osx/$(curl -fs https://www.ncryptedcloud.com/static/downloads/osx/ | grep -o -i "href.*\".*\"" | cut -d '"' -f2)
+    versionKey="CFBundleVersion"
+    appNewVersion=$( echo "${downloadURL}" | sed -E 's/.*\/[a-zA-Z\-]*_([0-9.]*)\.pkg/\1/g' )
+    expectedTeamID="X9MBQS7DDC"
+    ;;
