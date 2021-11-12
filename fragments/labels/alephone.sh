28d5d1de0c124e5d250d2067eb7680f15942fc65
Søren Theilgaard <soren@theilgaard.dk>
2021-11-12 11:29:08 +0100

    Marathon alephone

---
+++ fragments/labels/alephone.sh
@@ -0,0 +1,7 @@
+alephone)
+    name="Aleph One"
+    type="dmg"
+    downloadURL=$(downloadURLFromGit Aleph-One-Marathon alephone)
+    appNewVersion=$(versionFromGit Aleph-One-Marathon alephone)
+    expectedTeamID="E8K89CXZE7"
+    ;;
