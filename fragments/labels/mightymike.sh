168d19cd56d773fa62f265486d13f5717881b2b7
Søren Theilgaard <soren@theilgaard.dk>
2021-11-12 11:14:10 +0100

    Pangea Software games
    Example installation
    ```
    % sudo /Users/st/Documents/GitHub/Installomator/utils/assemble.sh -r ottomatic DEBUG=0
    2021-11-12 10:28:35 ottomatic setting variable from argument DEBUG=0
    2021-11-12 10:28:35 ottomatic ################## Start Installomator v. 0.8.0
    2021-11-12 10:28:35 ottomatic ################## ottomatic
    2021-11-12 10:28:36 ottomatic BLOCKING_PROCESS_ACTION=tell_user
    2021-11-12 10:28:36 ottomatic NOTIFY=success
    2021-11-12 10:28:36 ottomatic LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2021-11-12 10:28:36 ottomatic no blocking processes defined, using Otto Matic as default
    2021-11-12 10:28:36 ottomatic Changing directory to /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.xzx3iWjQ
    2021-11-12 10:28:36 ottomatic App(s) found:
    2021-11-12 10:28:36 ottomatic could not find Otto Matic.app
    2021-11-12 10:28:36 ottomatic appversion:
    2021-11-12 10:28:36 ottomatic Latest version of Otto Matic is 4.0.0
    2021-11-12 10:28:36 ottomatic Downloading https://github.com/jorio/OttoMatic/releases/download/4.0.0/OttoMatic-4.0.0-mac.dmg to Otto Matic.dmg
    2021-11-12 10:29:52 ottomatic no more blocking processes, continue with update
    2021-11-12 10:29:52 ottomatic Installing Otto Matic
    2021-11-12 10:29:52 ottomatic Mounting /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.xzx3iWjQ/Otto Matic.dmg
    2021-11-12 10:29:55 ottomatic Mounted: /Volumes/Otto Matic 4.0.0
    2021-11-12 10:29:55 ottomatic Verifying: /Volumes/Otto Matic 4.0.0/Otto Matic.app
    2021-11-12 10:29:57 ottomatic Team ID matching: RVNL7XC27G (expected: RVNL7XC27G )
    2021-11-12 10:29:57 ottomatic Downloaded version of Otto Matic is 4.0.0 (replacing version ).
    2021-11-12 10:29:57 ottomatic Copy /Volumes/Otto Matic 4.0.0/Otto Matic.app to /Applications
    2021-11-12 10:29:57 ottomatic Changing owner to st
    2021-11-12 10:29:57 ottomatic Finishing…
    2021-11-12 10:30:07 ottomatic App(s) found: /Applications/Otto Matic.app
    2021-11-12 10:30:07 ottomatic found app at /Applications/Otto Matic.app, version 4.0.0
    2021-11-12 10:30:07 ottomatic Installed Otto Matic, version 4.0.0
    2021-11-12 10:30:07 ottomatic notifying
    2021-11-12 10:30:07 ottomatic Deleting /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.xzx3iWjQ
    2021-11-12 10:30:07 ottomatic Unmounting /Volumes/Otto Matic 4.0.0
    "disk6" ejected.
    2021-11-12 10:30:07 ottomatic App not closed, so no reopen.
    2021-11-12 10:30:08 ottomatic ################## End Installomator, exit code 0
    
    % sudo /Users/st/Documents/GitHub/Installomator/utils/assemble.sh -r ottomatic DEBUG=0
    Password:
    2021-11-12 10:58:24 ottomatic setting variable from argument DEBUG=0
    2021-11-12 10:58:24 ottomatic ################## Start Installomator v. 0.8.0
    2021-11-12 10:58:24 ottomatic ################## ottomatic
    2021-11-12 10:58:25 ottomatic BLOCKING_PROCESS_ACTION=tell_user
    2021-11-12 10:58:25 ottomatic NOTIFY=success
    2021-11-12 10:58:25 ottomatic LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2021-11-12 10:58:25 ottomatic no blocking processes defined, using Otto Matic as default
    2021-11-12 10:58:25 ottomatic Changing directory to /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.nNqO5vSr
    2021-11-12 10:58:25 ottomatic App(s) found: /Applications/Otto Matic.app
    2021-11-12 10:58:25 ottomatic found app at /Applications/Otto Matic.app, version 4.0.0
    2021-11-12 10:58:25 ottomatic appversion: 4.0.0
    2021-11-12 10:58:25 ottomatic Latest version of Otto Matic is 4.0.0
    2021-11-12 10:58:25 ottomatic There is no newer version available.
    2021-11-12 10:58:25 ottomatic Deleting /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.nNqO5vSr
    2021-11-12 10:58:25 ottomatic App not closed, so no reopen.
    2021-11-12 10:58:25 ottomatic ################## End Installomator, exit code 0
    ```

---
+++ fragments/labels/mightymike.sh
@@ -0,0 +1,7 @@
+mightymike)
+    name="Nanosaur"
+    type="dmg"
+    downloadURL=$(downloadURLFromGit jorio MightyMike)
+    appNewVersion=$(versionFromGit jorio MightyMike)
+    expectedTeamID="RVNL7XC27G"
+    ;;
---
+++ fragments/labels/nanosaur.sh
@@ -0,0 +1,7 @@
+nanosaur)
+    name="Nanosaur"
+    type="dmg"
+    downloadURL=$(downloadURLFromGit jorio Nanosaur)
+    appNewVersion=$(versionFromGit jorio Nanosaur)
+    expectedTeamID="RVNL7XC27G"
+    ;;
