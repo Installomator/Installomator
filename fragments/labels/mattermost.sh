f062d49bfdf2418353afa15854a5d63d2f964aca
Søren Theilgaard <soren@theilgaard.dk>
2021-11-17 19:36:05 +0100

    mattermost universal

--- fragments/labels/mattermost.sh
+++ fragments/labels/mattermost.sh
@@ -1,11 +1,7 @@
 mattermost)
     name="Mattermost"
     type="dmg"
-    if [[ $(arch) == i386 ]]; then
-      archiveName="mac-x64.dmg"
-    elif [[ $(arch) == arm64 ]]; then
-      archiveName="mac-m1.dmg"
-    fi
+    archiveName="mac-universal.dmg"
     downloadURL=$(downloadURLFromGit mattermost desktop)
     appNewVersion=$(versionFromGit mattermost desktop )
     expectedTeamID="UQ8HT4Q2XM"
