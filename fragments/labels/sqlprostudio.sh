c6237ca1936f85b51872b8d71aead3a3c5e0080e
AP Orlebeke <aporlebeke@gmail.com>
2021-11-22 10:52:36 -0500

    Add SQLPro Studio

---
+++ fragments/labels/sqlprostudio.sh
@@ -0,0 +1,7 @@
+sqlprostudio)
+    name="SQLPro Studio"
+    type="zip"
+    downloadURL="https://www.sqlprostudio.com/download.php"
+    expectedTeamID="LKJB72232C"
+    blockingProcesses=( "SQLPro Studio" )
+    ;;
