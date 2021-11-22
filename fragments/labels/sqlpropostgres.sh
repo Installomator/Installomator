4117b687e87abd4d056670f92311044457b6c802
AP Orlebeke <aporlebeke@gmail.com>
2021-11-22 11:15:43 -0500

    Add SQLPro Postgres

---
+++ fragments/labels/sqlpropostgres.sh
@@ -0,0 +1,7 @@
+sqlpropostgres)
+    name="SQLPro for Postgres"
+    type="zip"
+    downloadURL="https://macpostgresclient.com/download.php"
+    expectedTeamID="LKJB72232C"
+    blockingProcesses=( "SQLPro for Postgres" )
+    ;;
