9923a03a1261865868dfec0d98484c1d4c215dac
Søren Theilgaard <soren@theilgaard.dk>
2021-11-22 14:50:00 +0100

    Update inkscape.sh

--- fragments/labels/inkscape.sh
+++ fragments/labels/inkscape.sh
@@ -3,6 +3,7 @@ inkscape)
     name="Inkscape"
     type="dmg"
     downloadURL="https://inkscape.org$(curl -fs https://inkscape.org$(curl -fsJL https://inkscape.org/release/  | grep "/release/" | grep en | head -n 1 | cut -d '"' -f 6)mac-os-x/dmg/dl/ | grep "click here" | cut -d '"' -f 2)"
-    #appNewVersion=$(curl -fsJL https://inkscape.org/release/  | grep "<h2>Inkscape" | cut -d '>' -f 3 | cut -d '<' -f 1 | sed 's/[^0-9.]*//g') # Can't figure out where exact new version is found. Currently returns 1.0, but version is "1.0.0 (4035a4f)"
+    appCustomVersion() { /Applications/Inkscape.app/Contents/MacOS/inkscape --version | cut -d " " -f2 }
+    appNewVersion=$(curl -fsJL https://inkscape.org/release/  | grep "<title>" | grep -o -e "[0-9.]*")
     expectedTeamID="SW3D6BB6A6"
     ;;
