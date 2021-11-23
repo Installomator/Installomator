3dd9818d9a7bf70c7c19094c65c309bfd05c7d6c
fleish <2106430+fleish@users.noreply.github.com>
2021-11-23 08:29:05 -0800

    Update protonvpn.sh
    appNewVersion

--- fragments/labels/protonvpn.sh
+++ fragments/labels/protonvpn.sh
@@ -3,4 +3,5 @@ protonvpn)
     type="dmg"
     downloadURL=$(curl -fs "https://protonvpn.com/download" | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | grep dmg | tail -1)
     expectedTeamID="J6S6Q257EK"
+    appNewVersion=$(echo $downloadURL | awk -F\_ '{print $3}' | awk -F\. '{print $1"."$2"."$3}')
     ;;
