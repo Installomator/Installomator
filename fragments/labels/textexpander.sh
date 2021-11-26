98c56d505b221e2d656c3c6b5d2a991860e75d98
AP Orlebeke <aporlebeke@gmail.com>
2021-11-26 01:35:58 -0500

    Fix TextExpander

--- fragments/labels/textexpander.sh
+++ fragments/labels/textexpander.sh
@@ -1,6 +1,6 @@
 textexpander)
     name="TextExpander"
-    type="zip"
+    type="dmg"
     downloadURL="https://textexpander.com/cgi-bin/redirect.pl?cmd=download&platform=osx"
     appNewVersion=$( curl -fsIL "https://textexpander.com/cgi-bin/redirect.pl?cmd=download&platform=osx" | grep -i "^location" | awk '{print $2}' | tail -1 | cut -d "_" -f2 | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p' )
     expectedTeamID="7PKJ6G4DXL"
