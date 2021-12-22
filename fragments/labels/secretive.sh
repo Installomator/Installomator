61196b3da6f8b84a695f0f1844f563f3118d0415
AP Orlebeke <aporlebeke@gmail.com>
2021-12-22 14:43:16 -0500

    Add secretive

---
+++ fragments/labels/secretive.sh
@@ -0,0 +1,7 @@
+secretive)
+    name="Secretive"
+    type="zip"
+    downloadURL=$(downloadURLFromGit maxgoedjen secretive)
+    appNewVersion=$(versionFromGit maxgoedjen secretive)
+    expectedTeamID="Z72PRUAWF6"
+    ;;
