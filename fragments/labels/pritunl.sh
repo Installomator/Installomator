c302cf63769082d06736c50050878efe294473e0
nestyurkin <nestyurkin@gmail.com>
2021-12-28 19:22:11 +0300

    Create Pritunl
    Label for Pritunl

---
+++ fragments/labels/pritunl.sh
@@ -0,0 +1,13 @@
+pritunl)
+    name="Pritunl"
+    type="pkgInZip"
+    if [[ $(arch) == "arm64" ]]; then
+        archiveName="Pritunl.arm64.pkg.zip"
+        downloadURL=$(downloadURLFromGit pritunl pritunl-client-electron)
+    elif [[ $(arch) == "i386" ]]; then
+        archiveName="Pritunl.pkg.zip"
+        downloadURL=$(downloadURLFromGit pritunl pritunl-client-electron)
+    fi
+    appNewVersion=$(versionFromGit pritunl pritunl-client-electron)
+    expectedTeamID="U22BLATN63"
+    ;;
