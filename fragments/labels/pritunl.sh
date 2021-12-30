bd261b1c63794b754bc9f7ee11ccc25c792f9d74
nestyurkin <nestyurkin@gmail.com>
2021-12-30 23:49:35 +0300

    minimize

--- fragments/labels/pritunl.sh
+++ fragments/labels/pritunl.sh
@@ -4,11 +4,10 @@ pritunl)
     packageID="com.pritunl.pkg.Pritunl"
     if [[ $(arch) == "arm64" ]]; then
         archiveName="Pritunl.arm64.pkg.zip"
-        downloadURL=$(downloadURLFromGit pritunl pritunl-client-electron)
     elif [[ $(arch) == "i386" ]]; then
         archiveName="Pritunl.pkg.zip"
-        downloadURL=$(downloadURLFromGit pritunl pritunl-client-electron)
     fi
+    downloadURL=$(downloadURLFromGit pritunl pritunl-client-electron)
     appNewVersion=$(versionFromGit pritunl pritunl-client-electron)
     expectedTeamID="U22BLATN63"
     ;;
