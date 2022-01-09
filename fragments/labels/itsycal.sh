02b0829877c2a0f79d153c89c7d644dc59b6ce5a
Søren Theilgaard <soren@theilgaard.dk>
2022-01-09 20:55:13 +0100

    Update itsycal.sh

--- fragments/labels/itsycal.sh
+++ fragments/labels/itsycal.sh
@@ -3,6 +3,5 @@ itsycal)
     type="zip"
     downloadURL=$(curl -fs https://s3.amazonaws.com/itsycal/itsycal-apple-silicon.xml | xpath '(//rss/channel/item/enclosure/@url)[1]' 2>/dev/null | head -1 | cut -d '"' -f 2)
     appNewVersion=$(curl -fs https://s3.amazonaws.com/itsycal/itsycal-apple-silicon.xml | xpath '(//rss/channel/item/enclosure/@sparkle:shortVersionString)[1]' 2>/dev/null | head -1 | cut -d '"' -f 2)
-    blockingProcesses=( "Itsycal" )
     expectedTeamID="HFT3T55WND"
     ;;
