4df1a6dbaa6298909ead63d96492235d5a2febe1
Søren Theilgaard <soren@theilgaard.dk>
2022-01-11 23:39:55 +0100

    canva and logitechoptions labels fixed

--- fragments/labels/canva.sh
+++ fragments/labels/canva.sh
@@ -2,10 +2,12 @@ canva)
     name="Canva"
     type="dmg"
     if [[ $(arch) == "arm64" ]]; then
-        downloadURL=$(curl -fsLI -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "accept-encoding: gzip, deflate, br" -H "accept-language: en-US,en;q=0.9" -H "Referrer Policy: strict-origin-when-cross-origin" -H "upgrade-insecure-requests: 1" -H "sec-fetch-dest: document" -H "sec-gpc: 1" -H "sec-fetch-user: ?1" -H "sec-fetch-mode: navigate" -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" "https://www.canva.com/download/mac/arm/canva-desktop/" | grep -i "^location" | cut -d " " -f2 | tr -d '\r')
+        downloadURL=https://desktop-release.canva.com/$(curl -fs https://desktop-release.canva.com/latest-mac.yml | grep -e "url.*dmg" | grep -oe "Canva-[0-9.]*-arm64.dmg")
+        # https://www.canva.com/download/mac/arm/canva-desktop/
     elif [[ $(arch) == "i386" ]]; then
-        downloadURL=$(curl -fsLI -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" -H "accept-encoding: gzip, deflate, br" -H "Referrer Policy: strict-origin-when-cross-origin" -H "upgrade-insecure-requests: 1" -H "sec-fetch-dest: document" -H "sec-gpc: 1" -H "sec-fetch-user: ?1" -H "accept-language: en-US,en;q=0.9" -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "sec-fetch-mode: navigate" "https://www.canva.com/download/mac/intel/canva-desktop/" | grep -i "^location" | cut -d " " -f2 | tr -d '\r')
+        downloadURL=https://desktop-release.canva.com/$(curl -fs https://desktop-release.canva.com/latest-mac.yml | grep -e "url.*dmg" | grep -oe "Canva-[0-9.]*.dmg")
+        # https://www.canva.com/download/mac/intel/canva-desktop/
     fi
-    appNewVersion=$( echo "${downloadURL}" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g' )
+    appNewVersion=$( curl -fs https://desktop-release.canva.com/latest-mac.yml | grep version | cut -d " " -f2 )
     expectedTeamID="5HD2ARTBFS"
     ;;
