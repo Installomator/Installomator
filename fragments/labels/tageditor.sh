7d02cf1da448d7e8385d5b2695aa834df04eeac8
Søren Theilgaard <soren@theilgaard.dk>
2022-01-20 10:05:31 +0100

    tageditor label fix

--- fragments/labels/tageditor.sh
+++ fragments/labels/tageditor.sh
@@ -2,6 +2,6 @@ tageditor)
      name="Tag Editor"
      type="dmg"
      downloadURL="https://amvidia.com/downloads/tag-editor-mac.dmg"
-     appNewVersion=curl -sf "https://amvidia.com/tag-editor" | grep -o -E '"softwareVersion":.'"{8}" | sed 's/\"//g' | awk -F ': ' '{print $2}'
+     appNewVersion=$(curl -sf "https://amvidia.com/tag-editor" | grep -o -E '"softwareVersion":.'"{8}" | sed -E 's/.*"([0-9.]*).*/\1/g')
      expectedTeamID="F2TH9XX9CJ"
      ;;
