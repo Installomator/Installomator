449f7e5261a92a9a5bb9224aab64d8c48a60c9e9
Søren Theilgaard <soren@theilgaard.dk>
2022-01-20 08:35:06 +0100

    Update daisydisk.sh

--- fragments/labels/daisydisk.sh
+++ fragments/labels/daisydisk.sh
@@ -2,6 +2,6 @@ daisydisk)
     name="DaisyDisk"
     type="zip"
     downloadURL="https://daisydiskapp.com/downloads/DaisyDisk.zip"
-    appNewVersion=$( curl -fs 'https://daisydiskapp.com/downloads/appcastReleaseNotes.php?appEdition=Standard' | grep Version | head -1 | cut -d \> -f 3 | cut -d \< -f 1 | awk '{print $2}' )
+    appNewVersion=$( curl -fs 'https://daisydiskapp.com/downloads/appcastReleaseNotes.php?appEdition=Standard' | grep Version | head -1 | sed -E 's/.*Version ([0-9.]*).*/\1/g' )
     expectedTeamID="4CBU3JHV97"
     ;;
