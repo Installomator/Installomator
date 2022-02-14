fbe58f8ee395299cad899269395cdb1a5fcada5f
taboc741 <34924920+taboc741@users.noreply.github.com>
2022-02-14 12:46:46 -0500

    Update and rename NordVPNTeams.sh to NordLayer.sh
    Fixed legacy branding from their documentation and pointed to modern URL.

--- fragments/labels/NordLayer.sh
+++ fragments/labels/NordLayer.sh
@@ -2,6 +2,6 @@ nordlayer)
     # credit: Taboc741 (https://github.com/taboc741)
     name="NordLayer"
     type="pkg"
-    downloadURL="https://downloads.nordlayer.com/mac/latest/NordVPNTeams.pkg"
+    downloadURL="https://downloads.nordlayer.com/mac/latest/NordLayer.pkg"
     expectedTeamID="W5W395V82Y"
     ;;
