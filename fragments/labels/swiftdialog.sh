715fdc597b327c46513d12bad1c35aa05dcd826c
Bart Reardon <bart.reardon@csiro.au>
2022-03-06 08:53:19 +1100

    add swiftdialog label (same as dialog but with an updated name)

---
+++ fragments/labels/swiftdialog.sh
@@ -0,0 +1,8 @@
+swiftdialog)
+    name="swiftDialog"
+    type="pkg"
+    packageID="au.csiro.dialogcli"
+    downloadURL="$(downloadURLFromGit bartreardon swiftDialog)"
+    appNewVersion="$(versionFromGit bartreardon swiftDialog)"
+    expectedTeamID="PWA5E9TQ59"
+    ;;
