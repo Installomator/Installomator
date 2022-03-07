4b14b31ff3f9ea2035bf2a88655ca5a298288e1f
Søren Theilgaard <soren@theilgaard.dk>
2022-03-07 15:11:02 +0100

    Fixed version
    Issue #433 fixed.

--- fragments/labels/zoom.sh
+++ fragments/labels/zoom.sh
@@ -4,4 +4,5 @@ zoom)
     downloadURL="https://zoom.us/client/latest/ZoomInstallerIT.pkg"
     appNewVersion="$(curl -fsIL ${downloadURL} | grep -i ^location | cut -d "/" -f5)"
     expectedTeamID="BJ4HAAB9B3"
+    versionKey="CFBundleVersion"
     ;;
