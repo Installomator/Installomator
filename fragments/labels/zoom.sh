322e9ab2ff4587320648c878498c9a589cb40345
marcelpogorzelski <42753253+marcelpogorzelski@users.noreply.github.com>
2022-03-15 13:01:43 +0100

    Changed versionKey
    CFBundleVersion has the correct formatting for version check

--- fragments/labels/zoom.sh
+++ fragments/labels/zoom.sh
@@ -3,5 +3,6 @@ zoom)
     type="pkg"
     downloadURL="https://zoom.us/client/latest/ZoomInstallerIT.pkg"
     appNewVersion="$(curl -fsIL ${downloadURL} | grep -i ^location | cut -d "/" -f5)"
+    versionKey="CFBundleVersion"
     expectedTeamID="BJ4HAAB9B3"
     ;;
