e972494484880ef05af6b51b02a947c366741ce8
Tom Bartlett <unfo33@gmail.com>
2022-03-15 15:07:29 -0400

    added grammarly

--- Installomator.sh
+++ Installomator.sh
@@ -2353,6 +2353,15 @@ gpgsync)
     appNewVersion="$(versionFromGit firstlookmedia gpgsync)"
     expectedTeamID="P24U45L8P5"
     ;;
+grammarly)
+    name="Grammarly Desktop"
+    type="dmg"
+    packageID="com.grammarly.ProjectLlama"
+    downloadURL=$(curl -fsL "https://download-mac.grammarly.com/appcast.xml" | xpath '//rss/channel/item[1]/enclosure/@url' 2>/dev/null  | cut -d '"' -f 2)
+    expectedTeamID="W8F64X92K3"
+    appNewVersion=$(curl -is "https://download-mac.grammarly.com/appcast.xml" | grep sparkle:version | tr ',' '\n' | grep sparkle:version | cut -d '"' -f 4)
+    appName="Grammarly Installer.app"
+    ;;
 grandperspective)
     name="GrandPerspective"
     type="dmg"
