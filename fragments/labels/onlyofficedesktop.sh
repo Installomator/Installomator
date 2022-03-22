600657b0741c0b4f7a54a8cc4ec148e75ccba468
Adrian Bühler <48823479+adibue@users.noreply.github.com>
2022-03-22 11:40:43 +0100

    Add downloadURL for ARM. Add appNewVersion
    Unfortunately I could not find any other version source for `appNewVersion`.
    Should work just fine tho.

--- fragments/labels/onlyofficedesktop.sh
+++ fragments/labels/onlyofficedesktop.sh
@@ -1,6 +1,11 @@
 onlyofficedesktop)
     name="ONLYOFFICE"
     type="dmg"
-    downloadURL="https://download.onlyoffice.com/install/desktop/editors/mac/distrib/onlyoffice/ONLYOFFICE.dmg"
+    if [[ $(arch) == "arm64" ]]; then
+    downloadURL="https://download.onlyoffice.com/install/desktop/editors/mac/arm/distrib/ONLYOFFICE.dmg"
+    elif [[ $(arch) == "i386" ]]; then
+    downloadURL="https://download.onlyoffice.com/install/desktop/editors/mac/x86_64/distrib/ONLYOFFICE.dmg"
+    fi
+    appNewVersion=$(versionFromGit ONLYOFFICE DesktopEditors)
     expectedTeamID="2WH24U26GJ"
     ;;
