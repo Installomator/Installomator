c728c25fc45c7a6409c0998f2adb8e2ded5af0ca
Liam Steckler <553265+buckbanzai@users.noreply.github.com>
2022-03-21 17:06:26 -0700

    Updated label sed and downloadURL

--- fragments/labels/talkdeskcallbar.sh
+++ fragments/labels/talkdeskcallbar.sh
@@ -1,7 +1,7 @@
 talkdeskcallbar)
     name="Callbar"
     type="dmg"
-    downloadURL=https://downloadcallbar.talkdesk.com/Callbar-$(curl -fsL https://downloadcallbar.talkdesk.com/release_metadata.json | sed -n 's/^.*"version":"\([^"]*\)".*$/\1/p').dmg
-    appNewVersion=$(curl -fsL https://downloadcallbar.talkdesk.com/release_metadata.json | sed -n 's/^.*"version":"\([^"]*\)".*$/\1/p')
+    appNewVersion=$(curl -fsL https://downloadcallbar.talkdesk.com/release_metadata.json | sed -n 's/^.*"version"[[:space:]]*:[[:space:]]*"\([^"]*\)".*$/\1/p')
+    downloadURL=https://downloadcallbar.talkdesk.com/Callbar-${appNewVersion}.dmg
     expectedTeamID="YGGJX44TB8"
     ;;
