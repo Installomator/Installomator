affe0f4073e79cca46312de05e1b74ca6c805f01
Søren Theilgaard <soren@theilgaard.dk>
2022-03-23 14:11:05 +0100

    log levels on localization

--- fragments/labels/firefox_intl.sh
+++ fragments/labels/firefox_intl.sh
@@ -4,17 +4,17 @@ firefox_intl)
     name="Firefox"
     type="dmg"
     userLanguage=$(runAsUser defaults read .GlobalPreferences AppleLocale)
-    printlog "Found language $userLanguage to be used for Firefox."
+    printlog "Found language $userLanguage to be used for Firefox." WARN
     if ! curl -fs "https://ftp.mozilla.org/pub/firefox/releases/latest/README.txt" | grep -o "=$userLanguage"; then
         userLanguage=$(echo $userLanguage | cut -c 1-2)
         if ! curl -fs "https://ftp.mozilla.org/pub/firefox/releases/latest/README.txt" | grep "=$userLanguage"; then
             userLanguage="en_US"
         fi
     fi
-    printlog "Using language $userLanguage for download."
+    printlog "Using language $userLanguage for download." WARN
     downloadURL="https://download.mozilla.org/?product=firefox-latest&amp;os=osx&amp;lang=$userLanguage"
     if ! curl -sfL --output /dev/null -r 0-0 "$downloadURL" ; then
-        printlog "Download not found for that language. Using en-US"
+        printlog "Download not found for that language. Using en-US" WARN
         downloadURL="https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
     fi
     appNewVersion=$(curl -fs https://www.mozilla.org/en-US/firefox/releases/ | grep '<html' | grep -o -i -e "data-latest-firefox=\"[0-9.]*\"" | cut -d '"' -f2)
--- fragments/labels/firefoxesr_intl.sh
+++ fragments/labels/firefoxesr_intl.sh
@@ -4,18 +4,18 @@ firefoxesr_intl)
     name="Firefox"
     type="dmg"
     userLanguage=$(runAsUser defaults read .GlobalPreferences AppleLocale)
-    printlog "Found language $userLanguage to be used for Firefox."
+    printlog "Found language $userLanguage to be used for Firefox." WARN
     if ! curl -fs "https://ftp.mozilla.org/pub/firefox/releases/latest-esr/README.txt" | grep -o "=$userLanguage"; then
         userLanguage=$(echo $userLanguage | cut -c 1-2)
         if ! curl -fs "https://ftp.mozilla.org/pub/firefox/releases/latest-esr/README.txt" | grep "=$userLanguage"; then
             userLanguage="en_US"
         fi
     fi
-    printlog "Using language $userLanguage for download."
+    printlog "Using language $userLanguage for download." WARN
     downloadURL="https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=osx&lang=$userLanguage"
     # https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=osx&lang=en-US
     if ! curl -sfL --output /dev/null -r 0-0 "$downloadURL" ; then
-        printlog "Download not found for that language. Using en-US"
+        printlog "Download not found for that language. Using en-US" WARN
         downloadURL="https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
     fi
     appNewVersion=$(curl -fsIL "$downloadURL" | grep -i "^location" | awk '{print $2}' | sed -E 's/.*releases\/([0-9.]*)esr.*/\1/g')
