351cc07afaf9d6ba7db605ba0bb8644b48af1acf
Maarten Wijnants <wijnants.maarten@gmail.com>
2022-04-05 16:12:08 +0200

    Add latest version check to gpgsuite

--- fragments/labels/gpgsuite.sh
+++ fragments/labels/gpgsuite.sh
@@ -4,6 +4,7 @@ gpgsuite)
     type="pkgInDmg"
     pkgName="Install.pkg"
     downloadURL=$(curl -s https://gpgtools.org/ | grep https://releases.gpgtools.org/GPG_Suite- | grep Download | cut -d'"' -f4)
+    appNewVersion=$(echo $downloadURL | cut -d "-" -f 2 | cut -d "." -f 1-2)
     expectedTeamID="PKV8ZPD836"
     blockingProcesses=( "GPG Keychain" )
     ;;
