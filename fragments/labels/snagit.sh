9407918bd770aff33a9ceada3f72da83aba3cc0c
marcelpogorzelski <42753253+marcelpogorzelski@users.noreply.github.com>
2022-04-26 08:42:43 +0200

    Update Snagit name
    Updated Snagit name to Snagit 2022

--- fragments/labels/snagit.sh
+++ fragments/labels/snagit.sh
@@ -1,7 +1,8 @@
 snagit|\
+snagit2022|\
 snagit2021|\
 snagit2020)
-    name="Snagit 2021"
+    name="Snagit 2022"
     type="dmg"
     downloadURL="https://download.techsmith.com/snagitmac/releases/Snagit.dmg"
     expectedTeamID="7TQL462TU8"
