1ce70bc8f346be757a23d0096dd14612507b3821
marcelpogorzelski <42753253+marcelpogorzelski@users.noreply.github.com>
2022-04-26 09:47:10 +0200

    Update Snagit
    Added appNewVersion

--- fragments/labels/snagit.sh
+++ fragments/labels/snagit.sh
@@ -5,5 +5,6 @@ snagit2020)
     name="Snagit 2022"
     type="dmg"
     downloadURL="https://download.techsmith.com/snagitmac/releases/Snagit.dmg"
+    appNewVersion=$( curl -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" -fs "https://support.techsmith.com/hc/en-us/articles/360004908652-Desktop-Product-Download-Links"  | grep "Snagit (Mac)" | head -1 | sed -e 's/.*Snagit (Mac) //' -e 's/<\/td>.*//')
     expectedTeamID="7TQL462TU8"
     ;;
