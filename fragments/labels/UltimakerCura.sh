0266f74e46e8725e347813df54b54216787b7838
MacMike077 <104210875+MacMike077@users.noreply.github.com>
2022-05-02 16:47:59 +0200

    Create UltimakerCura.sh
    2022-05-02 16:45:27 : REQ   : ultimakercura : ################## Start Installomator v. 9.1, date 2022-03-18
    2022-05-02 16:45:27 : INFO  : ultimakercura : ################## Version: 9.1
    2022-05-02 16:45:27 : INFO  : ultimakercura : ################## Date: 2022-03-18
    2022-05-02 16:45:27 : INFO  : ultimakercura : ################## ultimakercura
    2022-05-02 16:45:28 : INFO  : ultimakercura : BLOCKING_PROCESS_ACTION=tell_user
    2022-05-02 16:45:28 : INFO  : ultimakercura : NOTIFY=success
    2022-05-02 16:45:28 : INFO  : ultimakercura : LOGGING=INFO
    2022-05-02 16:45:28 : INFO  : ultimakercura : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2022-05-02 16:45:28 : INFO  : ultimakercura : Label type: dmg
    2022-05-02 16:45:28 : INFO  : ultimakercura : archiveName: Ultimaker_Cura-[0-9].*-Darwin.dmg
    2022-05-02 16:45:28 : INFO  : ultimakercura : no blocking processes defined, using Ultimaker Cura as default
    2022-05-02 16:45:28 : INFO  : ultimakercura : name: Ultimaker Cura, appName: Ultimaker Cura.app
    2022-05-02 16:45:28 : INFO  : ultimakercura : App(s) found: 
    2022-05-02 16:45:28 : WARN  : ultimakercura : could not find Ultimaker Cura.app
    2022-05-02 16:45:28 : INFO  : ultimakercura : appversion: 
    2022-05-02 16:45:28 : INFO  : ultimakercura : Latest version of Ultimaker Cura is 4.13.1
    2022-05-02 16:45:28 : REQ   : ultimakercura : Downloading https://github.com/Ultimaker/Cura/releases/download/4.13.1/Ultimaker_Cura-4.13.1-Darwin.dmg to Ultimaker_Cura-[0-9].*-Darwin.dmg
    2022-05-02 16:45:34 : REQ   : ultimakercura : no more blocking processes, continue with update
    2022-05-02 16:45:34 : REQ   : ultimakercura : Installing Ultimaker Cura
    2022-05-02 16:45:34 : INFO  : ultimakercura : Mounting /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.WFz2hZIw/Ultimaker_Cura-[0-9].*-Darwin.dmg
    2022-05-02 16:45:38 : INFO  : ultimakercura : Mounted: /Volumes/Ultimaker_Cura-4.13.1-Darwin
    2022-05-02 16:45:38 : INFO  : ultimakercura : Verifying: /Volumes/Ultimaker_Cura-4.13.1-Darwin/Ultimaker Cura.app
    2022-05-02 16:45:51 : INFO  : ultimakercura : Team ID matching: V4B3JXRRQS (expected: V4B3JXRRQS )
    2022-05-02 16:45:51 : INFO  : ultimakercura : Installing Ultimaker Cura version 4.13 on versionKey CFBundleShortVersionString.
    2022-05-02 16:45:51 : INFO  : ultimakercura : Copy /Volumes/Ultimaker_Cura-4.13.1-Darwin/Ultimaker Cura.app to /Applications
    2022-05-02 16:46:04 : WARN  : ultimakercura : No user logged in or SYSTEMOWNER=1, setting owner to root:wheel
    2022-05-02 16:46:04 : INFO  : ultimakercura : Finishing...
    2022-05-02 16:46:14 : INFO  : ultimakercura : App(s) found: /Applications/Ultimaker Cura.app
    2022-05-02 16:46:14 : INFO  : ultimakercura : found app at /Applications/Ultimaker Cura.app, version 4.13, on versionKey CFBundleShortVersionString
    2022-05-02 16:46:14 : REQ   : ultimakercura : Installed Ultimaker Cura, version 4.13
    2022-05-02 16:46:14 : INFO  : ultimakercura : notifying
    2022-05-02 16:46:15 : INFO  : ultimakercura : App not closed, so no reopen.
    2022-05-02 16:46:15 : REQ   : ultimakercura : All done!
    2022-05-02 16:46:15 : REQ   : ultimakercura : ################## End Installomator, exit code 0

---
+++ fragments/labels/UltimakerCura.sh
@@ -0,0 +1,8 @@
+ultimakercura)
+    name="Ultimaker Cura"
+    type="dmg"
+    downloadURL="$(downloadURLFromGit Ultimaker Cura)"
+    archiveName="Ultimaker_Cura-[0-9].*-Darwin.dmg"
+    appNewVersion=$(versionFromGit Ultimaker Cura )
+    expectedTeamID="V4B3JXRRQS"
+    ;;
