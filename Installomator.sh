13864a0cd7abaa23c1bd3999faeb5cf6c7cc69a5
Micah Lee <micah@micahflee.com>
2022-05-04 15:23:04 -0700

    Add appName and archiveName for 1password8

--- Installomator.sh
+++ Installomator.sh
@@ -1304,10 +1304,13 @@ valuesfromarguments)
     ;;
 1password8)
     name="1Password 8"
+    appName="1Password.app"
     type="zip"
     if [[ $(arch) == "arm64" ]]; then
+        archiveName="1Password-latest-aarch64.zip"
         downloadURL="https://downloads.1password.com/mac/1Password-latest-aarch64.zip"
     elif [[ $(arch) == "i386" ]]; then
+        archiveName="1Password-latest-x86_64.zip"
         downloadURL="https://downloads.1password.com/mac/1Password-latest-x86_64.zip"
     fi
     expectedTeamID="2BUA8C4S2C"
