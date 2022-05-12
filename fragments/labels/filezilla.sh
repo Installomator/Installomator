069651cf9f732cc6cb1f953652ed173cbf1b2a32
madtice <50694988+madtice@users.noreply.github.com>
2022-05-12 20:53:23 +0200

    added L flag to curl
    Changed curl -fs to curl -fsL to cleanup logging. The usual -fsIL doesn't give me the newest version number.
    
    2022-05-12 20:52:03 : REQ   : filezilla : ################## Start Installomator v. 9.2beta, date 2022-05-12
    2022-05-12 20:52:03 : INFO  : filezilla : ################## Version: 9.2beta
    2022-05-12 20:52:03 : INFO  : filezilla : ################## Date: 2022-05-12
    2022-05-12 20:52:03 : INFO  : filezilla : ################## filezilla
    2022-05-12 20:52:03 : DEBUG : filezilla : DEBUG mode 1 enabled.
    2022-05-12 20:52:03 : INFO  : filezilla : BLOCKING_PROCESS_ACTION=tell_user
    2022-05-12 20:52:03 : INFO  : filezilla : NOTIFY=success
    2022-05-12 20:52:03 : INFO  : filezilla : LOGGING=DEBUG
    2022-05-12 20:52:04 : INFO  : filezilla : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2022-05-12 20:52:04 : INFO  : filezilla : Label type: tbz
    2022-05-12 20:52:04 : INFO  : filezilla : archiveName: FileZilla.tbz
    2022-05-12 20:52:04 : DEBUG : filezilla : Changing directory to /Users/thijs/gits/Installomator/build
    2022-05-12 20:52:04 : INFO  : filezilla : No version found using packageID org.filezilla-project.filezilla
    2022-05-12 20:52:04 : INFO  : filezilla : App(s) found: /Applications/FileZilla.app
    2022-05-12 20:52:04 : INFO  : filezilla : found app at /Applications/FileZilla.app, version 3.59.0, on versionKey CFBundleShortVersionString
    2022-05-12 20:52:04 : INFO  : filezilla : appversion: 3.59.0
    2022-05-12 20:52:04 : INFO  : filezilla : Latest version of FileZilla is 3.59.0
    2022-05-12 20:52:04 : WARN  : filezilla : DEBUG mode 1 enabled, not exiting, but there is no new version of app.
    2022-05-12 20:52:04 : INFO  : filezilla : FileZilla.tbz exists and DEBUG mode 1 enabled, skipping download
    2022-05-12 20:52:04 : REQ   : filezilla : Installing FileZilla
    2022-05-12 20:52:04 : INFO  : filezilla : Unzipping FileZilla.tbz
    2022-05-12 20:52:07 : INFO  : filezilla : Verifying: /Users/thijs/gits/Installomator/build/FileZilla.app
    2022-05-12 20:52:07 : DEBUG : filezilla : App size:  44M	/Users/thijs/gits/Installomator/build/FileZilla.app
    2022-05-12 20:52:07 : DEBUG : filezilla : Debugging enabled, App Verification output was:
    /Users/thijs/gits/Installomator/build/FileZilla.app: accepted
    source=Notarized Developer ID
    origin=Developer ID Application: Tim Kosse (5VPGKXL75N)
    
    2022-05-12 20:52:07 : INFO  : filezilla : Team ID matching: 5VPGKXL75N (expected: 5VPGKXL75N )
    2022-05-12 20:52:07 : INFO  : filezilla : Downloaded version of FileZilla is 3.59.0 on versionKey CFBundleShortVersionString, same as installed.
    2022-05-12 20:52:07 : DEBUG : filezilla : DEBUG mode 1, not reopening anything
    2022-05-12 20:52:07 : REG   : filezilla : No new version to install
    2022-05-12 20:52:07 : REQ   : filezilla : ################## End Installomator, exit code 0

--- fragments/labels/filezilla.sh
+++ fragments/labels/filezilla.sh
@@ -2,8 +2,8 @@ filezilla)
     name="FileZilla"
     type="tbz"
     packageID="org.filezilla-project.filezilla"
-    downloadURL=$(curl -fs https://filezilla-project.org/download.php\?show_all=1 | grep macosx | head -n 1 | awk -F '"' '{print $2}' )
-    appNewVersion=$( curl -fs https://filezilla-project.org/download.php\?show_all=1 | grep macosx | head -n 1 | awk -F '_' '{print $2}' )
+    downloadURL=$(curl -fsL https://filezilla-project.org/download.php\?show_all=1 | grep macosx | head -n 1 | awk -F '"' '{print $2}' )
+    appNewVersion=$( curl -fsL https://filezilla-project.org/download.php\?show_all=1 | grep macosx | head -n 1 | awk -F '_' '{print $2}' )
     expectedTeamID="5VPGKXL75N"
     blockingProcesses=( NONE )
     ;;
