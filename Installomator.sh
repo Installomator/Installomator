d566eb566cf34befdb8c85e553972b320bc0ea8b
Chris Abbs <chris@meissa.net>
2022-05-17 11:55:24 -0500

    Update Installomator.sh
    fixed spaciing

--- Installomator.sh
+++ Installomator.sh
@@ -1748,8 +1748,8 @@ blender)
     elif [[ $(arch) == "i386" ]]; then
         downloadURL=$(curl -sfL "https://www.blender.org/download/" | xmllint --html --format - 2>/dev/null | grep -o "https://.*blender.*x64.*.dmg" | sed '2p;d' | sed 's/www.blender.org\/download/download.blender.org/g')
     fi
-appNewVersion=$( echo "${downloadURL}" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)-.*/\1/g' )
-expectedTeamID="68UA947AUU"
+    appNewVersion=$( echo "${downloadURL}" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)-.*/\1/g' )
+    expectedTeamID="68UA947AUU"
 bluejeans)
     name="BlueJeans"
     type="pkg"
