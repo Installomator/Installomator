8be5f4866ed0415e89662070ab3b2bb12db8884a
Liam Steckler <553265+buckbanzai@users.noreply.github.com>
2022-05-17 14:41:57 -0700

    Update talkdeskcallbar to use JSON parsing

--- fragments/labels/talkdeskcallbar.sh
+++ fragments/labels/talkdeskcallbar.sh
@@ -1,7 +1,8 @@
 talkdeskcallbar)
     name="Callbar"
     type="dmg"
-    appNewVersion=$(curl -fsL https://downloadcallbar.talkdesk.com/release_metadata.json | sed -n 's/^.*"version"[[:space:]]*:[[:space:]]*"\([^"]*\)".*$/\1/p')
+    talkdeskcallbarVersions=$(curl -fsL "https://downloadcallbar.talkdesk.com/release_metadata.json")
+    appNewVersion=$(getJSONValue "$talkdeskcallbarVersions" "version")
     downloadURL=https://downloadcallbar.talkdesk.com/Callbar-${appNewVersion}.dmg
     expectedTeamID="YGGJX44TB8"
     ;;
