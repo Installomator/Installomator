124c0cf85f90272e612b61311047645a76fde0b1
MacMike077 <104210875+MacMike077@users.noreply.github.com>
2022-05-18 11:07:54 +0200

    Create googleadseditor.sh
    2022-05-18 11:07:11 : REQ   : googleadseditor : ################## Start Installomator v. 9.2, date 2022-05-16
    2022-05-18 11:07:11 : INFO  : googleadseditor : ################## Version: 9.2
    2022-05-18 11:07:11 : INFO  : googleadseditor : ################## Date: 2022-05-16
    2022-05-18 11:07:11 : INFO  : googleadseditor : ################## googleadseditor
    2022-05-18 11:07:11 : INFO  : googleadseditor : BLOCKING_PROCESS_ACTION=tell_user
    2022-05-18 11:07:11 : INFO  : googleadseditor : NOTIFY=success
    2022-05-18 11:07:11 : INFO  : googleadseditor : LOGGING=INFO
    2022-05-18 11:07:11 : INFO  : googleadseditor : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2022-05-18 11:07:11 : INFO  : googleadseditor : Label type: dmg
    2022-05-18 11:07:11 : INFO  : googleadseditor : archiveName: Google Ads Editor.dmg
    2022-05-18 11:07:11 : INFO  : googleadseditor : no blocking processes defined, using Google Ads Editor as default
    2022-05-18 11:07:11 : INFO  : googleadseditor : name: Google Ads Editor, appName: Google Ads Editor.app
    2022-05-18 11:07:12 : WARN  : googleadseditor : No previous app found
    2022-05-18 11:07:12 : WARN  : googleadseditor : could not find Google Ads Editor.app
    2022-05-18 11:07:12 : INFO  : googleadseditor : appversion:
    2022-05-18 11:07:12 : INFO  : googleadseditor : Latest version not specified.
    2022-05-18 11:07:12 : REQ   : googleadseditor : Downloading https://dl.google.com/adwords_editor/google_ads_editor.dmg to Google Ads Editor.dmg
    2022-05-18 11:07:17 : REQ   : googleadseditor : no more blocking processes, continue with update
    2022-05-18 11:07:17 : REQ   : googleadseditor : Installing Google Ads Editor
    2022-05-18 11:07:17 : INFO  : googleadseditor : Mounting /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.q9auZnh3/Google Ads Editor.dmg
    2022-05-18 11:07:19 : INFO  : googleadseditor : Mounted: /Volumes/Google Ads Editor 1
    2022-05-18 11:07:19 : INFO  : googleadseditor : Verifying: /Volumes/Google Ads Editor 1/Google Ads Editor.app
    2022-05-18 11:07:21 : INFO  : googleadseditor : Team ID matching: EQHXZ8M8AV (expected: EQHXZ8M8AV )
    2022-05-18 11:07:21.509 defaults[65632:614893]
    The domain/default pair of (/Volumes/Google Ads Editor 1/Google Ads Editor.app/Contents/Info.plist, CFBundleShortVersionString) does not exist
    2022-05-18 11:07:21 : INFO  : googleadseditor : Installing Google Ads Editor version  on versionKey CFBundleShortVersionString.
    2022-05-18 11:07:21 : INFO  : googleadseditor : App has LSMinimumSystemVersion: 10.13
    2022-05-18 11:07:21 : INFO  : googleadseditor : Copy /Volumes/Google Ads Editor 1/Google Ads Editor.app to /Applications
    2022-05-18 11:07:22 : WARN  : googleadseditor : No user logged in or SYSTEMOWNER=1, setting owner to root:wheel
    2022-05-18 11:07:22 : INFO  : googleadseditor : Finishing...
    2022-05-18 11:07:32 : INFO  : googleadseditor : App(s) found: /Applications/Google Ads Editor.app
    2022-05-18 11:07:32.448 defaults[65669:615163]
    The domain/default pair of (/Applications/Google Ads Editor.app/Contents/Info.plist, CFBundleShortVersionString) does not exist
    2022-05-18 11:07:32 : INFO  : googleadseditor : found app at /Applications/Google Ads Editor.app, version , on versionKey CFBundleShortVersionString
    2022-05-18 11:07:32 : REQ   : googleadseditor : Installed Google Ads Editor
    2022-05-18 11:07:32 : INFO  : googleadseditor : notifying
    2022-05-18 11:07:32 : INFO  : googleadseditor : App not closed, so no reopen.
    2022-05-18 11:07:32 : REQ   : googleadseditor : All done!
    2022-05-18 11:07:32 : REQ   : googleadseditor : ################## End Installomator, exit code 0

---
+++ googleadseditor.sh
@@ -0,0 +1,7 @@
+googleadseditor)
+    name="Google Ads Editor"
+    type="dmg"
+    downloadURL="https://dl.google.com/adwords_editor/google_ads_editor.dmg"
+    appNewVersion=""
+    expectedTeamID="EQHXZ8M8AV"
+    ;;
