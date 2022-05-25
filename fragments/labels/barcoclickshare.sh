c219f38459aa92ebc8670fc858904ff1eeb7b7f2
Søren Theilgaard <soren@theilgaard.dk>
2022-05-25 13:26:37 +0200

    Create barcoclickshare.sh

---
+++ fragments/labels/barcoclickshare.sh
@@ -0,0 +1,7 @@
+barcoclickshare)
+    name="ClickShare"
+    type="appInDmgInZip"
+    downloadURL="https://www.barco.com$( curl -fs "https://www.barco.com/en/clickshare/app" | grep -A6 -i "macos" | grep -i "FileNumber" | tr '"' "\n" | grep -i "FileNumber" )"
+    appNewVersion="$(eval "$( echo $downloadURL | sed -E 's/.*(MajorVersion.*BuildVersion=[0-9]*).*/\1/' | sed 's/&amp//g' )" ; ((MajorVersion++)) ; ((MajorVersion--)); ((MinorVersion++)) ; ((MinorVersion--)); ((PatchVersion++)) ; ((PatchVersion--)); ((BuildVersion++)) ; ((BuildVersion--)); echo "${MajorVersion}.${MinorVersion}.${PatchVersion}-b${BuildVersion}")"
+    expectedTeamID="P6CDJZR997"
+    ;;
