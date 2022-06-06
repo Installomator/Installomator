a6f1224b414a847928b54ee1940739c0c4c527c0
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2022-06-06 15:36:30 -0500

    Removed redundant variable and added arm64 download logic

--- fragments/labels/kap.sh
+++ fragments/labels/kap.sh
@@ -2,13 +2,12 @@ kap)
     # credit: Lance Stephens (@pythoninthegrass on MacAdmins Slack)
     name="Kap"
     type="dmg"
-    archiveName=
     if [[ $(arch) = "i386" ]]; then
         archiveName="${name}-[0-9.]*-x64.${type}"
         downloadURL=$(downloadURLFromGit wulkano kap | grep -i x64)
     else
         archiveName="${name}-[0-9.]*-arm64.${type}"
-        downloadURL=$(downloadURLFromGit wulkano kap)
+        downloadURL=$(downloadURLFromGit wulkano kap | grep -i arm64)
     fi
     appNewVersion=$(versionFromGit wulkano Kap)
     expectedTeamID="2KEEHXF6R6"
