a84b14234e02395cf80dda401d106c08a82b1051
Chriscodinglife <chrisdotorellana@gmail.com>
2022-06-28 16:36:41 -0400

    Corrected appnewversion variable with sed grabbing the version number displayed in the package file

--- fragments/labels/fujifilmwebcam.sh
+++ fragments/labels/fujifilmwebcam.sh
@@ -2,6 +2,6 @@ fujifilmwebcam)
      name="FUJIFILM X Webcam 2"
      type="pkg"
      downloadURL=$(curl -fs "https://fujifilm-x.com/en-us/support/download/software/x-webcam/" | grep "https.*pkg" | sed -E 's/.*(https:\/\/dl.fujifilm-x\.com\/support\/software\/.*\.pkg[^\<]).*/\1/g' | sed -e 's/^"//' -e 's/"$//')
-     appNewVersion=$( echo “${downloadURL}” | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g' )
+     appNewVersion=$( echo “${downloadURL}” | sed -E 's/.*XWebcamIns([0-9]*).*/\1/g' )
      expectedTeamID="34LRP8AV2M"
      ;;
