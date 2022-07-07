42ea03744fdc496d79a28be594019c286e3eed46
Eddie Vartanessian <evartanessian@splunk.com>
2022-07-07 17:59:16 -0400

    Edited Google Drive label to be correct. It was searching for Google Drive File Stream.app instead of Google Drive.app. Also updated appNewVersion, but left it out due to latest version not being available for download

--- fragments/labels/googledrive.sh
+++ fragments/labels/googledrive.sh
@@ -1,10 +1,16 @@
 googledrive|\
 googledrivefilestream)
-    # credit: Isaac Ordonez, Mann consulting (@mannconsulting)
-    name="Google Drive File Stream"
+    # credit: Eddie Vartanessian (@eddiev2)
+    name="GoogleDrive"
     type="pkgInDmg"
-    packageID="com.google.drivefs"
-    downloadURL="https://dl.google.com/drive-file-stream/GoogleDriveFileStream.dmg" # downloadURL="https://dl.google.com/drive-file-stream/GoogleDrive.dmg"
-    blockingProcesses=( "Google Docs" "Google Drive" "Google Sheets" "Google Slides" )
+    if [[ $(arch) == "i386" ]]; then
+    packageID="com.google.drivefs.x86_64"
+    fi
+
+    if [[ $(arch) == "arm*" ]]; then
+    packageID="com.google.drivefs.arm64"
+    fi
+    downloadURL="https://dl.google.com/drive-file-stream/GoogleDrive.dmg"
+    #appNewVersion=$(curl -fs https://support.google.com/a/answer/7577057\?hl\=en | grep "Version " | sed '1p;d;/Version /!d' | awk -F' ' '{print $5}' | cut -b 1-4)
     expectedTeamID="EQHXZ8M8AV"
     ;;
