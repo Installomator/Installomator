6f65f5d2cb486d5e16da8d5e489afa0f839d6aad
Bradley Benkle <brad.benkle@gmail.com>
2022-07-18 11:25:00 -0400

    Update sublimetext.sh

--- fragments/labels/sublimetext.sh
+++ fragments/labels/sublimetext.sh
@@ -2,7 +2,7 @@ sublimetext)
     # credit: Søren Theilgaard (@theilgaard)
     name="Sublime Text"
     type="zip"
-    downloadURL="$(curl -fs https://www.sublimetext.com/download | grep -io "https://download.*_mac.zip")"
+    downloadURL=downloadURL="$(curl -fs "https://www.sublimetext.com/download_thanks?target=mac#direct-downloads" | grep "mac.zip" | grep -o -i -E "https.*"| cut -d '"' -f1 | head -1)"
     appNewVersion=$(curl -fs https://www.sublimetext.com/download | grep -i -A 4 "id.*changelog" | grep -io "Build [0-9]*")
     expectedTeamID="Z6D26JE4Y4"
     ;;
