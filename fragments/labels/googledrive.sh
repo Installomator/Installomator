a38f835eaf1a9885dbc06b89ddf00d22b14aa2a6
Eddie Vartanessian <evartanessian@splunk.com>
2022-07-18 16:11:47 -0400

    Fixed grep line to properly spit out version number

--- fragments/labels/googledrive.sh
+++ fragments/labels/googledrive.sh
@@ -11,6 +11,6 @@ googledrivefilestream)
     packageID="com.google.drivefs.arm64"
     fi
     downloadURL="https://dl.google.com/drive-file-stream/GoogleDrive.dmg"
-    #appNewVersion=$(curl -fs https://support.google.com/a/answer/7577057\?hl\=en | grep "Version " | sed '1p;d;/Version /!d' | awk -F' ' '{print $5}' | cut -b 1-4)
+    #appNewVersion=$(curl -fs https://support.google.com/a/answer/7577057\?hl\=en |  grep -o "Version [0-9][0-9]" | sed '1p;d;/Version /!d')
     expectedTeamID="EQHXZ8M8AV"
     ;;
