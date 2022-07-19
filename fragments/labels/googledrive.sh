6b78cdba4e2e8bbe104669d1c540fd30aead427d
Eddie Vartanessian <evartanessian@splunk.com>
2022-07-19 00:13:25 -0400

    Simplified grep and added in possible solution for appNewVersion, but still commented out until further tested

--- fragments/labels/googledrive.sh
+++ fragments/labels/googledrive.sh
@@ -11,6 +11,6 @@ googledrivefilestream)
     packageID="com.google.drivefs.arm64"
     fi
     downloadURL="https://dl.google.com/drive-file-stream/GoogleDrive.dmg"
-    #appNewVersion=$(curl -fs https://support.google.com/a/answer/7577057\?hl\=en |  grep -o "Version [0-9][0-9]" | sed '1p;d;/Version /!d')
+    #appNewVersion=$(curl -fs https://support.google.com/a/answer/7577057\?hl\=en |  grep -o "Version [0-9][0-9][.0-9]*" | sed '2p;d;/Version /!d')
     expectedTeamID="EQHXZ8M8AV"
     ;;
