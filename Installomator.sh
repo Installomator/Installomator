60756c84483595e539a7fdc66647b4c249be70f7
vdmchkv <106989523+vdmchkv@users.noreply.github.com>
2022-07-23 23:57:12 +0200

    Add ARM version of Miro
    Recently Miro published ARM version of their desktop application. This changes enables download of a native ARM application on capable MacBook's.

--- Installomator.sh
+++ Installomator.sh
@@ -3569,7 +3569,11 @@ miro)
     # credit: @matins
     name="Miro"
     type="dmg"
-    downloadURL="https://desktop.miro.com/platforms/darwin/Miro.dmg"
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL="https://desktop.miro.com/platforms/darwin-arm64/Miro.dmg"
+    elif [[ $(arch) == "i386" ]]; then
+        downloadURL="https://desktop.miro.com/platforms/darwin/Miro.dmg"
+    fi    
     expectedTeamID="M3GM7MFY7U"
     ;;
 mobikinassistantforandroid)
