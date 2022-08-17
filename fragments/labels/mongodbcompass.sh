cfa05aa902bc0715be8fe811a661df228841c5be
Søren Theilgaard <st@ENVO-st.local>
2022-08-17 09:14:32 +0200

    Create mongodbcompass.sh

---
+++ fragments/labels/mongodbcompass.sh
@@ -0,0 +1,7 @@
+mongodbcompass)
+    name="MongoDB Compass"
+    type="dmg"
+    downloadURL="$(downloadURLFromGit mongodb-js compass)"
+    appNewVersion="$(versionFromGit mongodb-js compass)"
+    expectedTeamID="4XWMY46275"
+    ;;
