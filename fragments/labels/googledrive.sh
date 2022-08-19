764f40b650797623b7add9f271c55d403ed9539d
EddieV2 <e_vartanessian@live.com>
2022-08-19 12:37:34 -0400

    Added blocking processes

--- fragments/labels/googledrive.sh
+++ fragments/labels/googledrive.sh
@@ -13,4 +13,5 @@ googledrivefilestream)
     downloadURL="https://dl.google.com/drive-file-stream/GoogleDrive.dmg"
     #appNewVersion=$(curl -fs https://support.google.com/a/answer/7577057\?hl\=en |  grep -o "[0-9][0-9][.0-9]*" | sed '2p;d;/Version /!d')
     expectedTeamID="EQHXZ8M8AV"
+    blockingProcesses=( "Google Docs" "Google Drive" "Google Sheets" "Google Slides" )
     ;;
