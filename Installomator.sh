31e6774344d60ed655c1d7ff68f7e4d4b0bff820
EddieV2 <e_vartanessian@live.com>
2022-08-19 12:40:01 -0400

    Replaced googledrive label with my own

--- Installomator.sh
+++ Installomator.sh
@@ -2552,13 +2552,20 @@ googlechromepkg)
     ;;
 googledrive|\
 googledrivefilestream)
-    # credit: Isaac Ordonez, Mann consulting (@mannconsulting)
-    name="Google Drive File Stream"
+    # Eddie Vartanessian (@eddiev2)
+    name="GoogleDrive"
     type="pkgInDmg"
-    packageID="com.google.drivefs"
-    downloadURL="https://dl.google.com/drive-file-stream/GoogleDriveFileStream.dmg" # downloadURL="https://dl.google.com/drive-file-stream/GoogleDrive.dmg"
-    blockingProcesses=( "Google Docs" "Google Drive" "Google Sheets" "Google Slides" )
+    if [[ $(arch) == "i386" ]]; then
+    packageID="com.google.drivefs.x86_64"
+    fi
+
+    if [[ $(arch) == "arm64" ]]; then
+    packageID="com.google.drivefs.arm64"
+    fi
+    downloadURL="https://dl.google.com/drive-file-stream/GoogleDrive.dmg"
+    #appNewVersion=$(curl -fs https://support.google.com/a/answer/7577057\?hl\=en |  grep -o "[0-9][0-9][.0-9]*" | sed '2p;d;/Version /!d')
     expectedTeamID="EQHXZ8M8AV"
+    blockingProcesses=( "Google Docs" "Google Drive" "Google Sheets" "Google Slides" )
     ;;
 googledrivebackupandsync)
     name="Backup and Sync"
