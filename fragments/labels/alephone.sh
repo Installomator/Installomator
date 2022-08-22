84da5ac16847eb53a85a8bb023b34a1502950132
Søren Theilgaard <st@ENVO-st.local>
2022-08-22 11:43:13 +0200

    Update alephone.sh

--- fragments/labels/alephone.sh
+++ fragments/labels/alephone.sh
@@ -1,6 +1,7 @@
 alephone)
     name="Aleph One"
     type="dmg"
+    archiveName="AlephOne-[0-9]*-Mac.dmg"
     downloadURL=$(downloadURLFromGit Aleph-One-Marathon alephone)
     appNewVersion=$(versionFromGit Aleph-One-Marathon alephone)
     expectedTeamID="E8K89CXZE7"
