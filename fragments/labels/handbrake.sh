7d1067f379a9a59b55a75dd5494815b530e4beb5
Søren Theilgaard <st@ENVO-st.local>
2022-08-23 14:05:01 +0200

    Update handbrake.sh
    Specified the correct binary in the distribution, it would automatically find CLI-version.

--- fragments/labels/handbrake.sh
+++ fragments/labels/handbrake.sh
@@ -1,6 +1,7 @@
 handbrake)
     name="HandBrake"
     type="dmg"
+    archiveName="HandBrake-[0-9.]*.dmg$"
     downloadURL=$(downloadURLFromGit HandBrake HandBrake )
     appNewVersion=$(versionFromGit HandBrake HandBrake )
     expectedTeamID="5X9DE89KYV"
