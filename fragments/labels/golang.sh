b87c5e1ada21426dd5b300cb4d7b29b2df4b5671
Søren Theilgaard <st@ENVO-st.local>
2022-08-23 13:56:55 +0200

    Update golang.sh
    Different `downloadURL` for architectures. They even call `x86` for `amd`.

--- fragments/labels/golang.sh
+++ fragments/labels/golang.sh
@@ -2,7 +2,11 @@ golang)
     name="GoLang"
     type="pkg"
     packageID="org.golang.go"
-    downloadURL="https://go.dev$(curl -fs "https://go.dev/dl/" | grep -i "downloadBox" | grep "pkg" | tr '"' '\n' | grep "pkg")"
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL="https://go.dev$(curl -fs "https://go.dev/dl/" | grep -i "downloadBox" | grep "darwin-arm" | tr '"' '\n' | grep "pkg")"
+    elif [[ $(arch) == "i386" ]]; then
+        downloadURL="https://go.dev$(curl -fs "https://go.dev/dl/" | grep -i "downloadBox" | grep "darwin-amd" | tr '"' '\n' | grep "pkg")"
+    fi
     appNewVersion="$( echo "${downloadURL}" | sed -E 's/.*\/(go[0-9.]*)\..*/\1/g' )" # Version includes letters "go" in the beginning
     expectedTeamID="EQHXZ8M8AV"
     blockingProcesses=( NONE )
