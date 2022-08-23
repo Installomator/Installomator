9047d5d68f5e80071013af3d4b7e44d96d8b4fbf
Søren Theilgaard <st@ENVO-st.local>
2022-08-23 17:03:25 +0200

    `sap` renamed to `SAP`
    And added `archiveName` to make sure we find the correct file and not the source code.

--- fragments/labels/icons.sh
+++ fragments/labels/icons.sh
@@ -2,7 +2,8 @@ icons)
     # credit: Mischa van der Bent (@mischavdbent)
     name="Icons"
     type="zip"
-    downloadURL=$(downloadURLFromGit sap macOS-icon-generator )
-    appNewVersion=$(versionFromGit sap macOS-icon-generator )
+    archiveName="Icons.*.zip"
+    downloadURL=$(downloadURLFromGit SAP macOS-icon-generator )
+    appNewVersion=$(versionFromGit SAP macOS-icon-generator )
     expectedTeamID="7R5ZEU67FQ"
     ;;
