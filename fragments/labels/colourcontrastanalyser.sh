9b029ee174867492cd66fe716567f99aa755f91d
Søren Theilgaard <st@ENVO-st.local>
2022-08-23 08:43:53 +0200

    Fixed `colourcontrastanalyser`, `cryptomator`, and `dangerzone`

--- fragments/labels/colourcontrastanalyser.sh
+++ fragments/labels/colourcontrastanalyser.sh
@@ -1,6 +1,7 @@
 colourcontrastanalyser)
     name="Colour Contrast Analyser (CCA)"
     type="dmg"
+    archiveName="CCA-[0-9.]*.dmg"
     downloadURL=$(downloadURLFromGit ThePacielloGroup CCAe)
     appNewVersion=$(versionFromGit ThePacielloGroup CCAe)
     expectedTeamID="34RS4UC3M6"
--- fragments/labels/dangerzone.sh
+++ fragments/labels/dangerzone.sh
@@ -1,5 +1,6 @@
 dangerzone)
     name="Dangerzone"
+    printlog "Label for $name broken in test" WARN
     type="dmg"
     downloadURL="$(downloadURLFromGit firstlookmedia dangerzone)"
     appNewVersion="$(versionFromGit firstlookmedia dangerzone)"
