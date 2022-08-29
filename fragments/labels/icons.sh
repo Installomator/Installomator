7458f8656b0352aab1f0950894d3af00dbc0bc13
Søren Theilgaard <st@ENVO-st.local>
2022-08-29 14:24:09 +0200

    Label `icons` with no credit line

--- fragments/labels/icons.sh
+++ fragments/labels/icons.sh
@@ -1,8 +1,6 @@
 icons)
-    # credit: Mischa van der Bent (@mischavdbent)
     name="Icons"
     type="zip"
-    archiveName="Icons.*.zip"
     downloadURL=$(downloadURLFromGit SAP macOS-icon-generator )
     appNewVersion=$(versionFromGit SAP macOS-icon-generator )
     expectedTeamID="7R5ZEU67FQ"
