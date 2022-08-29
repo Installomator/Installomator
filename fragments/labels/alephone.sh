1155a9ee1eb153dca8af61117047921ebf2d4679
Søren Theilgaard <st@ENVO-st.local>
2022-08-29 14:25:09 +0200

    Reverse my previous fix
    Not needed…

--- fragments/labels/alephone.sh
+++ fragments/labels/alephone.sh
@@ -1,7 +1,6 @@
 alephone)
     name="Aleph One"
     type="dmg"
-    archiveName="AlephOne-[0-9]*-Mac.dmg"
     downloadURL=$(downloadURLFromGit Aleph-One-Marathon alephone)
     appNewVersion=$(versionFromGit Aleph-One-Marathon alephone)
     expectedTeamID="E8K89CXZE7"
