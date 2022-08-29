2399dc5c1eedc1eda733a393d295f677393fdd67
Søren Theilgaard <st@ENVO-st.local>
2022-08-29 20:39:22 +0200

    No "fix" needed after github download fix.

--- fragments/labels/handbrake.sh
+++ fragments/labels/handbrake.sh
@@ -1,7 +1,6 @@
 handbrake)
     name="HandBrake"
     type="dmg"
-    archiveName="HandBrake-[0-9.]*.dmg$"
     downloadURL=$(downloadURLFromGit HandBrake HandBrake )
     appNewVersion=$(versionFromGit HandBrake HandBrake )
     expectedTeamID="5X9DE89KYV"
