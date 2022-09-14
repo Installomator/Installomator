57ed4e77f4ab197076c468ac114a2d9b4ab8aded
Fletcher Salesky <fletcher@salesky.dev>
2022-09-14 16:34:03 -0700

    Move archiveName before downloadURL

--- fragments/labels/nudgesuite.sh
+++ fragments/labels/nudgesuite.sh
@@ -2,9 +2,9 @@ nudgesuite)
     name="Nudge Suite"
     appName="Nudge.app"
     type="pkg"
+    archiveName="Nudge_Suite-[0-9.]*.pkg"
     downloadURL=$(downloadURLFromGit macadmins Nudge )
     appNewVersion=$(versionFromGit macadmins Nudge )
     expectedTeamID="9GQZ7KUFR6"
     blockingProcesses=( "Nudge" )
-    archiveName="Nudge_Suite-[0-9.]*.pkg"
     ;;
