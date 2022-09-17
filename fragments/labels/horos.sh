6c829ed0d672f9a6c52b0f8fe2bd4c4b08a2c278
macmangr <106015597+macmangr@users.noreply.github.com>
2022-09-17 16:53:15 +0200

    Added architecture check and ARM version of Horos
    The developer has now also released an Apple Silicon version of the software, so a review of the processor and the appropriate download links have been added
    
    The expectedTeamID is now a variable because the Apple Silicon version of the app has a different TeamID

--- fragments/labels/horos.sh
+++ fragments/labels/horos.sh
@@ -3,6 +3,12 @@ horos)
     type="dmg"
     versionKey="CFBundleGetInfoString"
     appNewVersion=$(curl -fs https://github.com/horosproject/horos/blob/horos/Horos/Info.plist | grep -A 4 "CFBundleGetInfoString" | tail -1 | sed -r 's/.*Horos v([^<]+).*/\1/' | sed 's/ //g')
-    downloadURL="https://horosproject.org/horos-content/Horos"$appNewVersion".dmg"
-    expectedTeamID="TPT6TVH8UY"
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL="https://horosproject.org/horos-content/Horos"$appNewVersion"_Apple.dmg"
+        TeamID="8NDFEW7285"
+    elif [[ $(arch) == "i386" ]]; then
+        downloadURL="https://horosproject.org/horos-content/Horos"$appNewVersion".dmg"
+        TeamID="TPT6TVH8UY"
+    fi
+    expectedTeamID=$TeamID
     ;;
