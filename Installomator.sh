b45c2f81eafb2c8ca3723fff937fc8b2f10c33b3
Theile <soren@theilgaard.dk>
2022-09-30 22:18:29 +0200

    10.0beta3, 2022-09-30

--- Installomator.sh
+++ Installomator.sh
@@ -2656,6 +2656,15 @@ egnyte)
     expectedTeamID="FELUD555VC"
     blockingProcesses=( NONE )
     ;;
+egnytecore)
+    name="Egnyte Core"
+    appName="Egnyte.app"
+    type="dmg"
+    downloadURL=$(curl -fs "https://egnyte-cdn.egnyte.com/desktopapp/mac/en-us/versions/default.xml" | xpath '(//rss/channel/item/enclosure/@url)[1]' 2>/dev/null | cut -d '"' -f 2)
+    appNewVersion=$(curl -fs "https://egnyte-cdn.egnyte.com/desktopapp/mac/en-us/versions/default.xml" | xpath '(//rss/channel/item/enclosure/@sparkle:shortVersionString)[1]' 2>/dev/null | cut -d '"' -f 2)
+    expectedTeamID="FELUD555VC"
+    blockingProcesses=( NONE )
+    ;;
 egnytewebedit)
     name="EgnyteWebEdit"
     type="pkg"
