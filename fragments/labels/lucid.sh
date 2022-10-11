a6263759063925d35b7509a3c279665948597c40
Iñaky Martinez <inaky.martinez@teamlewis.com>
2022-10-11 13:07:25 +0200

    Update lucid.sh

--- fragments/labels/lucid.sh
+++ fragments/labels/lucid.sh
@@ -1,9 +1,9 @@
 lucid)
-    name="lucid-2.1.4032"
+    name="lucid"
     type="pkg"
     packageID="com.lucidlink"
-    downloadURL="https://d3il9duqikhdqy.cloudfront.net/latest/osx/lucid-2.1.4032.pkg"
-    appNewVersion="2.1.4032"
+    downloadURL="https://www.lucidlink.com/download/latest/osx/stable/"
+    appNewVersion=$( curl -fsIL "${downloadURL}" | grep -i "^location" | awk '{print $2}' | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g' )
     expectedTeamID="Y4KMJPU2B4"
     blockingProcesses=( Lucid )
     ;;
