b5a51c3cbf141c7817bd974cad19ca6fcd177c41
Theile <soren@theilgaard.dk>
2022-10-12 08:37:34 +0200

    Update lucidlink.sh
    `name` is now the name of the app. App has perfect values in it's version, so no need for `packageID`. Since `name` matches the name of the app, `blockingProcesses` comes for free.

--- fragments/labels/lucidlink.sh
+++ fragments/labels/lucidlink.sh
@@ -1,9 +1,8 @@
 lucidlink)
-    name="lucidlink"
+    name="Lucid"
+    # https://www.lucidlink.com/download
     type="pkg"
-    packageID="com.lucidlink"
     downloadURL="https://www.lucidlink.com/download/latest/osx/stable/"
     appNewVersion=$( curl -fsIL "${downloadURL}" | grep -i "^location" | awk '{print $2}' | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g' )
     expectedTeamID="Y4KMJPU2B4"
-    blockingProcesses=( Lucid )
     ;;
