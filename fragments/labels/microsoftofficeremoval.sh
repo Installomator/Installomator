d4df866e4d699e0ea1e80c75f0140b3c08c3dbea
wantedwriter <github@wezlab.net>
2022-10-24 18:30:58 -0500

    Update microsoftofficeremoval.sh

--- fragments/labels/microsoftofficeremoval.sh
+++ fragments/labels/microsoftofficeremoval.sh
@@ -1,5 +1,5 @@
 microsoftofficeremoval)
-    name="Microsoft Office Removal"
+    name="microsoftofficeremoval"
     type="pkg"
     packageID="com.microsoft.remove.Office"
     downloadURL="https://office-reset.com"$(curl -fs https://office-reset.com/macadmins/ | grep -o -i "href.*\".*\"*Office_Removal.*.pkg" | cut -d '"' -f2)
