626c8485d8a667781e3ed8c294cc517dc3ab52c3
Fredrik Larsson <33235315+kryptonit1@users.noreply.github.com>
2022-10-31 12:42:11 +0100

    Added appNewVersion variable to Prism 9 label

--- fragments/labels/prism9.sh
+++ fragments/labels/prism9.sh
@@ -2,5 +2,6 @@ prism9)
     name="Prism 9"
     type="dmg"
     downloadURL="https://cdn.graphpad.com/downloads/prism/9/InstallPrism9.dmg"
+    appNewVersion=$(curl -fs "https://www.graphpad.com/updates" | grep -Eio 'The latest Prism version is.*' | cut -d "(" -f 1 | awk -F '<!-- --> <!-- -->' '{print $2}' | cut -d "<" -f 1)
     expectedTeamID="YQ2D36NS9M"
     ;;
