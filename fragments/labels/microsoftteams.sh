07ee329f3de540b611a31045cb7dae299c2d8f3d
Sean Culligan <40404664+sculliga@users.noreply.github.com>
2022-11-04 13:44:23 -0400

    Support blocking using an alternative name

--- fragments/labels/microsoftteams.sh
+++ fragments/labels/microsoftteams.sh
@@ -7,6 +7,7 @@ microsoftteams)
     # Looks like macadmin.software has package ID version. At least on 202105-28 version 1.00.411161 is matched on installed version and homepage.
     expectedTeamID="UBF8T346G9"
     blockingProcesses=( Teams "Microsoft Teams Helper" )
+    blockingProcessesAltName=( [Teams]="Microsoft Teams")
     # Commenting out msupdate as it is not really supported *yet* for teams
     # updateTool="/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate --list; /Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate"
     # updateToolArguments=( --install --apps TEAM01 )
