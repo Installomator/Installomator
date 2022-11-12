fa268675837761fecbbe8db49db51c7a2e45b17c
macmangr <106015597+macmangr@users.noreply.github.com>
2022-11-12 17:46:21 +0100

    Changed download link
    Changed download link to grab the latest version

--- fragments/labels/onscreencontrol.sh
+++ fragments/labels/onscreencontrol.sh
@@ -2,7 +2,8 @@ onscreencontrol)
     name="OnScreen Control"
     type="pkgInZip"
     packageID="com.LGSI.OnScreen-Control"
-    downloadURL=$(currentFileName=`curl -s https://lmu.lge.com/ExternalService/onscreencontrol/mac/2.0/OnScreenControlLatestVersion.txt` && echo https://lmu.lge.com/ExternalService/onscreencontrol/mac/2.0/"$currentFileName")
-    appNewVersion=$(curl -s https://lmu.lge.com/ExternalService/onscreencontrol/mac/2.0/OnScreenControlLatestVersion.txt | sed -E 's/.*_([0-9.]*)\.zip/\1/g')
+    releaseURL="https://www.lg.com/de/support/software-select-category-result?csSalesCode=34WK95U-W.AEU"
+    appNewVersion=$(curl -sf $releaseURL | grep -m 1 "Mac_OSC_" | sed -E 's/.*OSC_([0-9.]*).zip.*/\1/g')
+    downloadURL=$(curl -sf $releaseURL | grep -m 1 "Mac_OSC_" | sed "s|.*href=\"\(.*\)\" title.*|\\1|")
     expectedTeamID="5SKT5H4CPQ"
     ;;
