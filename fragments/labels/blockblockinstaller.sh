0d2601f57378bd9f113f7ccd51659041f8b8f6d8
naschenbrenner <nick@gbpackers.org>
2022-11-15 21:34:59 -0600

    Create blockblockisntaller label
    this is an app created by Objective See and provides continual protection by monitoring persistence locations.
    
    https://github.com/objective-see/BlockBlock/

---
+++ fragments/labels/blockblockinstaller.sh
@@ -0,0 +1,7 @@
+blockblockinstaller)
+name="BlockBlock Installer"
+type="zip"
+downloadURL="$(downloadURLFromGit objective-see BlockBlock)"
+appNewVersion="$(versionFromGit objective-see BlockBlock)"
+expectedTeamID="VBG97UB4TA"
+;;
