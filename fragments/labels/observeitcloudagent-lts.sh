ffa14524f5ed31f598c664ba7aea8ff645c311db
wantedwriter <github@wezlab.net>
2022-11-17 14:30:53 -0600

    Cleanup

---
+++ fragments/labels/observeitcloudagent-lts.sh
@@ -0,0 +1,6 @@
+observeitcloudagent-lts)
+    name="ObserveIT Cloud Agent LTS (1.x)"
+    type="pkgInZip"
+    downloadURL=$(curl -fs https://app.us-east-1-op1.op.analyze.proofpoint.com/downloads/default/ | grep -o -i "href.*\".*\"*observeit-cloudagent-OSX-bundle-1.*.tar.gz" | sed -n '1p' | cut -c 9-)
+    expectedTeamID="DJR63QYCGL"
+    ;;
