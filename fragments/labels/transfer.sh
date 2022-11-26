04ab11f15f7def6f3ba56f2517a98ba5adba9d7e
macmangr <106015597+macmangr@users.noreply.github.com>
2022-11-26 18:29:56 +0100

    Removed -e from xpath
    With the -e option in the xpath command, no version number is read out

--- fragments/labels/transfer.sh
+++ fragments/labels/transfer.sh
@@ -2,6 +2,6 @@ transfer)
     name="Transfer"
     type="dmg"
     downloadURL="https://www.intuitibits.com/products/transfer/download"
-    appNewVersion=$(curl -fs https://www.intuitibits.com/appcasts/transfercast.xml | xpath -e '(//rss/channel/item/sparkle:shortVersionString)[1]' 2>/dev/null | cut -d ">" -f2 | cut -d "<" -f1)
+    appNewVersion=$(curl -fs "https://www.intuitibits.com/appcasts/transfercast.xml" | xpath '(//rss/channel/item/sparkle:shortVersionString)[1]' 2>/dev/null | cut -d ">" -f2 | cut -d "<" -f1)
     expectedTeamID="2B9R362QNU"
     ;;
