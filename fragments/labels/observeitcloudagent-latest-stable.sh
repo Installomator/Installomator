1b4fc7b2f8d73e2e8e3dab40859e09f9e825797a
wantedwriter <github@wezlab.net>
2022-12-07 10:50:14 -0600

    Updates

--- fragments/labels/observeitcloudagent-latest-stable.sh
+++ fragments/labels/observeitcloudagent-latest-stable.sh
@@ -2,6 +2,6 @@ observeitcloudagent-latest-stable)
     name="ObserveIT Cloud Agent Latest Stable"
     type="pkgInZip"
     pkgName="observeit-cloudagent-OSX-2."*".pkg"
-    downloadURL=$(curl -fs https://app.us-east-1-op1.op.analyze.proofpoint.com/downloads/default/ | grep -o -i "href.*\".*\"*observeit-cloudagent-OSX-bundle-1.*.tar.gz" | sed -n '1p' | cut -c 9-)
+    downloadURL=$(curl -fs https://app.us-east-1-op1.op.analyze.proofpoint.com/downloads/default/ | grep -o -i "href.*\".*\"*observeit-cloudagent-OSX-bundle-2.*.tar.gz" | sed -n '1p' | cut -c 9-)
     expectedTeamID="DJR63QYCGL"
     ;;
