4fdd45cba516d11bd59fea6a21437cb2f3eb95cd
wantedwriter <github@wezlab.net>
2022-12-06 20:32:59 -0600

    10.1 Updates

--- Installomator-EPM-10.1.sh
+++ Installomator-EPM-10.1.sh
@@ -4641,6 +4641,13 @@ observeitautoupdater)
     downloadURL=$(curl -fs https://app.us-east-1-op1.op.analyze.proofpoint.com/downloads/default/ | grep -o -i "href.*\".*\"*observeit-autoupdater.*.tar.gz" | sed -n '1p' | cut -c 9-)
     expectedTeamID="DJR63QYCGL"
     ;;
+observeitcloudagent-bundle-lts)
+    name="ObserveIT Cloud Agent (LTS)"
+    type="pkgInZip"
+    pkgName="observeit-cloudagent-OSX-bundle-1."*"9.0.11.pkg"
+    downloadURL=$(curl -fs https://app.us-east-1-op1.op.analyze.proofpoint.com/downloads/default/ | grep -o -i "href.*\".*\"*observeit-cloudagent-OSX-bundle-1.*.tar.gz" | sed -n '1p' | cut -c 9-)
+    expectedTeamID="DJR63QYCGL"
+    ;;
 observeitcloudagent-lts)
     name="ObserveIT Cloud Agent (LTS)"
     type="pkgInZip"
