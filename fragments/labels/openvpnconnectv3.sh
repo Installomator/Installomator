61ec7ff2ce31687276f29df4398961f6b04c7da1
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2022-12-08 11:42:00 +0100

    removed extraneous / from app name as per #784

--- fragments/labels/openvpnconnectv3.sh
+++ fragments/labels/openvpnconnectv3.sh
@@ -3,7 +3,7 @@ openvpnconnectv3)
     name="OpenVPN Connect"
     type="pkgInDmg"
     if [[ $(arch) == "arm64" ]]; then
-        pkgName="/OpenVPN_Connect_[0-9_()]*_arm64_Installer_signed.pkg"
+        pkgName="OpenVPN_Connect_[0-9_()]*_arm64_Installer_signed.pkg"
     elif [[ $(arch) == "i386" ]]; then
         pkgName="OpenVPN_Connect_[0-9_()]*_x86_64_Installer_signed.pkg"
     fi
