ec20656f7fe55991ebe2132688b53163fe987ec9
Asri Zainal <hello@asri.pro>
2022-12-14 12:16:34 +0800

    add betterdisplay label

---
+++ fragments/labels/betterdisplay.sh
@@ -0,0 +1,7 @@
+betterdisplay)
+    name="BetterDisplay"
+    type="dmg"
+    downloadURL=$(downloadURLFromGit waydabber BetterDisplay)
+    appNewVersion=$(versionFromGit waydabber BetterDisplay)
+    expectedTeamID="299YSU96J7"
+    ;;
