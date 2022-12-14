0bc9cd0fab153ced293f9e46df3ed808fa90209a
Asri Zainal <hello@asri.pro>
2022-12-14 16:43:12 +0800

    remove archivename

--- fragments/labels/archimate.sh
+++ fragments/labels/archimate.sh
@@ -2,11 +2,9 @@ archimate)
     name="Archi"
     type="dmg"
     if [[ $(arch) == "arm64" ]]; then
-      archiveName="Archi-Mac-Silicon-[0-9.]*.dmg"
       downloadURL="https://www.archimatetool.com"
       downloadURL+=$(curl -s https://www.archimatetool.com/download/ | grep dmg | sed -n 's/.*href="\([^"]*\)".*/\1/p' | awk 'NR==2')
     elif [[ $(arch) == "i386" ]]; then
-      archiveName="Archi-Mac-[0-9.]*.dmg"
       downloadURL="https://www.archimatetool.com"
       downloadURL+=$(curl -s https://www.archimatetool.com/download/ | grep dmg | sed -n 's/.*href="\([^"]*\)".*/\1/p' | awk 'NR==1')
     fi
