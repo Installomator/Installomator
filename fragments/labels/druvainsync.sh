c436ca363c974ea7554d634c67c93a78715a0f1b
dnikles <dnikles@gmail.com>
2022-12-15 15:28:18 -0500

    use getJSONValue
    I didn't realize it worked with parsing lists in json too.

--- fragments/labels/druvainsync.sh
+++ fragments/labels/druvainsync.sh
@@ -1,7 +1,7 @@
 druvainsync)
     name="Druva inSync"
     type="pkgInDmg"
-    appNewVersion=$(curl -fs https://downloads.druva.com/insync/js/data.json | grep -m1 dmg |awk -F '/' '{print $7}')
-    downloadURL=$(curl -fs https://downloads.druva.com/insync/js/data.json | grep -m1 dmg |awk -F '"' '{print $4}')
+    appNewVersion=$(getJSONValue "$(curl -fsL curl -fs https://downloads.druva.com/insync/js/data.json)" "[1].supportedVersions[0]")
+    downloadURL=$(getJSONValue "$(curl -fsL curl -fs https://downloads.druva.com/insync/js/data.json)" "[1].installerDetails[0].downloadURL")
     expectedTeamID="JN6HK3RMAP"
     ;;
