3aa2567188303762cd599905a4ace5682f655c42
Theile <soren@theilgaard.dk>
2022-12-19 13:21:16 +0100

    Fix for LOGO_PATH for ws1, and only kill the caffeinate process we create
    Quite a few files to fix. Also fixed the `pkill caffeinate` line, that should not be there, as we should only `kill` the `caffeinate` process we create.

--- MDM/Installomator 1st Auto-install.sh
+++ MDM/Installomator 1st Auto-install.sh
@@ -52,7 +52,8 @@ errorMessage="A problem was encountered setting up this Mac. Please contact IT."
 #  https://github.com/Installomator/Installomator
 #
 ######################################################################
-scriptVersion="9.6"
+scriptVersion="9.7"
+# v.  9.7   : 2022-12-19 : Only kill the caffeinate process we create
 # v.  9.6   : 2022-11-15 : GitHub API call is first, only try alternative if that fails.
 # v.  9.5   : 2022-09-21 : change of GitHub download
 # v.  9.4   : 2022-09-14 : Making error message optional. downloadURL can fall back on GitHub API.
