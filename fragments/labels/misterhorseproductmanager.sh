7cea2ec754e928f89de35db718aec09e03df5e99
meschwartz <57272629+meschwartz@users.noreply.github.com>
2022-12-20 12:20:26 -0800

    Update misterhorseproductmanager.sh

--- fragments/labels/misterhorseproductmanager.sh
+++ fragments/labels/misterhorseproductmanager.sh
@@ -1,8 +1,8 @@
 misterhorseproductmanager)
     name="Mister Horse Product Manager"
     type="dmg"
-    downloadURL=$(curl -fsIL "https://misterhorse.com/downloads/product-manager/osx" | grep -i ^location | sed -E 's/.*(https.*\.dmg).*/\1/g')
-    appNewVersion=$(sed -E 's/.*_([0-9.]*)\.dmg/\1/' <<< $downloadURL)
+    downloadURL="https://misterhorse.com/downloads/product-manager/osx"
+    appNewVersion=$(curl -fsIL "https://misterhorse.com/downloads/product-manager/osx" | sed -nE 's/location.*Manager_([0-9.]*)\.dmg.*/\1/p')
     expectedTeamID="H6366SAGL3"
     ;;
 
