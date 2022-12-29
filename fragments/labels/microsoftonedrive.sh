b4fa6adabb1b26f9f0f269c1115c4e24f5416bf1
Theile <soren@theilgaard.dk>
2022-12-29 15:21:09 +0100

    Update microsoftonedrive.sh

--- fragments/labels/microsoftonedrive.sh
+++ fragments/labels/microsoftonedrive.sh
@@ -4,6 +4,14 @@ microsoftonedrive)
     downloadURL="https://go.microsoft.com/fwlink/?linkid=823060"
     #appNewVersion=$(curl -fs https://macadmins.software/latest.xml | xpath '//latest/package[id="com.microsoft.onedrive.standalone"]/cfbundleshortversionstring' 2>/dev/null | sed -E 's/<cfbundleshortversionstring>([0-9.]*)<.*/\1/')
     appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | cut -d "/" -f 6 | cut -d "." -f 1-3)
+    if [[ $1 == "/" ]]; then
+        printlog "Running through Jamf: $0." INFO
+        $0 $1 $2 $3 microsoftonedrive-rollingout ${5} ${6} ${7} ${8} ${9} ${10} ${11}
+        exit
+    #else
+        #printlog "Installomator running locally: $0." INFO
+        #$0 microsoftonedrive-rollingout ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11}
+    fi
     expectedTeamID="UBF8T346G9"
     if [[ -x "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" && $INSTALL != "force" && $DEBUG -eq 0 ]]; then
         printlog "Running msupdate --list"
