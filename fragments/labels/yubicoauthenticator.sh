e0bef61f180b160ba4a2d6adea4ee9661c4cf92c
Christian Braun <christian.braun@greenfield.xyz>
2023-01-09 10:43:17 +0100

    add yubico authenticator

---
+++ fragments/labels/yubicoauthenticator.sh
@@ -0,0 +1,7 @@
+yubicoauthenticator)
+    name="Yubico Authenticator"
+    type="dmg"
+    downloadURL="https://developers.yubico.com/yubioath-flutter/Releases/yubico-authenticator-latest-mac.dmg"
+    appNewVersion=""
+    expectedTeamID="LQA3CS5MM7"
+    ;;
