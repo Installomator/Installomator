bd50ad8099c5cbf0f65d92cf70d810c7eace863d
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2023-01-21 09:14:06 +0100

    fixed file ending

--- fragments/labels/openvpnconnectv3.sh
+++ fragments/labels/openvpnconnectv3.sh
@@ -12,4 +12,3 @@ openvpnconnectv3)
     curlOptions=( -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" )
     expectedTeamID="ACV7L3WCD8"
     ;;
-    
