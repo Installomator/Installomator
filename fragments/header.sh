05a2f258265a0aa6baba2da08cd19d98520bfbf4
dionparsons <123446461+dionparsons@users.noreply.github.com>
2023-01-25 12:05:35 +1100

    Update header.sh
    Fixed Spelling and Uppercase Variable Values

--- fragments/header.sh
+++ fragments/header.sh
@@ -1,19 +1,19 @@
 #!/bin/zsh
 label="" # if no label is sent to the script, this will be used
 
-# Optional Setting for Proxy to be configured, JAMF Paramter Options 7 = Proxy Server and Parameter Options 8 = Proxy Port
-# If Paramters not set then blank values are put in for HTTP and HTTPS Proxy
+# Optional Setting for Proxy to be configured, JAMF Parameter Options 7 = Proxy Server and Parameter Options 8 = Proxy Port
+# If Parameters not set then blank values are put in for HTTP and HTTPS Proxy
 
 # Parameter 7: Proxy Server
-proxyserv=${7:-""} #eg. 192.168.1.20
+PROXYSERV=${7:-""} #eg. 192.168.1.20
 #
 # Parameter 8: Proxy Server Port
-proxyport=${8:-""} #eg. 8080
+PROXYPORT=${8:-""} #eg. 8080
 #
-export HTTP_PROXY=$proxyserv:$proxyport
-export HTTPS_PROXY=$proxyserv:$proxyport
-export http_proxy=$proxyserv:$proxyport
-export https_proxy=$proxyserv:$proxyport
+export HTTP_PROXY=$PROXYSERV:$PROXYPORT
+export HTTPS_PROXY=$PROXYSERV:$PROXYPORT
+export http_proxy=$PROXYSERV:$PROXYPORT
+export https_proxy=$PROXYSERV:$PROXYPORT
 #
 
 # Installomator
