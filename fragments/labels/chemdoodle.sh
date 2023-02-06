bbdb3108e1d8363770bc57c4543467166e3a0616
Bart Reardon <bart.reardon@csiro.au>
2023-02-06 22:14:50 +1100

    Updated chemdoodle labels downloadURL command to retreive the correct URL

--- fragments/labels/chemdoodle.sh
+++ fragments/labels/chemdoodle.sh
@@ -2,7 +2,7 @@ chemdoodle|\
 chemdoodle2d)
      name="ChemDoodle"
      type="dmg"
-     downloadURL="https://www.ichemlabs.com$(curl -s -L https://www.ichemlabs.com/download | xmllint --html --format - 2>&1 | grep -e "ChemDoodle-macos" | sed -r 's/.*href="([^"]+).*/\1/g')"
+     downloadURL="https://www.ichemlabs.com$(curl -s -L https://www.ichemlabs.com/download | xmllint --html --format - 2>&1 | grep -e "ChemDoodle-macos" | sed -r 's/.*href="([^"]+).*/\1/g' | head -n1)"
      expectedTeamID="9XP397UW95"
      folderName="ChemDoodle"
      appName="${folderName}/ChemDoodle.app"
@@ -12,7 +12,7 @@ chemdoodle2d)
 chemdoodle3d)
      name="ChemDoodle3D"
      type="dmg"
-     downloadURL="https://www.ichemlabs.com$(curl -s -L https://www.ichemlabs.com/download | xmllint --html --format - 2>&1 | grep -e "ChemDoodle3D-macos" | sed -r 's/.*href="([^"]+).*/\1/g')"
+     downloadURL="https://www.ichemlabs.com$(curl -s -L https://www.ichemlabs.com/download | xmllint --html --format - 2>&1 | grep -e "ChemDoodle3D-macos" | sed -r 's/.*href="([^"]+).*/\1/g' | head -n1)"
      expectedTeamID="9XP397UW95"
      folderName="ChemDoodle3D"
      appName="${folderName}/ChemDoodle3D.app"
