359394204435bd6f78fe36524003909372ed331d
asri <asri@thoughtmachine.net>
2023-02-08 16:47:52 +0800

    Nudge: Update Team ID

--- fragments/labels/nudgesuite.sh
+++ fragments/labels/nudgesuite.sh
@@ -5,6 +5,6 @@ nudgesuite)
     archiveName="Nudge_Suite-[0-9.]*.pkg"
     downloadURL=$(downloadURLFromGit macadmins Nudge )
     appNewVersion=$(versionFromGit macadmins Nudge )
-    expectedTeamID="9GQZ7KUFR6"
+    expectedTeamID="T4SK8ZXCXG"
     blockingProcesses=( "Nudge" )
     ;;
