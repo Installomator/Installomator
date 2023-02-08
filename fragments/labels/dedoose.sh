b62de4283f649a88d732aaa9319bb0c9cb5b2c1f
Thomas Martin <tmartin@hewittschool.org>
2023-02-08 12:14:22 -0500

    Revert "Create dedoose.sh"
    This reverts commit e5417bd35094620d0d528feab5752796c7442377.

--- fragments/labels/dedoose.sh
+++
@@ -1,7 +0,0 @@
-dedoose)
-    name="Dedoose"
-    type="dmg"
-    downloadURL=$(echo -n "https:" && curl -fs "https://www.dedoose.com/resources/articledetail/dedoose-desktop-app" | grep -o '//.*dmg')
-    appNewVersion=$(echo "$downloadURL" | awk -F'[-.]' '{print $2}') # Cannot be compared to anything
-    expectedTeamID="9U74Q6K62X"
-    ;;
