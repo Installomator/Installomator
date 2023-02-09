cba71d74e94b462c700be7005439041564b0f9a5
Sylvain Roche <5450786+niavlysAO@users.noreply.github.com>
2023-02-09 12:57:16 +0100

    uniformise dl link hardcoded version

--- fragments/labels/wrikeformac.sh
+++ fragments/labels/wrikeformac.sh
@@ -2,11 +2,12 @@ wrikeformac)
     name="Wrike for Mac"
     type="dmg"
     if [[ $(arch) == i386 ]]; then
-        downloadURL="https://dl.wrike.com/download/WrikeDesktopApp.latest.dmg"
+        #downloadURL="https://dl.wrike.com/download/WrikeDesktopApp.latest.dmg"
+        downloadURL="https://dl.wrike.com/download/WrikeDesktopApp.v.4.0.6.dmg"   # pour la coherence avec silicon
     elif [[ $(arch) == arm64 ]]; then
         #downloadURL="https://dl.wrike.com/download/WrikeDesktopApp_ARM.latest.dmg"  # ne marche pas avec latest, il faut obligatoirement un numéro de version 
         downloadURL="https://dl.wrike.com/download/WrikeDesktopApp_ARM.v4.0.6.dmg"
     fi
-    appNewVersion=""
+    appNewVersion="4.0.6"
     expectedTeamID="BD3YL53XT4"
     ;;
