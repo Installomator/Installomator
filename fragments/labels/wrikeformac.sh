03026f79eb2f9a46f5da2ecb6d85fcd373ab2b9c
Sylvain Roche <5450786+niavlysAO@users.noreply.github.com>
2023-02-09 14:24:21 +0100

    variabilize wrikedesktopapp vesrion in dl link

--- fragments/labels/wrikeformac.sh
+++ fragments/labels/wrikeformac.sh
@@ -1,13 +1,13 @@
 wrikeformac)
     name="Wrike for Mac"
     type="dmg"
-    if [[ $(arch) == i386 ]]; then
-        #downloadURL="https://dl.wrike.com/download/WrikeDesktopApp.latest.dmg"
-        downloadURL="https://dl.wrike.com/download/WrikeDesktopApp.v.4.0.6.dmg"   # pour la coherence avec silicon
-    elif [[ $(arch) == arm64 ]]; then
-        #downloadURL="https://dl.wrike.com/download/WrikeDesktopApp_ARM.latest.dmg"  # ne marche pas avec latest, il faut obligatoirement un numéro de version 
-        downloadURL="https://dl.wrike.com/download/WrikeDesktopApp_ARM.v4.0.6.dmg"
-    fi
     appNewVersion="4.0.6"
+    if [[ $(arch) == i386 ]]; then
+        #downloadURL="https://dl.wrike.com/download/WrikeDesktopApp.latest.dmg"      # valide pour arch i386
+        downloadURL="https://dl.wrike.com/download/WrikeDesktopApp.v${appNewVersion}.dmg"      # pour la coherence avec silicon, on hardcode le numéro de vesrion
+    elif [[ $(arch) == arm64 ]]; then
+        #downloadURL="https://dl.wrike.com/download/WrikeDesktopApp_ARM.latest.dmg"  # ne marche pas avec latest, il faut obligatoirement un numéro de version précis
+        downloadURL="https://dl.wrike.com/download/WrikeDesktopApp_ARM.v${appNewVersion}.dmg"
+    fi
     expectedTeamID="BD3YL53XT4"
     ;;
