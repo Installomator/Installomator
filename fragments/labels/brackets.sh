3e26e943a06f470d1fd047b6dd42540463507109
Richard <xplora@wak.co.nz>
2023-02-17 16:31:15 +1300

    Create brackets.sh
    Adobe Brackets has been discontinued, however brackets.io has continued its development.

---
+++ fragments/labels/brackets.sh
@@ -0,0 +1,7 @@
+brackets)
+    name="Brackets"
+    type="dmg"
+    downloadURL=$(downloadURLFromGit brackets-cont brackets )
+    appNewVersion=$(versionFromGit brackets-cont brackets )
+    expectedTeamID="8F632A866K"
+    ;;
