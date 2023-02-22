228c2d3f558435ef902e14aaf7e830a98ce842fc
Richard <xplora@wak.co.nz>
2023-02-22 17:51:20 +1300

    Update mkuser.sh

--- fragments/labels/mkuser.sh
+++ fragments/labels/mkuser.sh
@@ -4,7 +4,7 @@ mkuser)
     packageID="org.freegeek.pkg.mkuser"
     downloadURL="$(downloadURLFromGit freegeek-pdx mkuser)"
     # appNewVersion="$(versionFromGit freegeek-pdx mkuser unfiltered)"
-    # mkuser does not adhere to numbers and dots only for version numbers, Pull request in to add the unfiltered option to versionFromGit
+    # mkuser does not adhere to numbers and dots only for version numbers, Pull request submitted to add an unfiltered option to versionFromGit
     appNewVersion="$(curl -sLI "https://github.com/freegeek-pdx/mkuser/releases/latest" | grep -i "^location" | tr "/" "\n" | tail -1)"
     expectedTeamID="YRW6NUGA63"
     ;;
