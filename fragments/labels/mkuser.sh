2f82596e2adca9e84fa8f37c69b23f558b5bc8ef
Richard <xplora@wak.co.nz>
2023-02-22 17:48:55 +1300

    Create mkuser.sh
    Creating a Label for mkuser

---
+++ fragments/labels/mkuser.sh
@@ -0,0 +1,10 @@
+mkuser)
+    name="mkuser"
+    type="pkg"
+    packageID="org.freegeek.pkg.mkuser"
+    downloadURL="$(downloadURLFromGit freegeek-pdx mkuser)"
+    # appNewVersion="$(versionFromGit freegeek-pdx mkuser unfiltered)"
+    # mkuser does not adhere to numbers and dots only for version numbers, Pull request in to add the unfiltered option to versionFromGit
+    appNewVersion="$(curl -sLI "https://github.com/freegeek-pdx/mkuser/releases/latest" | grep -i "^location" | tr "/" "\n" | tail -1)"
+    expectedTeamID="YRW6NUGA63"
+    ;;
