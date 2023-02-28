54e80a9ae72f1fb1f03e12d04b7015fa44c01f65
Richard <xplora@wak.co.nz>
2023-03-01 10:16:37 +1300

    Update panopto.sh
    For an accurate version match, don't set packageID.

--- fragments/labels/panopto.sh
+++ fragments/labels/panopto.sh
@@ -12,7 +12,7 @@ panopto)
         dscl . create /Users/panopto_upload dsAttrTypeNative:IsHidden 1
     fi
     downloadURL="http://$panoptoServer/$(curl -Ls http://$panoptoServer/ | grep cacheRoot | cut -d "'" -f 2 | awk -F '\\' '{ printf  $4 $5 $6 }' | awk -F 'x2f' '{ printf $2 "/" $3 "/" $4 }')/Software/Panopto%20Recorder.pkg?arch=None&useCustomBinary=True"
-    packageID="com.panopto.mac"
+    # not setting packageID as the version will never match (i.e. 13.0.0.12345), while appNewVersion matches CFBundleShortVersionString (i.e. 13.0.0)
     appNewVersion=$(curl -s "$downloadURL" | grep "Object moved to" | sed -e 's/.*Version\=\mac\%2f\(.*\)\&amp\;server.*/\1/')
     expectedTeamID="G7FR783UED"
     ;;
