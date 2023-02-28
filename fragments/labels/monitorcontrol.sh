f399050266fe8f2b847a105aa860e7dbcbed0dca
alexfinn <91119429+alexfinn@users.noreply.github.com>
2023-02-28 09:59:42 +0000

    Create monitorcontrol.sh

---
+++ fragments/labels/monitorcontrol.sh
@@ -0,0 +1,7 @@
+monitorcontrol)
+    name="MonitorControl"
+    type="dmg"
+    downloadURL="$(downloadURLFromGit MonitorControl MonitorControl)"
+    appNewVersion="$(versionFromGit MonitorControl MonitorControl)"
+    expectedTeamID="CYC8C8R4K9"
+    ;;
