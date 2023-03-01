f26163cf6e7d4ea568cfcf19333737b701c967a1
Richard <xplora@wak.co.nz>
2023-03-01 18:09:06 +1300

    Update panopto.sh
    Removed the bad account check and removal, because removing the account would trigger a permission request. Stopped using sysadminctl per @PicoMitchell's suggestion.

--- fragments/labels/panopto.sh
+++ fragments/labels/panopto.sh
@@ -7,27 +7,29 @@ panopto)
         cleanupAndExit 89 "This label requires more parameters: panoptoServer=host.name.panopto.com, this will ensure the downloaded version is both current and supports your server." ERROR
     fi
     # The installer pkg will try to create this account and fail when it attempts to update the
-    #   NFSHomeDirectory (or it triggers a request for permission from the user).
+    #   NFSHomeDirectory and home settings (or it triggers a request for permission from the user).
     # However, the installer pkg will detect if the account has already been created, and use it,
     #   so manually creating it stops the installer from bothering the user or failing.
-    # The following is based on what the package does, but should handle it with the permission required.
+    # The following is based on what the package does, but should handle it better.
     panoptoUser="panopto_upload"
     panoptoPath="/Users/$panoptoUser"
-    if [ "$(dscl . -read $panoptoPath NFSHomeDirectory 2>&1 | grep -c "No such key")" -gt 0 ]; then
-        sysadminctl -deleteUser $panoptoUser
-    fi
     if [ "$(dscl . read $panoptoPath 2>&1 | grep -c eDSRecordNotFound)" -gt 0 ]; then
-        for ((new_uid = 401; new_uid < 500; new_uid++)); do
-            if [ "$(dscl . -search /Users UniqueID $new_uid | wc -l)" -eq 0 ]; then
+        for ((panoptoUID = 401; panoptoUID < 500; panoptoUID++)); do
+            if [ "$(dscl . -search /Users UniqueID $panoptoUID | wc -l)" -eq 0 ]; then
                 break  
             fi
-        done
-        sysadminctl -addUser $panoptoUser -fullName $panoptoUser -home /private/var/panopto -shell /sbin/nologin -UID $new_uid -GID 1
-        dscl . create $panoptoPath this_password_is_disabled
-        dscl . create $panoptoPath dsAttrTypeNative:IsHidden 1
+        done 
+        dscl . -create $panoptoPath UniqueID $panoptoUID
+        dscl . -append /Users/$username PrimaryGroupID 1
+        dscl . -append /Users/$username NFSHomeDirectory /private/var/panopto
+        dscl . -append /Users/$username UserShell /sbin/nologin
+        dscl . -passwd /Users/$username this_password_is_disabled
+        dscl . -append $panoptoPath dsAttrTypeNative:IsHidden 1
     # The package does do more, however the above covers what is skipped by mk_hidden_user.sh when
     #   the account already exists. Installomator may still require Full Disk Access, but should
     #   inherit that permission from the calling process, which doesn't happen for mk_hidden_user.sh.
+    # mk_hidden_user.sh will fail (or request permission) if the NFSHomeDirectory setting is missing,
+    #   because it will attempt to remove the account first.
     fi
     downloadURL="http://$panoptoServer/$(curl -Ls http://$panoptoServer/ | grep cacheRoot | cut -d "'" -f 2 | awk -F '\\' '{ printf  $4 $5 $6 }' | awk -F 'x2f' '{ printf $2 "/" $3 "/" $4 }')/Software/Panopto%20Recorder.pkg?arch=None&useCustomBinary=True"
     # not setting packageID as the version will never match (i.e. 13.0.0.12345), while appNewVersion matches CFBundleShortVersionString (i.e. 13.0.0)
