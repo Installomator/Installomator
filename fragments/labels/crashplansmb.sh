1bb0d118767bd98e739d0df644eefaa2e29cbbbb
Jake <nicholsjacob35@gmail.com>
2023-03-02 11:55:40 -0500

    fix typo in label

--- fragments/labels/crashplansmb.sh
+++ fragments/labels/crashplansmb.sh
@@ -1,7 +1,7 @@
 crashplansmb)
     name="Code42"
     type="pkgInDmg"
-    pkgName="Install Crashplan for SMall Business.pkg"
+    pkgName="Install Crashplan for Small Business.pkg"
     downloadURL="https://download.crashplan.com/installs/agent/latest-smb-mac.dmg"
     expectedTeamID="9YV9435DHD"
     ;;
