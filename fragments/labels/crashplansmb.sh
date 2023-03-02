c917100637eed5b322050f88a4f5724053848bff
Jake <nicholsjacob35@gmail.com>
2023-03-02 11:37:59 -0500

    add crashpan label

---
+++ fragments/labels/crashplansmb.sh
@@ -0,0 +1,7 @@
+crashplansmb)
+    name="Crashplan"
+    type="pkgInDmg"
+    pkgName="Install Crashplan for SMall Business.pkg"
+    downloadURL="https://download.crashplan.com/installs/agent/latest-smb-mac.dmg"
+    expectedTeamID="9YV9435DHD"
+    ;;
