45bc605e40e6545d6a90b9a5697420a2c5900332
Richard <xplora@wak.co.nz>
2023-03-06 10:56:10 +1300

    Update and rename JASP.sh to jasp.sh
    Create label for JASP desktop app

--- fragments/labels/JASP.sh
+++
@@ -1,7 +0,0 @@
-jasp)
-    name="JASP"
-    type="dmg"
-    downloadURL=$(downloadURLFromGit jasp-stats jasp-desktop )
-    appNewVersion=$(versionFromGit jasp-stats jasp-desktop )
-    expectedTeamID="?"
-    ;;
