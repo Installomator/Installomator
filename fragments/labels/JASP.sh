d45ed3244e6363a1dcf5706f5b62822e28c21b5a
Richard <xplora@wak.co.nz>
2023-03-06 10:35:34 +1300

    Create JASP.sh

---
+++ fragments/labels/JASP.sh
@@ -0,0 +1,7 @@
+jasp)
+    name="JASP"
+    type="dmg"
+    downloadURL=$(downloadURLFromGit jasp-stats jasp-desktop )
+    appNewVersion=$(versionFromGit jasp-stats jasp-desktop )
+    expectedTeamID="?"
+    ;;
