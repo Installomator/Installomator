f507db730a19e90832ccd45e9b0bd6441ee31b97
Richard <xplora@wak.co.nz>
2023-03-08 16:13:39 +1300

    Update jamovi.sh

--- fragments/labels/jamovi.sh
+++ fragments/labels/jamovi.sh
@@ -1,9 +1,14 @@
 jamovi)
     name="jamovi"
     type="dmg"
-    downloadSERVER="http://www.jamovi.org"
-    downloadPATH="$(curl -s "$downloadSERVER/download.html" | grep macos | grep -m1 data-href | cut -d '"' -f 2)"
-    downloadURL="${downloadSERVER}${downloadPATH}"
+    downloadURL="http://www.jamovi.org"
+    if [[ -n $jamoviLatest ]]; then
+        downloadURL="${downloadURL}$(curl -s "$downloadURL/download.html" | grep macos | grep "download-button" | head -1 | cut -d '"' -f 4)"
+    else
+        downloadURL="${downloadURL}$(curl -s "$downloadURL/download.html" | grep macos | grep "download-button" | tail -1 | cut -d '"' -f 4)"
+    fi
+    # The above is a cheat, they list both the "Latest" version and the "Solid" version twice on the page, but in opposing order.
+    #     I'm also assuming they mean Latest = beta, and Solid = Stable.
     appNewVersion="$(echo $downloadPATH | cut -d '-' -f 2)"
     expectedTeamID="9NCBP559AB"
     ;;
