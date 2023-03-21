9897cc5a7d3ced159852e585599f3da6d530b641
Gabriel Marcelino <109539964+gabemarcelino@users.noreply.github.com>
2023-03-21 09:47:49 -0400

    Update Installomator.sh

--- Installomator.sh
+++ Installomator.sh
@@ -2784,7 +2784,8 @@ dymoconnectdesktop)
     appNewVersion=$(curl -fs -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" "https://www.dymo.com/compatibility-chart.html" | grep -oE 'https?://[^"]+\.pkg' | awk -F/ '{print $NF}' | sed 's/DCDMac\([0-9\.]*\)\.pkg/\1.pkg/' | cut -d"." -f1-4 | sort -rV | head -n 1)
     expectedTeamID="N3S6676K3E"
     blockingProcesses="DYMO Connect"
-    ;;dynalist)
+    ;;
+dynalist)
     name="Dynalist"
     type="dmg"
     downloadURL="https://dynalist.io/standalone/download?file=Dynalist.dmg"
