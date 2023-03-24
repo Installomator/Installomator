429f7ffeda0202fdf067aedf8cbd7d3604b33194
Jake <nicholsjacob35@gmail.com>
2023-03-24 13:02:33 -0400

    add crashplan label

---
+++ fragments/labels/crashplan.sh
@@ -0,0 +1,8 @@
+crashplan)
+    name="CrashPlan"
+    type="pkgInDmg"
+    pkgName="Install CrashPlan.pkg"
+    downloadURL="https://download.crashplan.com/installs/agent/latest-mac.dmg"
+    expectedTeamID="UGHXR79U6M"
+    blockingProcesses=( NONE )
+    ;;
