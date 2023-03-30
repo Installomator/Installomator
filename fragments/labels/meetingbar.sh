c37ed8bfc2fffba6d00162f018968ae613889d3e
northalpha <stefan+github@xn--klpping-b1a.de>
2023-03-30 19:05:53 +0000

    Add Meetingbar

---
+++ fragments/labels/meetingbar.sh
@@ -0,0 +1,7 @@
+meetingbar)
+    name="Meetingbar"
+    type="dmg"
+    downloadURL=$(downloadURLFromGit leits MeetingBar)
+    appNewVersion=$(versionFromGit leits MeetingBar)
+    expectedTeamID="KGH289N6T8"
+    ;;
