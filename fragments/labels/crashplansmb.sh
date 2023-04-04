f581cc857e88d780536e67569f0295d53996a679
Jake <nicholsjacob35@gmail.com>
2023-04-04 11:42:41 -0400

    add addNewVersion

--- fragments/labels/crashplansmb.sh
+++ fragments/labels/crashplansmb.sh
@@ -3,6 +3,7 @@ crashplansmb)
     type="pkgInDmg"
     pkgName="Install Crashplan.pkg"
     downloadURL="https://download.crashplan.com/installs/agent/latest-smb-mac.dmg"
+    appNewVersion=$( curl https://download.crashplan.com/installs/agent/latest-smb-mac.dmg  -s -L -I -o /dev/null -w '%{url_effective}' | cut -d "/" -f7 )
     expectedTeamID="UGHXR79U6M"
     blockingProcesses=( NONE )
     ;;
