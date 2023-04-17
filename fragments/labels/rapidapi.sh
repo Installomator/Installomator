3b71b78f0d14f33166693f5028be815f4e6b1276
Ray Lyon <raymond.lyon@riskified.com>
2023-04-17 15:24:52 -0400

    initial working label

---
+++ fragments/labels/rapidapi.sh
@@ -0,0 +1,8 @@
+rapidapi)
+    name="RapidAPI"
+    type="zip"
+    downloadURL="https://paw.cloud/download"
+    appNewVersion="$(curl -fsIL ${downloadURL} | grep -i ^location | cut -d '/' -f5 | awk -F '-' '{ print $2 }')"
+    expectedTeamID="84599RL58A"
+    blockingProcesses=( "RapidAPI" )
+    ;;
