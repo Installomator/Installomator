0c437b2f81f997b81f575c7e3ea9f7967c66e593
northalpha <stefan+github@xn--klpping-b1a.de>
2023-04-24 08:58:00 +0000

    Enhance Label inkscape
    changed the downloadURL to incorporate arch based downloads

--- fragments/labels/inkscape.sh
+++ fragments/labels/inkscape.sh
@@ -2,8 +2,8 @@ inkscape)
     # credit: Søren Theilgaard (@theilgaard)
     name="Inkscape"
     type="dmg"
-    downloadURL="https://inkscape.org$(curl -fs https://inkscape.org$(curl -fsJL https://inkscape.org/release/  | grep "/release/" | grep en | head -n 1 | cut -d '"' -f 6)mac-os-x/dmg/dl/ | grep "click here" | cut -d '"' -f 2)"
     appCustomVersion() { /Applications/Inkscape.app/Contents/MacOS/inkscape --version | cut -d " " -f2 }
     appNewVersion=$(curl -fsJL https://inkscape.org/release/  | grep "<title>" | grep -o -e "[0-9.]*")
+    downloadURL=https://media.inkscape.org/dl/resources/file/Inkscape-"$appNewVersion"_"$(arch)".dmg
     expectedTeamID="SW3D6BB6A6"
     ;;
