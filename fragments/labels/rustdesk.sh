06b1161d54140208fb1d7866cbd69825b798749f
duderin0 <87411326+duderin0@users.noreply.github.com>
2023-04-27 12:48:18 -0700

    Create rustdesk label

---
+++ fragments/labels/rustdesk.sh
@@ -0,0 +1,7 @@
+rustdesk)
+    name="RustDesk"
+    type="dmg"
+    downloadURL=$(downloadURLFromGit rustdesk rustdesk)
+    appNewVersion=$(versionFromGit rustdesk rustdesk)
+    expectedTeamID="HZF9JMC8YN"
+    ;;
