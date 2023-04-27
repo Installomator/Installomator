b4a94533d8e3073e725c4147985b95204be44e94
JordyThery <106578958+JordyThery@users.noreply.github.com>
2023-04-27 06:14:18 +0200

    Create patchomator.sh

---
+++ fragments/labels/patchomator.sh
@@ -0,0 +1,8 @@
+patchomator)
+    name="patchomator"
+    type="pkg"
+    packageID="com.option8.patchomator"
+    downloadURL="$(downloadURLFromGit Mac-Nerd patchomator)"
+    appNewVersion="$(versionFromGit Mac-Nerd patchomator)"
+    expectedTeamID="4VAAB6AM7X"
+    ;;
