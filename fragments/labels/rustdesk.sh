712281fb53dce1de29b61bdf1490c961eac74a79
duderin0 <87411326+duderin0@users.noreply.github.com>
2023-05-01 13:48:46 -0700

    Update rustdesk.sh
    added archiveName line

--- fragments/labels/rustdesk.sh
+++ fragments/labels/rustdesk.sh
@@ -3,5 +3,6 @@ rustdesk)
     type="dmg"
     downloadURL=$(downloadURLFromGit rustdesk rustdesk)
     appNewVersion=$(versionFromGit rustdesk rustdesk)
+    archiveName="rustesk-$appNewVersion.dmg"
     expectedTeamID="HZF9JMC8YN"
     ;;
