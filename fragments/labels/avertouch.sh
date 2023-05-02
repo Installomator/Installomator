7e0765000b8d95d3f101926f012d0ad6b427fd79
duderin0 <87411326+duderin0@users.noreply.github.com>
2023-05-02 15:20:37 -0700

    Update avertouch.sh
    Fix naming mistake, and add versionKey that matches with a build number that increments more regularly than their presented version number.  I tried looking on archive.org to see how often the v1.15 changes but couldn't get more info there.

--- fragments/labels/avertouch.sh
+++ fragments/labels/avertouch.sh
@@ -1,9 +1,9 @@
 avertouch)
-    name="AverSphere"
+    name="AverTouch"
     type="appInDmgInZip"
     pkgname="AVerTouch.dmg"
     appNewVersion="$(curl -s https://www.averusa.com/education/support/avertouch | grep -o 'AVerTouch_mac.*\.zip' | sed 's/.*(\([^)]*\)).*/\1/')"
     downloadURL="https://www.averusa.com/education/downloads/AVerTouch_mac_v1.15(${appNewVersion}).zip"
     expectedTeamID="B6T3WCD59Q"
+    versionKey="CFBundleVersion"
     ;;
-    
