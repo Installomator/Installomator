4434203de930b137f4eca951e0b739deeda0551c
Bart Reardon <bart.reardon@me.com>
2023-05-10 20:44:48 +1000

    Added Outset label

---
+++ fragments/labels/Outset.sh
@@ -0,0 +1,8 @@
+outset)
+    name="Outset"
+    type="pkg"
+    packageID="io.macadmins.Outset"
+    downloadURL="$(downloadURLFromGit macadmins outset)"
+    appNewVersion="$(versionFromGit macadmins outset)"
+    expectedTeamID="T4SK8ZXCXG"
+    ;;
