cd2f820db731c1ac8dd31beaad660509393a8237
Fredrik Larsson <33235315+kryptonit1@users.noreply.github.com>
2023-05-24 14:29:34 +0200

    Update teamviewerhost.sh
    Fixed appNewVersion variable due to change of URL by TeamViewer.

--- fragments/labels/teamviewerhost.sh
+++ fragments/labels/teamviewerhost.sh
@@ -3,7 +3,7 @@ teamviewerhost)
     type="pkgInDmg"
     packageID="com.teamviewer.teamviewerhost"
     pkgName="Install TeamViewerHost.app/Contents/Resources/Install TeamViewerHost.pkg"    downloadURL="https://download.teamviewer.com/download/TeamViewerHost.dmg"
-    appNewVersion=$(curl -fs "https://www.teamviewer.com/en/download/mac-os/" | grep "Current version" | cut -d " " -f3 | cut -d "<" -f1)
+    appNewVersion=$(curl -fs "https://www.teamviewer.com/en/download/macos/" | grep "Current version" | cut -d " " -f3 | cut -d "<" -f1)
     expectedTeamID="H7UGFBUGV6"
     #blockingProcessesMaxCPU="5" # Future feature
     ;;
