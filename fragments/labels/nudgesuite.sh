c30629b5ac964642a01e410de27184c56d467a8b
wantedwriter <github@wezlab.net>
2023-05-24 16:22:29 -0500

    Update nudgesuite.sh
    Adjust archivename variable location.

--- fragments/labels/nudgesuite.sh
+++ fragments/labels/nudgesuite.sh
@@ -2,8 +2,8 @@ nudgesuite)
     name="Nudge Suite"
     appName="Nudge.app"
     type="pkg"
-    appNewVersion=$(versionFromGit macadmins Nudge )
     archiveName="Nudge_Suite-$appNewVersion.pkg"
+    appNewVersion=$(versionFromGit macadmins Nudge )
     downloadURL=$(downloadURLFromGit macadmins Nudge )
     expectedTeamID="T4SK8ZXCXG"
     blockingProcesses=( "Nudge" )
