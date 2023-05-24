24687a1da892e75449973042f532a7921a335364
Fredrik Larsson <33235315+kryptonit1@users.noreply.github.com>
2023-05-24 14:31:28 +0200

    Update teamviewerhost.sh

--- fragments/labels/teamviewerhost.sh
+++ fragments/labels/teamviewerhost.sh
@@ -2,7 +2,8 @@ teamviewerhost)
     name="TeamViewerHost"
     type="pkgInDmg"
     packageID="com.teamviewer.teamviewerhost"
-    pkgName="Install TeamViewerHost.app/Contents/Resources/Install TeamViewerHost.pkg"    downloadURL="https://download.teamviewer.com/download/TeamViewerHost.dmg"
+    pkgName="Install TeamViewerHost.app/Contents/Resources/Install TeamViewerHost.pkg"
+    downloadURL="https://download.teamviewer.com/download/TeamViewerHost.dmg"
     appNewVersion=$(curl -fs "https://www.teamviewer.com/en/download/macos/" | grep "Current version" | cut -d " " -f3 | cut -d "<" -f1)
     expectedTeamID="H7UGFBUGV6"
     #blockingProcessesMaxCPU="5" # Future feature
