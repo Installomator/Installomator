4cb8caa2f0d44ed44449350d9456ca1002c7b4f3
Alexander Duffner <47379999+aduffner@users.noreply.github.com>
2023-05-25 12:32:30 +0200

    Update universe.sh

--- fragments/labels/universe.sh
+++ fragments/labels/universe.sh
@@ -7,11 +7,6 @@ universe)
     appNewVersion="$(curl -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" -fs "https://support.maxon.net/hc/en-us/sections/4406405441426-Universe" | grep "#icon-star" -B3 | grep -Eo "202[0-9]+\.[0-9]+\.[0-9]+" | head -n 30 | sort -gru)"
     downloadURL="https://mx-app-blob-prod.maxon.net/mx-package-production/installer/macos/redgiant/universe/releases/${appNewVersion}/Universe-${appNewVersion}_Mac.zip"
     installerTool="Universe Installer.app"
-<<<<<<< Updated upstream
     CLIInstaller="Universe Installer.app/Contents/MacOS/Universe Installer"
-=======
-    CLIInstaller="Universe Installer.app/Contents/Scripts/install.sh"
-    CLIArguments=()
->>>>>>> Stashed changes
     expectedTeamID="4ZY22YGXQG"
     ;;
