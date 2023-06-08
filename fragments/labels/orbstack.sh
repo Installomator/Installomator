c8d217b8fec0b4b3879b597a99d05c160f62a2d2
Jake <nicholsjacob35@gmail.com>
2023-06-07 23:02:52 -0400

    add orbstack label

---
+++ fragments/labels/orbstack.sh
@@ -0,0 +1,11 @@
+orbstack)
+    name="OrbStack"
+    type="dmg"
+    fi
+    if [[ "$(arch)" == "arm64" ]]; then
+        downloadURL=$(curl -fs "https://orbstack.dev/download/beta/latest/amd64" | grep -i -o -E 'https.*\.dmg' | head -1 | cut -d '"' -f1)
+    else
+        downloadURL=$(curl -fs "https://orbstack.dev/download/beta/latest/arm64" | grep -i -o -E 'https.*\.dmg' | head -1 | cut -d '"' -f1)
+    fi
+    expectedTeamID="HUAQ24HBR6"
+    ;;
