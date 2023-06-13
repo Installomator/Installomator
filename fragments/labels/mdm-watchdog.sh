94e20f8e466c8003d05114649a57142c0962d37a
Scripts and Things <52809959+scriptsandthings@users.noreply.github.com>
2023-06-13 15:35:53 -0400

    Create mdm-watchdog.sh
    Create label for Addigy MDM-Watchdog - https://addigy.com/mdm-watchdog/
    
    Download URL and package name appear to be static as of an update to the Addigy site this morning. Previously it was displaying a version 5 specific URL on the download page but it has since updated to a generic/no version included URL.
    
    Addigy removed the version info from the download page and does not appear to have MDM-Watchdog current version listed anywhere else on their site in a location that can be checked for latest version.
    
    ./buildLabel.sh https://agents.addigy.com/tools/mdm-watchdog/latest/mdm-watchdog.pkg
    Changing directory to /Users/gknackstedt/GitHub/Installomator/utils/2023-06-13-14-21-28
    Working dir: /Users/gknackstedt/GitHub/Installomator/utils/2023-06-13-14-21-28
    Downloading https://agents.addigy.com/tools/mdm-watchdog/latest/mdm-watchdog.pkg
    mdm-watchdog.pkg
    Redirecting to (maybe this can help us with version):
    HTTP/1.1 302 Found
    Content-Length: 0
    Connection: keep-alive
    Server: CloudFront
    Date: Tue, 13 Jun 2023 18:21:30 GMT
    Location: https://agents.addigy.com/tools/mdm-watchdog/6/mdm-watchdog.pkg
    X-Cache: LambdaGeneratedResponse from cloudfront
    Via: 1.1 4076c139caa3b19374b9d2d1784ca5e0.cloudfront.net (CloudFront)
    X-Amz-Cf-Pop: ORD56-P4
    X-Amz-Cf-Id: SnK5q8OQHKVchqBH68Tifw9QVyP1nPif5asCsdl9xzC9Ep9-TKUaOw==
    
    HTTP/1.1 200 OK
    Content-Type: application/octet-stream
    Content-Length: 2381802
    Connection: keep-alive
    Date: Tue, 13 Jun 2023 16:16:20 GMT
    Last-Modified: Tue, 13 Jun 2023 15:51:20 GMT
    ETag: "0ef52f897eced5cda84a5521e8657076"
    x-amz-server-side-encryption: AES256
    x-amz-version-id: uw0BiqlRSubTWThSgzgU1g2JAJeUGIs1
    Accept-Ranges: bytes
    Server: AmazonS3
    X-Cache: Hit from cloudfront
    Via: 1.1 4076c139caa3b19374b9d2d1784ca5e0.cloudfront.net (CloudFront)
    X-Amz-Cf-Pop: ORD56-P4
    X-Amz-Cf-Id: hjFUTqAr77i3XcGVL9Ydo8CAGvJpYQh9QyMcpsv5j_7WICkLMrjNNw==
    Age: 7511
    
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
      0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0
    100 2325k  100 2325k    0     0  3029k      0 --:--:-- --:--:-- --:--:-- 34.5M
    archiveTempName: mdm-watchdog.pkg
    archivePath: https://agents.addigy.com/tools/mdm-watchdog/6/mdm-watchdog.pkg
    Calculated archiveName: mdm-watchdog.pkg
    name: mdm-watchdog
    archiveExt: pkg
    PKG found: mdm-watchdog.pkg
    Package investigation.
    Team ID found for PKG: R5LEJ8Y242
    For PKGs it's advised to find packageID for version checking, so extracting those
    <pkg-ref id="com.addigy.mdm-watchdog" version="6" onConclusion="none" installKBytes="0">#addigy-component.pkg</pkg-ref>
    com.addigy.mdm-watchdog
    Above is the possible packageIDs that can be used, and the correct one is probably one of those with a version number. More investigation might be needed to figure out correct packageID if several are displayed.
    identifier: mdmwatchdog
    
    **********
    
    Labels should be named in small caps, numbers 0-9, “-”, and “_”. No other characters allowed.
    
    appNewVersion is often difficult to find. Can sometimes be found in the filename, sometimes as part of the download redirects, but also on a web page. See redirect and archivePath above if link contains information about this. That is a good place to start
    
    mdmwatchdog)
        name="mdm-watchdog"
        type="pkg"
        packageID="com.addigy.mdm-watchdog"
        downloadURL="https://agents.addigy.com/tools/mdm-watchdog/latest/mdm-watchdog.pkg"
        appNewVersion=""
        expectedTeamID="R5LEJ8Y242"
        ;;
    
    Above should be saved in a file with exact same name as label, and given extension “.sh”.
    Put this file in folder “fragments/labels”.

---
+++ fragments/labels/mdm-watchdog.sh
@@ -0,0 +1,8 @@
+mdm-watchdog)
+    name="mdm-watchdog"
+    type="pkg"
+    packageID="com.addigy.mdm-watchdog"
+    downloadURL="https://agents.addigy.com/tools/mdm-watchdog/latest/mdm-watchdog.pkg"
+    appNewVersion=""
+    expectedTeamID="R5LEJ8Y242"
+    ;;
