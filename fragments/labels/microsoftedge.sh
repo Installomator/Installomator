a16ca34f16075e727b3bdda2d266719e338fd8df
Theile <soren@theilgaard.dk>
2023-06-14 15:07:21 +0200

    Update microsoftedge.sh
    I post this for others to investigate, as it is not fully working yet.
    
    `updateToolArguments` might not be correct.

--- fragments/labels/microsoftedge.sh
+++ fragments/labels/microsoftedge.sh
@@ -5,12 +5,15 @@ microsoftedgeenterprisestable)
     type="pkg"
     downloadURL="https://go.microsoft.com/fwlink/?linkid=2093504"
     #appNewVersion=$(curl -fs https://macadmins.software/latest.xml | xpath '//latest/package[id="com.microsoft.edge"]/cfbundleversion' 2>/dev/null | sed -E 's/<cfbundleversion>([0-9.]*)<.*/\1/')
-    appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | grep -o "/MicrosoftEdge.*pkg" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
+    #appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | grep -o "/MicrosoftEdge.*pkg" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
+    appNewVersion="113.2"
     expectedTeamID="UBF8T346G9"
-    if [[ -x "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" && $INSTALL != "force" && $DEBUG -eq 0 ]]; then
-        printlog "Running msupdate --list"
-        "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" --list
+    orgFolderPath=$(pwd)
+    folderPath=$(cd /Library/Microsoft/EdgeUpdater/[0-9.]*/EdgeUpdater.app/Contents/MacOS/ && pwd)
+    if [[ -x "${folderPath}/EdgeUpdater" && $INSTALL != "force" && $DEBUG -eq 0 ]]; then
+        # This part might not be needed
     fi
-    updateTool="/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate"
-    updateToolArguments=( --install --apps EDGE01 )
+    updateTool=${folderPath}/EdgeUpdater
+    updateToolArguments=( --service=update --enable-logging --system )
+    cd "$orgFolderPath"
     ;;
