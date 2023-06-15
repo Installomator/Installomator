c671df5d514d6a0fe176dd00073b4b5c70bafda7
Owen Sullivan <sulliops@gmail.com>
2023-06-15 12:24:47 -0400

    Update Installomator.sh

--- Installomator.sh
+++ Installomator.sh
@@ -4839,9 +4839,9 @@ microsoftonedrive)
     updateToolArguments=( --install --apps ONDR18 )
     ;;
 microsoftonedrivereset)
-    name="Microsoft Outlook Reset"
+    name="Microsoft OneDrive Reset"
     type="pkg"
-    packageID="com.microsoft.reset.Outlook"
+    packageID="com.microsoft.reset.OneDrive"
     downloadURL="https://office-reset.com"$(curl -fs https://office-reset.com/macadmins/ | grep -o -i "href.*\".*\"*OneDrive_Reset.*.pkg" | cut -d '"' -f2)
     expectedTeamID="QGS93ZLCU7"
     ;;
