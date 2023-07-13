d49d967c7ef12731b8b5ad3d79890b2df0b5ba92
Gil Burns <gburns@coinflip.tech>
2023-07-13 09:32:13 -0500

    Update jre8.sh
    updated the xpath call for the versionRUL

--- fragments/labels/jre8.sh
+++ fragments/labels/jre8.sh
@@ -2,7 +2,7 @@ jre8)
     name="Java Runtime Environment 8"
     type="pkgInDmg"
     versionKey="CFBundleVersion"
-    versionURL=$(curl -fs "https://javadl-esd-secure.oracle.com/update/mac/map-mac-1.8.0.xml" | xpath '( //java-update-map/mapping/url)' 2>/dev/null | tail -n1 | cut -d\> -f2 | cut -d\< -f1)
+    versionURL=$(curl -fs "https://javadl-esd-secure.oracle.com/update/mac/map-mac-1.8.0.xml" | xpath '( //java-update-map/mapping/url)[last()]' 2>/dev/null | cut -d\> -f2 | cut -d\< -f1)
     appNewVersion=$(curl -fs "${versionURL}" | xpath '(//rss/channel/item/enclosure/@sparkle:version)' 2>/dev/null | cut -d '"' -f 2)
     appBuildVersion=$(echo $appNewVersion | cut -d. -f3)
     downloadURL="$(curl -fs "${versionURL}" | xpath '(//rss/channel/item/enclosure/@url)[last()]' 2>/dev/null | cut -d '"' -f 2)"
