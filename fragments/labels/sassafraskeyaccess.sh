a61abb4e3fddb38b120ec0c2d8ad0bf45bef4467
Richard <xplora@wak.co.nz>
2023-07-25 15:39:42 +1200

    Update sassafraskeyaccess.sh
    Details appear to be correct, just need to verify.

--- fragments/labels/sassafraskeyaccess.sh
+++ fragments/labels/sassafraskeyaccess.sh
@@ -1,2 +1,11 @@
 keyaccess)
-  
+    name="KeyAccess"
+    type="pkg"
+    downloadPAGE="http://www.sassafras.com/client-download/"
+    downloadURL="$(curl -s "$downloadPAGE" | tr '>' '\n' | grep "https.*ksp-client.*pkg" | cut -d '"' -f 2)"
+    appNewVersion="$(curl -s "$downloadPAGE" | tr '>' '\n' | grep "KeyAccess.*for Mac" | cut -d ' ' -f 2)"
+    expectedTeamID="7Z2KSDFMVY"
+    blockingProcesses=( NONE )
+    # Application is not installed in /Applications
+    targetDir="/Library/KeyAccess"
+    ;;
