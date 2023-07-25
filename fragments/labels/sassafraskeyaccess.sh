21b4242af3c146bdea6ad9d108447fa8b1083c1d
Richard <xplora@wak.co.nz>
2023-07-25 15:53:40 +1200

    Update sassafraskeyaccess.sh
    Code cleanup, this way the website is only connected to twice, once to cache the download page containing downloadURL and appNewVersion, and a second time to download the pkg.

--- fragments/labels/sassafraskeyaccess.sh
+++ fragments/labels/sassafraskeyaccess.sh
@@ -1,9 +1,9 @@
 keyaccess)
     name="KeyAccess"
     type="pkg"
-    downloadPAGE="http://www.sassafras.com/client-download/"
-    downloadURL="$(curl -s "$downloadPAGE" | tr '>' '\n' | grep "https.*ksp-client.*pkg" | cut -d '"' -f 2)"
-    appNewVersion="$(curl -s "$downloadPAGE" | tr '>' '\n' | grep "KeyAccess.*for Mac" | cut -d ' ' -f 2)"
+    downloadStore="$(curl -s "http://www.sassafras.com/client-download/" | tr '>' '\n')"
+    downloadURL="$(echo "$downloadStore" | grep "https.*ksp-client.*pkg" | cut -d '"' -f 2)"
+    appNewVersion="$(echo "$downloadStore" | grep "KeyAccess.*for Mac" | cut -d ' ' -f 2)"
     expectedTeamID="7Z2KSDFMVY"
     blockingProcesses=( NONE )
     # Application is not installed in /Applications
