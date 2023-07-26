4c78e40d2cc66608f9557920d7a15187f03f3613
northalpha <stefan+github@xn--klpping-b1a.de>
2023-07-26 15:58:26 +0200

    remove debug lines in burpsuitecommunityedition

--- fragments/labels/burpsuitecommunityedition.sh
+++ fragments/labels/burpsuitecommunityedition.sh
@@ -4,10 +4,8 @@ burpsuitecommunityedition)
     appNewVersion=$(curl -s https://portswigger.net/burp/releases | grep 'releases/professional-community' | head -n 1 | sed 's/.*href="//' | sed 's/".*//' | cut -d '/' -f4 | cut -d '-' -f3-6 | sed -r 's/-/./g')
     if [[ $(arch) == "arm64" ]]; then
         downloadURL="https://portswigger.net/burp/releases/startdownload/?product=community&version&="$appNewVersion"&type=macosarm64"
-        echo $downloadURL
     elif [[ $(arch) == "i386" ]]; then
         downloadURL="https://portswigger.net/burp/releases/startdownload/?product=community&version&="$appNewVersion"&type=macosx"
-        echo $downloadURL
     fi
     expectedTeamID="N82YM748DZ"
     ;;
