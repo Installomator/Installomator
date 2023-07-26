be8ca4f25b9efe8982e74a6c2212441a9be0b24d
Mark Skrzypczyk <mark@kheironmed.com>
2023-07-26 15:44:12 +0100

    Initial commit

---
+++ fragments/labels/weprint.sh
@@ -0,0 +1,8 @@
+weprint)
+    name="Print"
+    type="appInDmgInZip"
+    downloadURL="https://it-assets.s3.amazonaws.com/print-by-we/Print-By-We-Mac-Installer.zip"
+    appNewVersion=""
+    expectedTeamID="2D42ACMA8R"
+    versionKey="CFBundleVersion"
+    ;;
