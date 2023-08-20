4d48af37e3e324dd97e217054a2b06b025456d1f
Richard <xplora@wak.co.nz>
2023-08-21 11:28:42 +1200

    Update sassafraskeyaccess.sh
    need to use appName instead of targetDir

--- fragments/labels/sassafraskeyaccess.sh
+++ fragments/labels/sassafraskeyaccess.sh
@@ -6,8 +6,9 @@ keyaccess)
     appNewVersion="$(echo "$downloadStore" | grep "KeyAccess.*for Mac" | cut -d ' ' -f 2)"
     expectedTeamID="7Z2KSDFMVY"
     BLOCKING_PROCESS_ACTION=ignore
+    blockingProcesses=( NONE )
     # Application is not installed in /Applications
-    targetDir="/Library/KeyAccess"
+    appName="Library/KeyAccess/KeyAccess.app"
     # Allowing for setting host as it is the only setting required for a fresh install.
     if [[ -n $keyaccessHost ]]; then
         defaults write /Library/Preferences/com.sassafras.KeyAccess host -string "${keyaccessHost}"
