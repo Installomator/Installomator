a4caf2180e0d532add2baa09daff2e645868621a
macmangr <106015597+macmangr@users.noreply.github.com>
2023-08-22 09:54:04 +0200

    Added blockingProcesses

--- fragments/labels/elgatostreamdeck.sh
+++ fragments/labels/elgatostreamdeck.sh
@@ -5,4 +5,5 @@ elgatostreamdeck)
 	downloadURL="https://gc-updates.elgato.com/mac/sd-update/final/download-website.php"
     appNewVersion=$(curl -fsI "https://gc-updates.elgato.com/mac/sd-update/final/download-website.php" | grep -i ^location | sed -E 's/.*Stream_Deck_([0-9.]*).pkg/\1/g' | sed 's/\.[^.]*//3')
     expectedTeamID="Y93VXCB8Q5"
+    blockingProcesses=( "Stream Deck" )
     ;;
