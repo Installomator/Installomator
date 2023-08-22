7b89a8ab75f0e30c9f55dce0f5248db0410a09ae
macmangr <106015597+macmangr@users.noreply.github.com>
2023-08-22 09:55:25 +0200

    Added blockingProcesses

--- fragments/labels/elgatocamerahub.sh
+++ fragments/labels/elgatocamerahub.sh
@@ -5,4 +5,5 @@ elgatocamerahub)
     downloadURL="https://gc-updates.elgato.com/mac/echm-update/final/download-website.php"
     appNewVersion=$(curl -fsI "https://gc-updates.elgato.com/mac/echm-update/final/download-website.php" | grep -i ^location | sed -E 's/.*Camera_Hub_([0-9.]*).pkg/\1/g' | sed 's/\.[^.]*//3')
     expectedTeamID="Y93VXCB8Q5"
+    blockingProcesses=( "Camera Hub" )
     ;;
