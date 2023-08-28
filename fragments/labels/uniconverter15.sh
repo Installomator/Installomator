4a86e8d0425d084c68231dc5e1daa1f1e7ae7214
Gabe Marchan <gabe@gabemarchan.com>
2023-08-27 22:13:55 -0500

    Fix arm64 and i386 archiveName values being swapped and specifically check for i386 instead of just defaulting to it.

--- fragments/labels/uniconverter15.sh
+++ fragments/labels/uniconverter15.sh
@@ -2,9 +2,9 @@ uniconverter15)
     name="Wondershare UniConverter 15"
     type="zip"
     if [[ "$(arch)" == "arm64" ]]; then
-        archiveName="uniconverter15-mac_15.0.2_full14207.zip"
-    else
         archiveName="uniconverter15-mac_arm_15.0.2_full14207.zip"
+    elif [[ $(arch) == "i386" ]]; then
+        archiveName="uniconverter15-mac_15.0.2_full14207.zip"
     fi
     downloadURL="https://download.wondershare.com/cbs_down/$archiveName"
     expectedTeamID="YZC2T44ZDX"
