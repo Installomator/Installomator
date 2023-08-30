7934da0461720ed056427c56d0c4f87f124aeb13
Jake <nicholsjacob35@gmail.com>
2023-08-30 18:12:34 -0400

    fix download code

--- fragments/labels/orbstack.sh
+++ fragments/labels/orbstack.sh
@@ -2,9 +2,9 @@ orbstack)
     name="OrbStack"
     type="dmg"
     if [[ "$(arch)" == "arm64" ]]; then
-        downloadURL=$(curl -fs "https://orbstack.dev/download/beta/latest/arm64" | grep -i -o -E 'https.*\.dmg' | head -1 | cut -d '"' -f1)
+        downloadURL=$(curl -fsI "https://orbstack.dev/download/beta/latest/arm64" | grep -i -o -E 'https.*\.dmg' | head -1 | cut -d '"' -f1)
     else
-        downloadURL=$(curl -fs "https://orbstack.dev/download/beta/latest/amd64" | grep -i -o -E 'https.*\.dmg' | head -1 | cut -d '"' -f1)
+        downloadURL=$(curl -fsI "https://orbstack.dev/download/beta/latest/amd64" | grep -i -o -E 'https.*\.dmg' | head -1 | cut -d '"' -f1)
     fi
     expectedTeamID="HUAQ24HBR6"
     ;;
