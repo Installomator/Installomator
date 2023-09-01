3d7016da1cb2fa14f753c0f31e99c6a6a5c82d5e
Jake <nicholsjacob35@gmail.com>
2023-09-01 13:14:49 -0400

    add label for suite studio

---
+++ fragments/labels/suitestudio.sh
@@ -0,0 +1,11 @@
+suitestudio)
+    name="Suite Studio"
+    type="pkg"
+    packageID="io.suite.suitefs"
+    if [[ $(arch) == arm64 ]]; then
+        downloadURL="https://saturn-installer-prd-124359286071-bucket.s3.amazonaws.com/suite-installer-osx-arm64.pkg"
+    elif [[ $(arch) == i386 ]]; then
+        downloadURL="https://saturn-installer-prd-124359286071-bucket.s3.amazonaws.com/suite-installer-osx-x64.pkg"
+    fi
+    expectedTeamID="58KZ58VMJ8"
+    ;;
