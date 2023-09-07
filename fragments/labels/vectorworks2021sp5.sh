5f71d8be020ef2cffceb6da4a5db1c3fe250029d
Benjamin Kollmer <b.kollmer@apfelwerk.de>
2023-09-07 18:09:38 +0200

    Create vectorworks2021sp5.sh
    Just the base, will be updated soon

---
+++ fragments/labels/vectorworks2021sp5.sh
@@ -0,0 +1,8 @@
+vectorworks2021)
+    name="Vectorworks2021SP5"
+    type="dmg"
+    packageID="net.vectorworks.2021.vectorworksinstaller"
+    downloadURL="https://download.vectorworks.de/2021/cw/macintosh"
+    appNewVersion=$(defaults read "/Volumes/Vectorworks/Vectorworks 2021 installieren.app/Contents/Info" CFBundleShortVersionString)
+    expectedTeamID="LFNG3Q6WX2"
+    ;;
