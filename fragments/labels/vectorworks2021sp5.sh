fca621fb0b230fb798fa0804e2e0a55109020b18
Benjamin Kollmer <b.kollmer@apfelwerk.de>
2023-09-13 12:21:47 +0200

    Update vectorworks2021sp5.sh

--- fragments/labels/vectorworks2021sp5.sh
+++ fragments/labels/vectorworks2021sp5.sh
@@ -1,8 +1,9 @@
-vectorworks2021)
-    name="Vectorworks2021SP5"
+vectorworks2021sp5)
+    # Credit: Benjamin Kollmer @Apfelwerk GmbH & Co. KG
+    name="Vectorworks 2021 SP5"
+    appName="Vectorworks 2021 installieren.app"
     type="dmg"
     packageID="net.vectorworks.2021.vectorworksinstaller"
     downloadURL="https://download.vectorworks.de/2021/cw/macintosh"
-    appNewVersion=$(defaults read "/Volumes/Vectorworks/Vectorworks 2021 installieren.app/Contents/Info" CFBundleShortVersionString)
     expectedTeamID="LFNG3Q6WX2"
     ;;
