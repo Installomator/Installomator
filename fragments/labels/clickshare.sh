99bcddb5a6d317364d210fa4902b07ab89b6d5a0
gazzer82 <contact@garethjeanne.co.uk>
2023-09-26 10:56:51 +0100

    Update clickshare label to work with updated pages

--- fragments/labels/clickshare.sh
+++ fragments/labels/clickshare.sh
@@ -1,7 +1,7 @@
 clickshare)
     name="ClickShare"
     type="appInDmgInZip"
-    downloadURL="https://www.barco.com$( curl -fs "https://www.barco.com/en/clickshare/app" | grep -A6 -i "macos" | grep -i "FileNumber" | tr '"' "\n" | grep -i "FileNumber" )"
-    appNewVersion="$(eval "$( echo $downloadURL | sed -E 's/.*(MajorVersion.*BuildVersion=[0-9]*).*/\1/' | sed 's/&amp//g' )" ; ((MajorVersion++)) ; ((MajorVersion--)); ((MinorVersion++)) ; ((MinorVersion--)); ((PatchVersion++)) ; ((PatchVersion--)); ((BuildVersion++)) ; ((BuildVersion--)); echo "${MajorVersion}.${MinorVersion}.${PatchVersion}-b${BuildVersion}")"
+    downloadURL=$(curl -fs "https://www.barco.com/bin/barco/tde/downloadUrl.json?fileNumber=R3306192&tdeType=3" | grep -o 'https://[^"]*')
+    appNewVersion=$(curl -fs "https://www.barco.com/content/dxp/regions-countries/global/en/support/software/R3306192/jcr:content/root/main_section/supportdownloadversi.asynccomponent.html"| xmllint --html -xpath "substring-after(string(//dd[starts-with(text(), 'v')]),'v')" - 2> /dev/null)
     expectedTeamID="P6CDJZR997"
     ;;
