2d0a0f83505e4ac2ad7d6cc40cd62b684e58bfd1
Theile <soren@theilgaard.dk>
2023-10-12 08:51:50 +0200

    Microsoft Teams classic and new
    `microsoftteams` label is now also `microsoftteamsclassic` as Microsoft has chosen to rename the app and the Helper.
    
    Added `microsoftteamsnew` to point to Teams version 2. (This label might not be correct for `msupdate`app, as I would guess that it should be `TEAM02` instead, but cannot locate what it should be.
    
    Some time in the future, we will move `microsoftteams` from classic to new…

--- fragments/labels/microsoftteamsnew.sh
+++ fragments/labels/microsoftteamsnew.sh
@@ -1,13 +1,12 @@
-microsoftteamsclassic|\
-microsoftteams)
-    name="Microsoft Teams classic"
+microsoftteamsnew)
+    name="Microsoft Teams (work or school)"
     type="pkg"
-    #packageID="com.microsoft.teams"
-    downloadURL="https://go.microsoft.com/fwlink/?linkid=869428"
+    #packageID="com.microsoft.teams2"
+    downloadURL="https://go.microsoft.com/fwlink/?linkid=2249065"
     appNewVersion=$(curl -fsIL "${downloadURL}" | grep -i "^location" | tail -1 | cut -d "/" -f5)
     versionKey="CFBundleGetInfoString"
     expectedTeamID="UBF8T346G9"
-    blockingProcesses=( Teams "Microsoft Teams classic Helper" )
+    blockingProcesses=( MSTeams "Microsoft Teams" "Microsoft Teams WebView" "Microsoft Teams Launcher")
     # msupdate requires a PPPC profile pushed out from Jamf to work, https://github.com/pbowden-msft/MobileConfigs/tree/master/Jamf-MSUpdate
     if [[ -x "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" && $INSTALL != "force" && $DEBUG -eq 0 ]]; then
         printlog "Running msupdate --list"
