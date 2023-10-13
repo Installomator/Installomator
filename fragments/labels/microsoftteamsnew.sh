02421c6642a9cbcf9f6bea61c0e9af124d3afbda
Søren Theilgaard <soren@theilgaard.dk>
2023-10-13 15:45:55 +0200

    Update microsoftteamsnew.sh
    `appNewVersion` from macadmins.software added

--- fragments/labels/microsoftteamsnew.sh
+++ fragments/labels/microsoftteamsnew.sh
@@ -4,6 +4,8 @@ microsoftteamsnew)
     #packageID="com.microsoft.teams2"
     downloadURL="https://go.microsoft.com/fwlink/?linkid=2249065"
     #appNewVersion=$(curl -fsIL "${downloadURL}" | grep -i "^location" | tail -1 | cut -d "/" -f5)
+    # No version in download path, so grab it from homepage
+    appNewVersion=$(curl -fs https://macadmins.software/latest.xml | xpath '//latest/package[id="com.microsoft.teams2.standalone"]/version' 2>/dev/null | sed -E 's/<version>([0-9.]*) .*/\1/')
     #versionKey="CFBundleGetInfoString"
     expectedTeamID="UBF8T346G9"
     blockingProcesses=( MSTeams "Microsoft Teams" "Microsoft Teams WebView" "Microsoft Teams Launcher" "Microsoft Teams (work preview)")
