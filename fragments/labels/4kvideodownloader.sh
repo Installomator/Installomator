6a7acac1019dbbfb104877c187ecc7e3d1fee94c
Richard <xplora@wak.co.nz>
2023-10-19 15:29:56 +1300

    Update 4kvideodownloader.sh
    fix space inconsistency

--- fragments/labels/4kvideodownloader.sh
+++ fragments/labels/4kvideodownloader.sh
@@ -4,6 +4,6 @@
     downloadURL="$(curl -Ls "https://www.4kdownload.com/downloads" | grep dmg | cut -d '"' -f 4 | grep -e videodownloader_ | cut -d '?' -f 1)"
     # matching with videodownloader_ required to avoid confusion with videodownloaderplus
     appNewVersion=$(echo "${downloadURL}" | sed -E 's/.*\/[0-9a-zA-Z]*_([0-9.]*)\.dmg.*/\1/g')
-	versionKey="CFBundleVersion"
+    versionKey="CFBundleVersion"
     expectedTeamID="GHQ37VJF83"
     ;;
