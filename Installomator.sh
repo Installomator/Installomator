3bce35e580cc122849b81dedaf42b7dd75d5ace0
Andrew Barnett <138056529+AndrewMBarnett@users.noreply.github.com>
2023-10-23 16:15:29 -0500

    Added Java Se Development Kit 21

--- Installomator.sh
+++ Installomator.sh
@@ -4155,6 +4155,18 @@ jdk20)
     appCustomVersion(){ java --version | grep java | awk '{print $2}' }
     expectedTeamID="VB5E2TV963"
     ;;
+ jdk21)
+    name="Java SE Development Kit 21"
+    type="pkgInDmg"
+    versionKey="CFBundleShortVersionString"
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL="https://download.oracle.com/java/21/latest/jdk-21_macos-aarch64_bin.dmg"
+    elif [[ $(arch) == "i386" ]]; then
+        downloadURL="https://download.oracle.com/java/21/latest/jdk-21_macos-x64_bin.dmg"
+    fi
+    appCustomVersion(){ java --version | grep java | awk '{print $2}' }
+    expectedTeamID="VB5E2TV963"
+    ;;   
 jetbrainsclion)
     name="CLion"
     type="dmg"
