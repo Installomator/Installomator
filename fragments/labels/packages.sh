6f243edbf5ec152893c7493d460c0d5a55ea51d0
Theile <soren@theilgaard.dk>
2023-10-23 20:18:31 +0200

    `appNewVersion` added

--- fragments/labels/packages.sh
+++ fragments/labels/packages.sh
@@ -1,9 +1,8 @@
-
 packages)
-   #NOTE: Packages is signed but _not_ notarized, so spctl will reject it
    name="Packages"
    type="pkgInDmg"
    pkgName="Install Packages.pkg"
    downloadURL="http://s.sudre.free.fr/Software/files/Packages.dmg"
+   appNewVersion=$(curl -fs "http://s.sudre.free.fr/Software/Packages/release_notes.html" | grep "Release_notes_Version" | head -1 | grep -Eo "\d+.\d+.\d+")
    expectedTeamID="NL5M9E394P"
    ;;
