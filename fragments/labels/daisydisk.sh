b86d74a0dd71e901e18cdd55bad87a74a433a785
Theile <soren@theilgaard.dk>
2023-10-24 14:20:32 +0200

    Fixed `appNewVersion`

--- fragments/labels/daisydisk.sh
+++ fragments/labels/daisydisk.sh
@@ -2,6 +2,6 @@ daisydisk)
     name="DaisyDisk"
     type="zip"
     downloadURL="https://daisydiskapp.com/downloads/DaisyDisk.zip"
-    appNewVersion=$( curl -fs 'https://daisydiskapp.com/downloads/appcastReleaseNotes.php?appEdition=Standard' | grep Version | head -1 | sed -E 's/.*Version ([0-9.]*).*/\1/g' )
+    appNewVersion=$( curl -fs 'https://daisydiskapp.com/downloads/appcastReleaseNotes.php?appEdition=Standard' | xmllint --html - 2>/dev/null | grep Version | head -1 | sed -E 's/.*Version ([0-9.]*).*/\1/g' )
     expectedTeamID="4CBU3JHV97"
     ;;
