aa5b0ecde28f268687297ad7540eea07432b82ce
Aleexiiiiiii <127441149+Aleexiiiiiii@users.noreply.github.com>
2023-10-25 15:13:06 +0200

    Update vlc.sh
    fix variable

--- fragments/labels/vlc.sh
+++ fragments/labels/vlc.sh
@@ -1,7 +1,7 @@
 vlc)
     name="VLC"
     type="dmg"
-    downloadURL="https://download.videolan.org/pub/videolan/vlc/last/macosx/vlc-$(curl -fs https://download.videolan.org/pub/videolan/vlc/last/macosx/ | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' | head -1)-universal.dmg"
     appNewVersion=$(curl -fs https://download.videolan.org/pub/videolan/vlc/last/macosx/ | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' | head -1)
+    downloadURL="https://download.videolan.org/pub/videolan/vlc/last/macosx/vlc-${appNewVersion}-universal.dmg"
     expectedTeamID="75GAHG3SZQ"
     ;;
