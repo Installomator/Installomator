21e7fba627e339c5faebf1174101487a8d75931d
cchambers-cap <36714039+cchambers-cap@users.noreply.github.com>
2023-10-30 10:53:23 -0400

    Update logitunes.sh
    Removed inaccurate comment related to downloadURL

--- fragments/labels/logitunes.sh
+++ fragments/labels/logitunes.sh
@@ -1,7 +1,6 @@
 logitunes)
     name="Logi Tune"
     type="dmg"
-    #downloadURL=$(curl -fs "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,webos=mac-macos-x-11.0" | tr "," "\n" | grep -A 10 "macOS" | grep -oie "https.*/.*/tune/.*\.dmg" | head -1)
     downloadURL="https://software.vc.logitech.com/downloads/tune/LogiTuneInstaller.dmg"
     appNewVersion=$(curl -fs "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,webos=mac-macos-x-11.0" | tr "," "\n" | grep -A 10 "macOS" | grep -B 5 -ie "https.*/.*/tune/.*\.dmg" | grep "Software Version" | sed 's/\\u[0-9a-z][0-9a-z][0-9a-z][0-9a-z]//g' | grep -ioe "Software Version.*[0-9.]*" | tr "/" "\n" | grep -oe "[0-9.]*" | head -1)
     appName="LogiTune.app"   
