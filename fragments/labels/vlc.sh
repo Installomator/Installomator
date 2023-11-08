6f61033daec90c11501ab83cc402d9ac57caee3a
Alexis <133279174+alex02th@users.noreply.github.com>
2023-11-08 22:20:10 +0100

    Update vlc.sh

--- fragments/labels/vlc.sh
+++ fragments/labels/vlc.sh
@@ -2,6 +2,6 @@ vlc)
     name="VLC"
     type="dmg"
     appNewVersion=$(curl -fs https://download.videolan.org/pub/videolan/vlc/last/macosx/ | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' | head -1)
-    downloadURL="https://download.videolan.org/pub/videolan/vlc/last/macosx/vlc-${appNewVersion}-universal.dmg"
+    downloadURL="https://get.videolan.org/vlc/${appNewVersion}/macosx/vlc-${appNewVersion}-arm64.dmg"
     expectedTeamID="75GAHG3SZQ"
     ;;
