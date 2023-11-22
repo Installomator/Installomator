4475c294428a8a6258f6a347ff14f2ae0996c13c
Tim de Jong <54109523+timnottom@users.noreply.github.com>
2023-11-22 16:19:41 +0100

    Update virtualbox.sh
    removed "jp" from the URL.

--- fragments/labels/virtualbox.sh
+++ fragments/labels/virtualbox.sh
@@ -8,7 +8,7 @@ virtualbox)
     elif [[ $(arch) == arm64 ]]; then
         platform="macOSArm64"
     fi
-    downloadURL="https:$(curl -fsL "https://www.oracle.com/jp/virtualization/technologies/vm/downloads/virtualbox-downloads.html" | grep "${platform}.dmg" | xmllint --html --xpath 'string(//a/@href)' -)"
+    downloadURL="https:$(curl -fsL "https://www.oracle.com/virtualization/technologies/vm/downloads/virtualbox-downloads.html" | grep "${platform}.dmg" | xmllint --html --xpath 'string(//a/@href)' -)"
     appNewVersion=$(echo "${downloadURL}" | awk -F '/' '{print $5}')
     expectedTeamID="VB5E2TV963"
     ;;
