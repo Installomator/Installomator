5f4ecd3502ba680d4ee5f5ba862c2a083a0096c5
eumelkind <mschulz@mx-it.com>
2023-12-21 17:39:53 +0100

    Create fontbase.sh

---
+++ fragments/labels/fontbase.sh
@@ -0,0 +1,8 @@
+fontbase)
+    # credit: Moritz Schulz (mschulz@mx-it.com - @eumelkind)
+    name="FontBase"
+    type="dmg"
+    appNewVersion=$(curl -s https://releases.fontba.se/mac/latest-mac.yml | grep version: | awk -F ':' '{print $2}' | sed -e 's/^ *//' -e 's/ *$//')
+    downloadURL="https://releases.fontba.se/mac/FontBase-$appNewVersion.dmg"
+    expectedTeamID="457B89RFCZ"
+    ;;
