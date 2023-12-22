b76c9b98ec95564557b5299c2281d90db6e3d527
alexfinn <91119429+alexfinn@users.noreply.github.com>
2023-12-22 12:32:33 +0000

    Create buttercup.sh
    Added the label for Buttercup password manager from Github.
    
    Script result: 2023-12-22 12:17:51 : REQ   :  : shifting arguments for Jamf
    2023-12-22 12:17:51 : REQ   : buttercup : ################## Start Installomator v. 10.5, date 2023-10-15
    2023-12-22 12:17:51 : INFO  : buttercup : ################## Version: 10.5
    2023-12-22 12:17:51 : INFO  : buttercup : ################## Date: 2023-10-15
    2023-12-22 12:17:51 : INFO  : buttercup : ################## buttercup
    2023-12-22 12:17:53 : INFO  : buttercup : setting variable from argument DIALOG_CMD_FILE=/var/tmp/dialog.log
    2023-12-22 12:17:53 : INFO  : buttercup : setting variable from argument NOTIFY=silent
    2023-12-22 12:17:53 : INFO  : buttercup : BLOCKING_PROCESS_ACTION=tell_user
    2023-12-22 12:17:53 : INFO  : buttercup : NOTIFY=silent
    2023-12-22 12:17:53 : INFO  : buttercup : LOGGING=INFO
    2023-12-22 12:17:53 : INFO  : buttercup : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2023-12-22 12:17:53 : INFO  : buttercup : Label type: zip
    2023-12-22 12:17:53 : INFO  : buttercup : archiveName: Buttercup.zip
    2023-12-22 12:17:53 : INFO  : buttercup : no blocking processes defined, using Buttercup as default
    2023-12-22 12:17:53 : INFO  : buttercup : name: Buttercup, appName: Buttercup.app
    2023-12-22 12:17:53.711 mdfind[13030:8286435] [UserQueryParser] Loading keywords and predicates for locale "en_GB"
    2023-12-22 12:17:54.161 mdfind[13030:8286435] Couldn't determine the mapping between prefab keywords and predicates.
    2023-12-22 12:17:54 : INFO  : buttercup : App(s) found: /Users/alex.finn/Downloads/Buttercup.app

---
+++ fragments/labels/buttercup.sh
@@ -0,0 +1,7 @@
+buttercup)
+    name="Buttercup"
+    type="zip"
+    downloadURL=$(downloadURLFromGit buttercup buttercup-desktop)
+    appNewVersion=$(versionFromGit buttercup buttercup-desktop)
+    expectedTeamID="9D8F4J769D"
+    ;;
