ab674e0caaeab6f3d1b7677d5b2f43beb2ac7b73
P-M Lejon <15982689+pmex@users.noreply.github.com>
2024-01-08 17:01:36 +0100

    Modified dialog.sh This will work with the separate commit for functions.sh that parses Git versions containing "-"

--- fragments/labels/dialog.sh
+++ fragments/labels/dialog.sh
@@ -4,6 +4,6 @@ swiftdialog)
     type="pkg"
     packageID="au.csiro.dialogcli"
     downloadURL="$(downloadURLFromGit swiftDialog swiftDialog)"
-    appNewVersion="$(versionFromGit swiftDialog swiftDialog)"
+    appNewVersion="$(versionFromGit swiftDialog swiftDialog | awk -F "-" '{print$1}')"
     expectedTeamID="PWA5E9TQ59"
     ;;
