6c92eea3314e8fbbc4d346503a20d171fcd33d67
Benjamin Kollmer <b.kollmer@apfelwerk.de>
2024-01-15 16:49:01 +0100

    Release 10.6.3
    - Added Vectorworks 2023 SP8

--- Installomator.sh
+++ Installomator.sh
@@ -7600,6 +7600,14 @@ vectorworks2023sp7)
     downloadURL="https://server3-d.vectorworks-online.de/cw/vw2023/mac/Vectorworks%202023%20SP7.dmg"
     expectedTeamID="LFNG3Q6WX2"
     ;;
+vectorworks2023sp8)
+    name="Vectorworks 2023 SP8"
+    appName="Vectorworks 2023 installieren.app"
+    type="dmg"
+    packageID="net.vectorworks.2023.vectorworksinstaller"
+    downloadURL="https://server3-d.vectorworks-online.de/cw/vw2023/mac/Vectorworks%202023%20SP8.dmg"
+    expectedTeamID="LFNG3Q6WX2"
+    ;;
 venturablocker)
     name="venturablocker"
     type="pkg"
