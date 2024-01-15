77852c025058b8135e9c515261fd67de683a909a
YanirTest <150788926+YanirTest@users.noreply.github.com>
2024-01-15 14:46:18 +0200

    Update Installomator.sh
    jdk21)
        name="Java SE Development Kit 21"
        type="pkgInDmg"
        versionKey="CFBundleShortVersionString"
        appNewVersion=$(curl -sf https://www.oracle.com/java/technologies/javase/jdk21-archive-downloads.html | grep -m 1 "Java SE Development Kit" | sed "s|.*Kit \(.*\)\<.*|\\1|")
        if [[ $(arch) == "arm64" ]]; then
            downloadURL="https://download.oracle.com/java/21/archive/jdk-"$appNewVersion"_macos-aarch64_bin.dmg"
        elif [[ $(arch) == "i386" ]]; then
            downloadURL="https://download.oracle.com/java/21/archive/jdk-"$appNewVersion"_macos-x64_bin.dmg"
        fi
        appCustomVersion(){ java --version | grep java | awk '{print $2}' }
        expectedTeamID="VB5E2TV963"

--- Installomator.sh
+++ Installomator.sh
@@ -4142,6 +4142,19 @@ jdk19)
     fi
     appCustomVersion(){ java --version | grep java | awk '{print $2}' }
     expectedTeamID="VB5E2TV963"
+        ;;
+jdk21)
+    name="Java SE Development Kit 21"
+    type="pkgInDmg"
+    versionKey="CFBundleShortVersionString"
+    appNewVersion=$(curl -sf https://www.oracle.com/java/technologies/javase/jdk21-archive-downloads.html | grep -m 1 "Java SE Development Kit" | sed "s|.*Kit \(.*\)\<.*|\\1|")
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL="https://download.oracle.com/java/21/archive/jdk-"$appNewVersion"_macos-aarch64_bin.dmg"
+    elif [[ $(arch) == "i386" ]]; then
+        downloadURL="https://download.oracle.com/java/21/archive/jdk-"$appNewVersion"_macos-x64_bin.dmg"
+    fi
+    appCustomVersion(){ java --version | grep java | awk '{print $2}' }
+    expectedTeamID="VB5E2TV963"
     ;;
 jdk20)
     name="Java SE Development Kit 20"
