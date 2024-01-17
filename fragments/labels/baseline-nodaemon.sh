0e7cd1dd2172c81d638a893ecd61b364f9b5b609
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2024-01-16 23:36:22 -0800

    fix name

--- fragments/labels/baseline-nodaemon.sh
+++ fragments/labels/baseline-nodaemon.sh
@@ -1,9 +1,9 @@
-baseline-noagent)
+baseline-nodaemon)
     #Baseline by @BigMacAdmin and Second Son Consulting
     #Use this label if you DO NOT want Baseline to run immediately upon install
     name="Baseline"
     type="pkg"
-    archiveName="Baseline_NoAgent_v[0-9.]*.pkg"
+    archiveName="Baseline_NoDaemon_v[0-9.]*.pkg"
     downloadURL=$(downloadURLFromGit secondsonconsulting Baseline )
     expectedTeamID="7Q6XP5698G"
     ;;
