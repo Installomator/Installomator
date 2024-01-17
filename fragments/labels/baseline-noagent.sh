910aadce7722ec077fb4b79e4e6faf38718994dc
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2024-01-16 23:30:47 -0800

    Updated comments, added "no agent" label for alternate pkg

--- fragments/labels/baseline-noagent.sh
+++ fragments/labels/baseline-noagent.sh
@@ -1,9 +1,9 @@
-baseline)
+baseline-noagent)
     #Baseline by @BigMacAdmin and Second Son Consulting
-    #Use this label if you want Baseline to run immediately upon install
+    #Use this label if you DO NOT want Baseline to run immediately upon install
     name="Baseline"
     type="pkg"
-    archiveName="Baseline_v[0-9.]*.pkg"
+    archiveName="Baseline_NoAgent_v[0-9.]*.pkg"
     downloadURL=$(downloadURLFromGit secondsonconsulting Baseline )
     expectedTeamID="7Q6XP5698G"
     ;;
