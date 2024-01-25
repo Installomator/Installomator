547d6b3e88b1c1e2e43066aa6116169fa06ae9da
Alexander Duffner <duffner@posteo.de>
2024-01-25 01:13:34 +0100

    Update maxonapp.sh

--- fragments/labels/maxonapp.sh
+++ fragments/labels/maxonapp.sh
@@ -2,7 +2,7 @@ maxonapp)
     name="Maxon"
     type="dmg"
     versionKey="CFBundleShortVersionString"
-    appNewVersion="$(curl -s "https://packages.maxon.net/manifests?platform=macos&org=maxon&type=products&family=fuse" | tr { '\n' | tr , '\n' | tr } '\n' | grep "version" | awk  -F'"' '{print $4}' | sort -gru | head -n 1)"
+    appNewVersion="$(curl -s "https://packages.maxon.net/manifests?platform=macos&org=maxon&type=products&family=fuse" | tr { '\n' | tr , '\n' | tr } '\n' | grep "version" | awk  -F'"' '{print $4}' | sort -gr | head -n 1)"
     downloadURL="https://mx-app-blob-prod.maxon.net/mx-package-production/website/macos/maxon/maxonapp/releases/${appNewVersion}/Maxon_App_${appNewVersion}_Mac.dmg"
     installerTool="Maxon App Installer.app"
     CLIInstaller="Maxon App Installer.app/Contents/MacOS/installbuilder.sh"
