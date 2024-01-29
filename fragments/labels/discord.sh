4c73a88a0b1eca468b48684c2b422632fde999d8
Fredrik Larsson <33235315+kryptonit1@users.noreply.github.com>
2024-01-29 08:57:19 +0100

    Update discord.sh

--- fragments/labels/discord.sh
+++ fragments/labels/discord.sh
@@ -2,6 +2,6 @@ discord)
     name="Discord"
     type="dmg"
     downloadURL="https://discordapp.com/api/download?platform=osx"
-    appNewVersion="$(curl -sL -o /dev/null -w %{url_effective} "${downloadURL}" | awk -F'/' '{print $(NF-1)}')"
+    appNewVersion="$(curl -fsL -o /dev/null -w %{url_effective} "${downloadURL}" | awk -F'/' '{print $(NF-1)}')"
     expectedTeamID="53Q6R32WPB"
     ;;
