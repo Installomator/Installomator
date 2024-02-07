98bd44b408f2082d9679be163a4a620cd2302f36
Theile <soren@theilgaard.dk>
2024-02-07 15:56:31 +0100

    Soundly

---
+++ fragments/labels/soundly-placeit.sh
@@ -0,0 +1,8 @@
+placeit)
+    name="Placeit"
+    # Other Tools: https://getsoundly.com/tools/
+    type="pkg"
+    downloadURL="https://storage.googleapis.com/soundly-plugins/Placeit.pkg"
+    #appNewVersion=""
+    expectedTeamID="67Y6N7VTDG"
+    ;;
---
+++ fragments/labels/soundly.sh
@@ -0,0 +1,8 @@
+soundly)
+    name="Soundly"
+    # From: https://getsoundly.com
+    type="dmg"
+    downloadURL="https://storage.googleapis.com/soundlyapp/arm/Soundly.dmg"
+    #appNewVersion=""
+    expectedTeamID="67Y6N7VTDG"
+    ;;
