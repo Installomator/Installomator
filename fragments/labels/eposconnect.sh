902b219eeb1f2f867c53164d4c1e5727a8e028b3
Theile <soren@theilgaard.dk>
2024-02-08 13:06:52 +0100

    Update eposconnect.sh

--- fragments/labels/eposconnect.sh
+++ fragments/labels/eposconnect.sh
@@ -6,6 +6,6 @@ eposconnect)
     downloadURL=$(curl -fs "https://www.eposaudio.com/en/dk/software/epos-connect" | tr '"' "\n" | grep -o "^https://www.eposaudio.*.pkg$")
     versionKey="CFBundleVersion"
     # 2024-02-06: appNewVersion commented out as the latest version is 7.8.1 but above page only shows 7.7.0
-    #appNewVersion=$(echo "$downloadURL" | sed -E 's/.*_([0-9.]*).pkg/\1/' | cut -d "." -f4)
+    appNewVersion=$(echo "$downloadURL" | sed -E 's/.*_([0-9.]*).pkg/\1/' | cut -d "." -f4)
     expectedTeamID="8956A7Y69J"
     ;;
