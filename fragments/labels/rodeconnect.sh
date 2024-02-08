4ed6097c355cea25496ed95d7f9f52edcaeaf184
Theile <soren@theilgaard.dk>
2024-02-08 13:26:30 +0100

    Update rodeconnect.sh
    DownloadURL fixed, but release notes are still incorrect.

--- fragments/labels/rodeconnect.sh
+++ fragments/labels/rodeconnect.sh
@@ -2,7 +2,7 @@ rodeconnect)
     name="RODE Connect"
     type="pkgInZip"
     #packageID="com.rodeconnect.installer" #Versioned wrong as 0 in 1.1.0 pkg
-    downloadURL="https://cdn1.rode.com/rodeconnect_installer_mac.zip"
+    downloadURL="https://update.rode.com/connect/RODE_Connect_MACOS.zip"
     #appNewVersion=$(curl -fs https://rode.com/en/release-notes/rode-connect | xmllint --html --format - 2>/dev/null | tr '"' '\n' | grep -i -o "Version .*" | head -1 | cut -w -f2)
     expectedTeamID="Z9T72PWTJA"
     ;;
