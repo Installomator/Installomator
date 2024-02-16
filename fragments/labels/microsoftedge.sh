9b697a0b62f703b6e100e2134143543366c27d63
Scot <ohls@mac.com>
2024-02-16 17:00:01 +0000

    Remove changes to microsoftedge.sh

--- fragments/labels/microsoftedge.sh
+++ fragments/labels/microsoftedge.sh
@@ -3,37 +3,14 @@ microsoftedgeconsumerstable|\
 microsoftedgeenterprisestable)
     name="Microsoft Edge"
     type="pkg"
-    appName="$name.app"
-    versionKey="CFBundleVersion"
+    downloadURL="https://go.microsoft.com/fwlink/?linkid=2093504"
+    #appNewVersion=$(curl -fs https://macadmins.software/latest.xml | xpath '//latest/package[id="com.microsoft.edge"]/cfbundleversion' 2>/dev/null | sed -E 's/<cfbundleversion>([0-9.]*)<.*/\1/')
+    appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | grep -o "/MicrosoftEdge.*pkg" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
     expectedTeamID="UBF8T346G9"
-    msid="EDGE01"
-    linkid="2093504"
-    downloadURL=$(curl -fsIL "https://go.microsoft.com/fwlink/?linkid=${linkid}" | grep -i location: | awk -F': ' '{ print $2 }' | tr -d '\n' | sed 's/pkg.*/pkg/')
-    getAppVersion
-    if [[ $appversion && $INSTALL != "force" ]]; then
-        printlog "Grabbing updater URL for Delta update"
-        updaterURL=$(curl -s https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409${msid}.xml | xmllint --format --xpath ".//*/key[text()='Location']/following-sibling::*[1]" - | awk -F'<string>' '{ print $2 }' | sed 's/<.*//' | grep "Update-"| tr -d '\n' | sed 's/pkg.*/pkg/')
-    fi
     if [[ -x "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" && $INSTALL != "force" && $DEBUG -eq 0 ]]; then
         printlog "Running msupdate --list"
-        toolOutput=$("/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" --list | grep -c 'XPC Connection to updater invalidated')
-    else
-        toolOutput="99"
+        "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" --list
     fi
-    if [[ $toolOutput -eq 0 ]]; then
-        printlog "XPC Connection to msupdate succesfull, setting msupdate as update tool"
-        updateTool="/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate"
-        updateToolArguments=( --install --apps ${msid} )
-    elif [[ $toolOutput -eq 99 ]]; then
-        printlog "force/debug set or msupdate unavailable, update tool not set"
-    elif [[ $toolOutput -ne 0 ]]; then
-        printlog "XPC Connection to updater invalidated or otherwise failed, update tool not set"
-    fi
-    if [[ $updaterURL && $INSTALL != "force" ]]; then
-        printlog "Setting downloadURL to updaterURL"
-        downloadURL="$updaterURL"
-    fi
-
-    appNewVersion=$(echo $downloadURL | grep -o "/Microsoft.*pkg" | cut -d "-" -f 2 | cut -d "." -f 1-4)
-
+    updateTool="/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate"
+    updateToolArguments=( --install --apps EDGE01 )
     ;;
