2e47eeea307454bbc47f2c5e80a12c77a4564f87
Lucas Cantor <lucas@cantor.cloud>
2024-03-04 10:42:43 -0800

    use versionFromGit for appNewVersion

--- fragments/labels/droplr.sh
+++ fragments/labels/droplr.sh
@@ -3,11 +3,10 @@ droplr)
     type="dmg"
     if [[ $(arch) == arm64 ]]; then
      downloadURL="https://files.droplr.com/apps/mac-m1-current"
-     appNewVersion=$(curl -fsLIXGET "https://files.droplr.com/apps/mac-m1-current" | grep -i "^content-disposition" | sed -e 's/.*filename\=Droplr\-\(.*\)\-arm64\.dmg.*/\1/')
     elif [[ $(arch) == i386 ]]; then
      downloadURL="https://files.droplr.com/apps/mac-intel-current"
-     appNewVersion=$(curl -fsLIXGET "https://files.droplr.com/apps/mac-intel-current" | grep -i "^content-disposition" | sed -e 's/.*filename\=Droplr\-\(.*\)\.dmg.*/\1/')
     fi
+    appNewVersion="$(versionFromGit Droplr droplr-desktop-releases)"
     expectedTeamID="MZ25PHMY7Y"
     blockingProcesses=( "Droplr" "Droplr Helper (Plugin)" "Droplr Helper (GPU)" "Droplr Helper (Renderer)" "Droplr Helper" )
     ;;
