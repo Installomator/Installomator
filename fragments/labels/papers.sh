e586d2c04ba86611f3943c4ac23835fb49715b1b
stephenw235 <stephen.j.weinstein@gmail.com>
2024-03-07 16:57:30 -0500

    Create papers.sh

---
+++ fragments/labels/papers.sh
@@ -0,0 +1,11 @@
+papers)
+    name="Papers"
+    type="dmg"
+    if [[ "$(arch)" == "arm64" ]]; then
+        downloadURL="https://download.readcube.com/app/Papers_Setup-arm64.dmg"
+    else
+        downloadURL="https://download.readcube.com/app/Papers_Setup-x64.dmg"
+    fi
+    appNewVersion=""
+    expectedTeamID="FY6R4ETYH7"
+    ;;
