4d3e293d28fb3209ed6d4491fb29412ca94c2ab3
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2024-03-07 23:10:16 -0800

    Update and rename grooveomnidialer.sh to grooveomnidialerenterpriseedition.sh
    The name and the label.sh file need to match.
    
    Removed hyphens

--- fragments/labels/grooveomnidialerenterpriseedition.sh
+++ fragments/labels/grooveomnidialerenterpriseedition.sh
@@ -1,4 +1,4 @@
-groove-omnidialer-enterprise-edition)
+grooveomnidialerenterpriseedition)
 	name="Groove Omnidialer Enterprise Edition"
 	type="zip"
 	appNewVersion=$( curl -fs 'https://groove-dialer.s3.us-west-2.amazonaws.com/electron/enterprise/latest-mac.yml' | grep ^version: | cut -c 10-21 ) 
