3adc613069a9d1c403f91797a1ba0665a4533ebf
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2024-03-09 09:59:20 -0800

    Update blender.sh
    Fixed the `name=blender` variable to match the app inside the dmg which is `name=Blender`
    
    This makes the `appName` variable and the `blockingProcesses` array unnecessary to include in the label, so I removed those as well.

--- fragments/labels/blender.sh
+++ fragments/labels/blender.sh
@@ -1,6 +1,5 @@
 blender)
-    name="blender"
-    appName="$name.app"
+    name="Blender"
     type="dmg"
     versionKey="CFBundleShortVersionString"
     appNewVersion=$(curl -sf https://ftp.nluug.nl/pub/graphics/blender/release/ | grep -o 'Blender[0-9]\+\.[0-9]\+' | cut -d 'r' -f 2 | sort -V | tail -1)
@@ -11,6 +10,5 @@ blender)
         archiveName=$(curl -sf "https://ftp.nluug.nl/pub/graphics/blender/release/Blender$appNewVersion/" | grep -o 'blender-[0-9]\+\.[0-9]\+\.[0-9]\+-macos-x64\.dmg' | sort -V | tail -1)
         downloadURL="https://ftp.nluug.nl/pub/graphics/blender/release/Blender$appNewVersion/$archiveName"
     fi
-    blockingProcesses=( "Blender" )
     expectedTeamID="68UA947AUU"
     ;;
