ab9386c8e5b6c104049873c3f0d5f95ff5b4d3de
Theile <soren@theilgaard.dk>
2024-03-12 09:30:41 +0100

    Create mockoon.sh

---
+++ fragments/labels/mockoon.sh
@@ -0,0 +1,8 @@
+mockoon)
+    name="Mockoon"
+    type="dmg"
+    archiveName="mockoon.setup[0-9.]*universal.dmg"
+    downloadURL="$(downloadURLFromGit mockoon mockoon)"
+    appNewVersion="$(versionFromGit mockoon mockoon)"
+    expectedTeamID="8443RQQKK6"
+    ;;
