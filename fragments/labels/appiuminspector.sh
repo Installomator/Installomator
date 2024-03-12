7fc0e41371b0f50616ab76433a668e52fa7b8346
Theile <soren@theilgaard.dk>
2024-03-12 09:22:05 +0100

    Create appiuminspector.sh
    Apium Desktop is unsecure and should not be included.

---
+++ fragments/labels/appiuminspector.sh
@@ -0,0 +1,7 @@
+appiuminspector)
+    name="Appium Inspector"
+    type="dmg"
+    downloadURL="$(downloadURLFromGit appium appium-inspector)"
+    appNewVersion="$(versionFromGit appium appium-inspector)"
+    expectedTeamID="UY52UFTVTM"
+    ;;
