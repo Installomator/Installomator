dc5b0fe79f7142ad62ad1754657d82ff31cb4f7b
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2024-03-11 22:28:28 -0700

    Update whatsapp.sh

--- fragments/labels/whatsapp.sh
+++ fragments/labels/whatsapp.sh
@@ -2,10 +2,7 @@ whatsapp)
     name="WhatsApp"
     type="dmg"
     downloadURL="https://web.whatsapp.com/desktop/mac_native/release/?configuration=Release"
-<<<<<<< HEAD
-=======
     appNewVersion=$(curl -fsLIXGET "https://web.whatsapp.com/desktop/mac_native/release/?configuration=Release" | grep -i "^location" | grep -m 1 -o "WhatsApp-.*dmg" | sed 's/.*WhatsApp-2.//g' | sed 's/.dmg//g')
->>>>>>> pr/1369
     expectedTeamID="57T9237FN3"
     appNewVersion=$(curl -s https://web.whatsapp.com/desktop/mac/releases | awk -F '"' '/"name"/ {print $4}')
     ;;
