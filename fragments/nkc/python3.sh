ea0adedfd32936ffaa310857fe579f1d9db9afd1
Brent Clark <brentac82@gmail.com>
2024-03-14 08:18:30 -0500

    Update python3.sh
    Update python label to compress curl output

--- fragments/nkc/python3.sh
+++ fragments/nkc/python3.sh
@@ -2,7 +2,7 @@ python3)
     # NKC Change
     name="Python 3"
     type="pkg"
-    appNewVersion=$(curl https://www.python.org/downloads/macos/ | grep -e "Latest Python 3 Release" | awk '{print $8}' | sed -r 's/.{9}$//')
+    appNewVersion=$(curl --compressed https://www.python.org/downloads/macos/ | grep -e "Latest Python 3 Release" | awk '{print $8}' | sed -r 's/.{9}$//')
     downloadURL="https://www.python.org/ftp/python/$appNewVersion/python-$appNewVersion-macos11.pkg"
     expectedTeamID="DJ3H93M7VJ"
     ;;
