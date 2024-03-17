9878deb0a67b964238c084b465301147ec048259
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2024-03-16 21:25:31 -0700

    Update buttercup.sh
    Adding the `archiveName` should make this more reliable

--- fragments/labels/buttercup.sh
+++ fragments/labels/buttercup.sh
@@ -1,7 +1,8 @@
 buttercup)
     name="Buttercup"
     type="zip"
-    downloadURL=$(downloadURLFromGit buttercup buttercup-desktop)
+	archiveName="Buttercup-[0-9.]*-mac.zip"
+	downloadURL=$(downloadURLFromGit buttercup buttercup-desktop)
     appNewVersion=$(versionFromGit buttercup buttercup-desktop)
     expectedTeamID="9D8F4J769D"
     ;;
