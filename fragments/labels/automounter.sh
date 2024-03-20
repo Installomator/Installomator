a2b545b9538dcd344f991924c65eb6a8ff43dbbc
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2024-03-19 20:49:48 -0700

    Update automounter.sh
    Please do not include blockingProcesses, appName, or archiveName if they are set to the default values.
    
    if `name=` is used for the blocking process, the archive name, and the app name, then it is not needed in the label.

--- fragments/labels/automounter.sh
+++ fragments/labels/automounter.sh
@@ -4,8 +4,5 @@ automounter)
     downloadURL="https://www.pixeleyes.co.nz/automounter/AutoMounter.dmg"
     appNewVersion="$( curl -fs https://www.pixeleyes.co.nz/automounter/version )"
     versionKey="CFBundleShortVersionString"
-    appName="$name.app"
-    archiveName="$name.$type"
-    blockingProcesses=( $name )
     expectedTeamID="UKWABN4MGL"
     ;;
