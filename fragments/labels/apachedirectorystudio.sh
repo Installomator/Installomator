b0bf7106f117e7757916d0a61e1393028880a7b9
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2024-03-19 20:52:33 -0700

    Update apachedirectorystudio.sh
    Same comment as last label. These are not necessary, as you are setting to the default values:
    
        `appName="$name.app"`
        `archiveName="$name.$type"`
        `blockingProcesses=( $name )`

--- fragments/labels/apachedirectorystudio.sh
+++ fragments/labels/apachedirectorystudio.sh
@@ -4,8 +4,5 @@ apachedirectorystudio)
     downloadURL=$( curl -fs https://directory.apache.org/studio/download/download-macosx.html | grep -o 'http[^"]*downloads[^"]*ApacheDirectoryStudio[^"]*.dmg' | head -1 )
     appNewVersion=$( curl -fs $downloadURL | grep -o 'studio/\([^/]*\)/ApacheDirectoryStudio' | cut -d'/' -f2 )
     versionKey="CFBundleVersion"
-    appName="$name.app"
-    archiveName="$name.$type"
-    blockingProcesses=( $name )
     expectedTeamID="2GLGAFWEQD"
     ;;
