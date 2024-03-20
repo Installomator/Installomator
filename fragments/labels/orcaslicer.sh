d52b23f4d2b4fa4018d5d37ae4b4cc8e7dd6ceb4
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2024-03-19 20:24:11 -0700

    Update orcaslicer.sh
    Implementing Theile's suggested fix. The `archiveName` variable does need to come before the github functions or there can be undesired results.

--- fragments/labels/orcaslicer.sh
+++ fragments/labels/orcaslicer.sh
@@ -2,13 +2,12 @@ orcaslicer)
     name="OrcaSlicer"
     type="dmg"
     if [[ $(arch) == "arm64" ]]; then
-       downloadURL="$(downloadURLFromGit SoftFever OrcaSlicer)"
-       appNewVersion="$(versionFromGit SoftFever OrcaSlicer)"
-       archiveName="OrcaSlicer_Mac_arm64_*.dmg"
+        archiveName="OrcaSlicer_Mac_arm64_*.dmg"
     elif [[ $(arch) == "i386" ]]; then
-      downloadURL="$(downloadURLFromGit SoftFever OrcaSlicer)"
-      appNewVersion="$(versionFromGit SoftFever OrcaSlicer)"
-      archiveName="OrcaSlicer_Mac_x86_64_*.dmg"
+        archiveName="OrcaSlicer_Mac_x86_64_*.dmg"
     fi
+    downloadURL="$(downloadURLFromGit SoftFever OrcaSlicer)"
+    appNewVersion="$(versionFromGit SoftFever OrcaSlicer)"
     expectedTeamID="XQK7C38HH5"
     ;;
+    
