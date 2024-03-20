09c890a755b914495c83f83544f25f0b7be9b5d4
Brent Clark <brentac82@gmail.com>
2024-03-20 10:10:10 -0500

    Update python3.sh
    Switched custom label to the main installomator with --compressed after the curl commands

--- fragments/nkc/python3.sh
+++ fragments/nkc/python3.sh
@@ -1,8 +1,20 @@
+
+# This is the official Installomator label with --compressed added. Also changed label name to python3
 python3)
     # NKC Change
-    name="Python 3"
+    name="Python"
     type="pkg"
-    appNewVersion=$(curl --compressed https://www.python.org/downloads/macos/ | grep -e "Latest Python 3 Release" | awk '{print $8}' | sed -r 's/.{9}$//')
-    downloadURL="https://www.python.org/ftp/python/$appNewVersion/python-$appNewVersion-macos11.pkg"
-    expectedTeamID="DJ3H93M7VJ"
+    appNewVersion="$( curl -s --compressed "https://www.python.org/downloads/macos/" | awk '/Latest Python 3 Release - Python/{gsub(/<\/?[^>]+(>|$)/, ""); print $NF}' )"
+    archiveName="$( curl -s --compressed "https://www.python.org/ftp/python/$appNewVersion/" | grep -om 1 "\"python.*macos.*\.pkg\"" | tr -d \" )"
+    downloadURL="https://www.python.org/ftp/python/$appNewVersion/$archiveName"
+    shortVersion=$( cut -d '.' -f1,2 <<< $appNewVersion )
+    packageID="org.python.Python.PythonFramework-$shortVersion"
+    expectedTeamID="BMM5U3QVKW"
+    blockingProcesses=( "IDLE" "Python Launcher" )
+    versionKey="CFBundleVersion"
+    appCustomVersion() {
+        if [ -d "/Library/Frameworks/Python.framework/Versions/$shortVersion/Resources/Python.app/" ]; then
+            /usr/bin/defaults read "/Library/Frameworks/Python.framework/Versions/$shortVersion/Resources/Python.app/Contents/Info" CFBundleVersion
+        fi
+    }
     ;;
