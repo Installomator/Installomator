6eae5458eb187e3071497dde0a0ca305f76e83da
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2024-03-19 20:32:45 -0700

    Update orcaslicer.sh
    test5

--- fragments/labels/orcaslicer.sh
+++ fragments/labels/orcaslicer.sh
@@ -2,13 +2,13 @@ orcaslicer)
     name="OrcaSlicer"
     type="dmg"
     if [[ $(arch) == "arm64" ]]; then
-       archiveName="OrcaSlicer_Mac_arm64_*.dmg"
        downloadURL="$(downloadURLFromGit SoftFever OrcaSlicer)"
        appNewVersion="$(versionFromGit SoftFever OrcaSlicer)"
+       archiveName="OrcaSlicer_Mac_arm64_*.dmg"
     elif [[ $(arch) == "i386" ]]; then
-      archiveName="OrcaSlicer_Mac_x86_64_*.dmg"
       downloadURL="$(downloadURLFromGit SoftFever OrcaSlicer)"
       appNewVersion="$(versionFromGit SoftFever OrcaSlicer)"
+      archiveName="OrcaSlicer_Mac_x86_64_*.dmg"
     fi
     expectedTeamID="XQK7C38HH5"
     ;;
