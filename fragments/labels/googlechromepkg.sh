68fbeadd94def46d46014186ab9caa8e53afaf5b
Aymen <123590232+AymenAbb@users.noreply.github.com>
2024-03-21 08:52:14 -0500

    Fix Version GoogleChromePKG
    Agreed upon solution requested by @BigMacAdmin as provided by @fraserhess and @mjabroux from #1366. Didn't see anyone else doing a PR on this and it's been a bit.

--- fragments/labels/googlechromepkg.sh
+++ fragments/labels/googlechromepkg.sh
@@ -5,7 +5,7 @@ googlechromepkg)
     # Note: this url acknowledges that you accept the terms of service
     # https://support.google.com/chrome/a/answer/9915669
     #
-    downloadURL="https://dl.google.com/chrome/mac/stable/accept_tos%3Dhttps%253A%252F%252Fwww.google.com%252Fintl%252Fen_ph%252Fchrome%252Fterms%252F%26_and_accept_tos%3Dhttps%253A%252F%252Fpolicies.google.com%252Fterms/googlechrome.pkg"
+    appNewVersion=$(getJSONValue "$(curl -fsL "https://versionhistory.googleapis.com/v1/chrome/platforms/mac/channels/stable/versions/all/releases?filter=fraction>0.01,endtime=none&order_by=version%20desc" )" "releases[0].version" )
     appNewVersion=$(curl -s https://omahaproxy.appspot.com/history | awk -F',' '/mac_arm64,stable/{print $3; exit}')
     expectedTeamID="EQHXZ8M8AV"
     updateTool="/Library/Google/GoogleSoftwareUpdate/GoogleSoftwareUpdate.bundle/Contents/Resources/GoogleSoftwareUpdateAgent.app/Contents/MacOS/GoogleSoftwareUpdateAgent"
