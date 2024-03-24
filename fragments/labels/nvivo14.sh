6ad9bc64629ce707cf7e2f2ea453924d34a84f66
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2024-03-23 21:13:32 -0700

    Update nvivo14.sh

--- fragments/labels/nvivo14.sh
+++ fragments/labels/nvivo14.sh
@@ -5,5 +5,5 @@ nvivo14)
     downloadURL="https://download.qsrinternational.com/Software/NVivo14forMac/NVivo.dmg"
     appNewVersion=$( curl -fsIL "${downloadURL}" | grep -i "^location" | awk '{print $2}' | awk -F'/' '{ print $6 }' | cut -d "." -f1-3 )
     expectedTeamID="8F4S7H8S59"
-    blockingProcesses=( NVivo NVivoHelper "Nvivo 14" )
+    blockingProcesses=( NVivo NVivoHelper \"Nvivo 14\" )
     ;;
