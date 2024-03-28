fdb0a52da5d01438d96f4ee12b4f7d46582783f6
Alexander Duffner <duffner@posteo.de>
2024-03-28 11:33:11 +0100

    Update pulsar.sh
    thank you @bigmacadmin for the hint

--- fragments/labels/pulsar.sh
+++ fragments/labels/pulsar.sh
@@ -4,13 +4,11 @@ pulsar)
     appNewVersion=$(versionFromGit pulsar-edit pulsar)
     if [[ $(arch) = "arm64" ]]; then
         printlog "Architecture: arm64"
-        downloadURL="https://github.com/pulsar-edit/pulsar/releases/download/v${appNewVersion}/Silicon.Mac.Pulsar-${appNewVersion}-arm64-mac.zip"
         archiveName="Silicon.Mac.Pulsar-${appNewVersion}-arm64-mac.zip"
     else
         printlog "Architecture: i386 (not arm64)"
-        downloadURL="https://github.com/pulsar-edit/pulsar/releases/download/v${appNewVersion}/Intel.Mac.Pulsar-${appNewVersion}-mac.zip"
         archiveName="Intel.Mac.Pulsar-${appNewVersion}-mac.zip"
     fi
+    downloadURL=$(downloadURLFromGit pulsar-edit pulsar)
     expectedTeamID="D3KV2P2CZ8"
     ;;
-
