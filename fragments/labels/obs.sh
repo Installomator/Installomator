11751b7163a91ad061e9a04e1e5b0445c5ed6387
Søren Theilgaard <st@theilgaard.dk>
2024-04-03 18:22:17 +0200

    Update obs.sh
    `name` and `type` at top

--- fragments/labels/obs.sh
+++ fragments/labels/obs.sh
@@ -1,11 +1,11 @@
 obs)
+    name="OBS"
+    type="dmg"
     if [[ $(arch) == "arm64" ]]; then
         archiveName="OBS-Studio-[0-9.]*-macOS-Apple.dmg"
     elif [[ $(arch) == "i386" ]]; then
         archiveName="OBS-Studio-[0-9.]*-macOS-Intel.dmg"
     fi
-    name="OBS"
-    type="dmg"
     downloadURL="$(downloadURLFromGit obsproject obs-studio)"
     appNewVersion="$(versionFromGit obsproject obs-studio)"
     expectedTeamID="2MMRE5MTB8"
