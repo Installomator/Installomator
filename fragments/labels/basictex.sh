34bdc585604f8a7a58e1550debe8d25674056f30
Theile <soren@theilgaard.dk>
2024-04-03 19:28:08 +0200

    Create basictex.sh

---
+++ fragments/labels/basictex.sh
@@ -0,0 +1,7 @@
+basictex)
+    # Small LaTeX alternative to mactex
+    name="BasicTeX"
+    type="pkg"
+    downloadURL="https://mirror.ctan.org/systems/mac/mactex/BasicTeX.pkg"
+    expectedTeamID="RBGCY5RJWM"
+    ;;
