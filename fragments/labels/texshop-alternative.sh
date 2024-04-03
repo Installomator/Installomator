0bc7f8da1d383c79a04ad7be64a365bd72156418
Theile <soren@theilgaard.dk>
2024-04-03 19:27:31 +0200

    Update texshop-alternative.sh

--- fragments/labels/texshop-alternative.sh
+++ fragments/labels/texshop-alternative.sh
@@ -1,5 +1,5 @@
 texshop-alternative)
-    # This label is really an alternative to the original TeXShop, as it might not have the latest version and is not maintained by original author of TeXShop.
+    # This label is really an alternative to the original TeXShop, as it might not have the latest version and is not maintained by original developers of TeXShop.
     name="TeXShop"
     type="zip"
     downloadURL="$(downloadURLFromGit TeXShop TeXShop)"
