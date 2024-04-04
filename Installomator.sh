f1113f28766eb56c2349dab88414dd03fc520c43
GaryPackage <42139030+GaryPackage@users.noreply.github.com>
2024-04-04 15:26:03 +0200

    Update Installomator.sh
    commented out label 'displaylinkmanager', since it should use label 'displaylinkmanagergraphicsconnectivity' instead.

--- Installomator.sh
+++ Installomator.sh
@@ -3069,14 +3069,14 @@ diskspace)
     appNewVersion="$(versionFromGit scriptingosx diskspace)"
     expectedTeamID="JME5BW3F3R"
     ;;
-displaylinkmanager)
-    name="DisplayLink Manager"
-    type="pkg"
-    #packageID="com.displaylink.displaylinkmanagerapp"
-    downloadURL=https://www.synaptics.com$(redirect=$(curl -sfL https://www.synaptics.com/products/displaylink-graphics/downloads/macos | grep 'class="download-link">Download' | sed -n '2p' | sed 's/.*href="//' | sed 's/".*//') && curl -sfL "https://www.synaptics.com$redirect" | grep 'class="no-link"' | awk -F 'href="' '{print $2}' | awk -F '"' '{print $1}')
-    appNewVersion=$(curl -sfL https://www.synaptics.com/products/displaylink-graphics/downloads/macos | grep "Release:" | sed -n '2p' | cut -d ' ' -f2)
-    expectedTeamID="73YQY62QM3"
-    ;;
+#displaylinkmanager)
+#    name="DisplayLink Manager"
+#    type="pkg"
+#    #packageID="com.displaylink.displaylinkmanagerapp"
+#    downloadURL=https://www.synaptics.com$(redirect=$(curl -sfL https://www.synaptics.com/products/displaylink-graphics/downloads/macos | grep 'class="download-link">Download' | sed -n '2p' | sed 's/.*href="//' | sed 's/".*//') && curl -sfL "https://www.synaptics.com$redirect" | grep 'class="no-link"' | awk -F 'href="' '{print $2}' | awk -F '"' '{print $1}')
+#    appNewVersion=$(curl -sfL https://www.synaptics.com/products/displaylink-graphics/downloads/macos | grep "Release:" | sed -n '2p' | cut -d ' ' -f2)
+#    expectedTeamID="73YQY62QM3"
+
 displaylinkmanagergraphicsconnectivity)
     name="DisplayLink Manager Graphics Connectivity"
     type="pkg"
