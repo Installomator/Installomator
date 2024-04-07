4ac81b61ffc43e9081d12536a0d3435cf14be574
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2024-04-06 21:35:00 -0700

    Update papers.sh
    Applying fix from @kryptonit1

--- fragments/labels/papers.sh
+++ fragments/labels/papers.sh
@@ -1,10 +1,12 @@
 papers)
     name="Papers"
     type="dmg"
-    if [[ "$(arch)" == "arm64" ]]; then
+    if [[ $(arch) == "arm64" ]]; then
         downloadURL="https://download.readcube.com/app/Papers_Setup-arm64.dmg"
-    else
+    elif [[ $(arch) == "i386" ]]; then
         downloadURL="https://download.readcube.com/app/Papers_Setup-x64.dmg"
-    fi    appNewVersion=""
+    fi
+    latestReleaseNote="$(curl -fsL "https://support.papersapp.com/support/solutions/folders/30000056499" | grep -oE "href=\".*-release-notes-.*\"" | cut -d '"' -f 2 | head -1)"
+    appNewVersion="$(curl -fsL "https://support.papersapp.com${latestReleaseNote}" | xmllint --html --xpath 'string(//div[@itemprop="articleBody"]/p/span[@dir="ltr"])' - 2> /dev/null | awk -F' ' '{ print $(NF) }')"
     expectedTeamID="FY6R4ETYH7"
     ;;
