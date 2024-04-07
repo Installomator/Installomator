22a444cf1d981c7f4b12b8923ef48efbfdb3d45b
Lucas Cantor <lucas@cantor.cloud>
2024-04-06 22:09:58 -0700

    use downloadURLFromGit

--- fragments/labels/droplr.sh
+++ fragments/labels/droplr.sh
@@ -1,11 +1,7 @@
 droplr)
     name="Droplr"
     type="dmg"
-    if [[ $(arch) == arm64 ]]; then
-     downloadURL="https://files.droplr.com/apps/mac-m1-current"
-    elif [[ $(arch) == i386 ]]; then
-     downloadURL="https://files.droplr.com/apps/mac-intel-current"
-    fi
+    downloadURL="$(downloadURLFromGit Droplr droplr-desktop-releases)"
     appNewVersion="$(versionFromGit Droplr droplr-desktop-releases)"
     expectedTeamID="MZ25PHMY7Y"
     blockingProcesses=( "Droplr" "Droplr Helper (Plugin)" "Droplr Helper (GPU)" "Droplr Helper (Renderer)" "Droplr Helper" )
