078eeeb2670f9fff2af008147e8b274c922a2f94
Lucas Cantor <lucas.cantor@intercom.io>
2024-04-08 10:01:24 -0700

    add blocking process

--- fragments/labels/tuple.sh
+++ fragments/labels/tuple.sh
@@ -4,4 +4,5 @@ tuple)
     downloadURL=$(curl -fs "https://d32ifkf9k9ezcg.cloudfront.net/production/sparkle/appcast.xml" | grep -o 'https://d32ifkf9k9ezcg.cloudfront.net/[^"]*.zip' | head -n 1)
     appNewVersion=$(curl -fs "https://d32ifkf9k9ezcg.cloudfront.net/production/sparkle/appcast.xml" | grep -o 'sparkle:shortVersionString="[^"]*"' | sed -E 's/sparkle:shortVersionString="([^"]*)"/\1/' | head -n 1)
     expectedTeamID="DQYU7DR9Q7"
+    blockingProcesses=( "Tuple" )
     ;;
