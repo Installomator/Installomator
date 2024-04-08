58b97152140e8ac0a22de8b6be5154b7c8ca1bf8
Lucas Cantor <lucas@cantor.cloud>
2024-04-08 10:04:10 -0700

    limit blockingProcesses to Drplor itself
    the others close when Droplr closes

--- fragments/labels/droplr.sh
+++ fragments/labels/droplr.sh
@@ -4,5 +4,5 @@ droplr)
     downloadURL="$(downloadURLFromGit Droplr droplr-desktop-releases)"
     appNewVersion="$(versionFromGit Droplr droplr-desktop-releases)"
     expectedTeamID="MZ25PHMY7Y"
-    blockingProcesses=( "Droplr" "Droplr Helper (Plugin)" "Droplr Helper (GPU)" "Droplr Helper (Renderer)" "Droplr Helper" )
+    blockingProcesses=( "Droplr" )
     ;;
