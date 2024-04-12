99714cc7dd9274106de3add0e8732879f02ddcc8
P-M Lejon (test-M1) <pme@bonniernews.se>
2024-04-12 16:47:53 +0200

    Correct indent for ;;

--- fragments/labels/connectfonts.sh
+++ fragments/labels/connectfonts.sh
@@ -4,4 +4,4 @@ connectfonts)
     downloadURL="https://links.extensis.com/connect_fonts/cf_latest?language=en&platform=mac"
     appNewVersion=$( curl -is "$downloadURL" | grep "location:" | grep -o "[[:digit:]]\+-[[:digit:]]\+-[[:digit:]]\+" | sed -e 's/-/./g' )
     expectedTeamID="J6MMHGD9D6"
-;;
+    ;;
