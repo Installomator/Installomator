bebcc2f061e1d008a51f235331995a6fb9e83fbf
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2024-04-14 11:17:38 -0700

    Update basecamp3.sh
    Deleted old "credit" line, and put `expectedTeamID` on it's own line (minor formatting error)

--- fragments/labels/basecamp3.sh
+++ fragments/labels/basecamp3.sh
@@ -1,11 +1,11 @@
 basecamp3)
-    #credit: @matins
     name="Basecamp 3"
     type="dmg"
     if [[ $(/usr/bin/arch) == "arm64" ]]; then 
         downloadURL="https://bc3-desktop.s3.amazonaws.com/mac_arm64/basecamp3_arm64.dmg"
     else
         downloadURL="https://bc3-desktop.s3.amazonaws.com/mac/basecamp3.dmg"
-    fi    expectedTeamID="2WNYUYRS7G"
+    fi
+    expectedTeamID="2WNYUYRS7G"
     appName="Basecamp 3.app"
     ;;
