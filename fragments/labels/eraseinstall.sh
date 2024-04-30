54b88265fab54b9671cac3e716336a9d589e7210
robotprom <98070647+robotprom@users.noreply.github.com>
2024-04-30 12:21:52 -0400

    Update eraseinstall.sh

--- fragments/labels/eraseinstall.sh
+++ fragments/labels/eraseinstall.sh
@@ -3,5 +3,6 @@ eraseinstall)
     type="pkg"
     downloadURL=$(downloadURLFromGit grahampugh erase-install)
     appNewVersion=$(versionFromGit grahampugh erase-install)
+    expectedTeamID="Software Update"
     appCustomVersion() { grep -i "version=" /Library/Management/erase-install/erase-install.sh | cut -d '"' -f2 }
     ;;
