89a330601aed457c356b14fe68cf3546200a3d01
GaryPackage <42139030+GaryPackage@users.noreply.github.com>
2024-04-30 14:57:16 +0200

    Update Installomator.sh
    Added CaptureWolf to Installomator

--- Installomator.sh
+++ Installomator.sh
@@ -2558,6 +2558,15 @@ canva)
 
     expectedTeamID="5HD2ARTBFS"
     ;;
+capturewolf)
+    name="CaptureWolf"
+    type="appInDmgInZip"
+    #packageID="com.schubergphilis.capture-wolf"
+    downloadURL=$(curl --silent --fail "https://api.github.com/repos/galadril/CaptureWolf/releases/latest" | awk -F '"' "/browser_download_url/ && /zip\"/ { print \$4; exit }")
+    appNewVersion=$(grep -o -E '\d+\.\d+\.\d+' <<< $downloadURL | head -n 1)
+    expectedTeamID="FN7VC8ZTQF"
+    blockingProcesses=( "CaptureWolf" )
+    ;;
 carboncopycloner)
     name="Carbon Copy Cloner"
     type="zip"
