c95f25dae492a51dc279d9a28aea9904d66be227
GaryPackage <42139030+GaryPackage@users.noreply.github.com>
2024-05-02 11:58:52 +0200

    Update Installomator.sh

--- Installomator.sh
+++ Installomator.sh
@@ -2561,7 +2561,7 @@ canva)
 capturewolf)
     name="CaptureWolf"
     type="appInDmgInZip"
-    #packageID="com.schubergphilis.capture-wolf"
+    packageID="com.schubergphilis.capture-wolf"
     downloadURL=$(curl --silent --fail "https://api.github.com/repos/galadril/CaptureWolf/releases/latest" | awk -F '"' "/browser_download_url/ && /zip\"/ { print \$4; exit }")
     appNewVersion=$(grep -o -E '\d+\.\d+\.\d+' <<< $downloadURL | head -n 1)
     expectedTeamID="FN7VC8ZTQF"
