0a6ba69a65ea8f9ff0d722b453377a8d60bdce74
GaryPackage <42139030+GaryPackage@users.noreply.github.com>
2024-05-02 11:58:00 +0200

    Update capturewolf.sh

--- fragments/labels/capturewolf.sh
+++ fragments/labels/capturewolf.sh
@@ -1,7 +1,7 @@
 capturewolf)
     name="CaptureWolf"
     type="appInDmgInZip"
-    #packageID=""
+    packageID="com.schubergphilis.capture-wolf"
     downloadURL=$(curl --silent --fail "https://api.github.com/repos/galadril/CaptureWolf/releases/latest" | awk -F '"' "/browser_download_url/ && /zip\"/ { print \$4; exit }")
     appNewVersion=$(grep -o -E '\d+\.\d+\.\d+' <<< $downloadURL | head -n 1)
     expectedTeamID="FN7VC8ZTQF"
