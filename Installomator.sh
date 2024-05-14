42f5939524e1a2fe92e70e474bde3efc8d279203
GaryPackage <42139030+GaryPackage@users.noreply.github.com>
2024-05-14 13:32:43 +0200

    Update Installomator.sh
    Made changed to iTerm2, where it now detects if the beta is installed, so that it updates out-of-date beta installations instead of updating to the stable. If the stable is installed, it will update to the latest stable.

--- Installomator.sh
+++ Installomator.sh
@@ -4265,8 +4265,13 @@ istatmenus)
 iterm2)
     name="iTerm"
     type="zip"
+    if [[ `defaults read /Applications/iTerm.app/Contents/Info.plist CFBundleShortVersionString` == *beta* ]] ; then
+    downloadURL=$(curl -fsL 'https://iterm2.com/downloads.html' | grep -oE 'https:[^"]*beta[^"]*zip'| head -1)
+    appNewVersion=$(sed -E 's/.*iTerm2-(.*)\.zip/\1/g; s/_/\./g' <<<"$downloadURL")
+    else
     downloadURL="https://iterm2.com/downloads/stable/latest"
     appNewVersion=$(curl -is https://iterm2.com/downloads/stable/latest | grep location: | grep -o "iTerm2.*zip" | cut -d "-" -f 2 | cut -d '.' -f1 | sed 's/_/./g')
+    fi
     expectedTeamID="H7V7XYVQ7D"
     blockingProcesses=( iTerm2 )
     ;;
