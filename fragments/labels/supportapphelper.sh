1fb296b677402cc135c5c9f0783ed452a2fee82e
Benjamin Kollmer <b.kollmer@apfelwerk.de>
2024-05-16 15:39:11 +0200

    Added Supportapphelper from SupportApp GitRepo

---
+++ fragments/labels/supportapphelper.sh
@@ -0,0 +1,10 @@
+supportapphelper)
+    name="SupportHelper"
+    archiveName="SupportHelper"
+    type="pkg"
+    packageID="nl.root3.support"
+    downloadURL=$(downloadURLFromGit root3nl SupportApp)
+    appNewVersion=$(versionFromGit root3nl SupportApp)
+    expectedTeamID="98LJ4XBGYK"
+    blockingProcesses=( NONE )
+    ;;
