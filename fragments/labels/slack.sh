44c2b8b3d97e4e50a7ebb41563355e02e70b48f2
alexii <a.thetiot@yogosha.com>
2024-05-21 16:06:33 +0200

    update slack downlaod url to universal

--- fragments/labels/slack.sh
+++ fragments/labels/slack.sh
@@ -1,11 +1,7 @@
 slack)
     name="Slack"
     type="dmg"
-    if [[ $(arch) == i386 ]]; then
-       downloadURL="https://slack.com/ssb/download-osx"
-    elif [[ $(arch) == arm64 ]]; then
-       downloadURL="https://slack.com/ssb/download-osx-silicon"
-    fi
+    downloadURL="https://slack.com/ssb/download-osx-universal"
     appNewVersion=$( curl -fsIL "${downloadURL}" | grep -i "^location" | cut -d "/" -f7 )
     expectedTeamID="BQR82RBBHL"
     ;;
