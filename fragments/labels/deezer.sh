7f0199bb0b263c8fbce4de8296eb009faf17bf2a
alexii <a.thetiot@yogosha.com>
2024-05-21 16:38:24 +0200

    add deezer label

---
+++ fragments/labels/deezer.sh
@@ -0,0 +1,8 @@
+deezer)
+    name="Deezer"
+    type="dmg"
+    # There is no arm/universal binary. Need rosetta2 to run it on apple silicon
+    downloadURL="https://www.deezer.com/desktop/download?platform=darwin&architecture=x64"
+    appNewVersion=""
+    expectedTeamID="7QLUP2K45C"
+    ;;
