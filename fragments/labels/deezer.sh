55c4de9df42fffbb85b3f3ad2ccb2fdfd4aee35d
alexii <a.thetiot@yogosha.com>
2024-05-21 16:53:09 +0200

    add appNewVersion

--- fragments/labels/deezer.sh
+++ fragments/labels/deezer.sh
@@ -3,6 +3,6 @@ deezer)
     type="dmg"
     # There is no arm/universal binary. Need rosetta2 to run it on apple silicon
     downloadURL="https://www.deezer.com/desktop/download?platform=darwin&architecture=x64"
-    appNewVersion=""
+    appNewVersion=$(curl -fsLI "$downloadURL" | grep -o "DeezerDesktop_[0-9]*\.[0-9]*\.[0-9]*" | cut -d'_' -f2)
     expectedTeamID="7QLUP2K45C"
     ;;
