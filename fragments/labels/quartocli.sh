6e0e44e8947593829c90e1318941480aa165df73
Scripts and Things <52809959+scriptsandthings@users.noreply.github.com>
2024-05-23 16:12:47 -0400

    Create quartocli.sh
    create label for https://github.com/quarto-dev/quarto-cli

---
+++ fragments/labels/quartocli.sh
@@ -0,0 +1,7 @@
+quartocli)
+    name="quarto-cli"
+    type="pkg"
+    downloadURL="$(downloadURLFromGit quarto-dev quarto-cli)"
+    appNewVersion="$(versionFromGit quarto-dev quarto-cli)"
+    expectedTeamID="Y9A5TA7V7H"
+    ;;
