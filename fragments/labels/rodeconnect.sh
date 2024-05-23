6933d96e8535d3d78a435fb985bf9aba14fcfea5
Theile <soren@theilgaard.dk>
2024-05-23 15:02:30 +0200

    Fixed `appNewVersion`

--- fragments/labels/rodeconnect.sh
+++ fragments/labels/rodeconnect.sh
@@ -3,6 +3,6 @@ rodeconnect)
     type="pkgInZip"
     #packageID="com.rodeconnect.installer" #Versioned wrong as 0 in 1.1.0 pkg
     downloadURL="https://update.rode.com/connect/RODE_Connect_MACOS.zip"
-    #appNewVersion=$(curl -fs https://rode.com/en/release-notes/rode-connect | xmllint --html --format - 2>/dev/null | tr '"' '\n' | grep -i -o "Version .*" | head -1 | cut -w -f2)
+    appNewVersion=$(curl -fs https://rode.com/en/release-notes/rode-connect | xmllint --html --format - 2>/dev/null | tr '"' '\n' | sed 's/\&quot\;/\n/g' | grep -i -o "Version .*" | head -1 | cut -w -f2)
     expectedTeamID="Z9T72PWTJA"
     ;;
--- fragments/labels/rodeunify.sh
+++ fragments/labels/rodeunify.sh
@@ -3,6 +3,6 @@ rodeunify)
     type="pkgInZip"
     #packageID="com.rodecentral.installer"
     downloadURL="https://update.rode.com/unify_new/macos/RODE_UNIFY_MACOS.zip"
-    #appNewVersion=$(curl -fs https://rode.com/en/release-notes/unify | xmllint --html --format - 2>/dev/null | tr '"' '\n' | grep -i -o "Version .*" | head -1 | cut -w -f2)
+    appNewVersion=$(curl -fs https://rode.com/en/release-notes/unify | xmllint --html --format - 2>/dev/null | tr '"' '\n' | sed 's/\&quot\;/\n/g' | grep -i -o "Version .*" | head -1 | cut -w -f2)
     expectedTeamID="Z9T72PWTJA"
     ;;
