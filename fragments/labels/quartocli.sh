431ce638f1139f5ef0e1cde2c4f7d729d54e0389
Scripts and Things <52809959+scriptsandthings@users.noreply.github.com>
2024-05-23 20:17:43 -0400

    Update quartocli.sh
    corrected/added "blockingProcesses"

--- fragments/labels/quartocli.sh
+++ fragments/labels/quartocli.sh
@@ -4,4 +4,5 @@ quartocli)
     downloadURL="$(downloadURLFromGit quarto-dev quarto-cli)"
     appNewVersion="$(versionFromGit quarto-dev quarto-cli)"
     expectedTeamID="Y9A5TA7V7H"
+    blockingProcesses=( quarto deno esbuild pandoc sass typst npm )
     ;;
