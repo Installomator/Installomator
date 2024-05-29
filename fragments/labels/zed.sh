84734e82fb0f609e3d81d2935ed4044847c2494f
Michael Jon <9003059+BiosPlus@users.noreply.github.com>
2024-05-29 14:02:14 +1000

    added zed.sh

---
+++ fragments/labels/zed.sh
@@ -0,0 +1,8 @@
+zed)
+    name="Zed"
+    type="dmg"
+    archiveName="Zed.dmg[0-9.]*"
+    downloadURL="$(downloadURLFromGit zed-industries zed)"
+    appNewVersion="$(versionFromGit zed-industries zed)"
+    expectedTeamID="MQ55VZLNZQ"
+    ;;
