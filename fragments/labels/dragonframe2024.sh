736545348126954322fd7b6d8eb465c8791d0c95
robotprom <98070647+robotprom@users.noreply.github.com>
2024-06-12 21:42:47 -0400

    Update dragonframe2024.sh

--- fragments/labels/dragonframe2024.sh
+++ fragments/labels/dragonframe2024.sh
@@ -6,4 +6,5 @@ dragonframe2024)
     curlOptions=( -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" )
     downloadURL="$( curl -s "https://www.dragonframe.com/downloads/" $curlOptions | tr '"' '\n' | grep -m1 "_2024.*pkg" )"
     appNewVersion="$( echo "$downloadURL" | cut -d '_' -f 2 | cut -d '.' -f 1-3 )"
+    versionKey="CFBundleShortVersionString"
     ;;
