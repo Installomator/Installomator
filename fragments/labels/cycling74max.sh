59721674029f5265277861cdd9145386e4c817e8
robotprom <98070647+robotprom@users.noreply.github.com>
2024-06-13 17:23:33 -0400

    Update cycling74max.sh

--- fragments/labels/cycling74max.sh
+++ fragments/labels/cycling74max.sh
@@ -2,7 +2,7 @@ cycling74max)
     name="Max"
     type="dmg"
     downloadURL="$( curl -s "https://cycling74.com/downloads" $curlOptions | tr '"' '\n' | grep -m1 "Max.*dmg" )"
-    appNewVersion=$(curl -fsL 'https://cycling74.com/downloads' | sed -E 's/.*-->([0-9]+\.[0-9.]*[0-9]).*/\1/g')
+    appNewVersion=$(curl -fsL 'https://cycling74.com/downloads' | sed -E 's/.*Max([12][0-9]|[1-9])([12][0-9]|[0-9])([0-9]+).*/\1.\2.\3/g')
     versionKey="CFBundleVersion"
     expectedTeamID="GBXXCFCVW5"
     ;;
