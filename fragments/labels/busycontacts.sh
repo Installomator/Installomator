2f607bd64bed98181fc48703f6623719a1260ba8
NoCokePepsi <matthew@secondsonconsulting.com>
2024-06-18 14:53:16 -0700

    busycontacts

---
+++ fragments/labels/busycontacts.sh
@@ -0,0 +1,8 @@
+busycontacts)
+    name="BusyContacts"
+    type="dmg"
+    downloadURL="https://www.busymac.com/download/BusyContacts.dmg"
+    expectedTeamID="N4RA379GBW"
+    appName="BusyContacts.app"
+    blockingProcesses=( "BusyContacts" )
+    ;;
