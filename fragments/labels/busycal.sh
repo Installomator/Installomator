990c0bb72fac4c7a5f1159c4fd85e357f5416b6e
NoCokePepsi <matthew@secondsonconsulting.com>
2024-06-18 15:12:27 -0700

    Update busycal.sh

--- fragments/labels/busycal.sh
+++ fragments/labels/busycal.sh
@@ -1,7 +1,7 @@
 busycal)
     name="BusyCal"
     type="dmg"
-    downloadURL="https://www.busymac.com/download/BusyCal.zip"
+    downloadURL="https://www.busymac.com/download/BusyCal.dmg"
     expectedTeamID="N4RA379GBW"
     appName="BusyCal.app"
     blockingProcesses=( "BusyCal" )
