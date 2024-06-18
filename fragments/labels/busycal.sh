219a17c685b25d93a7dd4e783f692daa6707f3eb
NoCokePepsi <matthew@secondsonconsulting.com>
2024-06-18 14:40:04 -0700

    Create busycal.sh

---
+++ fragments/labels/busycal.sh
@@ -0,0 +1,8 @@
+busycal)
+    name="BusyCal"
+    type="dmg"
+    downloadURL="https://www.busymac.com/download/BusyCal.zip"
+    expectedTeamID="N4RA379GBW"
+    appName="BusyCal.app"
+    blockingProcesses=( "BusyCal" )
+    ;;
