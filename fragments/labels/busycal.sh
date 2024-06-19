db8484e99d70b86be964fd53b33da5f5c83f88cd
NoCokePepsi <matthew@secondsonconsulting.com>
2024-06-18 17:01:50 -0700

    App new version for cts and cal

--- fragments/labels/busycal.sh
+++ fragments/labels/busycal.sh
@@ -2,7 +2,9 @@ busycal)
     name="BusyCal"
     type="dmg"
     downloadURL="https://www.busymac.com/download/BusyCal.dmg"
+    appNewVersion=$( curl -ILs "https://www.busymac.com/download/BusyCal.dmg" | grep -m 1 -i '^location' | sed 's/.*bcl-//' | sed 's/.dmg//' )
     expectedTeamID="N4RA379GBW"
     appName="BusyCal.app"
     blockingProcesses=( "BusyCal" )
     ;;
+    
