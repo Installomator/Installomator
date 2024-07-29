79362ddb881db1276ea86255a4071b99042ad000
Gerard Spierenburg <42139030+gspierenburg-sbp@users.noreply.github.com>
2024-07-29 10:30:55 +0200

    Update yubikeymanagerqt.sh

--- fragments/labels/yubikeymanagerqt.sh
+++ fragments/labels/yubikeymanagerqt.sh
@@ -1,5 +1,5 @@
 yubikeymanagerqt)
-    name="YubiKey Manager GUI"
+    name="YubiKey Manager"
     type="pkg"
     appNewVersion=$(curl -fs https://developers.yubico.com/yubikey-manager-qt/Releases/ | grep mac.pkg | head -1 | sed -E "s/.*-([0-9.]*)-mac.*/\1/")
     downloadURL="https://developers.yubico.com/yubikey-manager-qt/Releases/yubikey-manager-qt-$appNewVersion-mac.pkg"
