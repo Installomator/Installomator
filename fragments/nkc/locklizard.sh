9a014b9629f76e6b2e84e03ff88bc85410875968
brentac <31676385+brentac@users.noreply.github.com>
2024-07-29 11:57:27 -0500

    Create locklizard.sh

---
+++ fragments/nkc/locklizard.sh
@@ -0,0 +1,9 @@
+locklizard)
+	# NKC Change
+    name="locklizard"
+    type="pkgInZip"
+    packageID="com.locklizard.pkg.LockLizardSafeguardViewer"
+    downloadURL="https://downloads.locklizard.com/PDCViewerSetupOSX.zip?v2583"
+    appNewVersion=""
+    expectedTeamID="KSB4G4V9FZ"
+	;;
