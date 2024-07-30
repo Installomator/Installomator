a73abaf92e76e6c4b43776cdaac45f7ec194f690
Sean Christians <sean.christians@vention.cc>
2024-07-30 10:32:01 -0400

    Add label for zenangst/Gray

---
+++ fragments/labels/gray.sh
@@ -0,0 +1,7 @@
+gray)
+    name="Gray"
+    type="zip"
+    downloadURL=$(downloadURLFromGit zenangst Gray)
+    appNewVersion=$(versionFromGit zenangst Gray)
+    expectedTeamID=E23HUP39A3
+    archiveName=Gray.zip
