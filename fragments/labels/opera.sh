68f7ae79dfec753624c1eb92359ce09f9c799224
kbjoin <kyle.bjoin@gmail.com>
2024-08-01 14:26:39 -0500

    Updating Opera Fragment to use new method for detecting installURL

--- fragments/labels/opera.sh
+++ fragments/labels/opera.sh
@@ -1,8 +1,10 @@
 opera)
     name="Opera"
     type="dmg"
-    downloadURL="$(curl -fsIL "$(curl -fs "$(curl -fsL "https://download.opera.com/download/get/?partner=www&opsys=MacOS" | tr '"' "\n" | grep -e "www.opera.com.*thanks.*opera" | sed 's/\&amp\;/\&/g')" | tr '"' "\n" | grep "download.opera.com" | sed 's/\&amp\;/\&/g')" | grep -i "^location" | grep -io "https.*dmg")"
-    appNewVersion="$(printf "$downloadURL" | sed -E 's/https.*\/([0-9.]*)\/mac\/.*/\1/')"
+	latest_ver=$(curl -fs https://get.geo.opera.com/pub/opera/desktop/ | grep -Eo 'href="[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/' | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -n 1)
+	dmg_name=$(curl -fs https://get.geo.opera.com/pub/opera/desktop/$latest_ver/mac/ | grep -Eo 'href="[^"]+_Setup\.dmg"' | sed 's/href="//' | sed 's/"$//')
+	downloadURL="https://get.geo.opera.com/pub/opera/desktop/$latest_ver/mac/$dmg_name"
+	appNewVersion="$(printf "$downloadURL" | sed -E 's/https.*\/([0-9.]*)\/mac\/.*/\1/')"
 	versionKey="CFBundleVersion"
     expectedTeamID="A2P9LX4JPN"
     ;;
