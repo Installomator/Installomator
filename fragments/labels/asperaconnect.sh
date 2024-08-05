7bdc121379377a6d3085d810391b75f067528bcd
Fredrik Larsson <33235315+kryptonit1@users.noreply.github.com>
2024-08-05 14:48:39 +0200

    Update asperaconnect.sh
    The team ID has been changed from Aspera, Inc. (RJ747GSBCT) to International  Business Machines Corp (PETKK2G752).

--- fragments/labels/asperaconnect.sh
+++ fragments/labels/asperaconnect.sh
@@ -3,5 +3,5 @@ asperaconnect)
     type="pkg"
     downloadURL="https://d3gcli72yxqn2z.cloudfront.net/downloads/connect/latest/bin/$(curl -fs 'https://www.ibm.com/support/fixcentral/swg/selectFixes?parent=ibm~Other%20software&product=ibm/Other+software/IBM+Aspera+Connect' --data-raw 'showStatus=false' | egrep -o "ibm-aspera-connect_[0-9.]+_macOS" | head -n1)_x86_64.pkg"
     appNewVersion=$(echo "${downloadURL}" | sed -E 's/.*ibm-aspera-connect_([0-9]+(\.[0-9]+)*)_macOS.*\.pkg/\1/')
-    expectedTeamID="RJ747GSBCT"
+    expectedTeamID="PETKK2G752"
     ;;
