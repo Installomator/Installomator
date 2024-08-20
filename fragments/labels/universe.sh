7c4cea240098535e8b1aa388a1e999e011f4e550
Alexander Duffner <duffner@posteo.de>
2024-08-20 10:31:45 +0200

    Update universe.sh
    changed to dmg, since maxon changed the package format

--- fragments/labels/universe.sh
+++ fragments/labels/universe.sh
@@ -1,6 +1,6 @@
 universe)
     name="Universe"
-    type="zip"
+    type="dmg"
     appCustomVersion(){
       ls "/Users/Shared/Red Giant/uninstall" | grep universe | grep -Eo "([0-9][0-9][0-9][0-9]\.[0-9]+(\.[0-9])?)" | head -n 30 | sort -gru
     }
@@ -8,13 +8,13 @@ universe)
     if [[ "$appNewVersion" =~ ^[^.]*\.[^.]*$ ]]; then
 	    appNewVersion=$(sed 's/\([0-9]*\.[0-9]*\)/\1.0/' <<<"$appNewVersion")
     fi
-    downloadURL="https://mx-app-blob-prod.maxon.net/mx-package-production/installer/macos/redgiant/universe/releases/${appNewVersion}/Universe-${appNewVersion}_Mac.zip"
+    downloadURL="https://mx-app-blob-prod.maxon.net/mx-package-production/installer/macos/redgiant/universe/releases/${appNewVersion}/Universe-${appNewVersion}_Mac.dmg"
     universeResponse=$(curl -s -I -L "$downloadURL")
     universeHttpStatus=$(echo "$universeResponse" | head -n 1 | cut -d ' ' -f 2)
     if [[ "universeHttpStatus" == "200" ]]; then
       printlog "DownloadURL HTTP status code: $universeHttpStatus" INFO
     elif [[ "$universeHttpStatus" == "404" ]]; then
-      downloadURL="https://mx-app-blob-prod.maxon.net/mx-package-production/installer/macos/redgiant/universe/releases/${appNewVersion}/Universe-${appNewVersion}_mac.zip"
+      downloadURL="https://mx-app-blob-prod.maxon.net/mx-package-production/installer/macos/redgiant/universe/releases/${appNewVersion}/Universe-${appNewVersion}_mac.dmg"
       printlog "Had to change DownloadURL due HTTP Status." INFO
     else
       printlog "Unexpected HTTP status code: $universeHttpStatus" ERROR
