9b86197b7f909924021b637845d7b5249382d758
Patrick Roumanoff <patrick@roumanoff.com>
2024-08-22 14:50:51 +1000

    Update pgadmin4.sh to support arm64 arch

--- fragments/labels/pgadmin4.sh
+++ fragments/labels/pgadmin4.sh
@@ -1,8 +1,13 @@
 pgadmin4)
     name="pgAdmin 4"
     type="dmg"
-    downloadParent="https://www.postgresql.org/ftp/pgadmin/pgadmin4/"
-    appNewVersion=$(curl -fs "${downloadParent}" | grep -oE 'v[0-9]+.[0-9]+' | sort -V | tail -n 1 | sed 's/v//g')
-    downloadURL="https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v$appNewVersion/macos/pgadmin4-$appNewVersion-x86_64.dmg"
+    downloadParent="https://ftp.postgresql.org/pub/pgadmin/pgadmin4/"
+    appNewVersion=$(curl -fs "${downloadParent}" | grep -oE 'v\d+\.\d+' | sort --version-sort | tail -n 1 | sed 's/v//g')
+    if [[ $(arch) == "arm64" ]]; then
+        archLabel="arm64"
+    elif [[ $(arch) == "i386" ]]; then
+        archLabel="x86_64"
+    fi
+    downloadURL="${downloadParent}v${appNewVersion}/macos/pgadmin4-${appNewVersion}-${archLabel}.dmg"
     expectedTeamID="TCHGL2R7C5"
     ;;
