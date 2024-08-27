3538e01abe448c3f2e359990ec1d6f941c9201c7
Gerard Spierenburg <42139030+gspierenburg-sbp@users.noreply.github.com>
2024-08-27 13:42:09 +0200

    Update displaylinkmanager.sh
    Updated the $downloadURL

--- fragments/labels/displaylinkmanager.sh
+++ fragments/labels/displaylinkmanager.sh
@@ -2,7 +2,7 @@ displaylinkmanager)
     name="DisplayLink Manager"
     type="pkg"
     packageID="com.displaylink.displaylinkmanagerapp"
-    downloadURL=https://www.synaptics.com$(curl -fLs "https://www.synaptics.com$(curl -fLs https://www.synaptics.com/products/displaylink-graphics/downloads/macos | xmllint --html --format - 2>/dev/null | grep -oE '"/node/.+?"' | head -n1 | tr -d '"')" | xmllint --html --format - 2>/dev/null | grep -oE "/.+\.pkg")
+    downloadURL=https://www.synaptics.com$(redirect=$(curl -sfL https://www.synaptics.com/products/displaylink-graphics/downloads/macos | grep -m 1 'class="download-link">Download' | sed 's/.*href="//' | sed 's/".*//') && curl -sfL "https://www.synaptics.com$redirect" | grep 'class="no-link"' | awk -F 'href="' '{print $2}' | awk -F '"' '{print $1}')
     appNewVersion=$(echo "${downloadURL}" | grep -Eo '[0-9]\.[0-9]+(\.[0-9])?')
     expectedTeamID="73YQY62QM3"
     ;;
