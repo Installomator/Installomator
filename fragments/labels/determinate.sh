fa88ce7ae8548600ad953ef59f82b1f73dc6eb31
Graham Christensen <graham@grahamc.com>
2024-08-27 12:44:45 -0400

    Fix a TOCTOU issue with determinate

--- fragments/labels/determinate.sh
+++ fragments/labels/determinate.sh
@@ -2,7 +2,10 @@ determinate)
     name="Determinate"
     type="pkg"
     packageID="systems.determinate.Determinate"
-    downloadURL="https://install.determinate.systems/determinate-pkg/stable/Universal"
-    appNewVersion=$(curl -sfI "${downloadURL}"  | awk -F'/' '/location: /{print $4}')
+    # Note: https://install.determinate.systems/determinate-pkg/stable/Universal may return different URLs if a deploy is in progress.
+    # Fetching https://install.determinate.systems/determinate-pkg/stable/Universal and capturing the redirect target ensures the
+    # version printed is the version that will actually be installed.
+    downloadUrl=$(curl -sfI https://install.determinate.systems/determinate-pkg/stable/Universal  | awk -F' ' '/location: /{print $2}')
+    appNewVersion=$(echo "$downloadUrl" | cut -d/ -f4)
     expectedTeamID="X3JQ4VPJZ6"
     ;;
