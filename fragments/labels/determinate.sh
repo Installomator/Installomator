9e3971a63b1eaa57014c72d3f5a3bfa41c171894
Graham Christensen <graham@grahamc.com>
2024-08-27 12:48:53 -0400

    Switch to the effective URL

--- fragments/labels/determinate.sh
+++ fragments/labels/determinate.sh
@@ -5,7 +5,7 @@ determinate)
     # Note: https://install.determinate.systems/determinate-pkg/stable/Universal may return different URLs if a deploy is in progress.
     # Fetching https://install.determinate.systems/determinate-pkg/stable/Universal and capturing the redirect target ensures the
     # version printed is the version that will actually be installed.
-    downloadUrl=$(curl -sfI https://install.determinate.systems/determinate-pkg/stable/Universal  | awk -F' ' '/location: /{print $2}')
+    downloadUrl=$(curl -w "%{url_effective}\n" -I -L -s -S https://install.determinate.systems/determinate-pkg/stable/Universal -o /dev/null)
     appNewVersion=$(echo "$downloadUrl" | cut -d/ -f4)
     expectedTeamID="X3JQ4VPJZ6"
     ;;
