96d28bcfbf68fd3d3e17e4f52d2d0e66cf13e506
Gil Burns <gil@gilburns.com>
2024-09-02 15:06:22 -0500

    Create Cherry Audio DCO-106 label

---
+++ fragments/labels/cherryaudiodco106.sh
@@ -0,0 +1,9 @@
+cherryaudiodco106)
+    name="DCO-106"
+    type="pkg"
+    packageID="com.cherryaudio.pkg.DCO106Package-StandAlone"
+    blockingProcesses=( "$name" )
+    appNewVersion="$(curl -fs https://cherryaudio.com/products/dco-106/version-history | grep -A 6 "info" | grep -Eo "([0-9]+(\.[0-9]+)+)" | head -1 | xargs)"
+    downloadURL="https://store.cherryaudio.com/downloads/dco-106-macos-installer?file=DCO-106-Installer-macOS.pkg"
+    expectedTeamID="A2XFV22B2X"
+    ;;
