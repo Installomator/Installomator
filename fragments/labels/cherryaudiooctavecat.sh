bcf327ab32b4ae87d8f13650b8a61b1c0f6ec5a7
Gil Burns <gil@gilburns.com>
2024-09-02 15:28:24 -0500

    Create Cherry Audio Octave Cat label

---
+++ fragments/labels/cherryaudiooctavecat.sh
@@ -0,0 +1,9 @@
+cherryaudiooctavecat)
+    name="Octave Cat"
+    type="pkg"
+    packageID="com.cherryaudio.pkg.OctaveCatPackage-StandAlone"
+    blockingProcesses=( "$name" )
+    appNewVersion="$(curl -fs https://cherryaudio.com/products/octave-cat/version-history | grep -A 6 "info" | grep -Eo "([0-9]+(\.[0-9]+)+)" | head -1 | xargs)"
+    downloadURL="https://store.cherryaudio.com/downloads/octave-cat-macos-installer?file=Octave-Cat-Installer-macOS.pkg"
+    expectedTeamID="A2XFV22B2X"
+    ;;
