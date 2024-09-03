f10286a02d82819f9be1ebfcf7a37f14c961c914
Gil Burns <gil@gilburns.com>
2024-09-03 18:56:35 -0500

    Update cherryaudioca2600.sh

--- fragments/labels/cherryaudioca2600.sh
+++ fragments/labels/cherryaudioca2600.sh
@@ -2,7 +2,6 @@ cherryaudioca2600)
     name="CA2600"
     type="pkg"
     packageID="com.cherryaudio.pkg.CA2600Package-StandAlone"
-    blockingProcesses=( "$name" )
     appNewVersion="$(curl -fs https://cherryaudio.com/products/ca2600/version-history | grep -A 6 "info" | grep -Eo "([0-9]+(\.[0-9]+)+)" | head -1 | xargs)"
     downloadURL="https://store.cherryaudio.com/downloads/ca2600-macos-installer?file=CA2600-Installer-macOS.pkg"
     expectedTeamID="A2XFV22B2X"
