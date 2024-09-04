0c04bfe4d7bb901496eccd7b70629978f92a7b41
Gil Burns <gil@gilburns.com>
2024-09-03 19:00:45 -0500

    Update cherryaudiomemorymode.sh

--- fragments/labels/cherryaudiomemorymode.sh
+++ fragments/labels/cherryaudiomemorymode.sh
@@ -2,7 +2,6 @@ cherryaudiomemorymode)
     name="Memorymode"
     type="pkg"
     packageID="com.cherryaudio.pkg.MemorymodePackage-StandAlone"
-    blockingProcesses=( "$name" )
     appNewVersion="$(curl -fs https://cherryaudio.com/products/memorymode/version-history | grep -A 6 "info" | grep -Eo "([0-9]+(\.[0-9]+)+)" | head -1 | xargs)"
     downloadURL="https://store.cherryaudio.com/downloads/memorymode-macos-installer?file=Memorymode-Installer-macOS.pkg"
     expectedTeamID="A2XFV22B2X"
