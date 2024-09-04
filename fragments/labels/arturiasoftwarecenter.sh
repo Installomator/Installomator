3cad49f33ffbc7bfe6aa72245785d7188e58d93b
Gil Burns <gil@gilburns.com>
2024-09-03 22:59:06 -0500

    Update arturiasoftwarecenter.sh

--- fragments/labels/arturiasoftwarecenter.sh
+++ fragments/labels/arturiasoftwarecenter.sh
@@ -2,8 +2,10 @@ arturiasoftwarecenter)
     name="Arturia Software Center"
     type="pkg"
     packageID="com.Arturia.ArturiaSoftwareCenter.resources"
- 	appNewVersion=$(getJSONValue "$(curl -fsL 'https://www.arturia.com/api/resources?slugs=asc&types=soft')" '[0].version')
-	downloadVersion=$(echo "${appNewVersion}" | tr '.' '_')
+    versionKey="CFBundleVersion"
+ 	onlineVersion=$(getJSONValue "$(curl -fsL 'https://www.arturia.com/api/resources?slugs=asc&types=soft')" '[0].version')
+	downloadVersion=$(echo "${onlineVersion}" | tr '.' '_')
+ 	appNewVersion=$(echo "${onlineVersion}" | cut -d. -f1-3)
 	downloadURL="https://dl.arturia.net/products/asc/soft/Arturia_Software_Center__${downloadVersion}.pkg"
     expectedTeamID="T53ZHSF36C"
     ;;
