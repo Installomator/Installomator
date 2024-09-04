e0422f69bcba2ad9983d2314dcdc0203f1d16517
Gil Burns <gil@gilburns.com>
2024-09-03 19:04:55 -0500

    Update cherryaudiops3300.sh

--- fragments/labels/cherryaudiops3300.sh
+++ fragments/labels/cherryaudiops3300.sh
@@ -2,7 +2,6 @@ cherryaudiops3300)
     name="PS-3300"
     type="pkg"
     packageID="com.cherryaudio.pkg.PS-3300Package-StandAlone"
-    blockingProcesses=( "$name" )
     appNewVersion="$(curl -fs https://cherryaudio.com/products/ps-3300/version-history | grep -A 6 "info" | grep -Eo "([0-9]+(\.[0-9]+)+)" | head -1 | xargs)"
     downloadURL="https://store.cherryaudio.com/downloads/ps-3300-macos-installer?file=PS-3300-Installer-macOS.pkg"
     expectedTeamID="A2XFV22B2X"
