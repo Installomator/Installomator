34c9cc2dd79011f5b740229db946bcea54a68d05
Gil Burns <gil@gilburns.com>
2024-09-03 19:02:12 -0500

    Update cherryaudiomoduledesigner.sh

--- fragments/labels/cherryaudiomoduledesigner.sh
+++ fragments/labels/cherryaudiomoduledesigner.sh
@@ -2,7 +2,6 @@ cherryaudiomoduledesigner)
     name="Voltage Module Designer"
     type="pkg"
     packageID="com.cherryaudio.pkg.VoltageModuleDesigner"
-    blockingProcesses=( "$name" )
     appNewVersion="$(curl -fs https://cherryaudio.com/products/voltage-module-designer/version-history | grep -A 6 "info" | grep -Eo "([0-9]+(\.[0-9]+)+)" | head -1 | xargs)"
     downloadURL="https://store.cherryaudio.com/module-designer-mac/download?file=Voltage-Module-Designer-Installer-macOS.pkg"
     expectedTeamID="A2XFV22B2X"
