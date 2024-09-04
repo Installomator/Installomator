11a0b48e700d2db1086e6fdd8d0e3c785ef88d54
Gil Burns <gil@gilburns.com>
2024-09-03 19:09:25 -0500

    Update inmusicsoftwarecenter.sh

--- fragments/labels/inmusicsoftwarecenter.sh
+++ fragments/labels/inmusicsoftwarecenter.sh
@@ -1,7 +1,6 @@
 inmusicsoftwarecenter)
     name="inMusic Software Center"
     type="zip"
-    blockingProcesses=( "$name" )
     appNewVersion="$(curl -fs https://imb-software-center.s3.amazonaws.com/release/darwin/version.json | grep currentRelease | cut -d: -f2 | cut -d\" -f2 | xargs)"
     downloadURL="https://inmusic.to/ISCMAC"
     expectedTeamID="NDEA34327T"
