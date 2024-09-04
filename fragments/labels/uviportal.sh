6da23d7fb60a33010ac7ca8b6d1482c1822ca5c7
Gil Burns <gil@gilburns.com>
2024-09-03 21:59:39 -0500

    Update uviportal.sh

--- fragments/labels/uviportal.sh
+++ fragments/labels/uviportal.sh
@@ -2,7 +2,6 @@ uviportal)
     name="UVI Portal"
     type="pkgInDmg"
     packageID="net.uvi.pkg.uviportal"
-    blockingProcesses=( "$name" )
     appNewVersion="$(curl -s -i "https://www.uvi.net/dl-portal.php?p=mac" | grep "location" | cut -d\- -f2 | xargs)"
     downloadURL="https://www.uvi.net/dl-portal.php?p=mac"
     expectedTeamID="BB6L4C84AT"
