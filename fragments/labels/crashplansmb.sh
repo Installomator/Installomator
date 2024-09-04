7e267d16c5c3e8bffe42af660bc7241916beac9b
Andrew Orrison <128734817+orrison-CrashPlan@users.noreply.github.com>
2024-09-04 10:11:59 -0500

    Update crashplansmb.sh
    SMB specific download URLs were removed. Updating to point to the default latest application.

--- fragments/labels/crashplansmb.sh
+++ fragments/labels/crashplansmb.sh
@@ -2,8 +2,8 @@ crashplansmb)
     name="CrashPlan"
     type="pkgInDmg"
     pkgName="Install Crashplan.pkg"
-    downloadURL="https://download.crashplan.com/installs/agent/latest-smb-mac.dmg"
-    appNewVersion=$( curl https://download.crashplan.com/installs/agent/latest-smb-mac.dmg  -s -L -I -o /dev/null -w '%{url_effective}' | cut -d "/" -f7 )
+    downloadURL="https://download.crashplan.com/installs/agent/latest-mac.dmg"
+    appNewVersion=$( curl https://download.crashplan.com/installs/agent/latest-mac.dmg  -s -L -I -o /dev/null -w '%{url_effective}' | cut -d "/" -f7 )
     expectedTeamID="UGHXR79U6M"
     blockingProcesses=( NONE )
     ;;
