9c33475e1624e77729ce1d0c0e615ee5114f755e
Gil Burns <gil@gilburns.com>
2024-09-03 22:52:18 -0500

    Create Arturia Software Center label

---
+++ fragments/labels/arturiasoftwarecenter.sh
@@ -0,0 +1,9 @@
+arturiasoftwarecenter)
+    name="Arturia Software Center"
+    type="pkg"
+    packageID="com.Arturia.ArturiaSoftwareCenter.resources"
+ 	appNewVersion=$(getJSONValue "$(curl -fsL 'https://www.arturia.com/api/resources?slugs=asc&types=soft')" '[0].version')
+	downloadVersion=$(echo "${appNewVersion}" | tr '.' '_')
+	downloadURL="https://dl.arturia.net/products/asc/soft/Arturia_Software_Center__${downloadVersion}.pkg"
+    expectedTeamID="T53ZHSF36C"
+    ;;
