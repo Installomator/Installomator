aea4588f1978dbb01d43e2092c455dbecb31c03b
Gil Burns <gil@gilburns.com>
2024-09-03 19:10:22 -0500

    Update softubecentral.sh

--- fragments/labels/softubecentral.sh
+++ fragments/labels/softubecentral.sh
@@ -2,7 +2,6 @@ softubecentral)
     name="Softube Central"
     type="pkg"
     packageID="org.softube.com.softubecentral"
-    blockingProcesses=( "$name" )
     appNewVersion="$(curl -fs "https://www.softube.com/installers" | grep "mac-version=" | cut -d\" -f2 | xargs)"
     downloadURL="https://softubestorage.b-cdn.net/softubecentraldata/softubecentral/Softube%20Central-${appNewVersion}-universal.pkg"
     expectedTeamID="MQ5XL2PNWK"
