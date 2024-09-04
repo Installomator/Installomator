033315b3d65387c52dae9d99b20da4e62a35da47
Gil Burns <gil@gilburns.com>
2024-09-03 19:06:09 -0500

    Update cherryaudiosolovox.sh

--- fragments/labels/cherryaudiosolovox.sh
+++ fragments/labels/cherryaudiosolovox.sh
@@ -2,7 +2,6 @@ cherryaudiosolovox)
     name="Solovox"
     type="pkg"
     packageID="com.cherryaudio.pkg.SolovoxPackage-StandAlone"
-    blockingProcesses=( "$name" )
     appNewVersion="$(curl -fs curl -fs https://cherryaudio.com/products/novachord-solovox/version-history | grep -E -o ".<h4>Solovox</h4>.{0,40}" | grep -Eo "([0-9]+(\.[0-9]+)+)" | head -1 | xargs)"
     downloadURL="https://store.cherryaudio.com/downloads/solovox-macos-installer?file=Solovox-Installer-macOS.pkg"
     expectedTeamID="A2XFV22B2X"
