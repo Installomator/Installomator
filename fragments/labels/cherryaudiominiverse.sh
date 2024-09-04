1b14267d91da9e0db84f1d25dccbc850fcd29759
Gil Burns <gil@gilburns.com>
2024-09-03 19:01:49 -0500

    Update cherryaudiominiverse.sh

--- fragments/labels/cherryaudiominiverse.sh
+++ fragments/labels/cherryaudiominiverse.sh
@@ -2,7 +2,6 @@ cherryaudiominiverse)
     name="Miniverse"
     type="pkg"
     packageID="com.cherryaudio.pkg.MiniversePackage-StandAlone"
-    blockingProcesses=( "$name" )
     appNewVersion="$(curl -fs https://cherryaudio.com/products/miniverse/version-history | grep -A 6 "info" | grep -Eo "([0-9]+(\.[0-9]+)+)" | head -1 | xargs)"
     downloadURL="https://store.cherryaudio.com/downloads/miniverse-macos-installer?file=Miniverse-Installer-macOS.pkg"
     expectedTeamID="A2XFV22B2X"
