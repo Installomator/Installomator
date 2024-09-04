672c09565998a3c7c09a2b57b76a0196a594b1dc
Gil Burns <gil@gilburns.com>
2024-09-03 19:03:44 -0500

    Update cherryaudiopolymode.sh

--- fragments/labels/cherryaudiopolymode.sh
+++ fragments/labels/cherryaudiopolymode.sh
@@ -2,7 +2,6 @@ cherryaudiopolymode)
     name="Polymode Synthesizer"
     type="pkg"
     packageID="com.cherryaudio.pkg.PolymodePackage-StandAlone"
-    blockingProcesses=( "$name" )
     appNewVersion="$(curl -fs https://cherryaudio.com/products/polymode-synthesizer/version-history | grep -A 6 "info" | grep -Eo "([0-9]+(\.[0-9]+)+)" | head -1 | xargs)"
     downloadURL="https://store.cherryaudio.com/downloads/polymode-synthesizer-macos-installer?file=Polymode-Synthesizer-Installer-macOS.pkg"
     expectedTeamID="A2XFV22B2X"
