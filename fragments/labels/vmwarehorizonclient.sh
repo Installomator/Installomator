cc111fb11ddb2120861208097b5f4d3c7a749402
Gil Burns <gil@gilburns.com>
2024-09-05 21:12:00 -0500

    Update vmwarehorizonclient.sh

--- fragments/labels/vmwarehorizonclient.sh
+++ fragments/labels/vmwarehorizonclient.sh
@@ -1,9 +1,8 @@
 vmwarehorizonclient)
     name="VMware Horizon Client"
     type="pkgInDmg"
-    downloadGroup=$(curl -fsL "https://my.vmware.com/channel/public/api/v1.0/products/getRelatedDLGList?locale=en_US&category=desktop_end_user_computing&product=vmware_horizon_clients&version=horizon_8&dlgType=PRODUCT_BINARY" | grep -o '[^"]*_MAC_[^"]*')
-    fileName=$(curl -fsL "https://my.vmware.com/channel/public/api/v1.0/dlg/details?locale=en_US&category=desktop_end_user_computing&product=vmware_horizon_clients&dlgType=PRODUCT_BINARY&downloadGroup=${downloadGroup}" | grep -o '"fileName":"[^"]*"' | cut -d: -f2 | sed 's/"//g')
-    downloadURL="https://download3.vmware.com/software/$downloadGroup/${fileName}"
-    appNewVersion=$(curl -fsL "https://my.vmware.com/channel/public/api/v1.0/dlg/details?locale=en_US&downloadGroup=${downloadGroup}" | grep -o '[^"]*\.dmg[^"]*' | sed 's/.*-\(.*\)-.*/\1/')
+	downloadFiles=$(getJSONValue "$(curl -fsL 'https://customerconnect.omnissa.com/channel/public/api/v1.0/dlg/details?locale=en_US&downloadGroup=CART25FQ2_MAC_2406&productId=1027&rPId=118763' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.6 Safari/605.1.15')" downloadFiles)
+    downloadURL=$(echo $downloadFiles | grep thirdPartyDownloadUrl | cut -d\" -f4 | xargs)
+    appNewVersion=$(echo $downloadFiles | grep fileName | cut -d- -f5 | xargs)
     expectedTeamID="EG7KH642X6"
-    ;;
+    ;;``
