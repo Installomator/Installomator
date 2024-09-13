2839b1db7f79ed8f4ef84895d4906b5218d0014a
RIchard Glaser <richard.glaser@utah.edu>
2024-09-13 15:37:14 -0600

    new-codeedit
    output
    ```
    # sudo Installomator/utils/assemble.sh codeedit DEBUG=0
    2024-09-13 15:36:29 : REQ   : codeedit : ################## Start Installomator v. 10.7beta, date 2024-09-13
    2024-09-13 15:36:29 : INFO  : codeedit : ################## Version: 10.7beta
    2024-09-13 15:36:29 : INFO  : codeedit : ################## Date: 2024-09-13
    2024-09-13 15:36:29 : INFO  : codeedit : ################## codeedit
    2024-09-13 15:36:29 : DEBUG : codeedit : DEBUG mode 1 enabled.
    2024-09-13 15:36:30 : INFO  : codeedit : setting variable from argument DEBUG=0
    2024-09-13 15:36:30 : DEBUG : codeedit : name=CodeEdit
    2024-09-13 15:36:31 : DEBUG : codeedit : appName=
    2024-09-13 15:36:31 : DEBUG : codeedit : type=dmg
    2024-09-13 15:36:31 : DEBUG : codeedit : archiveName=
    2024-09-13 15:36:31 : DEBUG : codeedit : downloadURL=https://github.com/CodeEditApp/CodeEdit/releases/download/v0.3.1/CodeEdit.dmg
    2024-09-13 15:36:31 : DEBUG : codeedit : curlOptions=
    2024-09-13 15:36:31 : DEBUG : codeedit : appNewVersion=0.3.1
    2024-09-13 15:36:31 : DEBUG : codeedit : appCustomVersion function: Not defined
    2024-09-13 15:36:31 : DEBUG : codeedit : versionKey=CFBundleShortVersionString
    2024-09-13 15:36:31 : DEBUG : codeedit : packageID=
    2024-09-13 15:36:31 : DEBUG : codeedit : pkgName=
    2024-09-13 15:36:31 : DEBUG : codeedit : choiceChangesXML=
    2024-09-13 15:36:31 : DEBUG : codeedit : expectedTeamID=9VS2VKC5LQ
    2024-09-13 15:36:31 : DEBUG : codeedit : blockingProcesses=
    2024-09-13 15:36:31 : DEBUG : codeedit : installerTool=
    2024-09-13 15:36:31 : DEBUG : codeedit : CLIInstaller=
    2024-09-13 15:36:31 : DEBUG : codeedit : CLIArguments=
    2024-09-13 15:36:31 : DEBUG : codeedit : updateTool=
    2024-09-13 15:36:31 : DEBUG : codeedit : updateToolArguments=
    2024-09-13 15:36:31 : DEBUG : codeedit : updateToolRunAsCurrentUser=
    2024-09-13 15:36:31 : INFO  : codeedit : BLOCKING_PROCESS_ACTION=tell_user
    2024-09-13 15:36:31 : INFO  : codeedit : NOTIFY=success
    2024-09-13 15:36:31 : INFO  : codeedit : LOGGING=DEBUG
    2024-09-13 15:36:31 : INFO  : codeedit : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2024-09-13 15:36:31 : INFO  : codeedit : Label type: dmg
    2024-09-13 15:36:31 : INFO  : codeedit : archiveName: CodeEdit.dmg
    2024-09-13 15:36:31 : INFO  : codeedit : no blocking processes defined, using CodeEdit as default
    2024-09-13 15:36:31 : DEBUG : codeedit : Changing directory to /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.qNHhTHvaNi
    2024-09-13 15:36:31 : INFO  : codeedit : name: CodeEdit, appName: CodeEdit.app
    2024-09-13 15:36:31.395 mdfind[16953:50468840] [UserQueryParser] Loading keywords and predicates for locale "en_US"
    2024-09-13 15:36:31.395 mdfind[16953:50468840] [UserQueryParser] Loading keywords and predicates for locale "en"
    2024-09-13 15:36:31.518 mdfind[16953:50468840] Couldn't determine the mapping between prefab keywords and predicates.
    2024-09-13 15:36:31 : INFO  : codeedit : App(s) found: /Applications/Programming/CodeEdit.app
    2024-09-13 15:36:31 : INFO  : codeedit : found app at /Applications/Programming/CodeEdit.app, version 0.3.1, on versionKey CFBundleShortVersionString
    2024-09-13 15:36:31 : INFO  : codeedit : appversion: 0.3.1
    2024-09-13 15:36:31 : INFO  : codeedit : Latest version of CodeEdit is 0.3.1
    2024-09-13 15:36:31 : INFO  : codeedit : There is no newer version available.
    2024-09-13 15:36:31 : DEBUG : codeedit : Deleting /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.qNHhTHvaNi
    2024-09-13 15:36:31 : DEBUG : codeedit : Debugging enabled, Deleting tmpDir output was:
    /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.qNHhTHvaNi
    2024-09-13 15:36:31 : INFO  : codeedit : Installomator did not close any apps, so no need to reopen any apps.
    2024-09-13 15:36:31 : REQ   : codeedit : No newer version.
    2024-09-13 15:36:31 : REQ   : codeedit : ################## End Installomator, exit code 0
    
    ```

---
+++ fragments/labels/codeedit.sh
@@ -0,0 +1,8 @@
+codeedit)
+	# An is an open-source, free, and macOS-native code editor that aims to provide a lightweight and powerful development experience
+    name="CodeEdit"
+    type="dmg"
+    downloadURL="$(downloadURLFromGit CodeEditApp CodeEdit)"
+    appNewVersion="$(versionFromGit CodeEditApp CodeEdit)"
+    expectedTeamID="9VS2VKC5LQ"
+    ;;
