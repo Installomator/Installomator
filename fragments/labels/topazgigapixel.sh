3627d3bdc3fd7d14090e40ac8a4b2d0934b7a39f
Tully <tjagoe@systima.com.au>
2024-09-19 17:17:50 +1000

    fixed download commands

--- fragments/labels/topazgigapixel.sh
+++ fragments/labels/topazgigapixel.sh
@@ -4,7 +4,7 @@ topazgigapixelai)
     name="Topaz Gigapixel AI"
     type="pkg"
     appNewVersion=$(curl -sSL "https://formulae.brew.sh/api/cask/topaz-gigapixel-ai.json" | awk -F'"version":' '{split($2, a, "\""); print a[2]}' | grep -oE '[0-9.]+')
-    versionKey="CFBundleShortVersionString"
     downloadURL="https://downloads.topazlabs.com/deploy/TopazGigapixelAI/${appNewVersion}/TopazGigapixelAI-${appNewVersion}.pkg"
+    archiveName="TopazGigapixelAI-${appNewVersion}.pkg"
     expectedTeamID="3G3JE37ZHF"
     ;;
