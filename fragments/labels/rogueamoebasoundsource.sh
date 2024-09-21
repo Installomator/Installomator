3792d2d0becc1632fffe51e09156131eab4c8cee
Gil Burns <gil@gilburns.com>
2024-09-21 09:59:40 -0500

    Create Rogue Amoeba SoundSource 5 label

--- fragments/labels/rogueamoebasoundsource.sh
+++
@@ -1,7 +0,0 @@
-rogueamoebasoundsource5)
-    name="SoundSource"
-    type="zip"
-    downloadURL="https://rogueamoeba.com/soundsource/download.php"
-    appNewVersion="$(curl -fs "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.soundsource&platform=osx&version=5000000" | xpath 'string(//rss/channel/item/enclosure/@sparkle:version)' 2>/dev/null)"
-    expectedTeamID="7266XEXAPM"
-    ;;
