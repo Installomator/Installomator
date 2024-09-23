6e3dd75f58a6ff1f584195244d867dffaacd8622
Gil Burns <gil@gilburns.com>
2024-09-22 19:09:35 -0500

    Update Telegram label appNewVersion check

--- fragments/labels/telegram.sh
+++ fragments/labels/telegram.sh
@@ -2,6 +2,6 @@ telegram)
     name="Telegram"
     type="dmg"
     downloadURL="https://telegram.org/dl/macos"
-    appNewVersion="$(curl -s "https://osx.telegram.org/updates/versions.xml" | xpath -e 'string(//rss/channel/item[1]/enclosure/@sparkle:shortVersionString)' 2>/dev/null)"
+    appNewVersion="$(curl -s "https://osx.telegram.org/updates/versions.xml" | xpath 'string(//rss/channel/item[1]/enclosure/@sparkle:shortVersionString)' 2>/dev/null)"
     expectedTeamID="6N38VWS5BX"
     ;;
