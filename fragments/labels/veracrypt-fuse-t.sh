fa6c81f13ec22d6b8a5c5d162a7dc8c31a876370
Fredrik Larsson <33235315+kryptonit1@users.noreply.github.com>
2024-09-27 13:03:11 +0200

    Update veracrypt-fuse-t.sh
    Fixed appNewVersion varible for the FUSE-T version of VeraCrypt.

--- fragments/labels/veracrypt-fuse-t.sh
+++ fragments/labels/veracrypt-fuse-t.sh
@@ -3,6 +3,6 @@ veracrypt-fuse-t)
     type="pkgInDmg"
     archiveName="VeraCrypt_FUSE-T_[0-9.]*.dmg"
     downloadURL=$(curl -sfL "https://api.github.com/repos/veracrypt/VeraCrypt/releases" | awk -F '"' "/browser_download_url/ && /$archiveName\"/ { print \$4; exit }")
-    appNewVersion=$(echo "${downloadURL}" | sed -E 's/.*\/[a-zA-Z]*_([0-9.]*.*)\.dmg/\1/g')
+    appNewVersion=$(echo "${downloadURL}" | sed -E 's/.*\/[a-zA-Z]*_FUSE-T_([0-9.]*.*)\.dmg/\1/g')
     expectedTeamID="Z933746L2S"
     ;;
