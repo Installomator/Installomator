308c7f3ae78b5bb327fbe5b5bff86577d8c11dbd
Fredrik Larsson <33235315+kryptonit1@users.noreply.github.com>
2024-09-27 13:33:03 +0200

    Archive name fix
    Changed the archiveName variable values to take into account that some previous versions of VeraCrypt has had the version format "1.24-Update8", which might be used in future releases as well. The old archiveName values wouldn't have been able to find the dmgs for these versions.

--- fragments/labels/veracrypt.sh
+++ fragments/labels/veracrypt.sh
@@ -2,7 +2,7 @@ veracrypt|\
 veracrypt-macfuse)
     name="VeraCrypt"
     type="pkgInDmg"
-    archiveName="VeraCrypt_[0-9.]*.dmg"
+    archiveName="VeraCrypt_[0-9.].*\.dmg"
     downloadURL=$(curl -sfL "https://api.github.com/repos/veracrypt/VeraCrypt/releases" | awk -F '"' "/browser_download_url/ && /$archiveName\"/ { print \$4; exit }")
     appNewVersion=$(echo "${downloadURL}" | sed -E 's/.*\/[a-zA-Z]*_([0-9.]*.*)\.dmg/\1/g')
     expectedTeamID="Z933746L2S"
