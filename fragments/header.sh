64de2306a4a69c6f9fba21a096257438b7c2168e
Adam Codega <acodega@users.noreply.github.com>
2024-09-27 10:30:20 -0400

    Update header.sh
    Updated variable name and clarified explanation.

--- fragments/header.sh
+++ fragments/header.sh
@@ -297,8 +297,8 @@ NOTIFY_DIALOG=0
 #   differently than the installed app, then this variable can be used to name the
 #   installer that should be located after mounting/expanding the downloaded archive.
 #   See label adobecreativeclouddesktop
-# - ignoreInstallScriptExitCode:
-#   If this is set to one, then any non-zero exit code from a CLIInstaller will not be counted as a failure.
+# - ignoreCLIInstallerExitCode:
+#   If this is set to one, then any non-zero exit code from CLIInstaller will be ignored.
 #
 ### Logging
 # Logging behavior
