e13565a66b2768fe4718db3163d1b11f5f2a7125
Gil Burns <gil@gilburns.com>
2024-09-29 11:56:20 -0500

    Update steinberglibrarymanager.sh

--- fragments/labels/steinberglibrarymanager.sh
+++ fragments/labels/steinberglibrarymanager.sh
@@ -7,7 +7,7 @@ steinberglibrarymanager)
     name="Steinberg Library Manager"
     type="pkgInDmg"
     packageID="com.steinberg.SteinbergLibraryManager"
-	downloadURL="https://www.steinberg.net/slm-mac"
+    downloadURL="https://www.steinberg.net/slm-mac"
     appNewVersion="$( curl -LIs "${downloadURL}" | grep -i "location:" | grep "dmg" | cut -d\/ -f7 | cut -d'.' -f1-3 )"
     expectedTeamID="5PMY476BJ6"
     ;;
