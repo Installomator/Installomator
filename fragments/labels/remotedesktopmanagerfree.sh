c03a952defc57f4200906e3f344b2fc182f945ee
Mechman27 <drbedfo@yahoo.com>
2024-10-04 08:17:09 -0600

    Updating RDM Download URL

--- fragments/labels/remotedesktopmanagerfree.sh
+++ fragments/labels/remotedesktopmanagerfree.sh
@@ -1,7 +1,7 @@
 remotedesktopmanagerfree)
     name="Remote Desktop Manager"
     type="dmg"
-    downloadURL=$(curl -fsL https://devolutions.net/remote-desktop-manager/home/thankyou/rdmmacbin/ | grep -oe "http.*\.dmg" | head -1)
+    downloadURL=$(curl -fsL https://cdn.devolutions.net/download/Mac/Devolutions.RemoteDesktopManager.Mac.2024.3.4.3.dmg | grep -oe "http.*\.dmg" | head -1)
     appNewVersion=$(echo "$downloadURL" | sed -E 's/.*\.Mac\.([0-9.]*)\.dmg/\1/g')
     expectedTeamID="N592S9ASDB"
     ;;
