3596045b2b1f150433619c6e07c8ed8e8b35faf2
Benjamin Kollmer <b.kollmer@apfelwerk.de>
2024-10-10 09:53:23 +0200

    Create chatgpt.sh

---
+++ fragments/labels/chatgpt.sh
@@ -0,0 +1,9 @@
+chatgpt)
+    name="ChatGPT"
+    # Downloads the latest 8.1.x version of the STARFACE Client. The client depends on the version of the PBX, so the correct version should be selected for installation
+    type="dmg"
+    downloadURL="https://persistent.oaistatic.com/sidekick/public/ChatGPT.dmg"
+    # appNewVersion= not available
+    expectedTeamID="2DC432GLL2"
+    versionKey="CFBundleVersion"
+    ;;
