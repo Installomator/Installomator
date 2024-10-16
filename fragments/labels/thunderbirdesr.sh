7a89c28ef4416bfef6122633a09061857a8fa138
theadamcraig <adamcraig@me.com>
2024-10-16 09:37:02 -0400

    Update thunderbirdesr.sh
    Adjusted the appNewVersion to grep the last 5 esr releases and grab the newest version.

--- fragments/labels/thunderbirdesr.sh
+++ fragments/labels/thunderbirdesr.sh
@@ -1,8 +1,7 @@
 thunderbirdesr)
     name="Thunderbird"
     type="dmg"
-    esrVersion=$(curl -fsL "https://www.thunderbird.net/en-US/thunderbird/releases/atom.xml" | grep -m 1 "<title type=" | sed 's/<.*>\(.*\)<.*>/\1/' | awk '{ print $2 }' | awk '{$1=$1};1')
-    appNewVersion=${esrVersion%esr}
-    downloadURL="https://download.mozilla.org/?product=thunderbird-$esrVersion-SSL&os=osx&lang=en-US"
+    appNewVersion=$(curl -fsL "https://www.thunderbird.net/en-US/thunderbird/releases/atom.xml" | grep "esr" | grep -m 5 "<title type=" | sed 's/<.*>\(.*\)<.*>/\1/' | awk '{ print $2 }' | awk '{$1=$1};1' | tr -d "esr" | sort -V | tail -n1 )
+    downloadURL="https://download.mozilla.org/?product=thunderbird-${appNewVersion}esr-SSL&os=osx&lang=en-US"
     expectedTeamID="43AQ936H96"
     ;;
