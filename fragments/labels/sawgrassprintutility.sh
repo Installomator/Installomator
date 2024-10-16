070fb03249b24c12f0f8311d480c7a33d9392f66
John Bowman <jcmbowman@gmail.com>
2024-10-16 10:51:50 -0400

    Create sawgrassprintutility.sh

---
+++ fragments/labels/sawgrassprintutility.sh
@@ -0,0 +1,6 @@
+sawgrassprintutility)
+    name="SawgrassPrintUtility"
+    type="dmg"
+    downloadURL="https://print.sawgrassink.com/SawgrassPrintUtility.dmg"
+    expectedTeamID="2DD4HMHX5K"
+    ;;
