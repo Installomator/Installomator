5ddc5579c088040b878a5b84828a97fd7f6697d2
Tully <tjagoe@systima.com.au>
2024-10-22 14:05:47 +1100

    Update topazgigapixel.sh

--- fragments/labels/topazgigapixel.sh
+++ fragments/labels/topazgigapixel.sh
@@ -1,9 +1,10 @@
 topazgigapixel|\
 topazgigapixelai)
-    # credit: @tully_jagoe
+    # credit: Tully Jagoe
     name="Topaz Gigapixel AI"
     type="pkg"
     appNewVersion=$(curl -sSL "https://formulae.brew.sh/api/cask/topaz-gigapixel-ai.json" | awk -F'"version":' '{split($2, a, "\""); print a[2]}' | grep -oE '[0-9.]+')
+    versionKey="CFBundleShortVersionString"
     downloadURL="https://downloads.topazlabs.com/deploy/TopazGigapixelAI/${appNewVersion}/TopazGigapixelAI-${appNewVersion}.pkg"
     archiveName="TopazGigapixelAI-${appNewVersion}.pkg"
     expectedTeamID="3G3JE37ZHF"
