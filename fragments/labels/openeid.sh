424044de6f09a7960f1abfa066523c7c3a177960
P-M Lejon <15982689+pmex@users.noreply.github.com>
2024-10-24 14:36:02 +0200

    removed appName, since it's not used

--- fragments/labels/openeid.sh
+++ fragments/labels/openeid.sh
@@ -1,7 +1,6 @@
 openeid)
     name="Open-EID"
     type="pkgInDmg"
-    appName="EstEIDTokenApp.app"
     archiveName="Open-EID.pkg"
     downloadURL=$(curl -fs "https://www.id.ee/en/article/install-id-software/" | grep -oe "https://installer.id.ee/media/osx/.*\.dmg")
     appNewVersion=$(echo $downloadURL | sed -E 's/.*EID_|.dmg//g')
