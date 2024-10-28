ad7b746a7c1fce1819ce813d932f05e0df0388a1
worthypants <worthypants@yellowdoubledecker.com>
2024-10-28 10:25:41 +0100

    update webcatalog label to reflect download url and version changes
    update webcatalog label to reflect download url and version changes

--- fragments/labels/webcatalog.sh
+++ fragments/labels/webcatalog.sh
@@ -2,7 +2,7 @@ webcatalog)
     name="WebCatalog"
     type="dmg"
     packageID="com.webcatalog.jordan"
-    appNewVersion="$(curl -fs https://webcatalog.io/en/desktop/macos/ | sed -n 's/.*v<!-- -->\([0-9.]*\).*/\1/p')"
-    downloadURL="https://cdn-2.webcatalog.io/webcatalog/WebCatalog-${appNewVersion}-universal.dmg"
+    downloadURL="https://webcatalog.io/api/download/?platform=macos&arch=universal"
+    appNewVersion=$( curl -fsIL "${downloadURL}" | grep -i "^location" | tail -1 | awk '{print $2}' | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*).*/\1/g' )
     expectedTeamID="VKST52VQVP"
     ;;
