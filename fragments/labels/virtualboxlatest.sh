edf53f34151239ac895be6a3df3cbfa72a449382
Gil Burns <gil@gilburns.com>
2024-10-28 22:03:35 -0500

    Corrected the download URL

--- fragments/labels/virtualboxlatest.sh
+++ fragments/labels/virtualboxlatest.sh
@@ -4,7 +4,7 @@ virtualboxlatest)
     pkgName="VirtualBox.pkg"
     appNewVersion=$(curl -fs "https://download.virtualbox.org/virtualbox/LATEST.TXT")
     if [[ $(arch) == "arm64" ]]; then
-        downloadURL="https://download.virtualbox.org/virtualbox/${appNewVersion}/$(curl -fs "https://download.virtualbox.org/virtualbox/7.1.0/" | grep "macOSArm64" | cut -d\" -f2)"
+        downloadURL="https://download.virtualbox.org/virtualbox/${appNewVersion}/$(curl -fs "https://download.virtualbox.org/virtualbox/${appNewVersion}/" | grep "macOSArm64" | cut -d\" -f2)"
     elif [[ $(arch) == "i386" ]]; then
         downloadURL="https://download.virtualbox.org/virtualbox/${appNewVersion}/$(curl -fs "https://download.virtualbox.org/virtualbox/${appNewVersion}/" | grep "OSX.dmg" | cut -d\" -f2)"
     fi
