accb4c3ac2407336a6877f1c50a083768147a2d5
Fletcher Salesky <fletcher@salesky.dev>
2024-11-13 13:05:13 -0800

    Update Tableau labels

--- fragments/labels/tableaupublic.sh
+++ fragments/labels/tableaupublic.sh
@@ -2,7 +2,11 @@ tableaupublic)
     name="Tableau Public"
     type="pkgInDmg"
     packageID="com.tableausoftware.tableaudesktop"
-    downloadURL=$(curl -fs https://www.tableau.com/downloads/public/mac | awk '/TableauPublic/' | xmllint --recover --html --xpath "//a/text()" -)
-    appNewVersion=$( echo $downloadURL | sed -E 's/.*TableauPublic-([-0-9]*)\.dmg/\1/g' | tr "-" "." )
+    if [[ $(arch) == "arm64" ]]; then
+       downloadURL="https://www.tableau.com/downloads/public/mac-arm64"
+    elif [[ $(arch) == "i386" ]]; then
+       downloadURL="https://www.tableau.com/downloads/public/mac"
+    fi
+    appNewVersion=${${$(curl -fsIL "$downloadURL" | sed -nE 's/.*TableauPublic-([0-9-]*).*/\1/p')//-/.}%.}
     expectedTeamID="QJ4XPRK37C"
     ;;
