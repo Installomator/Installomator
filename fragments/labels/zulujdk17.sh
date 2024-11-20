f5c17422196ccf3a56a639654c49f4c528fc7135
Mark <PhantomPhixer@users.noreply.github.com>
2024-11-20 15:08:32 +0000

    removed typo in download url for intel

--- fragments/labels/zulujdk17.sh
+++ fragments/labels/zulujdk17.sh
@@ -3,7 +3,7 @@ name="Zulu JDK 17"
 type="pkgInDmg" 
 packageID="com.azulsystems.zulu.17" 
 if [[ $(arch) == i386 ]]; then 
-    downloadURL=https://cdn.azul.com/zulu/bin/$(curl -fs "https://cdn.azul.com/zulu/bin/" | grep -Eio '">zulu17.*ca-jdk17.*x64.dmg(.*)' | cut -c3- | sed 's/<\/a>//' | sed -E 's/([0-9.]*)M//' | awk '{print $2 $1}' | sort | cut -c11- | cut -d "<" -f 1 | tail -1)
+    downloadURL=https://cdn.azul.com/zulu/bin/$(curl -fs "https://cdn.azul.com/zulu/bin/" | grep -Eio '">zulu17.*ca-jdk17.*x64.dmg(.)' | cut -c3- | sed 's/<\/a>//' | sed -E 's/([0-9.]*)M//' | awk '{print $2 $1}' | sort | cut -d "<" -f 1 | tail -1)
  elif [[ $(arch) == arm64 ]]; then 
     downloadURL=https://cdn.azul.com/zulu/bin/$(curl -fs "https://cdn.azul.com/zulu/bin/" | grep -Eio '">zulu17.*ca-jdk17.*aarch64.dmg(.)' | cut -c3- | sed 's/<\/a>//' | sed -E 's/([0-9.]*)M//' | awk '{print $2 $1}' | sort | cut -d "<" -f 1 | tail -1)
 fi 
