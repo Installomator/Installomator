0defe634d3c5c8efedc8024c97715e493a6da399
Mark <PhantomPhixer@users.noreply.github.com>
2024-11-21 09:03:53 +0000

    updated format
    changed indents to match standard format

--- fragments/labels/zulujdk17.sh
+++ fragments/labels/zulujdk17.sh
@@ -1,13 +1,13 @@
 zulujdk17)
-name="Zulu JDK 17"
-type="pkgInDmg" 
-packageID="com.azulsystems.zulu.17" 
-if [[ $(arch) == i386 ]]; then 
-    downloadURL=https://cdn.azul.com/zulu/bin/$(curl -fs "https://cdn.azul.com/zulu/bin/" | grep -Eio '">zulu17.*ca-jdk17.*x64.dmg(.)' | cut -c3- | sed 's/<\/a>//' | sed -E 's/([0-9.]*)M//' | awk '{print $2 $1}' | sort | cut -d "<" -f 1 | tail -1)
- elif [[ $(arch) == arm64 ]]; then 
-    downloadURL=https://cdn.azul.com/zulu/bin/$(curl -fs "https://cdn.azul.com/zulu/bin/" | grep -Eio '">zulu17.*ca-jdk17.*aarch64.dmg(.)' | cut -c3- | sed 's/<\/a>//' | sed -E 's/([0-9.]*)M//' | awk '{print $2 $1}' | sort | cut -d "<" -f 1 | tail -1)
-fi 
-expectedTeamID="TDTHCUPYFR" 
-appCustomVersion(){ if [ -f "/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Info.plist" ]; then /usr/bin/defaults read "/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Info.plist" "CFBundleName" | sed 's/Zulu //'; fi }
-appNewVersion=$(echo "$downloadURL" | cut -d "-" -f 1 | sed -e "s/.*zulu//" | sed 's/jdk//') 
+    name="Zulu JDK 17"
+    type="pkgInDmg" 
+    packageID="com.azulsystems.zulu.17" 
+    if [[ $(arch) == i386 ]]; then 
+        downloadURL=https://cdn.azul.com/zulu/bin/$(curl -fs "https://cdn.azul.com/zulu/bin/" | grep -Eio '">zulu17.*ca-jdk17.*x64.dmg(.)' | cut -c3- | sed 's/<\/a>//' | sed -E 's/([0-9.]*)M//' | awk '{print $2 $1}' | sort | cut -d "<" -f 1 | tail -1)
+    elif [[ $(arch) == arm64 ]]; then 
+        downloadURL=https://cdn.azul.com/zulu/bin/$(curl -fs "https://cdn.azul.com/zulu/bin/" | grep -Eio '">zulu17.*ca-jdk17.*aarch64.dmg(.)' | cut -c3- | sed 's/<\/a>//' | sed -E 's/([0-9.]*)M//' | awk '{print $2 $1}' | sort | cut -d "<" -f 1 | tail -1)
+    fi 
+    expectedTeamID="TDTHCUPYFR" 
+    appCustomVersion(){ if [ -f "/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Info.plist" ]; then /usr/bin/defaults read "/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Info.plist" "CFBundleName" | sed 's/Zulu //'; fi }
+    appNewVersion=$(echo "$downloadURL" | cut -d "-" -f 1 | sed -e "s/.*zulu//" | sed 's/jdk//') 
 ;;
