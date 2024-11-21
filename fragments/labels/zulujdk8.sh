44ba24249f4387c24c152ed67b8a0afac27f7dbc
Mark <PhantomPhixer@users.noreply.github.com>
2024-11-21 09:05:13 +0000

    fixed format

--- fragments/labels/zulujdk8.sh
+++ fragments/labels/zulujdk8.sh
@@ -10,4 +10,4 @@ zulujdk8)
     expectedTeamID="TDTHCUPYFR"
     appCustomVersion(){ if [ -f "/Library/Java/JavaVirtualMachines/zulu-8.jdk/Contents/Info.plist" ]; then /usr/bin/defaults read "/Library/Java/JavaVirtualMachines/zulu-8.jdk/Contents/Info.plist" "CFBundleName" | sed 's/Zulu //'; fi }
     appNewVersion=$(echo "$downloadURL" | cut -d "-" -f 1 | sed -e "s/.*zulu//" | sed 's/jdk//') 
-    ;;
+;;
