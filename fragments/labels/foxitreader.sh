5b82acb6a07a4b44590fd707e94e89564c14ca9c
Benjamin Kollmer <109155293+TacoDienstag@users.noreply.github.com>
2024-11-26 17:07:43 +0100

    Delete fragments/labels/foxitreader.sh

--- fragments/labels/foxitreader.sh
+++
@@ -1,7 +0,0 @@
-foxitreader)
-    name="FoxitPDFReader"
-    type="pkg"
-    downloadURL="https://www.foxit.com/downloads/latest.html?product=Foxit-Reader&platform=Mac-OS-X"
-    appNewVersion=$(curl -fsSL "https://www.foxit.com/pdf-editor/version-history.html" | awk '/<div class="tab-product hide" id="tab-editor-suite-mac">/,/<\/div>/' | sed -n 's/.*<h3>Version \([0-9.]*\)<\/h3>.*/\1/p' | head -n 1)
-    expectedTeamID="8GN47HTP75"
-    ;;
