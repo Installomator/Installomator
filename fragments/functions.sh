65cb3c2e03e325841ae255ba5168e28306a5c765
Theile <soren@theilgaard.dk>
2024-12-02 10:00:40 +0100

    `mdfind` commands fixed

--- fragments/functions.sh
+++ fragments/functions.sh
@@ -264,11 +264,10 @@ getAppVersion() {
 #            targetDir="/Applications/Utilities"
 #        fi
     else
-    #    applist=$(mdfind "kind:application $appName" -0 )
         printlog "name: $name, appName: $appName"
-        applist=$(mdfind "kind:application AND name:$name" -0 )
+        applist=$(mdfind -name "$name" 'kMDItemKind:"App"' -0)
+#        applist=$(mdfind -name "$appName" 'kMDItemKind:"App"' -0)
 #        printlog "App(s) found: ${applist}" DEBUG
-#        applist=$(mdfind "kind:application AND name:$appName" -0 )
     fi
     if [[ -z $applist ]]; then
         printlog "No previous app found" WARN
