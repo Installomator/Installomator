4ee4709890ee57cabb0804f2dc997784963c2438
theadamcraig <adamcraig@me.com>
2024-12-03 12:44:59 -0500

    Update foxitpdfreader.sh
    Updated the branch per the notes from the existing pull request and the downloadURL from a related foxitpdfreader label

--- fragments/labels/foxitpdfreader.sh
+++ fragments/labels/foxitpdfreader.sh
@@ -3,7 +3,8 @@ foxitpdfreader)
     type="pkg"
     #the packageID versioning seems to be in line with Foxit PDF Editor and does not reflect the installed versionNumber
     #packageID="com.foxit.pkg.pdfreader"
-    downloadURL="https://www.foxit.com/downloads/latest.html?product=Foxit-Reader&platform=Mac-OS-X&version=&package_type=&distID="
-    appNewVersion="$(curl -fsL "https://www.foxit.com/pdf-reader/version-history.html" | grep -m 1 "name=\"Version" | awk -F "Version_" '{ print $2 }' | awk -F "\"" '{ print $1 }')"
+    downloadURL=$(curl -fsIL "https://www.foxit.com/downloads/latest.html?product=Foxit-Reader&platform=Mac-OS-X&version=&package_type=&language=English&distID=" | grep location | cut -d \  -f 2)
+    appNewVersion=$(curl -fsSL "https://www.foxit.com/pdf-editor/version-history.html" | awk '/<div class="tab-product hide" id="tab-editor-suite-mac">/,/<\/div>/' | sed -n 's/.*<h3>Version \([0-9.]*\)<\/h3>.*/\1/p' | head -n 1) 
     expectedTeamID="8GN47HTP75"
+    blockingProcesses=( "Foxit PDF Reader" "FoxitPDFReaderUpdateService" )
     ;;
