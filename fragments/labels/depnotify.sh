ecf5dd7edff5d9f64e2d0d7697578cdf61228d37
nick-f <nick@nickf.dev>
2024-12-18 09:42:15 +1100

    Update DEPNotify download URL
    The old domain's SSL certificate has expired. Updating the domain to a
    new supported home with a valid SSL cert.

--- fragments/labels/depnotify.sh
+++ fragments/labels/depnotify.sh
@@ -2,7 +2,7 @@ depnotify)
     name="DEPNotify"
     type="pkg"
     #packageID="menu.nomad.depnotify"
-    downloadURL="https://files.nomad.menu/DEPNotify.pkg"
+    downloadURL="https://files.jamfconnect.com/DEPNotify.pkg"
     #appNewVersion=$()
     expectedTeamID="VRPY9KHGX6"
     ;;
