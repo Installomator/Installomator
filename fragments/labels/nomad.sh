13b6abab8fd520ac88876ac54278109e088638f2
nick-f <nick@nickf.dev>
2024-12-18 09:27:39 +1100

    Update NoMAD download URL
    The old domain's SSL certificate has expired. Updating the domain to a
    new supported home with a valid SSL cert.

--- fragments/labels/nomad.sh
+++ fragments/labels/nomad.sh
@@ -2,7 +2,7 @@ nomad)
     # credit: Tadayuki Onishi (@kenchan0130)
     name="NoMAD"
     type="pkg"
-    downloadURL="https://files.nomad.menu/NoMAD.pkg"
+    downloadURL="https://files.jamfconnect.com/NoMAD.pkg"
     appNewVersion=$(curl -fs https://nomad.menu/support/ | grep "NoMAD Downloads" | sed -E 's/.*Current Version ([0-9\.]*)<.*/\1/g')
     expectedTeamID="VRPY9KHGX6"
     ;;
