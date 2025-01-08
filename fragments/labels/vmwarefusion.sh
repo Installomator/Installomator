4861316f8405b110fe491bb3bae912b14f895114
Tadayuki Onishi <tonishi@folio-sec.com>
2025-01-08 22:30:46 +0900

    fix label: vmwarefution

--- fragments/labels/vmwarefusion.sh
+++ fragments/labels/vmwarefusion.sh
@@ -1,8 +1,11 @@
 vmwarefusion)
     name="VMware Fusion"
-    type="dmg"
-    downloadURL="https://www.vmware.com/go/getfusion"
-    curlOptions=(-H "Accept: */*" -H "Accept-Encoding: gzip, deflate" -H "Connection: keep-alive" -H "Host: www.vmware.com" -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15")
-    appNewVersion=$(curl -fsIL ${curlOptions} "https://www.vmware.com/go/getfusion" | grep -i "^location" | awk '{print $2}' | sed 's/.*-\(.*\)-.*/\1/')
+    type="zip"
+    appNewVersion=$(curl -sfL https://softwareupdate.vmware.com/cds/vmw-desktop/fusion/ | grep -o '<li><a href="[^"]*' | sed 's|<li><a href="||' | awk -F/ '{print $1}' | grep -E '[0-9]' | sort -V | tail -n 1)
+    release=$(curl -sfL "https://softwareupdate.vmware.com/cds/vmw-desktop/fusion/${appNewVersion}" | grep -o '<li><a href="[^"]*' | sed 's|<li><a href="||' | awk -F/ '{print $1}' | tail -n 1)
+    tempZipDir=$(mktemp -d)
+    curl -sfL https://softwareupdate.vmware.com/cds/vmw-desktop/fusion/${appNewVersion}/${release}/universal/core/com.vmware.fusion.zip.tar | tar -xz -C "${tempZipDir}"
+    downloadURL="file://${tempZipDir}/com.vmware.fusion.zip"
+    appName="payload/VMware Fusion.app"
     expectedTeamID="EG7KH642X6"
     ;;
