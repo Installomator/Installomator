0c0fb263ac1e98f260a86a76edc2df0d183e2014
Tadayuki Onishi <tonishi@folio-sec.com>
2025-01-09 16:04:51 +0900

    Add new label: githubcopilotforxcode

---
+++ fragments/labels/githubcopilotforxcode.sh
@@ -0,0 +1,8 @@
+githubcopilotforxcode)
+    name="GitHub Copilot for Xcode"
+    type="dmg"
+    downloadURL="$(downloadURLFromGit github CopilotForXcode)"
+    appNewVersion="$(versionFromGit github CopilotForXcode)"
+    expectedTeamID="VEKTX9H2N7"
+    blockingProcesses=( "GitHub Copilot for Xcode" "GitHub Copilot for Xcode Extension" "Copilot" )
+    ;;
