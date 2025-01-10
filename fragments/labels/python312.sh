5919ad8b211e81b2322c24f313e6e25cbbf9a639
Tim de Jong <timdejong@bol.com>
2025-01-10 15:59:06 +0100

    Create Python version 3.12

---
+++ fragments/labels/python312.sh
@@ -0,0 +1,17 @@
+python312)
+    name="Python312"
+    type="pkg"
+    appNewVersion="$( curl --compressed -s "https://www.python.org/downloads/macos/" | awk -F'[<>]' '/Python 3.12./{print $3}' | awk -F' ' '{print $2}' | head -n 1 )"
+    archiveName="$( curl -s "https://www.python.org/ftp/python/$appNewVersion/" | grep -om 1 "\"python.*macos.*\.pkg\"" | tr -d \" )"
+    downloadURL="https://www.python.org/ftp/python/$appNewVersion/$archiveName"
+    shortVersion=$( cut -d '.' -f1,2 <<< $appNewVersion )
+    packageID="org.python.Python.PythonFramework-$shortVersion"
+    expectedTeamID="BMM5U3QVKW"
+    blockingProcesses=( "IDLE" "Python Launcher" )
+    versionKey="CFBundleVersion"
+    appCustomVersion() {
+        if [ -d "/Library/Frameworks/Python.framework/Versions/$shortVersion/Resources/Python.app/" ]; then
+            /usr/bin/defaults read "/Library/Frameworks/Python.framework/Versions/$shortVersion/Resources/Python.app/Contents/Info" CFBundleVersion
+        fi
+    }
+    ;;
