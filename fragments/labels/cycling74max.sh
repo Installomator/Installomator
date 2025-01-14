b6517e6dc3a9c0f1fbd3999d74353f15c67905ea
robotprom <98070647+robotprom@users.noreply.github.com>
2025-01-14 18:53:15 -0500

    Update cycling74max.sh

--- fragments/labels/cycling74max.sh
+++ fragments/labels/cycling74max.sh
@@ -2,7 +2,7 @@ cycling74max)
     name="Max"
     type="dmg"
     downloadURL="$( curl -s "https://cycling74.com/downloads" $curlOptions | tr '"' '\n' | grep -m1 "Max.*dmg" )"
-    appNewVersion=$(curl -fsL 'https://cycling74.com/downloads' | sed -E 's/.*Max([12][0-9]|[1-9])([12][0-9]|[0-9])([0-9]+).*/\1.\2.\3/g')
+    appNewVersion=$(curl -s https://cycling74.com/downloads | grep -oE '9\.\d+\.\d+' | head -n 1)
     versionKey="CFBundleVersion"
     expectedTeamID="GBXXCFCVW5"
     ;;
