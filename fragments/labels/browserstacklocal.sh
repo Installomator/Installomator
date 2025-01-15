7038bc5e8b2ee25feb67a7d0db0cb012576f2311
DanielRossEUPE <149118715+DanielRossEUPE@users.noreply.github.com>
2025-01-15 15:38:00 -0700

    Update browserstacklocal.sh
    Removed packageID and appNewVersion.

--- fragments/labels/browserstacklocal.sh
+++ fragments/labels/browserstacklocal.sh
@@ -1,8 +1,6 @@
 browserstacklocal)
     name="BrowserStackLocal"
     type="pkg"
-    packageID="org.sparkle-project.Sparkle"
     downloadURL="https://www.browserstack.com/local-testing/downloads/native-app/BrowserStackLocal.pkg"
-    appNewVersion=""
     expectedTeamID="YQ5FZQ855D"
     ;;
