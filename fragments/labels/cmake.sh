384d4292814be8924ed64b64edba8bef5dfccb25
Adam Codega <acodega@users.noreply.github.com>
2025-01-15 22:50:39 -0500

    Update cmake.sh

--- fragments/labels/cmake.sh
+++ fragments/labels/cmake.sh
@@ -1,5 +1,4 @@
 cmake)
-	# An open-source tool designed to manage the build process of software using a compiler-independent method
     name="CMake"
     type="dmg"
     downloadURL=$(downloadURLFromGit Kitware CMake)
