c6d0b39cbdadc88c0d0c659cdf866a4f655a41e3
Adam Codega <acodega@users.noreply.github.com>
2025-01-15 21:45:59 -0500

    Update rawtherapee.sh
    Removed comment lines from label

--- fragments/labels/rawtherapee.sh
+++ fragments/labels/rawtherapee.sh
@@ -1,10 +1,7 @@
 rawtherapee)
     name="RawTherapee"
-    # Open Source RAW Converter from https://rawtherapee.com
-    # Documentation: https://rawpedia.rawtherapee.com/
     type="appInDmgInZip"
     downloadURL=$(curl -fs "https://rawtherapee.com/downloads/" | grep -oE "https.*/builds/mac/.*\.zip")
     appNewVersion=$(echo "$downloadURL" | sed -E 's/.*mac.*Universal_([0-9.]*).zip/\1/')
-    #appNewVersion=$(versionFromGit "Beep6581" "RawTherapee")
     expectedTeamID="5SJ86G6Q2R"
     ;;
