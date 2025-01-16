2d6409132d3a896405a5a99263da6aa3b7f15c57
Adam Codega <acodega@users.noreply.github.com>
2025-01-15 22:38:27 -0500

    Update processing4.sh

--- fragments/labels/processing4.sh
+++ fragments/labels/processing4.sh
@@ -5,7 +5,5 @@ processing4)
     downloadURL=$(downloadURLFromGit processing processing4)
     appNewVersion=$(versionFromGit processing processing4)
     expectedTeamID="8SBRM6J77J"
-    # Github returned version number resolves in build and version numbers being combined, so this provides the best match.
-    # if you are manually replicating the label with valuesfromarguements use 'appNewVersion="4.$(versionFromGit processing processing | cut -d "." -f 2-)"' instead.
     appCustomVersion(){ echo "$(defaults read /Applications/Processing.app/Contents/Info.plist CFBundleVersion )$( defaults read /Applications/Processing.app/Contents/Info.plist CFBundleShortVersionString )" }
     ;;
