47df1b657ada796a1eddd13800f156cb10ff43e2
Brad White <87879319+07-C9@users.noreply.github.com>
2025-01-16 11:54:04 -0800

    Update vernierspectralanalysis.sh
    Fixed appNewVersion

--- fragments/labels/vernierspectralanalysis.sh
+++ fragments/labels/vernierspectralanalysis.sh
@@ -1,7 +1,8 @@
 vernierspectralanalysis)
     name="Vernier Spectral Analysis"
     type="dmg"
-    appNewVersion=$(curl -s "https://apps.apple.com/us/app/vernier-spectral-analysis/id1323245536" | egrep -o 'Version [0-9]+\.[0-9]+' | sed 's/Version //' | head -1)
+    appNewVersion=$(curl -s "https://www.apkturbo.com/apps/vernier-spectral-analysis/com.vernier.spectralanalysis/" | sed -En 's/.*<dt>Version<\/dt><dd>([0-9]+\.[0-9]+\.[0-9]+) \(([0-9]+)\)<\/dd>.*/\1-\2/p' | head -1)
     downloadURL="https://software-releases.graphicalanalysis.com/sa/mac/release/latest/Vernier-Spectral-Analysis.dmg"
     expectedTeamID="75WN2B2WR8"
     ;;
+
