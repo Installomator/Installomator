026c00a548d2b1ba7cadfc2e40ab3efced63e290
rougegoat <rougegoat@gmail.com>
2025-01-21 17:07:00 -0600

    Update and rename snapgeneviewer.sh to snapgene.sh
    SnapGene Viewer was merged into SnapGene.  Updating the label for SnapGene Viewer to pull the new unified application.

--- fragments/labels/snapgene.sh
+++ fragments/labels/snapgene.sh
@@ -1,5 +1,5 @@
-snapgeneviewer)
-    name="SnapGene Viewer"
+snapgene|snapgeneviewer)
+    name="SnapGene"
     type="dmg"
     downloadURL="https://www.snapgene.com/local/targets/download.php?variant=viewer&os=mac&majorRelease=latest&minorRelease=latest"
     appNewVersion=$( curl -fsIL "${downloadURL}" | grep -i "^location" | awk '{print $2}' | tr '/' '\n' | grep -i "dmg" | sed -E 's/[a-zA-Z_]*_([0-9.]*)_mac\.dmg/\1/g' )
