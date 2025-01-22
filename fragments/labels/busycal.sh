0e094a71b3115cacdd381da2441173d5ed57de74
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2025-01-22 08:52:07 -0700

    Update busycal.sh Updated Download link

--- fragments/labels/busycal.sh
+++ fragments/labels/busycal.sh
@@ -1,8 +1,6 @@
 busycal)
-	# A powerful calendar app for macOS and iOS that offers customizable views, integrated task management, and seamless cloud sync
     name="BusyCal"
     type="dmg"
-    downloadURL=$(curl -fs https://www.busymac.com/busycal/releasenotes.html | grep -o 'https://www.busymac.com/download/bcl-\S*\.dmg' | sort -V | tail -n 1)
-    appNewVersion=$(curl -fs https://www.busymac.com/busycal/releasenotes.html | awk -F 'BusyCal ' '/BusyCal [0-9]+\.[0-9]+\.[0-9]+/{print $2}' | sort -V | tail -n 1)
+    downloadURL="https://www.busymac.com/download/BusyCal.dmg"
     expectedTeamID="N4RA379GBW"
     ;;
