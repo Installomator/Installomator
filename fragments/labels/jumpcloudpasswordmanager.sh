1558e959bafc33d040e537b306133bc2474e020a
speedybutters <31848713+speedybutters@users.noreply.github.com>
2025-01-27 10:54:49 +0000

    Create jumpcloudpasswordmanager.sh

---
+++ fragments/labels/jumpcloudpasswordmanager.sh
@@ -0,0 +1,9 @@
+jumpcloudpasswordmanager|\
+jcpswdmgr)
+    name="JumpCloud Password Manager"
+    type="dmg"
+    downloadURL="https://cdn.pwm.jumpcloud.com/DA/release/JumpCloud-Password-Manager-latest.dmg"
+    appNewVersion=""
+    expectedTeamID="N985MXSH85"
+    blockingProcesses=( "JumpCloud Password Manager" )
+    ;;
