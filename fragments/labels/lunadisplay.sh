c2b3675a6eed990897833336a478e66cae03a5f3
John Bowman <jcmbowman@gmail.com>
2025-01-27 15:25:00 -0500

    Apply editorconfig fixes

--- fragments/labels/lunadisplay.sh
+++ fragments/labels/lunadisplay.sh
@@ -3,12 +3,12 @@ lunadisplay)
     type="dmg"
     downloadURL="https://downloads.astropad.com/luna/mac/latest"
     appNewVersion=$(curl -sI "${downloadURL}" | grep -o -E 'location:.*$' | grep -o -E '\d+\.\d+\.\d+\.\d+')
-    appCustomVersion(){ 
-        if [ -f "/Applications/Luna Display.app/Contents/Info.plist" ]; then 
+    appCustomVersion(){
+        if [ -f "/Applications/Luna Display.app/Contents/Info.plist" ]; then
             firstPart=$(/usr/bin/defaults read "/Applications/Luna Display.app/Contents/Info.plist" "CFBundleShortVersionString")
             secondPart=$(/usr/bin/defaults read "/Applications/Luna Display.app/Contents/Info.plist" "CFBundleVersion")
             echo "$firstPart.$secondPart"
-        fi 
+        fi
     }
     expectedTeamID="8356ZZ8Y5K"
     ;;
