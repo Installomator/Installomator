93889a882b5a17bfc4d685ab91a153759d021d12
speedybutters <31848713+speedybutters@users.noreply.github.com>
2025-01-27 14:33:27 +0000

    Delete jumpcloudpasswordmanager.sh

--- jumpcloudpasswordmanager.sh
+++
@@ -1,8 +0,0 @@
-jumpcloudpasswordmanager)
-    name="JumpCloud Password Manager"
-    type="dmg"
-    downloadURL="https://cdn.pwm.jumpcloud.com/DA/release/JumpCloud-Password-Manager-latest.dmg"
-    appNewVersion=""
-    expectedTeamID="N985MXSH85"
-    blockingProcesses=( "JumpCloud Password Manager" )
-    ;;
