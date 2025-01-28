a6a4670f31e410569deecf0430df46b898ae63fa
Adam Codega <acodega@users.noreply.github.com>
2025-01-27 22:19:51 -0500

    Update jumpcloudpasswordmanager.sh
    appNewVersion was blank, and blockingProcesses not needed if its the same as `name`

--- fragments/labels/jumpcloudpasswordmanager.sh
+++ fragments/labels/jumpcloudpasswordmanager.sh
@@ -3,7 +3,5 @@ jcpswdmgr)
     name="JumpCloud Password Manager"
     type="dmg"
     downloadURL="https://cdn.pwm.jumpcloud.com/DA/release/JumpCloud-Password-Manager-latest.dmg"
-    appNewVersion=""
     expectedTeamID="N985MXSH85"
-    blockingProcesses=( "JumpCloud Password Manager" )
     ;;
