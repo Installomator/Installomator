a5a2f673f12b3bc90e007a1388e08f925512a2b9
Adam Codega <acodega@users.noreply.github.com>
2025-01-27 22:14:13 -0500

    Update depnotify.sh

--- fragments/labels/depnotify.sh
+++ fragments/labels/depnotify.sh
@@ -1,8 +1,6 @@
 depnotify)
     name="DEPNotify"
     type="pkg"
-    #packageID="menu.nomad.depnotify"
     downloadURL="https://files.jamfconnect.com/DEPNotify.pkg"
-    #appNewVersion=$()
     expectedTeamID="VRPY9KHGX6"
     ;;
