7847869f0d729415c8472373de26b9991920d9b6
Adam Codega <acodega@users.noreply.github.com>
2025-01-27 22:16:41 -0500

    Update nomad.sh
    It's okay to remove the appNewVersion. App is stagnant now and not expected to update anyway.

--- fragments/labels/nomad.sh
+++ fragments/labels/nomad.sh
@@ -3,6 +3,5 @@ nomad)
     name="NoMAD"
     type="pkg"
     downloadURL="https://files.jamfconnect.com/NoMAD.pkg"
-    appNewVersion=$(curl -fs https://nomad.menu/support/ | grep "NoMAD Downloads" | sed -E 's/.*Current Version ([0-9\.]*)<.*/\1/g')
     expectedTeamID="VRPY9KHGX6"
     ;;
