aa0c1d5be31b8f97f7e3877b47ae9206aea83466
Graham Christensen <graham@grahamc.com>
2025-01-29 09:52:30 -0500

    Drop  fragments/labels/determinate.sh comments

--- fragments/labels/determinate.sh
+++ fragments/labels/determinate.sh
@@ -2,9 +2,6 @@ determinate)
     name="Determinate"
     type="pkg"
     packageID="systems.determinate.Determinate"
-    # Note: https://install.determinate.systems/determinate-pkg/stable/Universal may return different URLs if a deploy is in progress.
-    # Fetching https://install.determinate.systems/determinate-pkg/stable/Universal and capturing the redirect target ensures the
-    # version printed is the version that will actually be installed.
     downloadUrl=$(curl -w "%{url_effective}\n" -I -L -s -S https://install.determinate.systems/determinate-pkg/stable/Universal -o /dev/null)
     appNewVersion=$(echo "$downloadUrl" | cut -d/ -f4)
     expectedTeamID="X3JQ4VPJZ6"
