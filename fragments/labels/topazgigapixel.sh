c4a741bccd876d4d056b91f594d9a6b5712845e1
Tully <tjagoe@systima.com.au>
2025-02-17 15:39:26 +1100

    Update appNewVersion to topaz host
    Updated appNewVersion to parse version string from downloads.topazlabs.com
    
    Co-Authored-By: P-M Lejon <pme@bonniernews.se>

--- fragments/labels/topazgigapixel.sh
+++ fragments/labels/topazgigapixel.sh
@@ -3,7 +3,7 @@ topazgigapixelai)
     # credit: Tully Jagoe
     name="Topaz Gigapixel AI"
     type="pkg"
-    appNewVersion=$(curl -sSL "https://formulae.brew.sh/api/cask/topaz-gigapixel-ai.json" | awk -F'"version":' '{split($2, a, "\""); print a[2]}' | grep -oE '[0-9.]+')
+    appNewVersion=$(curl -fsL https://community.topazlabs.com/c/gigapixel-ai/gigapixel-ai/66 | grep -o "raw-topic-link'>.*<" | grep -o "v.*<" | sed -E 's/[v|<]//g' | head -1)
     versionKey="CFBundleShortVersionString"
     downloadURL="https://downloads.topazlabs.com/deploy/TopazGigapixelAI/${appNewVersion}/TopazGigapixelAI-${appNewVersion}.pkg"
     archiveName="TopazGigapixelAI-${appNewVersion}.pkg"
