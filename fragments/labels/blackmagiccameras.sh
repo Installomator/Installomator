79cb3b89ebf349254d8807f3558ecad10ee79038
P-M Lejon <15982689+pmex@users.noreply.github.com>
2025-02-18 11:46:43 +0100

    Added blockingProcesses

--- fragments/labels/blackmagiccameras.sh
+++ fragments/labels/blackmagiccameras.sh
@@ -12,5 +12,6 @@ blackmagiccameras)
  	    -e '"https://www.blackmagicdesign.com/api/register/us/download/" + download_id')")
     appNewVersion=$(echo ${downloadURL} | grep -oE '/v([0-9.]+)' | cut -d'v' -f2)
     appCustomVersion(){ grep "release_version" "/Applications/Blackmagic Cameras/Blackmagic Camera Setup.app/Contents/Resources/settings.ini" | awk -F "=" '{print$2}'}
+    blockingProcesses=( "Blackmagic Camera Setup" )
     expectedTeamID="9ZGFBWLSYP"
     ;;
