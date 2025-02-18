47afc10963a20530bf455274d3cae4ebe2fe4ab4
Cody Keats <95243190+cocopuff2u@users.noreply.github.com>
2025-02-18 12:59:39 -0500

    Update Installomator.sh

--- Installomator.sh
+++ Installomator.sh
@@ -6844,7 +6844,6 @@ microsoftteamsnew)
     packageID="com.microsoft.teams2"
     downloadURL="https://go.microsoft.com/fwlink/?linkid=2249065"
     appNewVersion=$(curl -fsL "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409TEAMS21.xml" | xmllint --xpath '//array/dict[1]/key[text()="Update Version"]/following-sibling::string[1]/text()' - 2>/dev/null)
-    echo "appNewVersion: $appNewVersion"
     versionKey="CFBundleShortVersionString"
     expectedTeamID="UBF8T346G9"
     blockingProcesses=( MSTeams "Microsoft Teams" "Microsoft Teams WebView" "Microsoft Teams Launcher" "Microsoft Teams (work preview)")
