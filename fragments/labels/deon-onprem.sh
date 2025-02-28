360837b93d8cc85a3dfac3516962bded1a55b720
Grahn, Jonas <jonas.grahn@dataport.de>
2025-02-28 20:13:39 +0100

    Fix label
    fix spaces

--- fragments/labels/deon-onprem.sh
+++ fragments/labels/deon-onprem.sh
@@ -1,8 +1,8 @@
 deon-onprem)
     name="Deon"
     type="zip"
-    # beta is correct
-    feedURL= "https://download.deon.de/index.php?action=1&product=Mac&channel=beta"
+    # beta channel for download is correct
+    feedURL="https://download.deon.de/index.php?action=1&product=Mac&channel=beta"
     downloadURL=$(curl -fs $feedURL | jq -r '.uris[1]')
     appNewVersion=$(curl -fs $feedURL | jq -r '.version')
     expectedTeamID="EW9H238RWQ"
