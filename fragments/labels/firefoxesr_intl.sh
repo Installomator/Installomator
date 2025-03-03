f39e2f592702c2e122393da4d4d96dd9a2764c95
Markus Kachel <33146694+allandanton@users.noreply.github.com>
2025-03-03 14:49:14 +0100

    Update firefoxesr_intl.sh
    Use correct version from Mozilla website for ESR and remove esr suffix from that version because the Firefox.app does not have it.

--- fragments/labels/firefoxesr_intl.sh
+++ fragments/labels/firefoxesr_intl.sh
@@ -20,7 +20,8 @@ firefoxesr_intl)
         downloadURL="https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=osx"
     fi
     firefoxVersions=$(curl -fs "https://product-details.mozilla.org/1.0/firefox_versions.json")
-    appNewVersion=$(getJSONValue "$firefoxVersions" "LATEST_FIREFOX_VERSION")
+    appNewVersion=$(getJSONValue "$firefoxVersions" "FIREFOX_ESR")
+    appNewVersion=${appNewVersion:0:-3}
     expectedTeamID="43AQ936H96"
     blockingProcesses=( firefox )
     printlog "WARNING for ERROR: Label firefox and firefox_intl should not be used. Instead use firefoxpkg and firefoxpkg_intl as per recommendations from Firefox. It's not fully certain that the app actually gets updated here. firefoxpkg and firefoxpkg_intl will have built in updates and make sure the client is updated in the future." REQ
