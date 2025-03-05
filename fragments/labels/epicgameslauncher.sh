832ac424e7922ddd328118d9475dee476972c3c0
robotprom <98070647+robotprom@users.noreply.github.com>
2025-03-05 12:10:23 -0500

    Update epicgameslauncher.sh

--- fragments/labels/epicgameslauncher.sh
+++ fragments/labels/epicgameslauncher.sh
@@ -5,3 +5,4 @@ epicgameslauncher)
 	appNewVersion=$( curl -s "https://trello.com/b/GXLc34hk.json" | jq -r '.cards[] | select(.name | test("Epic Games Launcher [0-9]+\\.[0-9]+(\\.[0-9]+)?")) | .name' | grep -oE '[0-9]+\.[0-9]+(\.[0-9]+)?' | sort -V | tail -n 1 )
 	versionKey="CFBundleShortVersionString"
 	expectedTeamID="96DBZ92D3Y"
+	;;
