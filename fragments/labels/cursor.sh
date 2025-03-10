34ca6b4a3bcf8736acb74be65c45baaf4521036c
danielboyer <danielboyer1@live.ca>
2025-03-10 08:39:50 -0500

    Create cursor.sh
    Adding cursor label

---
+++ fragments/labels/cursor.sh
@@ -0,0 +1,8 @@
+cursor)
+    name="Cursor"
+    type="dmg"
+    downloadURL="https://anysphere-binaries.s3.us-east-1.amazonaws.com/production/be4f0962469499f009005e66867c8402202ff0b7/darwin/arm64/Cursor-darwin-arm64.dmg"
+    appNewVersion=""
+    expectedTeamID="VDXQ22DGB9"
+     blockingProcesses=( "Cursor" )
+    ;;
