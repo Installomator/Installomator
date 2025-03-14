66320561eb22a815605581ca7815afa82921fe1e
Matt Bilson <matt@mattbilson.com>
2025-03-14 20:39:42 +1300

    Updates fellow.sh appNewVersion

--- fragments/labels/fellow.sh
+++ fragments/labels/fellow.sh
@@ -2,6 +2,6 @@ fellow)
     name="Fellow"
     type="dmg"
     downloadURL="https://fellow.app/desktop/download/darwin/latest/"
-    appNewVersion=""
+    appNewVersion="$(curl -fsIL "${downloadURL}" | grep -i ^location | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/' | head -1)"
     expectedTeamID="2NF46HY8D8"
     ;;
