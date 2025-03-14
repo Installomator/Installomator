c459a8834c6bd3acb2b011c581a78f1aa5437ee4
danielboyer <60440080+danielboyer@users.noreply.github.com>
2025-03-14 09:32:24 -0500

    Update Installomator.sh
    Removed quotes from 'blockingProcesses' from Cursor

--- Installomator.sh
+++ Installomator.sh
@@ -3620,7 +3620,7 @@ cursor)
     downloadURL="https://anysphere-binaries.s3.us-east-1.amazonaws.com/production/be4f0962469499f009005e66867c8402202ff0b7/darwin/arm64/Cursor-darwin-arm64.dmg"
     appNewVersion=""
     expectedTeamID="VDXQ22DGB9"
-    blockingProcesses=( "Cursor" )
+    blockingProcesses=( Cursor )
     ;;
 cyberduck)
     name="Cyberduck"
