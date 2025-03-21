a2d93f43c49c57494de639c454a96fedd49c6fd8
Graham Christensen <graham@grahamc.com>
2025-03-21 12:57:10 -0400

    downloadUrl -> downloadURL

--- fragments/labels/determinate.sh
+++ fragments/labels/determinate.sh
@@ -2,7 +2,7 @@ determinate)
     name="Determinate"
     type="pkg"
     packageID="systems.determinate.Determinate"
-    downloadUrl=$(curl -w "%{url_effective}\n" -I -L -s -S https://install.determinate.systems/determinate-pkg/stable/Universal -o /dev/null)
-    appNewVersion=$(echo "$downloadUrl" | cut -d/ -f4)
+    downloadURL=$(curl -w "%{url_effective}\n" -I -L -s -S https://install.determinate.systems/determinate-pkg/stable/Universal -o /dev/null)
+    appNewVersion=$(echo "$downloadURL" | cut -d/ -f4)
     expectedTeamID="X3JQ4VPJZ6"
     ;;
