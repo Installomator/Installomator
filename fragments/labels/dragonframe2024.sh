4b10121709ab950f13b1c21c8b83fc6ee617a8fe
robotprom <98070647+robotprom@users.noreply.github.com>
2025-03-21 16:48:37 -0400

    Create dragonframe2024.sh

---
+++ fragments/labels/dragonframe2024.sh
@@ -0,0 +1,12 @@
+dragonframe2024)
+    name="DragonFrame 2024"
+    type="pkg"
+    curlOptions=( -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" )
+    appNewVersion=$(curl -s -A "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" https://www.dragonframe.com/downloads/ | grep -o 'Dragonframe_2024\.[0-9]\+\.[0-9]\+\.pkg' | head -n 1)
+    downloadURL="https://www.dragonframe.com/download/${appNewVersion}"
+    appName="Applications/Dragonframe 2024/Dragonframe 2024.app"
+    versionKey="CFBundleShortVersionString"
+    expectedTeamID="PG7SM8SD8M"
+    ;;
+    
+    
