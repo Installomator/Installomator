af007e0c786201f0387c5bcae636063cc3857b85
Aaron Polley <aarondavidpolley@gmail.com>
2025-03-25 23:28:34 +1000

    Update microsoftcompanyportal.sh

--- fragments/labels/microsoftcompanyportal.sh
+++ fragments/labels/microsoftcompanyportal.sh
@@ -2,6 +2,7 @@ microsoftcompanyportal)
     name="Company Portal"
     type="pkg"
     downloadURL="https://go.microsoft.com/fwlink/?linkid=853070"
+    appNewVersion=$(curl -s https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409IMCP01.xml | xmllint --format --xpath "//*/array/dict[1]" - | awk -F'<key>Title</key>' '{ print $2 }' | awk -F'</string>' '{ print $1 }' | sed 's/<string>//' | rev | cut -d' ' -f1 | rev | tr -d "\n")
     expectedTeamID="UBF8T346G9"
     if [[ -x "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" && $INSTALL != "force" && $DEBUG -eq 0 ]]; then
         printlog "Running msupdate --list"
