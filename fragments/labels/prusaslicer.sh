f2e0a344d6227353bfdd51579fec5cb7fa14b20e
robotprom <98070647+robotprom@users.noreply.github.com>
2025-03-26 17:46:54 -0400

    Update prusaslicer.sh

--- fragments/labels/prusaslicer.sh
+++ fragments/labels/prusaslicer.sh
@@ -1,7 +1,7 @@
 prusaslicer)
     name="PrusaSlicer"
     type="dmg"
-    appName="/Original Prusa Drivers/PrusaSlicer.app"
+    appName="PrusaSlicer.app"
     downloadURL="$(downloadURLFromGit prusa3d PrusaSlicer)"
     appNewVersion="$(versionFromGit prusa3d PrusaSlicer)"
     expectedTeamID="DKPB65N43Z"
