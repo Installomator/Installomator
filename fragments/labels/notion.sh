253214cdd18d789a27d81203a3d2a74aa4b54959
Abraham Tarverdi <atarverdi@coreweave.com>
2025-03-30 09:24:22 -0700

    fix for appNewVersion

--- fragments/labels/notion.sh
+++ fragments/labels/notion.sh
@@ -2,6 +2,6 @@ notion)
     name="Notion"
     type="dmg"
     downloadURL=$(curl -fsIL "https://www.notion.so/desktop/mac/download" | grep -i "^location" | awk '{print $2}' | tr -d '\r\n' | xargs)
-    appNewVersion=$(curl -fsIL "$downloadURL" | sed -e 's/.*Notion-\(.*\).dmg.*/\1/' | cut -d '-' -f 1)
+    appNewVersion=$(echo "$downloadURL" | sed -e 's/.*Notion-\(.*\).dmg.*/\1/' | cut -d '-' -f 1)
     expectedTeamID="LBQJ96FQ8D"
     ;;
