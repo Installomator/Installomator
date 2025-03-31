16b89331f7c75c7c9087469327f322afa5152bc8
Richard <xplora@wak.co.nz>
2025-03-31 14:57:45 +1300

    Update arguments.sh

--- fragments/arguments.sh
+++ fragments/arguments.sh
@@ -43,101 +43,98 @@ printlog "argumentsArray: ${argumentsArray[*]}" INFO
 # lowercase the label
 label=${label:l}
 
-# separate check for 'version' in order to print plain version number without any other information
-if [[ $label == "version" ]]; then
+# MARK: Case statement to filter (long) version checks
+skipLabelLookup=false
+case $label in
+version)
     echo "$VERSION"
     exit 0
-fi
-
-# NOTE: Use proxy for network access if defined
-if [[ -n $PROXY ]]; then
-    printlog "Proxy defined: $PROXY, testing access to it" REQ
-    proxyAddress=$(echo $PROXY | cut -d ":" -f1)
-    portNumber=$(echo $PROXY | cut -d ":" -f2)
-    printlog "Proxy: $proxyAddress, Port: $portNumber"
-    if cmdOutput=$(! nc -z -v -G 10 ${proxyAddress} ${portNumber} 2>&1) ; then
-        printlog "$cmdOutput" REQ
-        printlog "ERROR : No proxy connection, skipping this." REQ
-    else
-        printlog "Proxy access detected, so using that." REQ
-        export ALL_PROXY="$PROXY"
-    fi
-fi
-
-# MARK: Logging
-log_location="/private/var/log/Installomator.log"
-
-# Check if we're in debug mode, if so then set logging to DEBUG, otherwise default to INFO
-# if no log level is specified.
-if [[ $DEBUG -ne 0 ]]; then
-    LOGGING=DEBUG
-elif [[ -z $LOGGING ]]; then
-    LOGGING=INFO
-    datadogLoggingLevel=INFO
-fi
-
-# Associate logging levels with a numerical value so that we are able to identify what
-# should be removed. For example if the LOGGING=ERROR only printlog statements with the
-# level REQ and ERROR will be displayed. LOGGING=DEBUG will show all printlog statements.
-# If a printlog statement has no level set it's automatically assigned INFO.
-
-declare -A levels=(DEBUG 0 INFO 1 WARN 2 ERROR 3 REQ 4)
-
-# If we are able to detect an MDM URL (Jamf Pro) or another identifier for a customer/instance we grab it here, this is useful if we're centrally logging multiple MDM instances.
-if [[ -f /Library/Preferences/com.jamfsoftware.jamf.plist ]]; then
-    mdmURL=$(defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url)
-elif [[ -n "$MDMProfileName" ]]; then
-    mdmURL=$(sudo profiles show | grep -A3 "$MDMProfileName" | sed -n -e 's/^.*organization: //p')
-else
-    mdmURL="Unknown"
-fi
-
-# Generate a session key for this run, this is useful to idenify streams when we're centrally logging.
-SESSION=$RANDOM
-
-# MARK: START
-printlog "################## Start Installomator v. $VERSION, date $VERSIONDATE" REQ
-printlog "################## Version: $VERSION" INFO
-printlog "################## Date: $VERSIONDATE" INFO
-printlog "################## $label" INFO
-
-# Check for DEBUG mode
-if [[ $DEBUG -gt 0 ]]; then
-    printlog "DEBUG mode $DEBUG enabled." DEBUG
-fi
-
-# How we get version number from app
-if [[ -z $versionKey ]]; then
-    versionKey="CFBundleShortVersionString"
-fi
-
-# get current user
-currentUser=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ { print $3 }')
-
-# NOTE: check for root
-if [[ "$(whoami)" != "root" && "$DEBUG" -eq 0 ]]; then
-    # not running as root
-    cleanupAndExit 6 "not running as root, exiting" ERROR
-fi
-
-# check Swift Dialog presence and version
-DIALOG_CMD="/usr/local/bin/dialog"
-
-if [[ ! -x $DIALOG_CMD ]]; then
-    # Swift Dialog is not installed, clear cmd file variable to ignore
-    printlog "SwiftDialog is not installed, clear cmd file var"
-    DIALOG_CMD_FILE=""
-fi
-
-# MARK: labels in case statement
-case $label in
+    ;;
 longversion)
     # print the script version
-    printlog "Installomater: version $VERSION ($VERSIONDATE)" REQ
+    echo "Installomator: version $VERSION ($VERSIONDATE)"
     exit 0
     ;;
-valuesfromarguments)
-    # no action necessary, all values should be provided in arguments
-    ;;
+*)
+
+    # Check if we're in debug mode, if so then set logging to DEBUG, otherwise default to INFO
+    # if no log level is specified.
+    if [[ $DEBUG -ne 0 ]]; then
+        LOGGING=DEBUG
+    elif [[ -z $LOGGING ]]; then
+        LOGGING=INFO
+        datadogLoggingLevel=INFO
+    fi
+
+    # NOTE: Use proxy for network access if defined
+    if [[ -n $PROXY ]]; then
+        printlog "Proxy defined: $PROXY, testing access to it" REQ
+        proxyAddress=$(echo $PROXY | cut -d ":" -f1)
+        portNumber=$(echo $PROXY | cut -d ":" -f2)
+        printlog "Proxy: $proxyAddress, Port: $portNumber"
+        if cmdOutput=$(! nc -z -v -G 10 ${proxyAddress} ${portNumber} 2>&1) ; then
+            printlog "$cmdOutput" REQ
+            printlog "ERROR : No proxy connection, skipping this." REQ
+        else
+            printlog "Proxy access detected, so using that." REQ
+            export ALL_PROXY="$PROXY"
+        fi
+    fi
+
+    # If we are able to detect an MDM URL (Jamf Pro) or another identifier for a customer/instance we grab it here, this is useful if we're centrally logging multiple MDM instances.
+    if [[ -f /Library/Preferences/com.jamfsoftware.jamf.plist ]]; then
+        mdmURL=$(defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url)
+    elif [[ -n "$MDMProfileName" ]]; then
+        mdmURL=$(sudo profiles show | grep -A3 "$MDMProfileName" | sed -n -e 's/^.*organization: //p')
+    else
+        mdmURL="Unknown"
+    fi
+
+# MARK: START
+    printlog "################## Start Installomator v. $VERSION, date $VERSIONDATE" REQ
+    printlog "################## Version: $VERSION" INFO
+    printlog "################## Date: $VERSIONDATE" INFO
+    printlog "################## $label" INFO
+
+    # Check for DEBUG mode
+    if [[ $DEBUG -gt 0 ]]; then
+        printlog "DEBUG mode $DEBUG enabled." DEBUG
+    fi
+
+    # get current user
+    currentUser=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ { print $3 }')
+
+    # NOTE: check for root
+    if [[ "$(whoami)" != "root" && "$DEBUG" -eq 0 ]]; then
+        # not running as root
+        cleanupAndExit 6 "not running as root, exiting" ERROR
+    fi
+
+    # check Swift Dialog presence and version
+    DIALOG_CMD="/usr/local/bin/dialog"
+
+    if [[ ! -x $DIALOG_CMD ]]; then
+        # Swift Dialog is not installed, clear cmd file variable to ignore
+        printlog "SwiftDialog is not installed, clear cmd file var"
+        DIALOG_CMD_FILE=""
+    fi
+
+    # prepare github
+    githubAUTH=()
+    if [[ -n $GITHUBAPI ]]; then
+        githubAUTH=( --header "Authorization: Bearer $GITHUBAPI" )
+        checkRATEfromGit API
+        githubauthfail=$?
+        if [ $githubauthfail -gt 0 ]; then
+            githubAUTH=()
+            printlog "ignoring GITHUBAPI due to access issues ($(case $githubauthfail in 1) echo "no access, is the token correct? does it exist?" ;; 2) echo "insufficient access, public_repo, and read:packages are required" ;; *) echo "unknown" ;; esac)" WARN
+        fi
+    fi
+
+# MARK: labels in case statement
+    case $label in
+    valuesfromarguments)
+        # no action necessary, all values should be provided in arguments
+        ;;
 
 # label descriptions start here
