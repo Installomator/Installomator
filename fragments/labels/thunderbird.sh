edb392a4b3e2bc178e3a3682d89da71503a79a0e
Maarten Wijnants <wijnants.maarten@gmail.com>
2025-04-01 11:37:55 +0200

    Update thunderbird label

--- fragments/labels/thunderbird.sh
+++ fragments/labels/thunderbird.sh
@@ -1,7 +1,7 @@
 thunderbird)
     name="Thunderbird"
     type="dmg"
-	appNewVersion=$(curl -fsL "https://www.thunderbird.net/en-US/thunderbird/releases/atom.xml" | grep -v "esr" | grep -m 5 "<title type=" | sed 's/<.*>\(.*\)<.*>/\1/' | awk '{ print $2 }' | awk '{$1=$1};1' | sort -V | tail -n1 )
     downloadURL="https://download.mozilla.org/?product=thunderbird-latest&os=osx&lang=en-US"
+    appNewVersion=$(curl -fsIL "$downloadURL" | grep -i Location | grep -o "/[0-9.]\+/" | grep -o "[0-9.]\+")
     expectedTeamID="43AQ936H96"
     ;;
