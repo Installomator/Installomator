8c38719668ea56980ee11a184bcba4aab08ae3a2
NekoPavel <neko@pavel.gg>
2025-04-02 13:17:34 +0200

    Removed unnecessary archiveName

--- fragments/labels/topazvideo.sh
+++ fragments/labels/topazvideo.sh
@@ -4,6 +4,5 @@ topazvideoai)
     type="dmg"
     appNewVersion=$(curl -fs https://www.topazlabs.com/downloads | grep  -o 'videoVersion = "v.*"' | grep -o ' "v.*"' | sed -E 's/[v|"| ]//g')
     downloadURL="https://topazlabs.com/d/tvai/latest/mac/full"
-    archiveName="TopazVideoAI-${appNewVersion}.dmg"
     expectedTeamID="3G3JE37ZHF"
     ;;
