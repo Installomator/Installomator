ff75b1a47aded1fbd675a8f68626a29e08d8ac3f
NekoPavel <neko@pavel.gg>
2025-04-02 10:06:43 +0200

    Changed package type to dmg

--- fragments/labels/topazvideo.sh
+++ fragments/labels/topazvideo.sh
@@ -1,9 +1,9 @@
 topazvideo|\
 topazvideoai)
     name="Topaz Video AI"
-    type="pkg"
+    type="dmg"
     appNewVersion=$(curl -fs https://www.topazlabs.com/downloads | grep  -o 'videoVersion = "v.*"' | grep -o ' "v.*"' | sed -E 's/[v|"| ]//g')
     downloadURL="https://topazlabs.com/d/tvai/latest/mac/full"
-    archiveName="TopazVideoAI-${appNewVersion}.pkg"
+    archiveName="TopazVideoAI-${appNewVersion}.dmg"
     expectedTeamID="3G3JE37ZHF"
     ;;
