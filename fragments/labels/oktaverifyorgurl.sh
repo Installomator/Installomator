f8e426247d15b125ec5797988286be810a1755fa
edb-we-ap <alex.powell@enterprisedb.com>
2025-04-07 13:00:16 -0400

    oktaverifyorgurl new label
    This label is different from oktaverify as if there is a orgurl set in the plist (via MDM) then it will use that. Here is the output, I changed the oktaTenant to be okta.okta.com for ambiguity
    
    ```sudo Installomator/utils/assemble.sh oktaverifyorgurl DEBUG=0
    
    2025-04-07 12:56:46 : INFO  : oktaverifyorgurl : setting variable from argument DEBUG=0
    2025-04-07 12:56:46 : INFO  : oktaverifyorgurl : Total items in argumentsArray: 1
    2025-04-07 12:56:46 : INFO  : oktaverifyorgurl : argumentsArray: DEBUG=0
    2025-04-07 12:56:46 : REQ   : oktaverifyorgurl : ################## Start Installomator v. 10.9beta, date 2025-04-07
    2025-04-07 12:56:46 : INFO  : oktaverifyorgurl : ################## Version: 10.9beta
    2025-04-07 12:56:46 : INFO  : oktaverifyorgurl : ################## Date: 2025-04-07
    2025-04-07 12:56:46 : INFO  : oktaverifyorgurl : ################## oktaverifyorgurl
    2025-04-07 12:56:46 : INFO  : oktaverifyorgurl : SwiftDialog is not installed, clear cmd file var
    2025-04-07 12:56:47 : INFO  : oktaverifyorgurl : Reading arguments again: DEBUG=0
    2025-04-07 12:56:47 : INFO  : oktaverifyorgurl : BLOCKING_PROCESS_ACTION=tell_user
    2025-04-07 12:56:47 : INFO  : oktaverifyorgurl : NOTIFY=success
    2025-04-07 12:56:47 : INFO  : oktaverifyorgurl : LOGGING=INFO
    2025-04-07 12:56:47 : INFO  : oktaverifyorgurl : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2025-04-07 12:56:47 : INFO  : oktaverifyorgurl : Label type: pkg
    2025-04-07 12:56:47 : INFO  : oktaverifyorgurl : archiveName: Okta Verify.pkg
    2025-04-07 12:56:47 : INFO  : oktaverifyorgurl : no blocking processes defined, using Okta Verify as default
    2025-04-07 12:56:47 : INFO  : oktaverifyorgurl : name: Okta Verify, appName: Okta Verify.app
    2025-04-07 12:56:48 : WARN  : oktaverifyorgurl : No previous app found
    2025-04-07 12:56:48 : WARN  : oktaverifyorgurl : could not find Okta Verify.app
    2025-04-07 12:56:48 : INFO  : oktaverifyorgurl : appversion:
    2025-04-07 12:56:48 : INFO  : oktaverifyorgurl : Latest version of Okta Verify is 9.37.0
    2025-04-07 12:56:48 : REQ   : oktaverifyorgurl : Downloading https://okta.okta.com/api/v1/artifacts/OKTA_VERIFY_MACOS/download?releaseChannel=GA to Okta Verify.pkg
    2025-04-07 12:56:52 : INFO  : oktaverifyorgurl : Downloaded Okta Verify.pkg – Type is  xar archive compressed TOC – SHA is ddb4ba8c0a64bf96a1f6fe112f8d75cd97c0b412 – Size is 65148 kB
    2025-04-07 12:56:52 : REQ   : oktaverifyorgurl : no more blocking processes, continue with update
    2025-04-07 12:56:52 : REQ   : oktaverifyorgurl : Installing Okta Verify
    2025-04-07 12:56:52 : INFO  : oktaverifyorgurl : Verifying: Okta Verify.pkg
    2025-04-07 12:56:53 : INFO  : oktaverifyorgurl : Team ID: B7F62B65BN (expected: B7F62B65BN )
    2025-04-07 12:56:53 : INFO  : oktaverifyorgurl : Installing Okta Verify.pkg to /
    2025-04-07 12:56:54 : INFO  : oktaverifyorgurl : Finishing...
    2025-04-07 12:56:57 : INFO  : oktaverifyorgurl : name: Okta Verify, appName: Okta Verify.app
    2025-04-07 12:56:57 : WARN  : oktaverifyorgurl : No previous app found
    2025-04-07 12:56:57 : WARN  : oktaverifyorgurl : could not find Okta Verify.app
    2025-04-07 12:56:57 : REQ   : oktaverifyorgurl : Installed Okta Verify, version 9.37.0
    2025-04-07 12:56:57 : INFO  : oktaverifyorgurl : notifying
    displaynotification:7: no such file or directory: /usr/local/bin/dialog
    2025-04-07 12:56:58 : INFO  : oktaverifyorgurl : Installomator did not close any apps, so no need to reopen any apps.
    2025-04-07 12:56:58 : REQ   : oktaverifyorgurl : All done!
    2025-04-07 12:56:58 : REQ   : oktaverifyorgurl : ################## End Installomator, exit code 0
    ```

--- fragments/labels/oktaverifyorgurl.sh
+++ fragments/labels/oktaverifyorgurl.sh
@@ -1,11 +1,11 @@
-oktaverify)
+oktaverifyorgurl)
     name="Okta Verify"
     type="pkg"
     oktaTenant=$(defaults read /Library/Managed\ Preferences/com.okta.mobile.plist OktaVerify.OrgUrl)
     if [[ -z "$oktaTenant" ]]; then
       cleanupAndExit "Okta Tenant not set" ERROR
     fi
-    downloadURL="https://$oktaTenant/api/v1/artifacts/OKTA_VERIFY_MACOS/download?releaseChannel=GA"
+    downloadURL="$oktaTenant/api/v1/artifacts/OKTA_VERIFY_MACOS/download?releaseChannel=GA"
     appNewVersion=$(curl -is "$downloadURL" | grep ocation: | grep -o "OktaVerify.*pkg" | cut -d "-" -f 2)
     expectedTeamID="B7F62B65BN"
     ;;
