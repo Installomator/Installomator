9ee6418f433fbfb618a475378c50e44ca5791895
Richard <xplora@wak.co.nz>
2025-04-14 13:31:17 +1200

    Update synologydriveclient.sh
    One last consistency check, making sure the sed's are formatted the same way, so the differences are more obvious.

--- fragments/labels/synologydriveclient.sh
+++ fragments/labels/synologydriveclient.sh
@@ -3,7 +3,7 @@ synologydriveclient)
     type="pkgInDmg"
     versionKey="CFBundleVersion"
     appFullVersion="$( curl -sf "https://archive.synology.com/download/Utility/SynologyDriveClient" | grep -m 1 "/download/Utility/SynologyDriveClient/" | sed "s|.*>\(.*\)<.*|\\1|" )"
-    appNewVersion="$( echo $appFullVersion | sed 's#.*-\(\)#\1#' )"
+    appNewVersion="$( echo $appFullVersion | sed "s|.*-\(\)|\\1|" )"
     downloadURL="https://global.download.synology.com/download/Utility/SynologyDriveClient/$appFullVersion/Mac/Installer/synology-drive-client-$appNewVersion.dmg"
     expectedTeamID="X85BAK35Y4"
     ;;
