7b48e5026cd4fbb9979ffe7700e600d359b3cc4a
Cory Funk <cafunk@scatcat.fhsu.edu>
2025-04-15 08:21:24 -0500

    Create networkutility.sh
    Added Neo Network Utility

---
+++ fragments/labels/networkutility.sh
@@ -0,0 +1,7 @@
+networkutility)
+    name="Network Utility"
+    type="appInDmgInZip"
+    downloadURL="https://download.devontechnologies.com/download/freeware/networkutility/1.1.1/Neo_Network_Utility.dmg.zip"
+    appNewVersion=""
+    expectedTeamID="679S2QUWR8"
+    ;;
