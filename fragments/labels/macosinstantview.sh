8106d659fa6cb34c315a0e02f10eb924a1db312e
Cory Funk <cafunk@scatcat.fhsu.edu>
2025-04-15 08:19:15 -0500

    Create macosinstantview.sh
    Added macOS InstantView

---
+++ fragments/labels/macosinstantview.sh
@@ -0,0 +1,7 @@
+macosinstantview)
+    name="macOS InstantView"
+    type="dmg"
+    downloadURL="https://plugable.s3.us-east-1.amazonaws.com/bin/SMI/mac/macOS_InstantView_V3.22R0002.dmg"
+    appNewVersion=""
+    expectedTeamID="JAGYSS7E9A"
+    ;;
