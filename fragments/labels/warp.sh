3ccdb54984c53011291ccd8426f7f5c4a08a361c
Malte Kiefer <59220985+MalteKiefer@users.noreply.github.com>
2025-05-25 07:08:02 +0200

    Update warp.sh

--- fragments/labels/warp.sh
+++ fragments/labels/warp.sh
@@ -2,6 +2,6 @@ warp)
     name="Warp"
     type="dmg"
     downloadURL="https://app.warp.dev/download"
-    appNewVersion="$(curl -s https://releases.warp.dev/channel_versions.json | grep -A 3 '"stable"' | grep '"version"' | head -n 1 | sed -E 's/.*"version": *"v([^"]+)".*/\1/')"
+    appNewVersion=$(curl -s https://releases.warp.dev/channel_versions.json | grep -A 3 '"stable"' | grep '"version"' | head -n 1 | sed -E 's/.*"version": *"v([^"]+)".*/\1/')
     expectedTeamID="2BBY89MBSN"
     ;;
