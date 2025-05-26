f05a1e1906903fdca6c710d22cb38bee77f57d37
Kellan St.Louis <kstlouis@me.com>
2025-05-26 16:12:24 -0600

    Create superwhisper.sh
    add new label "superwhisper"

---
+++ fragments/labels/superwhisper.sh
@@ -0,0 +1,7 @@
+superwhisper)
+    name="superwhisper"
+    type="dmg"
+    downloadURL="https://builds.superwhisper.com/latest/superwhisper.dmg"
+    appNewVersion=""
+    expectedTeamID="XDP69BYUP9"
+    ;;
