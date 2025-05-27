f6c1c7bc4563abc70ddb245ea61985e271182fc8
Kellan St.Louis <kstlouis@me.com>
2025-05-26 22:48:14 -0600

    get appNewVersion via SparkleFeed. add line feed.

--- fragments/labels/superwhisper.sh
+++ fragments/labels/superwhisper.sh
@@ -2,6 +2,8 @@ superwhisper)
     name="superwhisper"
     type="dmg"
     downloadURL="https://builds.superwhisper.com/latest/superwhisper.dmg"
-    appNewVersion=""
+    sparkleFeed=$(curl -fsL "https://superwhisper.com/appcast.xml")
+    appNewVersion=$(echo $sparkleFeed | xpath 'string(//rss/channel/item[1]/sparkle:version)' 2>/dev/null)
     expectedTeamID="XDP69BYUP9"
     ;;
+    
