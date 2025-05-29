45f886df88b599fa1f6b38a8137ff6da5075913e
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2025-05-29 09:30:54 -0600

    Updated New Label silentknight.sh
    Fix case with name variable

--- fragments/labels/silentknight.sh
+++ fragments/labels/silentknight.sh
@@ -1,6 +1,6 @@
 silentknight)
     # SilentKnight automatic checking of security systems
-    name="silentknight"
+    name="SilentKnight"
     type="zip"
     folderName="$(curl -fs https://eclecticlight.co/downloads/ | grep -o 'silentknight[0-9]*\.zip' | sort -V | tail -n 1 | sed -E 's/silentknight([0-9]+)\.zip/silentknight\1/')"
     appName="${folderName}/SilentKnight.app"
