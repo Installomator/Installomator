690c3dbbb02c5a557b4962969728c3621e17bcad
Tadayuki Onishi <tonishi@folio-sec.com>
2025-05-29 12:02:00 +0900

    new label: microsoftglobalsecureaccessclient

---
+++ fragments/labels/microsoftglobalsecureaccessclient.sh
@@ -0,0 +1,7 @@
+microsoftglobalsecureaccessclient)
+    name="Global Secure Access Client"
+    type="pkg"
+    packageID="com.microsoft.naas.globalsecure"
+    downloadURL="https://aka.ms/GlobalSecureAccess-macOS"
+    expectedTeamID="UBF8T346G9"
+    ;;
