be91924d266a0bdeee319805bad86b453ea49a7e
Jared Schwager <jared.schwager@gmail.com>
2025-06-09 13:46:49 -0400

    Updated label to utilize JSON feed for appNewVersion

--- fragments/labels/cursorai.sh
+++ fragments/labels/cursorai.sh
@@ -1,7 +1,9 @@
 cursorai)
     name="Cursor"
     type="dmg"
-    downloadURL=$(curl -fsL "https://www.cursor.com/api/download?platform=darwin-universal&releaseTrack=stable" | grep -o '"downloadUrl":"[^"]*"' | sed 's/"downloadUrl":"\([^"]*\)"/\1/')
+    updateFeed=$(curl -fsL  "https://www.cursor.com/api/download?platform=darwin-universal&releaseTrack=stable")
+    appNewVersion=$(getJSONValue "${updateFeed}" "version")
+    downloadURL=$(getJSONValue "${updateFeed}" "downloadUrl")
     expectedTeamID="VDXQ22DGB9"
     blockingProcesses=( "Cursor" )
     ;;
