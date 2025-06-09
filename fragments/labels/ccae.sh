d5913fdcd40199e7198f50e576f6826f851e0de0
jwbpzb <jwbpzb@umsystem.edu>
2025-06-09 11:19:10 -0500

    Missed the e in the packageID

--- fragments/labels/ccae.sh
+++ fragments/labels/ccae.sh
@@ -1,7 +1,7 @@
 ccae)
     name="ccae"
     type="dmg"
-    packageID="com.electron.cca"
+    packageID="com.electron.ccae"
     downloadURL=$(downloadURLFromGit "thepaciellogroup" "ccae")
     appNewVersion=$(versionFromGit "thepaciellogroup" "ccae")
     expectedTeamID="34RS4UC3M6"
