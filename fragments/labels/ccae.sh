659360af2943113520594d9450c14fef853779ce
jwbpzb <jwbpzb@umsystem.edu>
2025-06-09 10:58:03 -0500

    Create a label for CCAe

---
+++ fragments/labels/ccae.sh
@@ -0,0 +1,9 @@
+ccae)
+    name="ccae"
+    type="dmg"
+    packageID="com.electron.cca"
+    downloadURL=$(downloadURLFromGit "thepaciellogroup" "ccae")
+    appNewVersion=$(versionFromGit "thepaciellogroup" "ccae")
+    expectedTeamID="34RS4UC3M6"
+    blockingProcesses=( NONE )
+    ;;
