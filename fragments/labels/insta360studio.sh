f3f235c5da232a231129037ac536ef56524980a0
Tully <tjagoe@systima.com.au>
2025-06-16 16:47:56 +1000

    Expanded appNewVersion, removed redundant echo from testing

--- fragments/labels/insta360studio.sh
+++ fragments/labels/insta360studio.sh
@@ -4,8 +4,8 @@ insta360)
     name="Insta360 Studio"
     type="pkg"
     pkgName=$(curl -fs -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15" "https://www.insta360.com/download/hot-download" | sed -n 's/.*\(Insta360Studio_.*\.pkg\).*/\1/p')
-    appNewVersion=$(echo "${archiveName}" | sed -n 's/.*Insta360Studio_\([0-9.]*\)_release_insta360.*/\1/p')
+    appNewVersion=$(curl -fs -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15" "https://www.insta360.com/download/hot-download" | sed -n 's/.*Insta360Studio_\([0-9.]*\)_release_insta360.*/\1/p')
     versionKey="CFBundleShortVersionString"
-    downloadURL=$(curl -fs -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15" "https://www.insta360.com/download/hot-download" | sed -n 's/.*\(https:\/\/.*\.pkg\).*/\1/p') ; echo "${downloadURL}"
+    downloadURL=$(curl -fs -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15" "https://www.insta360.com/download/hot-download" | sed -n 's/.*\(https:\/\/.*\.pkg\).*/\1/p')
     expectedTeamID="UBX9CH9GDX"
     ;;
