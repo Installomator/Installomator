0cfc24b61e31fa38dcca635941048a3393025834
Tully <tjagoe@systima.com.au>
2025-06-16 16:36:29 +1000

    New app label: Insta360 Studio

---
+++ fragments/labels/insta360studio.sh
@@ -0,0 +1,11 @@
+insta360studio|\
+insta360)
+    # credit: Tully Jagoe
+    name="Insta360 Studio"
+    type="pkg"
+    pkgName=$(curl -fs -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15" "https://www.insta360.com/download/hot-download" | sed -n 's/.*\(Insta360Studio_.*\.pkg\).*/\1/p')
+    appNewVersion=$(echo "${archiveName}" | sed -n 's/.*Insta360Studio_\([0-9.]*\)_release_insta360.*/\1/p')
+    versionKey="CFBundleShortVersionString"
+    downloadURL=$(curl -fs -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15" "https://www.insta360.com/download/hot-download" | sed -n 's/.*\(https:\/\/.*\.pkg\).*/\1/p') ; echo "${downloadURL}"
+    expectedTeamID="UBX9CH9GDX"
+    ;;
