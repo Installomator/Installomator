05a74bc5ec647694a8b8a085808c65061a0c86b3
Gil Burns <gil@gilburns.com>
2025-06-19 22:44:06 -0500

    Update chatgpt.sh

--- fragments/labels/chatgpt.sh
+++ fragments/labels/chatgpt.sh
@@ -4,7 +4,7 @@ chatgpt)
     if [[ $(arch) == arm64 ]]; then
         downloadURL="https://persistent.oaistatic.com/sidekick/public/ChatGPT_Desktop_public_latest.dmg"
     else
-        downloadURL="" # Apple Silicon only
+        cleanupandexit 2 "No Intel-compatible download URL found. $appLabel is not Intel-compatible. Could not install app." ERROR
     fi
     appNewVersion="$(curl -fs "https://persistent.oaistatic.com/sidekick/public/sparkle_public_appcast.xml" | xpath '(//rss/channel/item/title)[1]/text()' 2>/dev/null)"
     expectedTeamID="2DC432GLL2"
