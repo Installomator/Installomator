b7a0d388d63a096610419ad9e47bb8ae7f3e6769
Maarten Wijnants <wijnants.maarten@gmail.com>
2025-07-15 15:48:00 +0200

    Update googledrive.sh

--- fragments/labels/googledrive.sh
+++ fragments/labels/googledrive.sh
@@ -8,7 +8,7 @@ googledrivefilestream)
     elif [[ $(arch) == "i386" ]]; then
        packageID="com.google.drivefs.x86_64"
     fi
-    appNewVersion=$(curl -s "https://community.chocolatey.org/packages/googledrive" | xmllint --html --xpath 'substring-after(string(//h1[@class="mb-0 text-center"]), "Google Drive")' - 2> /dev/null | xargs)
+    appNewVersion=$(curl -s "https://community.chocolatey.org/packages/googledrive" | xmllint --html --xpath 'substring-after(string(//h1[@class="mb-0 text-center"]), "Google Drive")' - 2> /dev/null | tr -d '[:space:]')
     downloadURL="https://dl.google.com/drive-file-stream/GoogleDriveFileStream.dmg" # downloadURL="https://dl.google.com/drive-file-stream/GoogleDrive.dmg"
     blockingProcesses=( "Google Docs" "Google Drive" "Google Sheets" "Google Slides" )
     appName="Google Drive.app"
