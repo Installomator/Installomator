85f9fd706a444e3aea800b3143673c527017c427
Maarten Wijnants <wijnants.maarten@gmail.com>
2025-07-15 11:18:55 +0200

    Update teamviewer.sh

--- fragments/labels/teamviewer.sh
+++ fragments/labels/teamviewer.sh
@@ -5,6 +5,6 @@ teamviewer)
     versionKey="CFBundleShortVersionString"
     pkgName="Install TeamViewer.app/Contents/Resources/Install TeamViewer.pkg"
     downloadURL="https://download.teamviewer.com/download/TeamViewer.dmg"
-    appNewVersion=$(curl -fsL "https://www.teamviewer.com/en/download/macos/" | grep "Current version" | awk -F': ' '{ print $2 }' | sed 's/<[^>]*>//g')
+    appNewVersion=$(curl -fsL "https://www.teamviewer.com/en-US/download/portal/macos/" | grep "Current version" | awk -F': ' '{ print $2 }' | sed 's/<[^>]*>//g')
     expectedTeamID="H7UGFBUGV6"
     ;;
