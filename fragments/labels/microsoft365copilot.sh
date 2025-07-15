6f515b44d93b70e5210ca570f6b888eb1cd645cc
Aimé Bertrand <aime.bertrand@macowners.club>
2025-07-15 14:34:43 +0200

    Update Label for PR: Microsoft 365 Copilot

--- fragments/labels/microsoft365copilot.sh
+++ fragments/labels/microsoft365copilot.sh
@@ -5,4 +5,10 @@ microsoft365copilot)
     versionKey="CFBundleVersion"
     appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | grep -o "/Microsoft_365_Copilot.*pkg" | sed -E 's/.*_([0-9]+\.[0-9]+\.[0-9]+)_.*\.pkg/\1/')
     expectedTeamID="UBF8T346G9"
+    if [[ -x "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" && $INSTALL != "force" ]]; then
+        printlog "Running msupdate --list"
+        "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" --list
+    fi
+    updateTool="/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate"
+    updateToolArguments=( --install --apps MSCP10 )
     ;;
