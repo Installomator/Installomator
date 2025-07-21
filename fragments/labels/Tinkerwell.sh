c1d8d5c2f62d9e04ece97d8934bd5c8d44abff33
Walez-9925 <waleed.ahmed@miswag.com>
2025-07-21 15:31:35 +0300

    Tinkerwell

---
+++ fragments/labels/Tinkerwell.sh
@@ -0,0 +1,7 @@
+Tinkerwell)
+    name="Tinkerwell"
+    type="dmg"
+    downloadURL="https://download.tinkerwell.app/tinkerwell/Tinkerwell-5.0.1-arm64.dmg"
+    appNewVersion="5.0.1"
+    expectedTeamID="8Z259RPWAC"
+    ;;
