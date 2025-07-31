f80a664953fa79db4e1b59a7fb476c0da5e3c106
macmangr <106015597+macmangr@users.noreply.github.com>
2025-07-31 12:19:15 +0200

    Update doctolib.sh
    The developer now provides static download links for the latest version of the software – for both ARM and Intel Macs.
    
    2025-07-31 12:18:10 : INFO  : doctolib : setting variable from argument DEBUG=0
    2025-07-31 12:18:10 : INFO  : doctolib : Total items in argumentsArray: 1
    2025-07-31 12:18:10 : INFO  : doctolib : argumentsArray: DEBUG=0
    2025-07-31 12:18:10 : REQ   : doctolib : ################## Start Installomator v. 10.8, date 2025-07-31
    2025-07-31 12:18:10 : INFO  : doctolib : ################## Version: 10.8
    2025-07-31 12:18:10 : INFO  : doctolib : ################## Date: 2025-07-31
    2025-07-31 12:18:10 : INFO  : doctolib : ################## doctolib
    2025-07-31 12:18:11 : INFO  : doctolib : Reading arguments again: DEBUG=0
    2025-07-31 12:18:11 : INFO  : doctolib : BLOCKING_PROCESS_ACTION=tell_user
    2025-07-31 12:18:11 : INFO  : doctolib : NOTIFY=success
    2025-07-31 12:18:11 : INFO  : doctolib : LOGGING=INFO
    2025-07-31 12:18:11 : INFO  : doctolib : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2025-07-31 12:18:11 : INFO  : doctolib : Label type: dmg
    2025-07-31 12:18:11 : INFO  : doctolib : archiveName: Doctolib.dmg
    2025-07-31 12:18:11 : INFO  : doctolib : no blocking processes defined, using Doctolib as default
    2025-07-31 12:18:11 : INFO  : doctolib : App(s) found: /Applications/Doctolib.app
    2025-07-31 12:18:11 : INFO  : doctolib : found app at /Applications/Doctolib.app, version 2.69.1, on versionKey CFBundleVersion
    2025-07-31 12:18:11 : INFO  : doctolib : appversion: 2.69.1
    2025-07-31 12:18:11 : INFO  : doctolib : Latest version not specified.
    2025-07-31 12:18:11 : REQ   : doctolib : Downloading https://ddv-install.doctolib.fr/DoctolibProDesktop-latest-arm64.dmg to Doctolib.dmg
    2025-07-31 12:18:17 : INFO  : doctolib : Downloaded Doctolib.dmg – Type is  bzip2 compressed data, block size = 100k – SHA is 801747dad89508f5288b8cfe9cc7c2003df2e7a4 – Size is 164200 kB
    2025-07-31 12:18:17 : REQ   : doctolib : no more blocking processes, continue with update
    2025-07-31 12:18:17 : REQ   : doctolib : Installing Doctolib
    2025-07-31 12:18:17 : INFO  : doctolib : Mounting /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.oN2UNNud2f/Doctolib.dmg
    2025-07-31 12:18:29 : INFO  : doctolib : Mounted: /Volumes/Doctolib 2.73.0-arm64
    2025-07-31 12:18:29 : INFO  : doctolib : Verifying: /Volumes/Doctolib 2.73.0-arm64/Doctolib.app
    2025-07-31 12:18:40 : INFO  : doctolib : Team ID matching: 84K7XVJ72Q (expected: 84K7XVJ72Q )
    2025-07-31 12:18:40 : INFO  : doctolib : Downloaded version of Doctolib is 2.73.0 on versionKey CFBundleVersion (replacing version 2.69.1).
    2025-07-31 12:18:40 : INFO  : doctolib : App has LSMinimumSystemVersion: 11.0
    2025-07-31 12:18:40 : WARN  : doctolib : Removing existing /Applications/Doctolib.app
    2025-07-31 12:18:40 : INFO  : doctolib : Copy /Volumes/Doctolib 2.73.0-arm64/Doctolib.app to /Applications
    2025-07-31 12:18:46 : WARN  : doctolib : Changing owner to savvas
    2025-07-31 12:18:46 : INFO  : doctolib : Finishing...
    2025-07-31 12:18:49 : INFO  : doctolib : App(s) found: /Applications/Doctolib.app
    2025-07-31 12:18:49 : INFO  : doctolib : found app at /Applications/Doctolib.app, version 2.73.0, on versionKey CFBundleVersion
    2025-07-31 12:18:49 : REQ   : doctolib : Installed Doctolib, version 2.73.0
    2025-07-31 12:18:49 : INFO  : doctolib : notifying
    2025-07-31 12:18:50 : INFO  : doctolib : Installomator did not close any apps, so no need to reopen any apps.
    2025-07-31 12:18:50 : REQ   : doctolib : All done!
    2025-07-31 12:18:50 : REQ   : doctolib : ################## End Installomator, exit code 0

--- fragments/labels/doctolib.sh
+++ fragments/labels/doctolib.sh
@@ -3,11 +3,11 @@ doctolib)
     type="dmg"
     versionKey="CFBundleVersion"
     if [[ $(arch) == "arm64" ]]; then
-    	downloadURL="$(curl -fs https://info.doctolib.de/desktop/ | sed -n '/macOS/,$p' | grep -o '"title":"Mac","url":"[^"]*' | sed -E 's/.*"url":"(.*)/\1/' | sed 's/\\//g' | sed -n '1p')"
-    	appNewVersion=""
+        downloadURL="https://ddv-install.doctolib.fr/DoctolibProDesktop-latest-arm64.dmg"
+        appNewVersion=""
     elif [[ $(arch) == "i386" ]]; then
-    	downloadURL="$(curl -fs https://info.doctolib.de/desktop/ | sed -n '/macOS/,$p' | grep -o '"title":"Mac","url":"[^"]*' | sed -E 's/.*"url":"(.*)/\1/' | sed 's/\\//g' | sed -n '2p')"
-    	appNewVersion=""
+        downloadURL="https://ddv-install.doctolib.fr/DoctolibProDesktop-latest.dmg"
+        appNewVersion=""
     fi
     expectedTeamID="84K7XVJ72Q"
     # blockingProcesses=( Doctolib )
