f42657458ca5bac0da62be1b6669f8e929d77ffe
John Bowman <jcmbowman@gmail.com>
2025-08-13 13:33:23 -0400

    Update keyshotstudio.sh
    Hardcode DL URL since the old one was pinned to v13.2 and no new 'latest' download URL seems to be available.

--- fragments/labels/keyshotstudio.sh
+++ fragments/labels/keyshotstudio.sh
@@ -2,8 +2,9 @@ keyshotstudio)
     name="KeyShot Studio"
     type="pkg"
     expectedTeamID="W7B24M74T3"
-    downloadURL="https://www.keyshot.com/download/370762/"
-    appNewVersion="$( curl -v "$downloadURL" 2>&1 | grep location | cut -d '_' -f 5 | rev | cut -d '.' -f2- | rev )"
+    downloadURL="https://download.keyshot.com/keyshot2025/keyshot_studio_mac64_2025.2_14.1.1.5.pkg"
+    # appNewVersion="$( curl -v "$downloadURL" 2>&1 | grep location | cut -d '_' -f 5 | rev | cut -d '.' -f2- | rev )"
+    appNewVersion=$( echo "$downloadURL" | cut -d '_' -f 5 | rev | cut -d '.' -f2- | rev )
     appCustomVersion() {
         if [ -d "/Applications/KeyShot Studio.app/Contents/" ]; then
             /usr/bin/defaults read "/Applications/KeyShot Studio.app/Contents/Info.plist" CFBundleVersion
