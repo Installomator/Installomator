4973b77dcf3b19b63edc6a42ba28965fa82a581a
P-M Lejon <15982689+pmex@users.noreply.github.com>
2025-09-09 11:01:51 +0200

    jq instead of javascript for appNewVersion

--- fragments/labels/davinciresolvestudio.sh
+++ fragments/labels/davinciresolvestudio.sh
@@ -3,14 +3,9 @@ blackmagicdavinciresolvestudio)
     name="DaVinci Resolve Studio"
     appName="DaVinci Resolve/DaVinci Resolve.app"
     type="pkgInDmgInZip"
-    downloadURL=$(curl --compressed --location --header "Content-Type: application/json;charset=UTF-8" --header "User-Agent: Mozilla/5.0" --data '{"country": "us", "platform": "Mac OS X", "product": "Videohub"}' \
-        "$(curl -fs https://www.blackmagicdesign.com/api/support/us/downloads.json | /usr/bin/osascript -l 'JavaScript' \
-		-e "let json = $.NSString.alloc.initWithDataEncoding($.NSFileHandle.fileHandleWithStandardInput.readDataToEndOfFile$(/usr/bin/uname -r | /usr/bin/awk -F '.' '($1 > 18) { print "AndReturnError(ObjC.wrap())" }'), $.NSUTF8StringEncoding)" \
-		-e 'if ($.NSFileManager.defaultManager.fileExistsAtPath(json)) json = $.NSString.stringWithContentsOfFileEncodingError(json, $.NSUTF8StringEncoding, ObjC.wrap())' \
-		-e 'parsed = JSON.parse(json.js)' \
-        -e "update = parsed.downloads.filter((download) => download.name.match(/^DaVinci Resolve Studio/))[0]" \
-        -e 'download_id = update.urls["Mac OS X"][0].downloadId' \
-	    -e '"https://www.blackmagicdesign.com/api/register/us/download/" + download_id')")
+    downloadID=$(curl -fs https://www.blackmagicdesign.com/api/support/us/downloads.json | jq -r '[.downloads.[]|select(.name | startswith ("DaVinci Resolve Studio"))][0].urls|."Mac OS X".[].downloadId')
+    downloadURL=$(curl --compressed --location --header "Content-Type: application/json;charset=UTF-8" --header "User-Agent: Mozilla/5.0" --data '{"country": "us", "platform": "Mac OS X", "product": "DaVinci Resolve Studio"}' \
+        "https://www.blackmagicdesign.com/api/register/us/download/${downloadID}")
     appNewVersion=$(echo ${downloadURL} | grep -oE '/v([0-9.]+)' | cut -d'v' -f2)
     blockingProcesses=( Resolve "DaVinci Control Panels Setup" "DaVinci Remote Monitor" "Fairlight Studio Utility" )
     expectedTeamID="9ZGFBWLSYP"
