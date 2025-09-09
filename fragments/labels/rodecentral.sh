555a85143db8220c134f794725525c0f32aaa5b2
P-M Lejon <15982689+pmex@users.noreply.github.com>
2025-09-09 11:09:35 +0200

    Added redirection for curl on appNewVersion

--- fragments/labels/rodecentral.sh
+++ fragments/labels/rodecentral.sh
@@ -3,6 +3,6 @@ rodecentral)
     type="pkgInZip"
     #packageID="com.rodecentral.installer"
     downloadURL="https://update.rode.com/central/RODE_Central_MACOS.zip"
-    appNewVersion=$(curl -fs https://rode.com/en/release-notes/rode-central | xmllint --html --format - 2>/dev/null | grep -o "release-notes-data='.*'>" | sed -E 's/release-notes-data='\''|'\''>//g' | jq -r '.[].version' | sed 's/[V|v]ersion //g' | sort --version-sort | tail -1)
+    appNewVersion=$(curl -fLs https://rode.com/en/release-notes/rode-central | xmllint --html --format - 2>/dev/null | grep -o "release-notes-data='.*'>" | sed -E 's/release-notes-data='\''|'\''>//g' | jq -r '.[].version' | sed 's/[V|v]ersion //g' | sort --version-sort | tail -1)
     expectedTeamID="Z9T72PWTJA"
     ;;
