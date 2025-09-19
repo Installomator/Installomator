0ffe679697c55f70e970401e25b1badf1ff2da49
Alen <alennavdic@gmail.com>
2025-09-19 09:27:19 -0500

    Add Tahoe

--- fragments/labels/macports.sh
+++ fragments/labels/macports.sh
@@ -3,6 +3,9 @@ macports)
     type="pkg"
     #buildVersion=$(uname -r | cut -d '.' -f 1)
     case $(uname -r | cut -d '.' -f 1) in
+        25)
+            archiveName="Tahoe.pkg"
+            ;;
 	    24)
 	        archiveName="Sequoia.pkg"
 	        ;;
