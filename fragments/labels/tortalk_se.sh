6a6a1fa7cd045bc363b4da450acc6d6db73b8655
P-M Lejon <15982689+pmex@users.noreply.github.com>
2025-09-22 19:25:28 +0200

    Split into SE and EN versions

--- fragments/labels/tortalk_se.sh
+++ fragments/labels/tortalk_se.sh
@@ -1,5 +1,6 @@
-tortalk)
-    name="TorTalk"
+tortalk_se)
+    name="TorTal SE"
+    appName="TorTalk.app"
     type="pkg"
     downloadURL=$(curl -fsL https://tortalk.se/student-elev/ | grep -o "https://.*.pkg")
     appNewVersion=$(echo "${downloadURL}" | grep -o "[0-9].*[0-9]"| awk -F "." '{print$1"."$2$3}')
