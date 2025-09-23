56cf30fa17919925d5cdd6bdbec11e5c5d214083
Fredrik Larsson <33235315+kryptonit1@users.noreply.github.com>
2025-09-23 10:06:31 +0200

    Update sketch.sh

--- fragments/labels/sketch.sh
+++ fragments/labels/sketch.sh
@@ -1,7 +1,7 @@
 sketch)
     name="Sketch"
     type="zip"
-    downloadURL=$(curl -sf https://www.sketch.com/downloads/mac/ | grep 'href="https://download.sketch.com' | tr '"' "\n" | grep -E "https.*.zip")
+    downloadURL=$(curl -sf https://www.sketch.com/downloads/mac/ | grep -m 1 'href="https://download.sketch.com' | tr '"' "\n" | grep -E "https.*.zip")
     appNewVersion=$( grep -oE '\d+\.\d+' <<< $downloadURL)
     expectedTeamID="WUGMZZ5K46"
     ;;
