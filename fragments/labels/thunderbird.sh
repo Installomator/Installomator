c13702b4b9069c3c760d5cab0b502ce634ca7c60
Kerns, Brandon W <bkerns@purdue.edu>
2025-09-24 10:11:26 -0400

    ESR Detection
    ESR label did not get the new detection style. Corrected.

--- fragments/labels/thunderbird.sh
+++ fragments/labels/thunderbird.sh
@@ -2,7 +2,7 @@ thunderbird)
     name="Thunderbird"
     type="dmg"
     versionKey="CFBundleShortVersionString"
-    appNewVersion=$(curl -s "https://www.thunderbird.net/en-US/thunderbird/releases/atom.xml" | xmllint --xpath "//*[local-name()='entry']/*[local-name()='title'][not(contains(text(), 'esr'))]/text()" - | head -1 | awk '{ print $2 }' )
+    appNewVersion=$(curl -s "https://www.thunderbird.net/en-US/thunderbird/releases/atom.xml" | xmllint --xpath "//*[local-name()='entry']/*[local-name()='title'][not(contains(text(), 'esr'))]/text()" - | head -1 | awk '{ print $2 }')
     downloadURL="https://download.mozilla.org/?product=thunderbird-${appNewVersion}-SSL&os=osx&lang=en-US"
     expectedTeamID="43AQ936H96"
     ;;
