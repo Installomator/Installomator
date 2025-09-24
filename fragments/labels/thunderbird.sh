0822918b523408883aa099f6b09305b7eb113b48
Kerns, Brandon W <bkerns@purdue.edu>
2025-09-24 09:25:19 -0400

    Adding versionKey
    Adding versionKey to potentially address a report where it looks like the local version detection was incorrect.

--- fragments/labels/thunderbird.sh
+++ fragments/labels/thunderbird.sh
@@ -1,7 +1,8 @@
 thunderbird)
     name="Thunderbird"
     type="dmg"
-	appNewVersion=$(curl -s "https://www.thunderbird.net/en-US/thunderbird/releases/atom.xml" | xmllint --xpath "//*[local-name()='entry']/*[local-name()='title'][not(contains(text(), 'esr'))]/text()" - | head -1 | awk '{ print $2 }' )
+    versionKey="CFBundleShortVersionString"
+    appNewVersion=$(curl -s "https://www.thunderbird.net/en-US/thunderbird/releases/atom.xml" | xmllint --xpath "//*[local-name()='entry']/*[local-name()='title'][not(contains(text(), 'esr'))]/text()" - | head -1 | awk '{ print $2 }' )
     downloadURL="https://download.mozilla.org/?product=thunderbird-${appNewVersion}-SSL&os=osx&lang=en-US"
     expectedTeamID="43AQ936H96"
     ;;
