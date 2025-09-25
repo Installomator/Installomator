af086f24c1797bab6863837892d51d5c2c260d22
Fredrik Larsson <33235315+kryptonit1@users.noreply.github.com>
2025-09-25 10:29:59 +0200

    Update citrixworkspace.sh
    Changed so that the label uses a Citrix provided XML as source for downloadURL and appNewVersion

--- fragments/labels/citrixworkspace.sh
+++ fragments/labels/citrixworkspace.sh
@@ -1,7 +1,8 @@
 citrixworkspace)
     name="Citrix Workspace"
-    type="pkgInDmg"
-    downloadURL="https://downloadplugins.citrix.com/Mac/CitrixWorkspaceApp.dmg"
+    type="pkg"
+    parseURL=$(curl -fs "https://downloadplugins.citrix.com/ReceiverUpdates/Prod/catalog_macos.xml" | xmllint --xpath 'string(//Installer/DownloadURL)' -)
+    downloadURL=https://downloadplugins.citrix.com/ReceiverUpdates/Prod/$parseURL
     appNewVersion=$(curl -fs "https://downloadplugins.citrix.com/ReceiverUpdates/Prod/catalog_macos.xml" | xmllint --xpath 'string(//Installer/Version)' -)
     versionKey="CitrixVersionString"
     expectedTeamID="S272Y5R93J"
