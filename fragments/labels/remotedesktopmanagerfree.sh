f7602abb64e1f3139c341dd0401851de4f427a88
P-M Lejon <15982689+pmex@users.noreply.github.com>
2025-09-25 10:14:02 +0200

    fix downloadURL for remotedesktopmanagerfree

--- fragments/labels/remotedesktopmanagerfree.sh
+++ fragments/labels/remotedesktopmanagerfree.sh
@@ -1,7 +1,7 @@
 remotedesktopmanagerfree)
     name="Remote Desktop Manager"
     type="dmg"
-    downloadURL=$(curl -fsL https://devolutions.net/remote-desktop-manager/home/thankyou/rdmmacbin/ | grep -oe "http.*\.dmg" | head -1)
+    downloadURL=$(curl -fsL https://devolutions.net/remote-desktop-manager/download/thank-you/\?platform\=RDMMacbin\&edition\=free\&os\=macos | grep "const productInfo" | grep -oe "{.*}" | jq -r '."RDMMacbin.Url"')
     appNewVersion=$(echo "$downloadURL" | sed -E 's/.*\.Mac\.([0-9.]*)\.dmg/\1/g')
     expectedTeamID="N592S9ASDB"
     ;;
