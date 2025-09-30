d6078a19e2961c50f6a3d88ff0e76e92b0933d53
Kerns, Brandon W <bkerns@purdue.edu>
2025-09-30 11:19:49 -0400

    Fix downloading wrong arch installer
    Output:
    
    ```
    2025-09-30 11:17:07 : INFO  : archimate : setting variable from argument DEBUG=0
    2025-09-30 11:17:07 : INFO  : archimate : Total items in argumentsArray: 1
    2025-09-30 11:17:07 : INFO  : archimate : argumentsArray: DEBUG=0
    2025-09-30 11:17:07 : REQ   : archimate : ################## Start Installomator v. 10.9beta, date 2025-09-30
    2025-09-30 11:17:07 : INFO  : archimate : ################## Version: 10.9beta
    2025-09-30 11:17:07 : INFO  : archimate : ################## Date: 2025-09-30
    2025-09-30 11:17:07 : INFO  : archimate : ################## archimate
    2025-09-30 11:17:08 : INFO  : archimate : Reading arguments again: DEBUG=0
    2025-09-30 11:17:08 : INFO  : archimate : BLOCKING_PROCESS_ACTION=tell_user
    2025-09-30 11:17:08 : INFO  : archimate : NOTIFY=success
    2025-09-30 11:17:08 : INFO  : archimate : LOGGING=INFO
    2025-09-30 11:17:08 : INFO  : archimate : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2025-09-30 11:17:09 : INFO  : archimate : Label type: dmg
    2025-09-30 11:17:09 : INFO  : archimate : archiveName: Archi.dmg
    2025-09-30 11:17:09 : INFO  : archimate : no blocking processes defined, using Archi as default
    2025-09-30 11:17:09 : INFO  : archimate : name: Archi, appName: Archi.app
    2025-09-30 11:17:09 : WARN  : archimate : No previous app found
    2025-09-30 11:17:09 : WARN  : archimate : could not find Archi.app
    2025-09-30 11:17:09 : INFO  : archimate : appversion:
    2025-09-30 11:17:09 : INFO  : archimate : Latest version of Archi is 5.7.0
    2025-09-30 11:17:09 : REQ   : archimate : Downloading https://github.com/archimatetool/archi.io/releases/download/5.7.0/Archi-Mac-Silicon-5.7.0.dmg to Archi.dmg
    2025-09-30 11:18:05 : INFO  : archimate : Downloaded Archi.dmg – Type is  zlib compressed data – SHA is 28c3bb5ec20b2e0517c91be8e5d1a0d35e796cf5 – Size is 197356 kB
    2025-09-30 11:18:05 : REQ   : archimate : no more blocking processes, continue with update
    2025-09-30 11:18:05 : REQ   : archimate : Installing Archi
    2025-09-30 11:18:05 : INFO  : archimate : Mounting /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.peuUyy9i6j/Archi.dmg
    2025-09-30 11:18:09 : INFO  : archimate : Mounted: /Volumes/Archi 5.7.0 1
    2025-09-30 11:18:09 : INFO  : archimate : Verifying: /Volumes/Archi 5.7.0 1/Archi.app
    2025-09-30 11:18:10 : INFO  : archimate : Team ID matching: 375WT5T296 (expected: 375WT5T296 )
    2025-09-30 11:18:10 : INFO  : archimate : Installing Archi version 5.7.0 on versionKey CFBundleShortVersionString.
    2025-09-30 11:18:10 : INFO  : archimate : Copy /Volumes/Archi 5.7.0 1/Archi.app to /Applications
    2025-09-30 11:18:11 : WARN  : archimate : Changing owner to bkerns
    2025-09-30 11:18:11 : INFO  : archimate : Finishing...
    2025-09-30 11:18:14 : INFO  : archimate : App(s) found: /Applications/Archi.app
    2025-09-30 11:18:14 : INFO  : archimate : found app at /Applications/Archi.app, version 5.7.0, on versionKey CFBundleShortVersionString
    2025-09-30 11:18:14 : REQ   : archimate : Installed Archi, version 5.7.0
    2025-09-30 11:18:14 : INFO  : archimate : notifying
    2025-09-30 11:18:14 : INFO  : archimate : Installomator did not close any apps, so no need to reopen any apps.
    2025-09-30 11:18:14 : REQ   : archimate : All done!
    2025-09-30 11:18:14 : REQ   : archimate : ################## End Installomator, exit code 0
    ```

--- fragments/labels/archimate.sh
+++ fragments/labels/archimate.sh
@@ -2,9 +2,9 @@ archimate)
     name="Archi"
     type="dmg"
     if [[ $(arch) == "arm64" ]]; then
-      downloadURL=$(curl -s https://www.archimatetool.com/download/ | grep dmg | sed -n 's/.*href="\([^"]*\)".*/\1/p' | awk 'NR==2')
+      downloadURL=$(curl -s https://www.archimatetool.com/download/ | grep -oE 'href="[^"]+\.dmg"' | cut -d'"' -f2 | grep -E '[sS]ilicon')
     elif [[ $(arch) == "i386" ]]; then
-      downloadURL=$(curl -s https://www.archimatetool.com/download/ | grep dmg | sed -n 's/.*href="\([^"]*\)".*/\1/p' | awk 'NR==1')
+      downloadURL=$(curl -s https://www.archimatetool.com/download/ | grep -oE 'href="[^"]+\.dmg"' | cut -d'"' -f2 | grep -Ev '[sS]ilicon')
     fi
     appNewVersion=$(echo "${downloadURL}" | sed -E 's/.*\/([0-9]+\.[0-9]+\.[0-9]+)\/.*/\1/')
     expectedTeamID="375WT5T296"
