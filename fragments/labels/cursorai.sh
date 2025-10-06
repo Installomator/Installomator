8a6345db24dc768fadeb26880ac53bf09f81db51
Jared Schwager <jared.schwager@gmail.com>
2025-10-06 11:10:02 -0400

    Update cursorai.sh
    Cursor changed their download page again. Updated downloadURL to grab correct version.

--- fragments/labels/cursorai.sh
+++ fragments/labels/cursorai.sh
@@ -1,7 +1,7 @@
 cursorai)
     name="Cursor"
     type="dmg"
-    downloadURL=$(curl -fsL "https://www.cursor.com/download" | grep -Eo 'https://[^"]*darwin/universal[^"]*\.dmg' | head -n 1)
+    downloadURL="https://api2.cursor.sh/updates/download/golden/darwin-universal/cursor/"
     expectedTeamID="VDXQ22DGB9"
     blockingProcesses=( "Cursor" )
     versionKey="CFBundleVersion"
