6b7522db524dbede97d0d2429a206c5678f824f3
Fredrik Larsson <33235315+kryptonit1@users.noreply.github.com>
2025-10-07 13:07:34 +0200

    Update camtasia2024.sh

--- fragments/labels/camtasia2024.sh
+++ fragments/labels/camtasia2024.sh
@@ -2,7 +2,7 @@ camtasia2024)
     name="Camtasia 2024"
     type="dmg"
     apiResult="$(curl -fsL "https://www.techsmith.com/api/v/1/products/getversioninfo/2200")"
-    downloadURL="https://download.techsmith.com/$(getJSONValue "$apiResult" "PrimaryDownloadInformation.RelativePath")$(getJSONValue "$apiResult" "PrimaryDownloadInformation.Name")"
+    downloadURL="https://download.techsmith.com$(getJSONValue "$apiResult" "PrimaryDownloadInformation.RelativePath")$(getJSONValue "$apiResult" "PrimaryDownloadInformation.Name")"
     appNewVersion="20$(getJSONValue "$apiResult" "PrimaryDownloadInformation.Major").$(getJSONValue "$apiResult" "PrimaryDownloadInformation.Minor").$(getJSONValue "$apiResult" "PrimaryDownloadInformation.Maintenance")"
     expectedTeamID="7TQL462TU8"
     ;;
