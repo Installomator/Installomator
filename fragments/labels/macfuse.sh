3f6b5d066b3e15c6f2147c8966cd9ebed4709b26
P-M Lejon <15982689+pmex@users.noreply.github.com>
2025-10-07 08:51:09 +0200

    add macfuse appCustomVersion

--- fragments/labels/macfuse.sh
+++ fragments/labels/macfuse.sh
@@ -4,5 +4,10 @@ macfuse)
     pkgName="Install macFUSE.pkg"
     downloadURL=$(downloadURLFromGit osxfuse osxfuse)
     appNewVersion=$(versionFromGit osxfuse osxfuse)
+    appCustomVersion(){
+        if [ -f "/Library/Filesystems/macfuse.fs/Contents/Info.plist" ]; then
+            defaults read /Library/Filesystems/macfuse.fs/Contents/Info.plist CFBundleShortVersionString
+        fi
+    }
     expectedTeamID="3T5GSNBU6W"
     ;;
