cceefabdf315fa26e55694c53372a6aba5e3dd6f
hwlans <175405805+hwlans@users.noreply.github.com>
2025-10-21 23:21:02 +0200

    Update cloudya.sh
    Updated appNewVersion

--- fragments/labels/cloudya.sh
+++ fragments/labels/cloudya.sh
@@ -2,6 +2,6 @@ cloudya)
     name="Cloudya"
     type="appInDmgInZip"
     downloadURL="$(curl -fsL https://www.nfon.com/de/service/downloads | grep -i -E -o "https://cdn.cloudya.com/Cloudya-[.0-9]+-mac.zip")"
-    appNewVersion="$(curl -fs https://www.nfon.com/de/service/downloads | grep -i -E -o "Cloudya Desktop App MAC [0-9.]*" | sed 's/^.*\ \([^ ]\{0,7\}\)$/\1/g')"
+    appNewVersion="$(curl -fsL https://www.nfon.com/de/service/downloads | grep -i -E -o "Cloudya Desktop App MAC [0-9.]*" | sed 's/^.*\ \([^ ]\{0,7\}\)$/\1/g' | head -n1)"
     expectedTeamID="X26F74J8TH"
     ;;
