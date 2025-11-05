987044a6f643254d9d123c3232d2890dcf063862
Adam Codega <acodega@users.noreply.github.com>
2025-11-04 21:51:12 -0500

    Clean up comments in androidstudio.sh
    Removed comments related to custom version handling.

--- fragments/labels/androidstudio.sh
+++ fragments/labels/androidstudio.sh
@@ -12,23 +12,11 @@ androidstudio)
     blockingProcesses=( androidstudio )
 
     appCustomVersion() {
-        # Android Studio only contains the Major.minor in the Info.plist
-        # We're going to use a hidden file to store the full Major.minor.minor.minor version number
         customVersionFile="/Users/Shared/.AndroidStudioInstalledVersion"
-
-        # Check it the hidden custom version file exists. If so, use that version number.
         if [ -f "${customVersionFile}" ]; then
             result=$(cat "${customVersionFile}")
         fi
-
-        # Write the appNewVersion value to the custom version file.
-        # This assumes any run of this label executed successfully and the installed version was updated to the
-        #   latest version because Installomator doesn't provide that kind of feedback to the label fragment code.
-        #   This is a hack, but it solves the problem of the software being re-installed on every run because the
-        #   installed version doesn't accurately report it's full version.
         echo $appNewVersion > "${customVersionFile}"
-
-        # Write out whichever installed version number we found into appCustomVersion
         echo $result
     }
     ;;
