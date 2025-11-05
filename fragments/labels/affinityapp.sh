5371124099800ce981c5feef56ecd52f02ef72b3
crissylines <c.dilman@apfelwerk.de>
2025-11-05 15:27:56 +0100

    Create affinityapp.sh
    2025-11-05 15:26:18 : INFO  : affinityapp : setting variable from argument LOGGING=INFO
    2025-11-05 15:26:18 : INFO  : affinityapp : Total items in argumentsArray: 1
    2025-11-05 15:26:18 : INFO  : affinityapp : argumentsArray: LOGGING=INFO
    2025-11-05 15:26:18 : REQ   : affinityapp : ################## Start Installomator v. 10.9beta, date 2025-11-05
    2025-11-05 15:26:18 : INFO  : affinityapp : ################## Version: 10.9beta
    2025-11-05 15:26:18 : INFO  : affinityapp : ################## Date: 2025-11-05
    2025-11-05 15:26:18 : INFO  : affinityapp : ################## affinityapp
    2025-11-05 15:26:18 : DEBUG : affinityapp : DEBUG mode 1 enabled.
    2025-11-05 15:26:18 : INFO  : affinityapp : Reading arguments again: LOGGING=INFO
    2025-11-05 15:26:18 : DEBUG : affinityapp : argument: LOGGING=INFO
    2025-11-05 15:26:18 : INFO  : affinityapp : BLOCKING_PROCESS_ACTION=tell_user
    2025-11-05 15:26:18 : INFO  : affinityapp : NOTIFY=success
    2025-11-05 15:26:18 : INFO  : affinityapp : LOGGING=INFO
    2025-11-05 15:26:18 : INFO  : affinityapp : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2025-11-05 15:26:18 : INFO  : affinityapp : Label type: dmg
    2025-11-05 15:26:18 : INFO  : affinityapp : archiveName: Affinity.dmg
    2025-11-05 15:26:18 : INFO  : affinityapp : no blocking processes defined, using Affinity as default
    2025-11-05 15:26:18 : INFO  : affinityapp : App(s) found: /Applications/Affinity.app
    2025-11-05 15:26:19 : INFO  : affinityapp : found app at /Applications/Affinity.app, version 3.0.0, on versionKey CFBundleShortVersionString
    2025-11-05 15:26:19 : INFO  : affinityapp : appversion: 3.0.0
    2025-11-05 15:26:19 : INFO  : affinityapp : Latest version not specified.
    2025-11-05 15:26:19 : INFO  : affinityapp : Affinity.dmg exists and DEBUG mode 1 enabled, skipping download
    2025-11-05 15:26:19 : REQ   : affinityapp : Installing Affinity
    2025-11-05 15:26:19 : INFO  : affinityapp : Mounting ./Affinity.dmg
    2025-11-05 15:26:19 : INFO  : affinityapp : Mounted: /Volumes/Affinity
    2025-11-05 15:26:19 : INFO  : affinityapp : Verifying: /Volumes/Affinity/Affinity.app
    2025-11-05 15:26:36 : INFO  : affinityapp : Team ID matching: 5HD2ARTBFS (expected: 5HD2ARTBFS )
    2025-11-05 15:26:36 : INFO  : affinityapp : Downloaded version of Affinity is 3.0.0 on versionKey CFBundleShortVersionString, same as installed.
    2025-11-05 15:26:36 : REQ   : affinityapp : ################## End Installomator, exit code 0

---
+++ fragments/labels/affinityapp.sh
@@ -0,0 +1,8 @@
+affinityapp)
+    name="Affinity"
+    type="dmg"
+    appName="Affinity.app"
+    downloadURL="https://downloads.affinity.studio/Affinity.dmg"
+    appNewVersion=""
+    expectedTeamID="5HD2ARTBFS"
+    ;;
