e65c2ed411ddbb71a78020b5636b2efb3b66b2f1
acodega <acodega@users.noreply.github.com>
2025-11-06 21:37:52 +0000

    Runner compiled Installomator.sh and Labels.txt using assemble.sh

--- Installomator.sh
+++ Installomator.sh
@@ -349,7 +349,7 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
     fi
 fi
 VERSION="10.9beta"
-VERSIONDATE="2025-11-05"
+VERSIONDATE="2025-11-06"
 
 # MARK: Functions
 
@@ -10146,8 +10146,12 @@ ticktick)
 tidal)
     name="TIDAL"
     type="dmg"
-    downloadURL="https://download.tidal.com/desktop/TIDAL.dmg"
-    appNewVersion=$(curl -fs https://update.tidal.com/updates/latest\?v\=1 | cut -d '"' -f4 | sed -E 's/https.*\/TIDAL\.([0-9.]*)\.zip/\1/g')
+    if [[ $(arch) == i386 ]]; then
+        downloadURL=https://download.tidal.com/desktop/TIDAL.x64.dmg
+    elif [[ $(arch) == arm64 ]]; then
+        downloadURL=https://download.tidal.com/desktop/TIDAL.arm64.dmg
+    fi
+    appNewVersion=$(curl -fs https://support.datajar.co.uk/hc/en-us/articles/360010333638-Jamf-Auto-Update-Application-Catalog | grep -ozpEi ">TIDAL<\/td>\n<td>(.*)<" | grep -oE "((?:[0-9]+\.?)+)" | head -1)
     expectedTeamID="GK2243L7KB"
     ;;
 todoist)
