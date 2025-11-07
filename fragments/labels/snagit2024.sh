ffcf7e188def0b0f3ccdd30de75bd51fe8261cd5
Kerns, Brandon W <bkerns@purdue.edu>
2025-11-07 15:23:24 -0500

    Snagit labels
    This fixes the following:
    
    - The feed URL and parsing to get the downloadURL and appNewVersion
    
    - Adds support for the "2025" version of Snagit

--- fragments/labels/snagit2024.sh
+++ fragments/labels/snagit2024.sh
@@ -1,8 +1,7 @@
-snagit|\
-snagit2025)
-    name="Snagit 2025"
+snagit2024)
+    name="Snagit 2024"
     type="dmg"
-    sparkleData=$(curl -fsL -H 'User-Agent: Snagit/2025.0.0 Sparkle/2.8.0' 'https://www.techsmith.com/redirect.asp?target=sparkleappcast&product=snagitmac&ver=2025.4.0&lang=enu&os=mac')
+    sparkleData=$(curl -fsL -H 'User-Agent: Snagit/2024.0.0 Sparkle/2.8.0' 'https://www.techsmith.com/redirect.asp?target=sufeedurl&product=snagitmac&ver=2024.0.0&lang=enu&os=mac')
     appNewVersion=$(
         echo "$sparkleData" | \
         xmllint -xpath 'string(//*[local-name()="item"][last()]/*[local-name()="shortVersionString"]/text())' -
