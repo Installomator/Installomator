f6ee86fd08cfd2903138e88e6da79e83b1310bf5
Adam Codega <acodega@users.noreply.github.com>
2025-11-07 08:28:20 -0500

    Remove error log for Clevershare label

--- fragments/labels/clevershare2.sh
+++ fragments/labels/clevershare2.sh
@@ -1,7 +1,6 @@
 clevershare2)
     name="Clevershare"
     type="dmg"
-    printlog "Label for $name broken in test" ERROR
     downloadURL=$(curl -fs https://www.clevertouch.com/uk/clevershare2g | grep -i -o -E "https.*validated.*\.dmg")
     appNewVersion=$(echo "${downloadURL}" | sed -E 's/.*\/([0-9.]*)\/[0-9]*\/.*\.dmg$/\1/')
     expectedTeamID="LHZP4ZQ39M"
