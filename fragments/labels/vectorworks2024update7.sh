7cde83a9a13b4b13a5baf7c5a8b8bbadb1d372fc
Benjamin Kollmer <b.kollmer@apfelwerk.de>
2025-11-07 08:30:32 +0100

    Update vectorworks2024update7.sh

--- fragments/labels/vectorworks2024update7.sh
+++ fragments/labels/vectorworks2024update7.sh
@@ -2,7 +2,7 @@ vectorworks2025update7)
     name="Vectorworks 2025 Update 7"
     appName="Vectorworks 2025 Installationsmanager.app"
     type="dmg"
-    packageID="net.vectorworks.2024.vectorworksinstaller"
+    packageID="net.vectorworks.2025.vectorworksinstaller"
     downloadURL="https://server1-d.vectorworks-online.de/cw/vw2025/mac/Vectorworks%202025%20Update%207.dmg"
     expectedTeamID="LFNG3Q6WX2"
     ;;
