4237b87be42d10c5179d20db78181251ddadc7a7
Oleh Sokhan <47741759+o-sokhan@users.noreply.github.com>
2025-11-07 16:18:27 +0200

    Update 8x8.sh
    The downloadURL variable is fully updated to dynamically detect and fetch the correct 8x8 Work DMG based on architecture (Intel or ARM), reflecting changes in the support portal's page structure.

--- fragments/labels/8x8.sh
+++ fragments/labels/8x8.sh
@@ -1,9 +1,15 @@
 8x8)
-    # credit: #D-A-James from MacAdmins Slack and Isaac Ordonez, Mann consulting (@mannconsulting)
     name="8x8 Work"
     type="dmg"
-    downloadURL=$(curl -fs -L https://support.8x8.com/cloud-phone-service/voice/work-desktop/download-8x8-work-for-desktop | grep -m 1 -o "https.*dmg" | sed 's/\"//' | awk '{print $1}')
-    # As for appNewVersion, it needs to be checked for newer version than 7.2.4
-    appNewVersion=$(curl -fs -L https://support.8x8.com/cloud-phone-service/voice/work-desktop/download-8x8-work-for-desktop | grep -m 1 -o "https.*dmg" | sed 's/\"//' | awk '{print $1}' | sed -E 's/.*-v([0-9\.]*)[-\.]*.*/\1/' )
+    downloadURL=$( \
+      arch=$(uname -m); \
+      curl -fsSL "https://support-portal.8x8.com/helpcenter/viewArticle.html?d=8bff4970-6fbf-4daf-842d-8ae9b533153d" | \
+      if [[ "$arch" == "arm64" ]]; then \
+        grep -oE 'https://work-desktop-assets\.8x8\.com[^"]*arm64[^"]*\.dmg' | head -n1; \
+      else \
+        grep -oE 'https://work-desktop-assets\.8x8\.com[^"]*\.dmg' | grep -v 'arm64' | head -n1; \
+      fi \
+    )
+    appNewVersion=""
     expectedTeamID="FC967L3QRG"
     ;;
