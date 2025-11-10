dedb92f890ff9685f35ce1913f307d53c08cf69f
Kerns, Brandon W <bkerns@purdue.edu>
2025-11-10 11:39:24 -0500

    Bugged version

--- fragments/labels/snagit.sh
+++ fragments/labels/snagit.sh
@@ -2,7 +2,7 @@ snagit|\
 snagit2025)
     name="Snagit"
     type="dmg"
-    sparkleData=$(curl -fsL -H 'User-Agent: Snagit/2025.0.0 Sparkle/2.8.0' 'https://www.techsmith.com/redirect.asp?target=sparkleappcast&product=snagitmac&ver=2025.4.0&lang=enu&os=mac')
+    sparkleData=$(curl -fsL -H 'User-Agent: Snagit/2025.0.0 Sparkle/2.8.0' 'https://www.techsmith.com/redirect.asp?target=sparkleappcast&product=snagitmac&ver=2025.0.0&lang=enu&os=mac')
     appNewVersion=$(
         echo "$sparkleData" | \
         xmllint -xpath 'string(//*[local-name()="item"][last()]/*[local-name()="shortVersionString"]/text())' -
--- fragments/labels/snagit2022.sh
+++ fragments/labels/snagit2022.sh
@@ -1,7 +1,7 @@
 snagit2022)
     name="Snagit 2022"
     type="dmg"
-    sparkleData=$(curl -fsL -H 'User-Agent: Snagit/2022.0.0 Sparkle/2.8.0' 'https://www.techsmith.com/redirect.asp?target=sufeedurl&product=snagitmac&ver=2022.0.0&lang=enu&os=mac')
+    sparkleData=$(curl -fsL -H 'User-Agent: Camtasia/2022.0.0 Sparkle/2.8.0' 'https://www.techsmith.com/redirect.asp?target=sufeedurl&product=snagitmac&ver=2022.0.0&lang=enu&os=mac')
     appNewVersion=$(
         echo "$sparkleData" | \
         xmllint -xpath 'string(//*[local-name()="item"][last()]/*[local-name()="shortVersionString"]/text())' -
