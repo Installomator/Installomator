c1c42246e417f604ebf9162bc38904ea91eadc6f
crissylines <c.dilman@apfelwerk.de>
2025-11-12 14:39:15 +0100

    Remove unused appNewVersion variable in affinityapp.sh
    Deleted the unused appNewVersion variable from the Affinity.app label fragment to clean up the script.

--- fragments/labels/affinityapp.sh
+++ fragments/labels/affinityapp.sh
@@ -3,6 +3,5 @@ affinityapp)
     type="dmg"
     appName="Affinity.app"
     downloadURL="https://downloads.affinity.studio/Affinity.dmg"
-    appNewVersion=""
     expectedTeamID="5HD2ARTBFS"
     ;;
