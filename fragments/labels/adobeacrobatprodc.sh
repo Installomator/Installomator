d16a424651c34d3915d6f14694d671994196d6c0
Skinflint <googla86@gmail.com>
2025-11-24 11:12:07 -0700

    New Release Notes URL
    Fixed the releaseNotesURL to the new URL, and changed the appNewVersion to properly check the new release Notes since it is now a list and not a table. It will filter out the Windows Only and Optional updates, and grab the (Mac) version if there is a difference between (Mac) and (Win).

--- fragments/labels/adobeacrobatprodc.sh
+++ fragments/labels/adobeacrobatprodc.sh
@@ -5,8 +5,8 @@ adobeacrobatprodc)
     pkgName="Acrobat/Acrobat DC Installer.pkg"
     packageID="com.adobe.acrobat.DC.viewer.app.pkg.MUI"
     downloadURL="https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/osx10/Acrobat_DC_Web_WWMUI.dmg"
-    releaseNotesURL="https://helpx.adobe.com/acrobat/release-note/release-notes-acrobat-reader.html"
-    appNewVersion=$(curl -s $(curl -s $releaseNotesURL | xmllint --html --xpath 'string(//table//tr[2]//a/@href)' - 2>/dev/null) | grep -m1 '.dmg' | sed -E 's/.*([0-9]{2})([0-9]{3})([0-9]{5}).*/\1.\2.\3/' )
+    releaseNotesURL="https://www.adobe.com/devnet-docs/acrobatetk/tools/ReleaseNotesDC/index.html"
+    appNewVersion=$(curl -sfL "$releaseNotesURL" | grep -m6 "continuous/dccontinuous.*#" | awk '!/Optional update/ { if ($0 ~ /\(Windows Only\)/) next; else if (match($0, /[0-9]+\.[0-9]+\.[0-9]+ *\(Mac\)/)) {v=substr($0,RSTART,RLENGTH); sub(/ *\(Mac\)/,"",v); print v} else if (match($0, /[0-9]+\.[0-9]+\.[0-9]+/)) print substr($0,RSTART,RLENGTH) }' | head -n1)
     expectedTeamID="JQ525L2MZD"
     blockingProcesses=( "Acrobat Pro DC" )
     Company="Adobe"
