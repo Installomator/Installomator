101722ccd475341205423ae1751eff141138ddaf
kfattic <6775519+kfattic@users.noreply.github.com>
2025-12-02 15:39:09 -0600

    Update elgatocamerahub.sh
    Fix sed pattern for CameraHub version extraction

--- fragments/labels/elgatocamerahub.sh
+++ fragments/labels/elgatocamerahub.sh
@@ -2,7 +2,7 @@ elgatocamerahub)
     name="Elgato Camera Hub"
     type="pkg"
     downloadURL="https://gc-updates.elgato.com/mac/echm-update/final/download-website.php"
-    appNewVersion=$(curl -fsI "https://gc-updates.elgato.com/mac/echm-update/final/download-website.php" | grep -i ^location | sed -E 's/.*Camera_Hub_([0-9.]*).pkg/\1/g')
+    appNewVersion=$(curl -fsI "https://gc-updates.elgato.com/mac/echm-update/final/download-website.php" | grep -i ^location | sed -E 's/.*CameraHub_([0-9.]+)\.pkg/\1/g')
     expectedTeamID="Y93VXCB8Q5"
     appCustomVersion() {
         local infoPath="$1"
