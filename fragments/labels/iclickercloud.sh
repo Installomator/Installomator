45a8c8c6079a094ad04377a965a9e5c284893266
rougegoat <rougegoat@gmail.com>
2025-12-09 09:11:42 -0600

    Update iclickercloud.sh
    Updated URLs following a site update

--- fragments/labels/iclickercloud.sh
+++ fragments/labels/iclickercloud.sh
@@ -1,7 +1,7 @@
 iclickercloud)
     name="iClicker Cloud"
     type="dmg"
-    appNewVersion=$( curl -fs "https://artifactory.reef-education.com/artifactory/app-downloads/" | grep -o 'iclicker-cloud-[0-9].[0-9].[0-9]-mac.dmg' | tail -1 | sed 's/^iclicker-cloud-//g' | sed 's/-mac.dmg$//g' )
-    downloadURL="https://artifactory.reef-education.com/artifactory/app-downloads/iclicker-cloud-$appNewVersion-mac.dmg"
+    appNewVersion=$( curl -fs "https://www.iclicker.com/downloads/iclicker-cloud/" | grep -o 'iclicker-cloud-[0-9].[0-9].[0-9]-mac.dmg' | tail -1 | sed 's/^iclicker-cloud-//g' | sed 's/-mac.dmg$//g' )
+    downloadURL="https://downloads.iclicker.com/app-downloads/iclicker-cloud-$appNewVersion-mac.dmg"
     expectedTeamID="5E845CEVPW"
     ;;
