02a264aadbedd92e308d1beb491a8517ac7b9e07
Rolando <rolandomurillo@hey.com>
2025-12-10 15:11:56 -0500

    Add architecture conditional

--- fragments/labels/airflow.sh
+++ fragments/labels/airflow.sh
@@ -1,6 +1,12 @@
 airflow)
     name="Air"
     type="dmg"
+    if [[ $(arch) == "arm64" ]]; then
+        archiveName="Air-arm64.dmg"
+
+    elif [[ $(arch) == "i386" ]]; then
+        archiveName="Air-x64.dmg"
+    fi
     downloadURL="$(downloadURLFromGit AirLabsTeam airflow-releases)"
     appNewVersion="$(versionFromGit AirLabsTeam airflow-releases)"
     expectedTeamID="8RBYE8TY7T"
