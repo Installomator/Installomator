0a51c08ca4d90076b4ef9fadca74014c58a7bd08
RIchard Glaser <richard.glaser@utah.edu>
2025-12-18 15:25:36 -0700

    xcreds-label-update-update
    output
    ```
    ```

--- fragments/labels/xcreds.sh
+++ fragments/labels/xcreds.sh
@@ -4,7 +4,6 @@ xcreds)
     packageID="com.twocanoes.pkg.secureremoteaccess"
     downloadURL="https://twocanoes-software-updates.s3.amazonaws.com/XCreds.pkg"
     appNewVersion=$(curl -fs "https://twocanoes.com/products/mac/xcreds/history/" | grep -A1 "<h3>Change Log</h3>" | sed -n 's/.*<h4>Version \(.*\) Build.*/\1/p')
-    # XCreds postinstall kills loginwindow and restarts the app, ignore blocking processes since the app manages its own restart
-    BLOCKING_PROCESS_ACTION=ignore
     expectedTeamID="UXP6YEHSPW"
+    blockingProcesses=( NONE )
     ;;
