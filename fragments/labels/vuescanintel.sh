7d12e52a07654378d9996dbb79748705403df26c
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2025-12-19 10:19:26 -0700

    Remove blockingProcesses from vuescanintel.sh
    Removed 'blockingProcesses' variable from the script.

--- fragments/labels/vuescanintel.sh
+++ fragments/labels/vuescanintel.sh
@@ -5,5 +5,4 @@ vuescanintel)
     appNewVersion=$(curl -sfL https://www.hamrick.com/vuescan/vuescan.htm | grep -Eo 'VueScan [0-9]+\.[0-9]+\.[0-9]+' | head -n 1 | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
     downloadURL="https://www.hamrick.com/files/vuex64$(echo "$appNewVersion" | cut -d'.' -f1,2 | tr -d '.').dmg"
     expectedTeamID="5D5BXT9KP5"
-    blockingProcesses=("VueScan")
     ;;
