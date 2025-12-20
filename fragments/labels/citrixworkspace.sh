c48454b02f468a9972318fbad1c2f937f5d7c6f4
Lucas Harrison <133052261+LGharrison@users.noreply.github.com>
2025-12-19 20:25:43 -0700

    Update Citrix Workspace Label
    Resolving error where Citrix Workspace would not download due to an extra "/" in the downloadURL variable after the parseURL variable finds the download version

--- fragments/labels/citrixworkspace.sh
+++ fragments/labels/citrixworkspace.sh
@@ -2,7 +2,7 @@ citrixworkspace)
     name="Citrix Workspace"
     type="pkg"
     parseURL=$(curl -fs "https://downloadplugins.citrix.com/ReceiverUpdates/Prod/catalog_macos.xml" | xmllint --xpath 'string(//Installer/DownloadURL)' -)
-    downloadURL=https://downloadplugins.citrix.com/ReceiverUpdates/Prod/$parseURL
+    downloadURL=https://downloadplugins.citrix.com/ReceiverUpdates/Prod"$parseURL"
     appNewVersion=$(curl -fs "https://downloadplugins.citrix.com/ReceiverUpdates/Prod/catalog_macos.xml" | xmllint --xpath 'string(//Installer/Version)' -)
     versionKey="CitrixVersionString"
     expectedTeamID="S272Y5R93J"
