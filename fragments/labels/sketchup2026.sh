863c5fd67cf93fe16eb79489c5b2c92e84a9031a
RIchard Glaser <richard.glaser@utah.edu>
2026-01-05 13:23:09 -0700

    sketchup-2026-label-update
    output
    ```
    sudo Installomator/utils/assemble.sh sketchup2026 DEBUG=0
    2026-01-05 13:16:06 : INFO  : sketchup2026 : setting variable from argument DEBUG=0
    2026-01-05 13:16:06 : INFO  : sketchup2026 : Total items in argumentsArray: 1
    2026-01-05 13:16:06 : INFO  : sketchup2026 : argumentsArray: DEBUG=0
    2026-01-05 13:16:06 : REQ   : sketchup2026 : ################## Start Installomator v. 10.9beta, date 2026-01-05
    2026-01-05 13:16:06 : INFO  : sketchup2026 : ################## Version: 10.9beta
    2026-01-05 13:16:06 : INFO  : sketchup2026 : ################## Date: 2026-01-05
    2026-01-05 13:16:06 : INFO  : sketchup2026 : ################## sketchup2026
    2026-01-05 13:16:07 : INFO  : sketchup2026 : Reading arguments again: DEBUG=0
    2026-01-05 13:16:07 : INFO  : sketchup2026 : BLOCKING_PROCESS_ACTION=tell_user
    2026-01-05 13:16:07 : INFO  : sketchup2026 : NOTIFY=success
    2026-01-05 13:16:07 : INFO  : sketchup2026 : LOGGING=INFO
    2026-01-05 13:16:07 : INFO  : sketchup2026 : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2026-01-05 13:16:07 : INFO  : sketchup2026 : Label type: dmg
    2026-01-05 13:16:07 : INFO  : sketchup2026 : archiveName: SketchUp 2026.dmg
    2026-01-05 13:16:07 : INFO  : sketchup2026 : no blocking processes defined, using SketchUp 2026 as default
    2026-01-05 13:16:07 : INFO  : sketchup2026 : name: SketchUp 2026, appName: SketchUp 2026/SketchUp.app
    2026-01-05 13:16:08 : WARN  : sketchup2026 : No previous app found
    2026-01-05 13:16:08 : WARN  : sketchup2026 : could not find SketchUp 2026/SketchUp.app
    2026-01-05 13:16:08 : INFO  : sketchup2026 : appversion:
    2026-01-05 13:16:08 : INFO  : sketchup2026 : Latest version of SketchUp 2026 is 26.1.188.46
    2026-01-05 13:16:08 : REQ   : sketchup2026 : Downloading https://download.sketchup.com/SketchUp-2026-1-188-46.dmg to SketchUp 2026.dmg
    2026-01-05 13:16:45 : INFO  : sketchup2026 : Downloaded SketchUp 2026.dmg – Type is  bzip2 compressed data, block size = 100k – SHA is 0875f095b647c3d8907d64cbfb11b5c143b77eea – Size is 1491736 kB
    2026-01-05 13:16:45 : REQ   : sketchup2026 : no more blocking processes, continue with update
    2026-01-05 13:16:45 : REQ   : sketchup2026 : Installing SketchUp 2026
    2026-01-05 13:16:45 : INFO  : sketchup2026 : Mounting /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.l9MDdk0UZB/SketchUp 2026.dmg
    2026-01-05 13:17:57 : INFO  : sketchup2026 : Mounted: /Volumes/SketchUp 2026
    2026-01-05 13:17:57 : INFO  : sketchup2026 : Verifying: /Volumes/SketchUp 2026/SketchUp 2026/SketchUp.app
    2026-01-05 13:19:59 : INFO  : sketchup2026 : Team ID matching: J8PVMCY7KL (expected: J8PVMCY7KL )
    2026-01-05 13:19:59 : INFO  : sketchup2026 : Installing SketchUp 2026 version 26.1.188 on versionKey CFBundleVersion.
    2026-01-05 13:19:59 : INFO  : sketchup2026 : App has LSMinimumSystemVersion: 13.0
    2026-01-05 13:19:59 : INFO  : sketchup2026 : Copy /Volumes/SketchUp 2026/SketchUp 2026/SketchUp.app to /Applications
    2026-01-05 13:22:37 : WARN  : sketchup2026 : Changing owner to u0105821
    2026-01-05 13:22:42 : INFO  : sketchup2026 : Finishing...
    2026-01-05 13:22:45 : INFO  : sketchup2026 : App(s) found: /Applications/SketchUp 2026/SketchUp.app
    2026-01-05 13:22:45 : INFO  : sketchup2026 : found app at /Applications/SketchUp 2026/SketchUp.app, version 26.1.188, on versionKey CFBundleVersion
    2026-01-05 13:22:45 : REQ   : sketchup2026 : Installed SketchUp 2026, version 26.1.188
    2026-01-05 13:22:45 : INFO  : sketchup2026 : notifying
    2026-01-05 13:22:46 : INFO  : sketchup2026 : Installomator did not close any apps, so no need to reopen any apps.
    2026-01-05 13:22:46 : REQ   : sketchup2026 : All done!
    2026-01-05 13:22:46 : REQ   : sketchup2026 : ################## End Installomator, exit code 0
    ```

--- fragments/labels/sketchup2026.sh
+++ fragments/labels/sketchup2026.sh
@@ -1,10 +1,10 @@
 sketchup2026)
     name="SketchUp 2026"
     type="dmg"
-    downloadURL="$(curl -s https://sketchup.trimble.com/en/download/all | grep -o 'https://download.sketchup.com/SketchUp-2026[^"]*.dmg')"
+    downloadURL=$(curl -sI https://sketchup.trimble.com/sketchup/2026/SketchUpPro-dmg | grep -i '^location:' | awk '{print $2}' | tr -d '\r')
     folderName="SketchUp 2026"
     appName="${folderName}/SketchUp.app"
-    appNewVersion=$(echo "$downloadURL" | grep -o 'SketchUp-20[0-9][0-9]-[0-9]*-[0-9]*' | awk -F '-' '{year=substr($2, 3, 2); if (year >= 24) printf "%d.0.%s", year, $NF; else printf "%d.%s", year+2000, $NF}')
+    appNewVersion=$(echo "$downloadURL" | grep -o '2026-[0-9]*-[0-9]*-[0-9]*' | sed 's/2026-/26./;s/-/./g')
     versionKey="CFBundleVersion"
     expectedTeamID="J8PVMCY7KL"
     ;;
