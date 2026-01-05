41c15580482f46559f94cdff8ccd100a050d67e4
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-01-05 12:41:22 -0700

    Remove packageID from displaylinkmanager.sh
    Since the DisplayLink Manager application is installed in /Applications, that line was deemed unnecessary.

--- fragments/labels/displaylinkmanager.sh
+++ fragments/labels/displaylinkmanager.sh
@@ -1,7 +1,6 @@
 displaylinkmanager)
     name="DisplayLink Manager"
     type="pkg"
-    packageID="com.displaylink.displaylinkmanagerapp"
     appNewVersion=$(curl -sfL https://www.synaptics.com/products/displaylink-graphics/downloads/macos | grep -o 'DisplayLink%20Manager%20Graphics%20Connectivity[0-9.]*-Release' | head -1 | sed 's/DisplayLink%20Manager%20Graphics%20Connectivity//' | sed 's/-Release//')
     productPage=$(curl -sfL https://www.synaptics.com/products/displaylink-graphics/downloads/macos | grep -o 'href="/products/displaylink-manager-graphics-connectivity-[^"]*?filetype=exe"' | head -1 | sed 's/href="//' | sed 's/"$//' | sed 's/?filetype=exe/?filetype=pkg/')
     downloadURL="https://www.synaptics.com$(curl -sfL "https://www.synaptics.com${productPage}" | grep -o '/sites/default/files/exe_files/[^"]*\.pkg' | head -1)"
