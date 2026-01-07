54b3eb79dd702012625176785236ce2870b81c21
RIchard Glaser <richard.glaser@utah.edu>
2026-01-07 13:38:47 -0700

    amadeus-pro-label-update
    output
    ```
    sudo Installomator/utils/assemble.sh amadeuspro2 DEBUG=0
    2026-01-07 13:36:59 : INFO  : amadeuspro2 : setting variable from argument DEBUG=0
    2026-01-07 13:36:59 : INFO  : amadeuspro2 : Total items in argumentsArray: 1
    2026-01-07 13:36:59 : INFO  : amadeuspro2 : argumentsArray: DEBUG=0
    2026-01-07 13:36:59 : REQ   : amadeuspro2 : ################## Start Installomator v. 10.9beta, date 2026-01-07
    2026-01-07 13:36:59 : INFO  : amadeuspro2 : ################## Version: 10.9beta
    2026-01-07 13:36:59 : INFO  : amadeuspro2 : ################## Date: 2026-01-07
    2026-01-07 13:36:59 : INFO  : amadeuspro2 : ################## amadeuspro2
    2026-01-07 13:37:00 : INFO  : amadeuspro2 : Reading arguments again: DEBUG=0
    2026-01-07 13:37:00 : INFO  : amadeuspro2 : BLOCKING_PROCESS_ACTION=tell_user
    2026-01-07 13:37:00 : INFO  : amadeuspro2 : NOTIFY=success
    2026-01-07 13:37:00 : INFO  : amadeuspro2 : LOGGING=INFO
    2026-01-07 13:37:00 : INFO  : amadeuspro2 : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2026-01-07 13:37:00 : INFO  : amadeuspro2 : Label type: zip
    2026-01-07 13:37:00 : INFO  : amadeuspro2 : archiveName: Amadeus Pro.zip
    2026-01-07 13:37:00 : INFO  : amadeuspro2 : no blocking processes defined, using Amadeus Pro as default
    2026-01-07 13:37:00 : INFO  : amadeuspro2 : name: Amadeus Pro, appName: Amadeus Pro.app
    2026-01-07 13:37:01 : WARN  : amadeuspro2 : No previous app found
    2026-01-07 13:37:01 : WARN  : amadeuspro2 : could not find Amadeus Pro.app
    2026-01-07 13:37:01 : INFO  : amadeuspro2 : appversion:
    2026-01-07 13:37:01 : INFO  : amadeuspro2 : Latest version of Amadeus Pro is 2.8.4
    2026-01-07 13:37:01 : REQ   : amadeuspro2 : Downloading https://s3.amazonaws.com/AmadeusPro2/AmadeusPro.zip to Amadeus Pro.zip
    2026-01-07 13:37:02 : INFO  : amadeuspro2 : Downloaded Amadeus Pro.zip – Type is  Zip archive data, at least v1.0 to extract, compression method=store – SHA is 51ae5d9017e283c2365e35d8da4c2211078ec674 – Size is 16196 kB
    2026-01-07 13:37:02 : REQ   : amadeuspro2 : no more blocking processes, continue with update
    2026-01-07 13:37:02 : REQ   : amadeuspro2 : Installing Amadeus Pro
    2026-01-07 13:37:02 : INFO  : amadeuspro2 : Unzipping Amadeus Pro.zip
    2026-01-07 13:37:04 : INFO  : amadeuspro2 : Verifying: /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.mvK4JqqLHY/Amadeus Pro.app
    2026-01-07 13:37:04 : INFO  : amadeuspro2 : Team ID matching: FWDH9W45C2 (expected: FWDH9W45C2 )
    2026-01-07 13:37:04 : INFO  : amadeuspro2 : Installing Amadeus Pro version 2.8.14 on versionKey CFBundleShortVersionString.
    2026-01-07 13:37:04 : INFO  : amadeuspro2 : App has LSMinimumSystemVersion: 10.13
    2026-01-07 13:37:04 : INFO  : amadeuspro2 : Copy /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.mvK4JqqLHY/Amadeus Pro.app to /Applications
    2026-01-07 13:37:06 : WARN  : amadeuspro2 : Changing owner to u0105821
    2026-01-07 13:37:06 : INFO  : amadeuspro2 : Finishing...
    2026-01-07 13:37:09 : INFO  : amadeuspro2 : App(s) found: /Applications/Amadeus Pro.app
    2026-01-07 13:37:09 : INFO  : amadeuspro2 : found app at /Applications/Amadeus Pro.app, version 2.8.14, on versionKey CFBundleShortVersionString
    2026-01-07 13:37:09 : REQ   : amadeuspro2 : Installed Amadeus Pro, version 2.8.14
    2026-01-07 13:37:09 : INFO  : amadeuspro2 : notifying
    2026-01-07 13:37:10 : INFO  : amadeuspro2 : Installomator did not close any apps, so no need to reopen any apps.
    2026-01-07 13:37:10 : REQ   : amadeuspro2 : All done!
    2026-01-07 13:37:10 : REQ   : amadeuspro2 : ################## End Installomator, exit code 0
    ```

--- fragments/labels/amadeuspro2.sh
+++ fragments/labels/amadeuspro2.sh
@@ -1,7 +1,8 @@
 amadeuspro2)
-	# A powerful audio editing tool for macOS, offering multi-track editing, batch processing, and a variety of sound analysis feature
+    # A powerful audio editing tool for macOS, offering multi-track editing, batch processing, and a variety of sound analysis features
     name="Amadeus Pro"
     type="zip"
     downloadURL="https://s3.amazonaws.com/AmadeusPro2/AmadeusPro.zip"
+    appNewVersion=$(curl -fs "https://www.hairersoft.com/pro.html" | grep -ioE "version[: ]+[0-9]+\.[0-9]+(\.[0-9]+)?" | head -1 | grep -oE "[0-9]+\.[0-9]+(\.[0-9]+)?")
     expectedTeamID="FWDH9W45C2"
     ;;
