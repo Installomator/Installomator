70cfac32d9aa1c5617d57376e8f5c65ae092ecfa
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-01-08 15:56:03 +0100

    Fix Topaz Gigapixel AI appNewVersion

--- fragments/labels/topazgigapixel.sh
+++ fragments/labels/topazgigapixel.sh
@@ -3,9 +3,9 @@ topazgigapixelai)
     # credit: Tully Jagoe
     name="Topaz Gigapixel AI"
     type="pkg"
-    appNewVersion=$(curl -fs https://www.topazlabs.com/downloads | grep  -o 'gigaVersion = "v.*"' | grep -o ' "v.*"' | sed -E 's/[v|"| ]//g')
     versionKey="CFBundleShortVersionString"
     downloadURL="https://topazlabs.com/d/gigapixel/latest/mac/full"
-    archiveName="TopazGigapixelAI-${appNewVersion}.pkg"
+    archiveName=$(curl -fsIL $downloadURL | grep -i ^location | awk -F "/" '{print $NF}')
+    appNewVersion=$(grep -oi "[0-9].*[0-9]" <<< $archiveName)
     expectedTeamID="3G3JE37ZHF"
     ;;
