a80598fdb037f263a1a783a3811a0e1dee3c8ec3
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-01-22 13:58:50 -0700

    Remove packageID from passwordutility.sh

--- fragments/labels/passwordutility.sh
+++ fragments/labels/passwordutility.sh
@@ -1,7 +1,6 @@
 passwordutility)
     name="Password Utility"
     type="pkg"
-    packageID="com.twocanoes.PasswordUtility"
     downloadURL="https://twocanoes-software-updates.s3.amazonaws.com/Password_Utility.pkg"
     expectedTeamID="UXP6YEHSPW"
     ;;
