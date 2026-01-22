6f1c7eff2444ec3441a6addd32e0b7c20d39bba7
RIchard Glaser <richard.glaser@utah.edu>
2026-01-22 14:37:46 -0700

    passwordutility-new-label
    output
    ```sudo Installomator/utils/assemble.sh passwordutility DEBUG=0
    2026-01-22 14:37:02 : INFO  : passwordutility : setting variable from argument DEBUG=0
    2026-01-22 14:37:02 : INFO  : passwordutility : Total items in argumentsArray: 1
    2026-01-22 14:37:03 : INFO  : passwordutility : argumentsArray: DEBUG=0
    2026-01-22 14:37:03 : REQ   : passwordutility : ################## Start Installomator v. 10.9beta, date 2026-01-22
    2026-01-22 14:37:03 : INFO  : passwordutility : ################## Version: 10.9beta
    2026-01-22 14:37:03 : INFO  : passwordutility : ################## Date: 2026-01-22
    2026-01-22 14:37:03 : INFO  : passwordutility : ################## passwordutility
    2026-01-22 14:37:03 : INFO  : passwordutility : Reading arguments again: DEBUG=0
    2026-01-22 14:37:04 : INFO  : passwordutility : BLOCKING_PROCESS_ACTION=tell_user
    2026-01-22 14:37:04 : INFO  : passwordutility : NOTIFY=success
    2026-01-22 14:37:04 : INFO  : passwordutility : LOGGING=INFO
    2026-01-22 14:37:04 : INFO  : passwordutility : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2026-01-22 14:37:04 : INFO  : passwordutility : Label type: pkg
    2026-01-22 14:37:04 : INFO  : passwordutility : archiveName: Password Utility.pkg
    2026-01-22 14:37:04 : INFO  : passwordutility : no blocking processes defined, using Password Utility as default
    2026-01-22 14:37:04 : INFO  : passwordutility : App(s) found: /Applications/Password Utility.app
    2026-01-22 14:37:04 : INFO  : passwordutility : found app at /Applications/Password Utility.app, version 1.1, on versionKey CFBundleShortVersionString
    2026-01-22 14:37:04 : INFO  : passwordutility : appversion: 1.1
    2026-01-22 14:37:04 : INFO  : passwordutility : Latest version of Password Utility is 1.1
    2026-01-22 14:37:04 : INFO  : passwordutility : There is no newer version available.
    2026-01-22 14:37:04 : INFO  : passwordutility : Installomator did not close any apps, so no need to reopen any apps.
    2026-01-22 14:37:04 : REQ   : passwordutility : No newer version.
    2026-01-22 14:37:04 : REQ   : passwordutility : ################## End Installomator, exit code 0
    ```

--- fragments/labels/passwordutility.sh
+++ fragments/labels/passwordutility.sh
@@ -2,5 +2,6 @@ passwordutility)
     name="Password Utility"
     type="pkg"
     downloadURL="https://twocanoes-software-updates.s3.amazonaws.com/Password_Utility.pkg"
-    expectedTeamID="UXP6YEHSPW"
+    appNewVersion=$(curl -fsL "https://data.twocanoes.com/api/version_info" | sed -n 's/.*"com.twocanoes.PasswordUtility".*"version":"\([^"]*\)".*/\1/p')
+    expectedTeamID="UXP6YRD6GV"
     ;;
