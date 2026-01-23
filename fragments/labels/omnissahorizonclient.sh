4a54e8d34f6b891c50420d99aa62b41f1c8bb140
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-01-23 12:03:20 -0700

    Update download URL and version extraction logic

--- fragments/labels/omnissahorizonclient.sh
+++ fragments/labels/omnissahorizonclient.sh
@@ -5,7 +5,7 @@ omnissahorizonclient)
     code=$(<<< "$jsonData" sed -nE 's/.*Omnissa Horizon Client for macOS[^}]*"code":"([^"]*).*/\1/p')
     productId=$(<<< "$jsonData" sed -nE 's/.*Omnissa Horizon Client for macOS[^}]*"productId":"([^"]*).*/\1/p')
     releasePackageId=$(<<< "$jsonData" sed -nE 's/.*Omnissa Horizon Client for macOS[^}]*"releasePackageId":"([^"]*).*/\1/p')
-    downloadURL=$(curl -fsL "https://customerconnect.omnissa.com/channel/public/api/v1.0/dlg/details?locale=en_US&downloadGroup=$code&productId=$productId&rPId=$releasePackageId" | grep -oE 'https://[^"]*' )
-    appNewVersion=$(<<< "$downloadURL" | grep -oE '\d+\.[0-9.]*\d')
+    downloadURL=$(curl -fsL "https://customerconnect.omnissa.com/channel/public/api/v1.0/dlg/details?locale=en_US&downloadGroup=$code&productId=$productId&rPId=$releasePackageId" | sed -nE 's/.*"thirdPartyDownloadUrl":"([^"]*).*/\1/p')
+    appNewVersion=$(<<< "$downloadURL" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
     expectedTeamID="S2ZMFGQM93"
     ;;
