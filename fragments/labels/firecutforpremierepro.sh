c8dc183c10f94bb9edc1d5315e4c02b83e3ebd81
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-01-26 13:08:50 +0100

    Add FireCut for Premiere Pro

---
+++ fragments/labels/firecutforpremierepro.sh
@@ -0,0 +1,9 @@
+firecutforpremierepro)
+    name="Firecut for Premiere Pro"
+    type="pkg"
+    downloadURL="$(curl -fsL https://firecut.ai/downloads | grep "FireCut_PP_Dist.*pkg" | grep -o "https://.*pkg")"
+    appNewVersion=$(echo $downloadURL | grep -o "[0-9].*[0-9]")
+    appCustomVersion(){ xmllint --xpath '//ExtensionList' '/Library/Application Support/Adobe/CEP/extensions/com.firecut.base/CSXS/manifest.xml' | grep -o "[0-9].*[0-9]" }
+    expectedTeamID="7ASRSVAEMS"
+    blockingProcesses=( "Adobe Premiere Pro 2024" "Adobe Premiere Pro 2025" "Adobe Premiere Pro 2026" "Adobe Premiere Pro 2027" )
+    ;;
