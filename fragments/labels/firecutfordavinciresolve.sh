47152b81298a8c2b4686714114ec7112f6e51098
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-01-26 13:17:51 +0100

    Correct capitalization of FireCut

--- fragments/labels/firecutfordavinciresolve.sh
+++ fragments/labels/firecutfordavinciresolve.sh
@@ -1,5 +1,5 @@
 firecutfordavinciresolve)
-    name="Firecut for DaVinci Resolve"
+    name="FireCut for DaVinci Resolve"
     type="dmg"
     downloadURL="$(curl -fsIL https://firecut.ai/downloads/davinci/apple_silicon/ | grep -i ^location | awk '{print $2}' | tr -d '\r')"
     appNewVersion=$(echo $downloadURL | awk -F "." '{print$7}' | sed 's/v//' | sed 's/_/./g')
