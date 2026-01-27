ddddf1804621e03fc486a53f4369dfefc7af9e30
Adde96 <85020840+Adde96@users.noreply.github.com>
2026-01-27 08:52:39 +0100

    Update praat.sh

--- fragments/labels/praat.sh
+++ fragments/labels/praat.sh
@@ -1,7 +1,7 @@
 praat)
     name="Praat"
     type="dmg"
-    praatVersion="$(curl -fs https://www.fon.hum.uva.nl/praat/download_mac.html | grep "for Intel and Apple Silicon):" | cut -d '_' -f2 | cut -c15-)" 
+    praatVersion="$(curl -fs https://www.fon.hum.uva.nl/praat/download_mac.html | grep "64-bit edition" | cut -d ">" -f4 | cut -d "_" -f1 | cut -c 6-)"
     downloadURL="https://www.fon.hum.uva.nl/praat/praat${praatVersion}_mac.dmg"
     appNewVersion="${praatVersion:0:1}.${praatVersion:1:1}.${praatVersion:2:2}"
     expectedTeamID="J9C6R9XA5W"
