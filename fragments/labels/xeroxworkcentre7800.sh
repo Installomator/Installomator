47b4e0151b0699135fa5ba639e905c4ee9550722
rougegoat <rougegoat@gmail.com>
2026-01-30 10:10:57 -0600

    Update xeroxworkcentre7800.sh
    Change the appCustomVersion to be less reliant on a specific model number

--- fragments/labels/xeroxworkcentre7800.sh
+++ fragments/labels/xeroxworkcentre7800.sh
@@ -2,7 +2,7 @@ xeroxprintandscan|\
 xeroxworkcentre7800)
     name="XeroxWorkCentre"
     type="pkgInDmg"
-    appCustomVersion(){ lpinfo -m | grep 783 | tail -n 1 | awk -F ', ' '{print $2}' }
+    appCustomVersion(){ defaults read "/Library/Printers/Xerox/PDEs/XeroxFeatures.plugin/Contents/Info" CFBundleShortVersionString }
     appNewVersion=$( curl -fsL "https://www.support.xerox.com/nl-nl/product/workcentre-7800-series/downloads?platform=macOS14" | grep .dmg | head -n 1 | awk -F '_' '{print $2}' )
     downloadURL=$( curl -fsL "https://www.support.xerox.com/nl-nl/product/workcentre-7800-series/downloads?platform=macOS14" | xmllint --html --format - 2>/dev/null | grep -o "https://.*XeroxDrivers.*.dmg" )
     expectedTeamID="G59Y3XFNFR"
