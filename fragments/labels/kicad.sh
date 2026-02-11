c37b17d8ee0f950c79d906f717304bad30bf66ca
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-02-11 15:46:26 -0700

    Update blockingProcesses to use lowercase names

--- fragments/labels/kicad.sh
+++ fragments/labels/kicad.sh
@@ -7,5 +7,5 @@ kicad)
     appName="${folderName}/KiCad.app"
     versionKey="CFBundleShortVersionString"
     expectedTeamID="9FQDHNY6U2"
-    blockingProcesses=("GerbView" "bitmap2component" "KiCad" "pl_editor" "pcb_calculator" "pcbnew" "eeschema")
+    blockingProcesses=("gerbview" "bitmap2component" "kicad" "pl_editor" "pcb_calculator" "pcbnew" "eeschema")
     ;;
