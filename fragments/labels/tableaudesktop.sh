5838b383d99e252ef31414ea7dd41f2418539747
PacketPalantir <136533429+PacketPalantir@users.noreply.github.com>
2026-02-21 11:19:23 -0500

    Updated Tableau URLs

--- fragments/labels/tableaudesktop.sh
+++ fragments/labels/tableaudesktop.sh
@@ -3,9 +3,9 @@ tableaudesktop)
     type="pkgInDmg"
     packageID="com.tableausoftware.tableaudesktop"
     if [[ $(/usr/bin/arch) == "arm64" ]]; then
-        downloadURL="https://www.tableau.com/downloads/desktop/mac-arm64"
+        downloadURL="https://www.tableau.com/downloads/desktop/reg-mac-arm64"
     else
-        downloadURL="https://www.tableau.com/downloads/desktop/mac"
+        downloadURL="https://www.tableau.com/downloads/desktop/reg-mac"
     fi
     appNewVersion=${$(curl -fsIL "$downloadURL" | sed -nE 's/.*Desktop-([0-9-]*).*/\1/p')//-/.}
     expectedTeamID="QJ4XPRK37C"
