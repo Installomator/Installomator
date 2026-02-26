ace17ca01db09c0bc4aedd7dc0466e9001a3820e
Daniel C <dcarrell713@gmail.com>
2026-02-26 12:25:52 -0700

    Added appCustomVersion

--- fragments/labels/1password8.sh
+++ fragments/labels/1password8.sh
@@ -6,5 +6,6 @@
     appNewVersion=$(curl -s https://releases.1password.com/mac/ | grep "1Password for Mac" | head -n 1 | grep -o ">Updated to.*on<" | grep -oE "[0-9].*[0-9]" | cut -d - -f 1)
     expectedTeamID="2BUA8C4S2C"
     blockingProcesses=( "1Password Extension Helper" "1Password 7" "1Password 8" "1Password" "1PasswordNativeMessageHost" "1PasswordSafariAppExtension" )
+    appCustomVersion(){ defaults read "/Applications/1Password.app/Contents/Info.plist" CFBundleShortVersionString }
     #forcefulQuit=YES
     ;;
