6058cd37cabe76e7ce7c5a6e83ba7b7c903cf47a
Nilon123456789 <dowers.purer0q@icloud.com>
2026-03-03 11:23:47 -0500

    fix dymoconnectdesktop downloadURL (#2788)

--- fragments/labels/dymoconnectdesktop.sh
+++ fragments/labels/dymoconnectdesktop.sh
@@ -3,6 +3,10 @@ dymoconnectdesktop)
     type="pkg"
     appNewVersion=$(curl -s https://formulae.brew.sh/cask/dymo-connect | grep -o 'Current version:.*[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | head -1)
     archiveName="DCDMac${appNewVersion}.pkg"
-    downloadURL="https://download.dymo.com/dymo/Software/Mac/DCDMac${appNewVersion}.pkg"
+    arch="Arm64"
+    if [[ $(/usr/bin/arch) != "arm64" ]]; then
+        arch="X64"
+    fi
+    downloadURL="https://dymoreleasecontent.blob.core.windows.net/dymo-release/DCDMAC/DCDMac${appNewVersion}-${arch}.pkg"
     expectedTeamID="N3S6676K3E"
     ;;
