f8224f21cf65b0974de156e709d2a3da767ff105
Julian Ortega <github@web.julianortega.dev>
2026-03-02 20:15:22 -0600

    Fixed targetDir and Name

--- fragments/labels/realvncserver.sh
+++ fragments/labels/realvncserver.sh
@@ -1,10 +1,9 @@
 vncconnect|\
 realvncserver)
-    name="Real VNC Server"
-    appName="VNC Server.app"
+    name="RealVNC Server"
     type="pkg"
-    packageID="com.realvnc.vncserver.pkg"
-    downloadURL="https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-Latest-MacOSX-universal.pkg"
+    targetDir="/Applications/RealVNC"
     appNewVersion="$(curl -sL https://realvnc.zendesk.com/api/v2/help_center/en-us/articles/5835892358941.json | sed -n 's/.*VNC-Server-\([0-9.]*\)-MacOSX.*/\1/p')"
+    downloadURL="https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-$appNewVersion-MacOSX-universal.pkg"
     expectedTeamID="ZNCQ8JEH7X"
     ;;
