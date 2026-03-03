3d75d6050502b6d252d9c7aed10863b30bd7e529
Julian Ortega <github@web.julianortega.dev>
2026-03-02 18:35:20 -0600

    fixed download URL

--- fragments/labels/realvncserver.sh
+++ fragments/labels/realvncserver.sh
@@ -4,7 +4,7 @@ realvncserver)
     appName="VNC Server.app"
     type="pkg"
     packageID="com.realvnc.vncserver.pkg"
-    downloadURL="$(curl -sL https://www.realvnc.com/en/connect/download/vnc/ | grep -i 'download-link-path-macos' | sed -r 's/.*href="([^"]+).*/\1/g')"
-    appNewVersion="$(echo ${downloadURL} | sed -n 's:.*VNC-Server-\(.*\)-MacOSX.*:\1:p')"
+    downloadURL="https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-Latest-MacOSX-universal.pkg"
+    appNewVersion="$(curl -sL https://realvnc.zendesk.com/api/v2/help_center/en-us/articles/5835892358941.json | sed -n 's/.*VNC-Server-\([0-9.]*\)-MacOSX.*/\1/p')"
     expectedTeamID="ZNCQ8JEH7X"
     ;;
