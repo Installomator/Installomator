4b3de216bb1190119a56a041f5f9ddef3492fc00
Dan Kuehling <dkuehling@district158.org>
2026-03-19 09:43:03 -0500

    Fixing errant )
    Sorry about that, had an extra ) in the downloadURL.

--- fragments/labels/overflow.sh
+++ fragments/labels/overflow.sh
@@ -1,7 +1,7 @@
 overflow)
     name="Overflow"
     type="dmg"
-    downloadURL="$(curl -LsS https://overflow.io/download/ | grep -oE 'https://[^"]+\.dmg'))"
+    downloadURL="$(curl -LsS https://overflow.io/download/ | grep -oE 'https://[^"]+\.dmg')"
     appNewVersion=$(echo "$downloadURL" | awk -F '-|[.]dmg' '{ print $(NF-1) }')
     expectedTeamID="7TK7YSGJFF"
     versionKey="CFBundleShortVersionString"
