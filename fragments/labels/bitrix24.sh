3af337b6bfb0a0ca6d0309d334e46eebfec82123
Jared Schwager <jared.schwager@gmail.com>
2026-03-24 13:27:02 -0400

    Fix indentation in bitrix24.sh for consistency

--- fragments/labels/bitrix24.sh
+++ fragments/labels/bitrix24.sh
@@ -1,12 +1,12 @@
 bitrix24)
-     name="Bitrix24"
-     type="dmg"
-     archiveName="bitrix24_desktop.dmg"
+    name="Bitrix24"
+    type="dmg"
+    archiveName="bitrix24_desktop.dmg"
     if [[ $(arch) == i386 ]]; then
         downloadURL="https://dl.bitrix24.com/b24/bitrix24_desktop.dmg"
     elif [[ $(arch) == arm64 ]]; then
         downloadURL="https://dl.bitrix24.com/b24/bitrix24_desktop_arm.dmg"
     fi
-     appNewVersion="$(curl -fs "https://www.bitrix24.com/osx_version.php" | xpath 'string(//rss/channel/item/title)' 2>/dev/null)"
-     expectedTeamID="5B3T3A994N"
+    appNewVersion="$(curl -fs "https://www.bitrix24.com/osx_version.php" | xpath 'string(//rss/channel/item/title)' 2>/dev/null)"
+    expectedTeamID="5B3T3A994N"
      ;;
