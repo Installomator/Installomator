209d166c9335fe37814f6db95d6e8b5f63b1d626
kstlouis <kstlouis@me.com>
2026-04-15 12:24:03 -0600

    [38;2;216;222;233m   1[0m [38;2;216;222;233mfix(conductor): add x86_64 download URL for Intel Mac support[0m

--- fragments/labels/conductor.sh
+++ fragments/labels/conductor.sh
@@ -1,7 +1,11 @@
 conductor)
     name="Conductor"
     type="dmg"
-    downloadURL="https://cdn.crabnebula.app/download/melty/conductor/latest/platform/dmg-aarch64"
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL="https://cdn.crabnebula.app/download/melty/conductor/latest/platform/dmg-aarch64"
+    else
+        downloadURL="https://cdn.crabnebula.app/download/melty/conductor/latest/platform/dmg-x86_64"
+    fi
     appNewVersion=$(curl -fsIL "$downloadURL" | grep -i "^content-disposition" | sed 's/.*Conductor_\([0-9.]*\)_.*/\1/')
     expectedTeamID="27XN666UJ7"
     ;;
