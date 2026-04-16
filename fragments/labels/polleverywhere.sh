4686db41e12f35fe602b4254b618def36b143353
davisbr1 <31282097+davisbr1@users.noreply.github.com>
2026-04-16 16:37:27 -0400

    Update polleverywhere.sh

--- fragments/labels/polleverywhere.sh
+++ fragments/labels/polleverywhere.sh
@@ -1,7 +1,7 @@
 polleverywhere)
     name="Poll Everywhere"
     type="dmg"
-    downloadURL="https://www.polleverywhere.com/app.dmg"
-    appNewVersion=""
+    downloadURL="$(curl -fs https://www.polleverywhere.com/app/releases/mac | grep -i mac-stable | grep -o -i -E "https.*" | cut -d '"' -f1 | head -n 1)"
+    appNewVersion="$(echo $downloadURL | cut -d "/" -f5)"
     expectedTeamID="W48F3X5M8W"
     ;;
