813506e73025e1e3da7539ad4ff8514fcf27b137
hwlans <175405805+hwlans@users.noreply.github.com>
2026-04-17 10:52:37 +0200

    Update rstudio.sh

--- fragments/labels/rstudio.sh
+++ fragments/labels/rstudio.sh
@@ -1,8 +1,7 @@
 rstudio)
     name="RStudio"
     type="dmg"
-    downloadPage=$(curl -s "https://docs.posit.co/supported-versions/rstudio.html" | grep -E "https.*Installers" | head -n 1 | grep -oE 'https?://[^"]+')
-    downloadURL=$(curl -s "$downloadPage" | grep -E "macos.*dmg" | head -1 | grep -oE 'https?://[^"]+')
-    appNewVersion=$(echo "$downloadPage" | grep -oE '[0-9]{4}\.[0-9]{2}\.[0-9]{1,2}\+[0-9]+')
+    downloadURL="https://rstudio.org/download/latest/stable/desktop/mac/RStudio-latest.dmg"
+    appNewVersion=$(curl -sfI "$downloadURL" | grep -i "^location" | grep -oE '[0-9]{4}\.[0-9]{2}\.[0-9]{1,2}\-[0-9]+' | sed 's/-/+/')
     expectedTeamID="FYF2F5GFX4"
     ;;
