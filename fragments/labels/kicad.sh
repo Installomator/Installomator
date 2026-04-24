56daa0b51af258e50c37646d79cc01120f615015
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-04-24 08:27:18 -0600

    Refactor KiCad download URL and version retrieval
    Migrated to built-in functions "downloadURLFromGit" and "versionFromGit" funcations

--- fragments/labels/kicad.sh
+++ fragments/labels/kicad.sh
@@ -1,8 +1,9 @@
 kicad)
     name="KiCad"
     type="dmg"
-    appNewVersion=$(curl -s "https://api.github.com/repos/KiCad/kicad-source-mirror/releases/latest" | grep '"tag_name":' | cut -d'"' -f4)
-    downloadURL="https://github.com/KiCad/kicad-source-mirror/releases/download/${appNewVersion}/kicad-unified-universal-${appNewVersion}.dmg"
+    archiveName="kicad-unified-universal-"
+    downloadURL="$(downloadURLFromGit KiCad kicad-source-mirror)"
+    appNewVersion="$(versionFromGit KiCad kicad-source-mirror)"
     folderName="KiCad"
     appName="${folderName}/KiCad.app"
     versionKey="CFBundleShortVersionString"
