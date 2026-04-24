d6d108342d1c48742f808c6199a4a53013c36795
brndnwds6 <bwoods@allcovered.com>
2026-04-24 15:31:15 -0500

    Update claudedesktop.sh

--- fragments/labels/claudedesktop.sh
+++ fragments/labels/claudedesktop.sh
@@ -5,6 +5,9 @@ claudedesktop)
 	downloadURL=$(curl -fs "https://downloads.claude.ai/releases/darwin/universal/RELEASES.json" | grep -o '"url":"[^"]*"' | cut -d'"' -f4)
 	appNewVersion=$(curl -fs "https://downloads.claude.ai/releases/darwin/universal/RELEASES.json" | grep -o '"currentRelease":"[^"]*"' | cut -d'"' -f4)
 	expectedTeamID="Q6L2SF6YDW"
+	blockingProcesses=( "Claude" "Claude Helper (Renderer)" "Claude Helper" )
 	;;
+	
+	
 	
 
