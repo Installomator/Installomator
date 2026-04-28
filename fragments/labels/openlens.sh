5d8db3bbe1d096205a5fe948dc63a3fa1df9f9cb
Michaël De Bona <debona.michael@gmail.com>
2026-04-28 13:49:44 +0200

    feat: adding the openlens app label
    Adds label for the OpenLens app which is the open source counterpart of the Lens app

---
+++ fragments/labels/openlens.sh
@@ -0,0 +1,7 @@
+openlens)
+    name="OpenLens"
+    type="dmg"
+    downloadURL="$(downloadURLFromGit MuhammedKalkan OpenLens)"
+    appNewVersion="$(versionFromGit MuhammedKalkan OpenLens)"
+    expectedTeamID="HGC72W36QJ"
+    ;;
