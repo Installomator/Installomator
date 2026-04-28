646369fbcb225abfcf02027e5fa2efa3e7892ec3
Adam Codega <acodega@users.noreply.github.com>
2026-04-28 15:38:41 -0400

    Change exit code for Gemini compatibility error

--- fragments/labels/gemini.sh
+++ fragments/labels/gemini.sh
@@ -5,7 +5,7 @@ gemini)
         downloadURL="https://dl.google.com/release2/j33ro/release/Gemini.dmg"
     else
         printlog "Gemini is only compatible with Apple Silicon (arm64) Macs." ERROR
-        cleanupAndExit 98 "Gemini requires Apple Silicon" ERROR
+        cleanupAndExit 95 "Gemini requires Apple Silicon" ERROR
     fi
     expectedTeamID="EQHXZ8M8AV"
     ;;
