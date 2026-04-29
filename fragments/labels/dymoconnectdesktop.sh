ed8ba7180baf73d0e7873b77a0ca9d484aa75621
Nilon123456789 <dowers.purer0q@icloud.com>
2026-04-29 15:08:03 -0400

    fix change arch condition to respect convention

--- fragments/labels/dymoconnectdesktop.sh
+++ fragments/labels/dymoconnectdesktop.sh
@@ -3,10 +3,10 @@ dymoconnectdesktop)
     type="pkg"
     appNewVersion=$(curl -s https://formulae.brew.sh/cask/dymo-connect | grep -o 'Current version:.*[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | head -1)
     archiveName="DCDMac${appNewVersion}.pkg"
-    arch="Arm64"
-    if [[ $(/usr/bin/arch) != "arm64" ]]; then
-        arch="X64"
-    fi
-    downloadURL="https://dymoreleasecontent.blob.core.windows.net/dymo-release/DCDMAC/DCDMac${appNewVersion}-${arch}.pkg"
+    if [[ $(/usr/bin/arch) == "arm64" ]]; then
+        downloadURL="https://dymoreleasecontent.blob.core.windows.net/dymo-release/DCDMAC/DCDMac${appNewVersion}-Arm64.pkg"
+     else
+        downloadURL="https://dymoreleasecontent.blob.core.windows.net/dymo-release/DCDMAC/DCDMac${appNewVersion}-X64.pkg"
+     fi
     expectedTeamID="N3S6676K3E"
     ;;
