ae80ef59624659f4c68d7d0808be3e433a4bb6b3
Tom Case <tcase@azurtechnology.com>
2026-04-29 11:43:43 +0100

    Update microsoftvisualstudiocode.sh
    Corrected assignment formatting

--- fragments/labels/microsoftvisualstudiocode.sh
+++ fragments/labels/microsoftvisualstudiocode.sh
@@ -2,10 +2,7 @@ microsoftvisualstudiocode|\
 visualstudiocode)
     name="Visual Studio Code"
     type="zip"
-    appNewVersion=$(curl -fsL "https://update.code.visualstudio.com/api/releases/stable" \
-        | tr ',' '\n' \
-        | head -1 \
-        | tr -d '["')
+    appNewVersion=$(curl -fsL "https://update.code.visualstudio.com/api/releases/stable" | tr ',' '\n' | head -1 | tr -d '["')
     downloadURL="https://update.code.visualstudio.com/latest/darwin-universal/stable"
     expectedTeamID="UBF8T346G9"
     appName="Visual Studio Code.app"
