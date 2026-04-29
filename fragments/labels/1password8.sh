2d2809b2b3404af439bf04b357b1f914bd3a8616
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-04-29 10:55:19 -0600

    Remove commented forcefulQuit variable
    Removed commented out forcefulQuit variable.

--- fragments/labels/1password8.sh
+++ fragments/labels/1password8.sh
@@ -5,5 +5,4 @@
     appNewVersion=$(curl -s https://releases.1password.com/mac/stable/index.xml | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | tail -n1)
     expectedTeamID="2BUA8C4S2C"
     blockingProcesses=( "1Password Extension Helper" "1Password 7" "1Password 8" "1Password" "1PasswordNativeMessageHost" "1PasswordSafariAppExtension" )
-    #forcefulQuit=YES
     ;;
