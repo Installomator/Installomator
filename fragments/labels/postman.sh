8a33712869f458539de2876029b6b8ed040835b9
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-04-29 11:14:56 -0600

    Fix indentation in postman.sh script
    Replaced tabs with spaces from `elif` and `fi` lines

--- fragments/labels/postman.sh
+++ fragments/labels/postman.sh
@@ -4,9 +4,9 @@ postman)
     curlOptions=( -H "accept-encoding: gzip, deflate, br")
     if [[ $(arch) == "arm64" ]]; then
         downloadURL="https://dl.pstmn.io/download/latest/osx_arm64"
-	elif [[ $(arch) == "i386" ]]; then
+    elif [[ $(arch) == "i386" ]]; then
         downloadURL="https://dl.pstmn.io/download/latest/osx_64"
-	fi
+    fi
     appNewVersion=$(getJSONValue "$(curl -fsL 'https://mkt.cdn.postman.com/www-next/release-notes/app-release-notes.json')" 'notes[0].version')
     expectedTeamID="H7H8Q7M5CK"
     ;;
