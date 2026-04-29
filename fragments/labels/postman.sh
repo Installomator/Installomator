26daf2f574d5d20f4aa899ab5cb7274eedc39e60
Fredrik Larsson <33235315+kryptonit1@users.noreply.github.com>
2026-04-29 09:13:11 +0200

    Update postman.sh
    Removed tabs

--- fragments/labels/postman.sh
+++ fragments/labels/postman.sh
@@ -3,10 +3,10 @@ postman)
     type="zip"
     curlOptions=( -H "accept-encoding: gzip, deflate, br")
     if [[ $(arch) == "arm64" ]]; then
-    	downloadURL="https://dl.pstmn.io/download/latest/osx_arm64"
+        downloadURL="https://dl.pstmn.io/download/latest/osx_arm64"
 	elif [[ $(arch) == "i386" ]]; then
-		downloadURL="https://dl.pstmn.io/download/latest/osx_64"
+        downloadURL="https://dl.pstmn.io/download/latest/osx_64"
 	fi
-	appNewVersion=$(getJSONValue "$(curl -fsL 'https://mkt.cdn.postman.com/www-next/release-notes/app-release-notes.json')" 'notes[0].version')
+    appNewVersion=$(getJSONValue "$(curl -fsL 'https://mkt.cdn.postman.com/www-next/release-notes/app-release-notes.json')" 'notes[0].version')
     expectedTeamID="H7H8Q7M5CK"
     ;;
