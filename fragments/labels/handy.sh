677f812e447e90b19adee1c7a5646e419a9a232c
Henk <hbokhoven+github@gmail.com>
2026-04-29 07:26:55 +0200

    chore: remove comments

--- fragments/labels/handy.sh
+++ fragments/labels/handy.sh
@@ -1,6 +1,4 @@
 handy)
-    # Handy - A free, open source, and extensible speech-to-text application
-    # https://github.com/cjpais/Handy
     name="Handy"
     type="dmg"
     if [[ $(arch) == "arm64" ]]; then
