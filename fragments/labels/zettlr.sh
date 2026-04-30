6ee3e2f305bc658f00efffb8e1e162bf31821b24
BrendonMB <11401256+BrendonMB@users.noreply.github.com>
2026-04-30 14:04:18 +1000

    Revert "label zettlr create"

--- fragments/labels/zettlr.sh
+++
@@ -1,7 +0,0 @@
-zettlr)
-    name="Zettlr"
-    type="dmg"
-    downloadURL=$(downloadURLFromGit Zettlr Zettlr)
-    appNewVersion=$(versionFromGit Zettlr Zettlr)
-    expectedTeamID="QS52BN8W68"
-    ;;
