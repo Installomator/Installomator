94c3c6da4cae1b483a3b1153665358af19a14df6
Jared Schwager <jared.schwager@gmail.com>
2026-05-01 15:54:36 -0400

    Removed blockingProcess as the process name matches the app name and is automatically infered by Installomator

--- fragments/labels/numi.sh
+++ fragments/labels/numi.sh
@@ -4,5 +4,4 @@ numi)
     downloadURL="https://s1.numi.app/download"
     appNewVersion="$(curl -fs https://github.com/nikolaeu/numi/tags | grep -Eo 'mac-[0-9]+(\.[0-9]+)*' | sort -V | tail -n 1 | sed 's/^mac-//')"
     expectedTeamID="4BT8G3UCSZ"
-    blockingProcesses=( "Numi" )
     ;;
