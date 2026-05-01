c471c556a21069bde3001fbbf16a5d0a1a3e7da1
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-05-01 13:28:31 -0600

    Fix download URL for VueScan

--- fragments/labels/vuescanuni.sh
+++ fragments/labels/vuescanuni.sh
@@ -2,6 +2,6 @@ vuescanuni)
     name="VueScan"
     type="dmg"
     appNewVersion=$(curl -sfL https://www.hamrick.com/vuescan/vuescan.htm | grep -Eo 'VueScan [0-9]+\.[0-9]+\.[0-9]+' | head -n 1 | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
-    downloadURL="https://www.hamrick.com/files/vuea64-${appNewVersion}.dmg"
+    downloadURL="https://files.hamrick.com/vuea64-${appNewVersion}.dmg"
     expectedTeamID="5D5BXT9KP5"
     ;;
