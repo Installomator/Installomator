34727ef5686ec5dc3942125a6c1bc3a284343aba
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-05-01 12:46:27 -0600

    Remove description comment from amadeuspro2.sh
    Removed description comment for Amadeus Pro.

--- fragments/labels/amadeuspro2.sh
+++ fragments/labels/amadeuspro2.sh
@@ -1,5 +1,4 @@
 amadeuspro2)
-    # A powerful audio editing tool for macOS, offering multi-track editing, batch processing, and a variety of sound analysis features
     name="Amadeus Pro"
     type="zip"
     downloadURL="https://s3.amazonaws.com/AmadeusPro2/AmadeusPro.zip"
