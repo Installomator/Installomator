7d17a942f0b1d3da2a5b6ee5409340bc51f14a89
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-05-01 14:04:35 -0600

    Remove blockingProcesses from xcreds.sh
    Removed blockingProcesses variable from xcreds.sh.

--- fragments/labels/xcreds.sh
+++ fragments/labels/xcreds.sh
@@ -5,5 +5,4 @@ xcreds)
     downloadURL="https://twocanoes-software-updates.s3.amazonaws.com/XCreds.pkg"
     appNewVersion=$(curl -fs "https://twocanoes.com/products/mac/xcreds/history/" | grep -A1 "<h3>Change Log</h3>" | sed -n 's/.*<h4>Version \(.*\) Build.*/\1/p')
     expectedTeamID="UXP6YEHSPW"
-    blockingProcesses=( NONE )
     ;;
