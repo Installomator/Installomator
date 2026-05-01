1e98278cd62cc9d91691edd80b590ed09dfc1f66
FriskyBurger <Brendon.Metcalf.Butcher@gmail.com>
2026-05-01 11:10:05 +1000

    Updated to use architecture detection
    Output:
    ```
    ./utils/assemble.sh zettlr DEBUG=0 INSTALL=force
    2026-05-01 11:08:42 : INFO  : zettlr : setting variable from argument DEBUG=0
    2026-05-01 11:08:42 : INFO  : zettlr : setting variable from argument INSTALL=force
    2026-05-01 11:08:42 : INFO  : zettlr : Total items in argumentsArray: 2
    2026-05-01 11:08:42 : INFO  : zettlr : argumentsArray: DEBUG=0 INSTALL=force
    2026-05-01 11:08:42 : REQ   : zettlr : ################## Start Installomator v. 10.9beta, date 2026-05-01
    2026-05-01 11:08:42 : INFO  : zettlr : ################## Version: 10.9beta
    2026-05-01 11:08:42 : INFO  : zettlr : ################## Date: 2026-05-01
    2026-05-01 11:08:42 : INFO  : zettlr : ################## zettlr
    2026-05-01 11:08:43 : INFO  : zettlr : Reading arguments again: DEBUG=0 INSTALL=force
    2026-05-01 11:08:43 : INFO  : zettlr : BLOCKING_PROCESS_ACTION=tell_user
    2026-05-01 11:08:43 : INFO  : zettlr : NOTIFY=success
    2026-05-01 11:08:43 : INFO  : zettlr : LOGGING=INFO
    2026-05-01 11:08:43 : INFO  : zettlr : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2026-05-01 11:08:43 : INFO  : zettlr : Label type: dmg
    2026-05-01 11:08:43 : INFO  : zettlr : archiveName: Zettlr.dmg
    2026-05-01 11:08:43 : INFO  : zettlr : no blocking processes defined, using Zettlr as default
    2026-05-01 11:08:43 : INFO  : zettlr : App(s) found: /Applications/Zettlr.app
    2026-05-01 11:08:43 : INFO  : zettlr : found app at /Applications/Zettlr.app, version 4.4.0, on versionKey CFBundleShortVersionString
    2026-05-01 11:08:43 : INFO  : zettlr : appversion: 4.4.0
    2026-05-01 11:08:43 : INFO  : zettlr : Label is not of type “updateronly”, and it’s set to use force to install or ignoring app store apps, so not using updateTool.
    2026-05-01 11:08:43 : INFO  : zettlr : Latest version of Zettlr is 4.4.0
    2026-05-01 11:08:43 : INFO  : zettlr : There is no newer version available.
    2026-05-01 11:08:43 : REQ   : zettlr : Downloading https://github.com/Zettlr/Zettlr/releases/download/v4.4.0/Zettlr-4.4.0-arm64.dmg to Zettlr.dmg
    2026-05-01 11:08:58 : INFO  : zettlr : Downloaded Zettlr.dmg – Type is  zlib compressed data – SHA is 24222b5edd5a8651f9d798d72393fdba4ae0b061 – Size is 180368 kB
    2026-05-01 11:08:58 : REQ   : zettlr : no more blocking processes, continue with update
    2026-05-01 11:08:58 : REQ   : zettlr : Installing Zettlr
    2026-05-01 11:08:58 : INFO  : zettlr : Mounting /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.8uvh2C4MDI/Zettlr.dmg
    2026-05-01 11:09:02 : INFO  : zettlr : Mounted: /Volumes/Zettlr 4.4.0-arm64
    2026-05-01 11:09:02 : INFO  : zettlr : Verifying: /Volumes/Zettlr 4.4.0-arm64/Zettlr.app
    2026-05-01 11:09:04 : INFO  : zettlr : Team ID matching: QS52BN8W68 (expected: QS52BN8W68 )
    2026-05-01 11:09:04 : INFO  : zettlr : Downloaded version of Zettlr is 4.4.0 on versionKey CFBundleShortVersionString, same as installed.
    2026-05-01 11:09:04 : INFO  : zettlr : Using force to install anyway.
    2026-05-01 11:09:04 : INFO  : zettlr : App has LSMinimumSystemVersion: 12.0
    2026-05-01 11:09:04 : WARN  : zettlr : Removing existing /Applications/Zettlr.app
    2026-05-01 11:09:05 : INFO  : zettlr : Copy /Volumes/Zettlr 4.4.0-arm64/Zettlr.app to /Applications
    2026-05-01 11:09:07 : WARN  : zettlr : Changing owner to brendon.m
    2026-05-01 11:09:07 : INFO  : zettlr : Finishing...
    2026-05-01 11:09:10 : INFO  : zettlr : App(s) found: /Applications/Zettlr.app
    2026-05-01 11:09:10 : INFO  : zettlr : found app at /Applications/Zettlr.app, version 4.4.0, on versionKey CFBundleShortVersionString
    2026-05-01 11:09:10 : REQ   : zettlr : Installed Zettlr, version 4.4.0
    2026-05-01 11:09:10 : INFO  : zettlr : notifying
    2026-05-01 11:09:10 : INFO  : zettlr : Installomator did not close any apps, so no need to reopen any apps.
    2026-05-01 11:09:10 : REQ   : zettlr : All done!
    2026-05-01 11:09:10 : REQ   : zettlr : ################## End Installomator, exit code 0
    ```

--- fragments/labels/zettlr.sh
+++ fragments/labels/zettlr.sh
@@ -1,7 +1,11 @@
 zettlr)
     name="Zettlr"
     type="dmg"
-    downloadURL=$(downloadURLFromGit Zettlr Zettlr)
     appNewVersion=$(versionFromGit Zettlr Zettlr)
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL="https://github.com/Zettlr/Zettlr/releases/download/v${appNewVersion}/Zettlr-${appNewVersion}-arm64.dmg"
+    else
+        downloadURL="https://github.com/Zettlr/Zettlr/releases/download/v${appNewVersion}/Zettlr-${appNewVersion}-x64.dmg"
+    fi
     expectedTeamID="QS52BN8W68"
     ;;
