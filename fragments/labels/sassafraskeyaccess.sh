46d62682de9f7abd62a9e1b6249d2d2048d2f054
robotprom <98070647+robotprom@users.noreply.github.com>
2026-05-02 15:25:15 -0400

    Update sassafraskeyaccess.sh
    fixed logic

--- fragments/labels/sassafraskeyaccess.sh
+++ fragments/labels/sassafraskeyaccess.sh
@@ -1,14 +1,14 @@
 keyaccess)
     name="KeyAccess"
     type="pkg"
-    keyaccessHost="INSERT_YOUR_SERVER_IP_HERE"
+    keyaccessHost="" # INSERT_YOUR_SERVER_IP_HERE
     downloadURL="https://download.sassafras.com/software/release/current/Installers/MacOS/Client/ksp-client.pkg"
     appNewVersion="$(curl -s "https://solutions.teamdynamix.com/TDClient/1965/Portal/KB/ArticleDet?ID=169236" | grep -Eo '[0-9]+\.[0-9]+(\.[0-9]+)+' | sort -V | tail -1)"
     expectedTeamID="7Z2KSDFMVY"
     # Application is not installed in /Applications
     appName="Library/KeyAccess/KeyAccess.app"
     # Allowing for setting host as it is the only setting required for a fresh install.
-    if [[ -n $keyaccessHost ]]; then
-        defaults write /Library/Preferences/com.sassafras.KeyAccess host -string "${keyaccessHost}"
+   if [[ ! -f "/Library/Preferences/com.sassafras.KeyAccess.plist" ]]; then
+    defaults write /Library/Preferences/com.sassafras.KeyAccess host -string "${keyaccessHost}"
     fi
     ;;
