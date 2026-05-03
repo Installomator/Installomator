eadb2f060584d424be49ffe31fa0b16283e55273
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-05-03 11:53:55 +0200

    Fix multiple responses for downloadURL

--- fragments/labels/firecutfordavinciresolve.sh
+++ fragments/labels/firecutfordavinciresolve.sh
@@ -1,7 +1,7 @@
 firecutfordavinciresolve)
     name="FireCut for DaVinci Resolve"
     type="dmg"
-    downloadURL="$(curl -fsIL https://firecut.ai/downloads/davinci/apple_silicon/ | grep -i ^location | awk '{print $2}' | tr -d '\r')"
+    downloadURL="$(curl -fsIL https://firecut.ai/downloads/davinci/apple_silicon/ | grep -i ^location | awk '{print $2}' | tr -d '\r' | grep "https://" | tail -1)"
     appNewVersion=$(echo $downloadURL | awk -F "." '{print$7}' | sed 's/v//' | sed 's/_/./g')
     expectedTeamID="7ASRSVAEMS"
     blockingProcesses=( "Resolve" )
