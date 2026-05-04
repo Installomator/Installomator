97d747cd6a89642890566f86e99ca0dc88c3cf07
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-05-04 09:12:01 +0200

    Changed appCustomVersion from pkginfo to CFBundleShortVersionString

--- fragments/labels/nditools.sh
+++ fragments/labels/nditools.sh
@@ -1,9 +1,7 @@
 nditools)
     name="NDI Tools"
     type="pkg"
-    appCustomVersion(){
-      pkgutil --pkg-info com.newtek.NDI-Tools 2>/dev/null | grep "version:" | awk '{print$2}' | awk -F "." '{print$1"."$2"."$3}'
-    }
+    appCustomVersion(){defaults read "/Applications/NDI Discovery.app/Contents/Info.plist" CFBundleShortVersionString}
     downloadURL=$(curl -sIkL $(curl -fsL https://ndi.video/tools/ | grep -E "get.ndi.video|MacOS" | grep -B 1 "MacOS" | grep -o '"https.*"' | sort | tail -1 | sed 's/"//g') | grep -o "https.*pkg")
     appNewVersion=$(curl -fsL https://ndi.video/tools/ | grep -o "Version [0-9].*[0-9]" | grep -o "[0-9].*[0-9]" | sort | tail -1)
     expectedTeamID="W8U66ET244"
