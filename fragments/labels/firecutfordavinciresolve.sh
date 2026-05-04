358bc4c3c365a20ae955bad626f8bf5adc287c8b
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-05-04 10:17:51 +0200

    Added support for x86

--- fragments/labels/firecutfordavinciresolve.sh
+++ fragments/labels/firecutfordavinciresolve.sh
@@ -1,7 +1,11 @@
 firecutfordavinciresolve)
     name="FireCut for DaVinci Resolve"
     type="dmg"
-    downloadURL="$(curl -fsIL https://firecut.ai/downloads/davinci/apple_silicon/ | grep -i ^location | awk '{print $2}' | tr -d '\r' | grep "https://" | tail -1)"
+    if [[ "$(arch)" == "arm64" ]]; then
+        downloadURL="$(curl -fsIL https://firecut.ai/downloads/davinci/apple_silicon/ | grep -i ^location | awk '{print $2}' | tr -d '\r' | grep "https://" | tail -1)"
+    else
+        downloadURL="$(curl -fsIL https://firecut.ai/downloads/davinci/apple_intel/ | grep -i ^location | awk '{print $2}' | tr -d '\r' | grep "https://" | tail -1)"
+    if
     appNewVersion=$(echo $downloadURL | awk -F "." '{print$7}' | sed 's/v//' | sed 's/_/./g')
     expectedTeamID="7ASRSVAEMS"
     blockingProcesses=( "Resolve" )
