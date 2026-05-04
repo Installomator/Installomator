1d752d989ec282f7006384a4d810ae4a57977008
Michaël De Bona <debona.michael@gmail.com>
2026-05-04 11:25:58 +0200

    fix:Add architecture check for OpenLens package type
    Update OpenLens installation script to handle intel-based Macs.

--- fragments/labels/openlens.sh
+++ fragments/labels/openlens.sh
@@ -1,6 +1,12 @@
 openlens)
     name="OpenLens"
-    type="dmg"
+    if [[ $(arch) == "arm64" ]]; then
+        type="dmg"
+        archiveName="OpenLens-[0-9.-]*-arm64.dmg"
+    else
+        type="zip"
+        archiveName="OpenLens-[0-9.-]*-mac.zip"
+    fi
     downloadURL="$(downloadURLFromGit MuhammedKalkan OpenLens)"
     appNewVersion="$(versionFromGit MuhammedKalkan OpenLens)"
     expectedTeamID="HGC72W36QJ"
