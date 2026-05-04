4042b43020e62eb46f6096f9668d8888177d2ebd
iliger <m.switon@apfelwerk.de>
2026-05-04 11:06:18 +0200

    Add notary label configuration to notary.sh

---
+++ fragments/labels/notary.sh
@@ -0,0 +1,8 @@
+notary)
+    name="notary"
+    type="pkg"
+    packageID="de.twocent.notary"
+    downloadURL="$(downloadURLFromGit lawnmowerwoman notary)"
+    appNewVersion="$(versionFromGit lawnmowerwoman notary)"
+    expectedTeamID="KP5T66DWT2"
+    ;;
