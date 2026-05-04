76f545df95e5f75a0aa520219d67579cd743d7cf
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-05-04 10:15:02 +0200

    correcting url for fetching downloadURL

--- fragments/labels/firecutforpremierepro.sh
+++ fragments/labels/firecutforpremierepro.sh
@@ -1,7 +1,7 @@
 firecutforpremierepro)
     name="FireCut for Premiere Pro"
     type="pkg"
-    downloadURL="$(curl -fsL https://firecut.ai/downloads | grep "FireCut_PP_Dist.*pkg" | grep -o "https://.*pkg")"
+    downloadURL="$(curl -fsL https://firecut.ai/downloads/premiere-pro/ | grep "FireCut_PP_Dist.*pkg" | grep -o "https://.*pkg")"
     appNewVersion=$(echo $downloadURL | grep -o "[0-9].*[0-9]")
     appCustomVersion(){ xmllint --xpath '//ExtensionList' '/Library/Application Support/Adobe/CEP/extensions/com.firecut.base/CSXS/manifest.xml' | grep -o "[0-9].*[0-9]" }
     expectedTeamID="7ASRSVAEMS"
