cce2b5bbddebaa31a09b0b857f1189b2e4d70e23
YHOUSSEAU <huexley@gmail.com>
2026-05-04 16:30:22 +0200

    Update jamovi.sh
    Typo sorry

--- fragments/labels/jamovi.sh
+++ fragments/labels/jamovi.sh
@@ -2,19 +2,20 @@ jamovi)
     name="jamovi"
     type="dmg"
     baseURL="https://dl.jamovi.org"
-    
     if [[ $(arch) == "arm64" ]]; then
-    archSuffix="arm64"
+        archSuffix="arm64"
     elif [[ $(arch) == "i386" ]]; then
-    archSuffix="x64"
+        archSuffix="x64"
     else
-    printlog "ERROR: Unsupported architecture $(arch) for jamovi label" ERROR
-    exit 1
+        printlog "ERROR: Unsupported architecture $(arch) for jamovi label" ERROR
+        exit 1
     fi
     if [[ -n $jamoviLatest ]]; then
-    downloadFile=$(curl -sf "${baseURL}/" | grep -o "jamovi-2\.[7-9][^\"]*-macos-${archSuffix}\.dmg" | sort -V | tail -1)
+        downloadFile=$(curl -sf "${baseURL}/" | grep -o "jamovi-2\.[7-9][^\"]*-macos-${archSuffix}\.dmg" | sort -V | tail -1)
     else
-    downloadFile=$(curl -sf "${baseURL}/" | grep -o "jamovi-2\.6[^\"]*-macos-${archSuffix}\.dmg" | sort -V | tail -1)
+        downloadFile=$(curl -sf "${baseURL}/" | grep -o "jamovi-2\.6[^\"]*-macos-${archSuffix}\.dmg" | sort -V | tail -1)
     fi
     downloadURL="${baseURL}/${downloadFile}"
     appNewVersion="$(echo "$downloadFile" | cut -d '-' -f 2)"
+    expectedTeamID="9NCBP559AB"
+    ;;
