d05236aff53c16433f3b7aea1dcccf478dd6e8da
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-05-04 12:58:15 +0200

    Corrected if … fi wordings

--- fragments/labels/firecutfordavinciresolve.sh
+++ fragments/labels/firecutfordavinciresolve.sh
@@ -5,7 +5,7 @@ firecutfordavinciresolve)
         downloadURL="$(curl -fsIL https://firecut.ai/downloads/davinci/apple_silicon/ | grep -i ^location | awk '{print $2}' | tr -d '\r' | grep "https://" | tail -1)"
     else
         downloadURL="$(curl -fsIL https://firecut.ai/downloads/davinci/apple_intel/ | grep -i ^location | awk '{print $2}' | tr -d '\r' | grep "https://" | tail -1)"
-    if
+    fi
     appNewVersion=$(echo $downloadURL | awk -F "." '{print$7}' | sed 's/v//' | sed 's/_/./g')
     expectedTeamID="7ASRSVAEMS"
     blockingProcesses=( "Resolve" )
