5dfb178d86e9a27b328a9c443bc900b9a1ddeac0
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-05-05 22:23:09 +0200

    Changed from appCustomVersion to appName

--- fragments/labels/nditools.sh
+++ fragments/labels/nditools.sh
@@ -1,7 +1,8 @@
 nditools)
     name="NDI Tools"
     type="pkg"
-    appCustomVersion(){defaults read "/Applications/NDI Discovery.app/Contents/Info.plist" CFBundleShortVersionString}
+    appName="NDI Access Manager.app"
+    versionKey="CFBundleShortVersionString"
     downloadURL=$(curl -sIkL $(curl -fsL https://ndi.video/tools/ | grep -E "get.ndi.video|MacOS" | grep -B 1 "MacOS" | grep -o '"https.*"' | sort | tail -1 | sed 's/"//g') | grep -o "https.*pkg")
     appNewVersion=$(curl -fsL https://ndi.video/tools/ | grep -o "Version [0-9].*[0-9]" | grep -o "[0-9].*[0-9]" | sort | tail -1)
     expectedTeamID="W8U66ET244"
