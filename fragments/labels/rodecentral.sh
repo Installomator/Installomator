c5c5e2d209cc13a1d9a0304384e9534f919a975e
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-05-06 00:47:38 +0200

    Replaced jq and other unnessecary code for appNewVersion

--- fragments/labels/rodecentral.sh
+++ fragments/labels/rodecentral.sh
@@ -3,6 +3,6 @@ rodecentral)
     type="pkgInZip"
     #packageID="com.rodecentral.installer"
     downloadURL="https://update.rode.com/central/RODE_Central_MACOS.zip"
-    appNewVersion=$(curl -fLs https://rode.com/en/release-notes/rode-central | xmllint --html --format - 2>/dev/null | grep -o "release-notes-data='.*'>" | sed -E 's/release-notes-data='\''|'\''>//g' | jq -r '.[].version' | sed 's/[V|v]ersion //g' | sort --version-sort | tail -1)
+    appNewVersion=$(curl -fLs https://rode.com/en/release-notes/rode-central | grep -oE "Version [0-9].[0-9].[0-9.]*" | awk '{print$2}' | sort --version-sort | tail -1)
     expectedTeamID="Z9T72PWTJA"
     ;;
