fdb00af1f0408c7f974747a6749c6f007063aa83
Fredrik Larsson <33235315+kryptonit1@users.noreply.github.com>
2026-05-05 12:45:28 +0200

    Update evernote.sh
    Changed source to yaml feed instead.
    
    ```
    2026-05-05 12:39:56 : INFO  : evernote : setting variable from argument DEBUG=0
    2026-05-05 12:39:56 : INFO  : evernote : Total items in argumentsArray: 1
    2026-05-05 12:39:56 : INFO  : evernote : argumentsArray: DEBUG=0
    2026-05-05 12:39:56 : REQ   : evernote : ################## Start Installomator v. 10.9beta, date 2026-05-05
    2026-05-05 12:39:56 : INFO  : evernote : ################## Version: 10.9beta
    2026-05-05 12:39:56 : INFO  : evernote : ################## Date: 2026-05-05
    2026-05-05 12:39:56 : INFO  : evernote : ################## evernote
    2026-05-05 12:39:56 : INFO  : evernote : SwiftDialog is not installed, clear cmd file var
    2026-05-05 12:39:57 : INFO  : evernote : Reading arguments again: DEBUG=0
    2026-05-05 12:39:57 : INFO  : evernote : BLOCKING_PROCESS_ACTION=tell_user
    2026-05-05 12:39:57 : INFO  : evernote : NOTIFY=success
    2026-05-05 12:39:57 : INFO  : evernote : LOGGING=INFO
    2026-05-05 12:39:57 : INFO  : evernote : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2026-05-05 12:39:57 : INFO  : evernote : Label type: dmg
    2026-05-05 12:39:57 : INFO  : evernote : archiveName: Evernote.dmg
    2026-05-05 12:39:57 : INFO  : evernote : no blocking processes defined, using Evernote as default
    2026-05-05 12:39:57 : INFO  : evernote : name: Evernote, appName: Evernote.app
    2026-05-05 12:39:57 : WARN  : evernote : No previous app found
    2026-05-05 12:39:57 : WARN  : evernote : could not find Evernote.app
    2026-05-05 12:39:57 : INFO  : evernote : appversion:
    2026-05-05 12:39:58 : INFO  : evernote : Latest version of Evernote is 11.14.2
    2026-05-05 12:39:58 : REQ   : evernote : Downloading https://mac.desktop.evernote.com/builds/Evernote-11.14.2-mac-ddl-stage-20260429175637-152d85d9ab4fff8b55244764933a920bbe5512fd.dmg to Evernote.dmg
    2026-05-05 12:40:01 : INFO  : evernote : Downloaded Evernote.dmg – Type is  zlib compressed data – SHA is 47274a42df2af05ea30ede12e3feeb624efdee9b – Size is 301116 kB
    2026-05-05 12:40:01 : REQ   : evernote : no more blocking processes, continue with update
    2026-05-05 12:40:01 : REQ   : evernote : Installing Evernote
    2026-05-05 12:40:01 : INFO  : evernote : Mounting /var/folders/zz/zyxvpxvq6csfxvn_n0000000000000/T/tmp.nF1hynorzi/Evernote.dmg
    2026-05-05 12:40:07 : INFO  : evernote : Mounted: /Volumes/Evernote 11.14.2-universal
    2026-05-05 12:40:07 : INFO  : evernote : Verifying: /Volumes/Evernote 11.14.2-universal/Evernote.app
    2026-05-05 12:40:10 : INFO  : evernote : Team ID matching: Q79WDW8YH9 (expected: Q79WDW8YH9 )
    2026-05-05 12:40:10 : INFO  : evernote : Installing Evernote version 11.14.2 on versionKey CFBundleShortVersionString.
    2026-05-05 12:40:10 : INFO  : evernote : App has LSMinimumSystemVersion: 11.0
    2026-05-05 12:40:11 : INFO  : evernote : Copy /Volumes/Evernote 11.14.2-universal/Evernote.app to /Applications
    2026-05-05 12:40:12 : WARN  : evernote : Changing owner to kryptonit
    2026-05-05 12:40:12 : INFO  : evernote : Finishing...
    2026-05-05 12:40:15 : INFO  : evernote : App(s) found: /Applications/Evernote.app
    2026-05-05 12:40:15 : INFO  : evernote : found app at /Applications/Evernote.app, version 11.14.2, on versionKey CFBundleShortVersionString
    2026-05-05 12:40:15 : REQ   : evernote : Installed Evernote, version 11.14.2
    2026-05-05 12:40:15 : INFO  : evernote : notifying
    displaynotification:7: no such file or directory: /usr/local/bin/dialog
    2026-05-05 12:40:16 : INFO  : evernote : Installomator did not close any apps, so no need to reopen any apps.
    2026-05-05 12:40:16 : REQ   : evernote : All done!
    2026-05-05 12:40:16 : REQ   : evernote : ################## End Installomator, exit code 0
    ```

--- fragments/labels/evernote.sh
+++ fragments/labels/evernote.sh
@@ -1,7 +1,7 @@
 evernote)
     name="Evernote"
     type="dmg"
-    downloadURL="https://mac.desktop.evernote.com/builds/Evernote-latest.dmg"
-    appNewVersion=$(curl -fs https://evernote.com/release-notes | sed -En 's/.*Version ([0-9.]+) - Latest.*/\1/p')
+    downloadURL="$(curl -fs 'https://public.evernote.com/ddl-updater/updater/mac/public/latest-mac.yml' -H 'x-app-version: 11.00.0' -H 'x-os-release: 26.0.0' | awk -F'url:[[:space:]]*' '/url:[[:space:]]*.*\.dmg$/ { print $2 }')"
+    appNewVersion=$(curl -fs 'https://public.evernote.com/ddl-updater/updater/mac/public/latest-mac.yml' -H 'x-app-version: 11.00.0' -H 'x-os-release: 26.0.0' | awk -F': *' '/^version:/ { print $2 }')
     expectedTeamID="Q79WDW8YH9"
     ;;
