30204bedfdf260c0a19e6d1a14b101ddb04a9146
John Bowman <jcmbowman@gmail.com>
2026-05-05 15:38:56 -0400

    Update duet.sh
    Use new sparkle feed.

--- fragments/labels/duet.sh
+++ fragments/labels/duet.sh
@@ -1,8 +1,9 @@
 duet)
-    name="Duet"
+    name="duet"
     type="dmg"
-    downloadURL="https://updates.duetdisplay.com/AppleSilicon"
-    appNewVersion="$(curl -fsIL ${downloadURL} | grep -i ^location | cut -d "/" -f6 | sed 's/duet-dd-//' | sed 's/.dmg//' | sed 's/-/./g')"
+    sparkleData=$(curl -fsL 'https://updater.duetdownload.com/dd/sparkle.xml?lang=en-US&appName=Duet')
+    appNewVersion=$( <<<"$sparkleData" xpath '//*[local-name()="enclosure"]/@*[local-name()="version"]' | sed 's/.*="//;s/"//' | sort -V | tail -n 1 )
+    downloadURL=$( <<<"$sparkleData" xpath 'string(//*[local-name()="enclosure"][@*[local-name()="version"] = "'"$appNewVersion"'"]/@url)' )
     expectedTeamID="J6L96W8A86"
     blockingProcesses=( "duet" "duet Networking" )
     ;;
