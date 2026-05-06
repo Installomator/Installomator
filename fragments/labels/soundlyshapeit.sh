91497183651c179aca248e6c42af15201ecb66c0
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-05-06 08:07:22 -0600

    Remove commented appNewVersion line
    Removed commented appNewVersion line from script.

--- fragments/labels/soundlyshapeit.sh
+++ fragments/labels/soundlyshapeit.sh
@@ -3,6 +3,5 @@ soundlyshapeit)
     type="pkg"
     packageID="com.soundly.pkg.au.shapeit"
     downloadURL="https://storage.googleapis.com/soundly-plugins/Shapeit.pkg"
-    #appNewVersion=""
     expectedTeamID="67Y6N7VTDG"
     ;;
