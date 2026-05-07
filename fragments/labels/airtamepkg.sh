bd945388144b00bac8612d40549be8be353ac552
hwlans <175405805+hwlans@users.noreply.github.com>
2026-05-07 22:38:38 +0200

    Update airtamepkg.sh

--- fragments/labels/airtamepkg.sh
+++ fragments/labels/airtamepkg.sh
@@ -2,5 +2,6 @@ airtamepkg)
     name="Airtame"
     type="pkg"
     downloadURL="https://downloads-website.airtame.com/get.php?platform=mac&pkg=true"
+    appNewVersion=$(curl -fsIL "$downloadURL" | grep -i "location" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
     expectedTeamID="4TPSP88HN2"
     ;;
