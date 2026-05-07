f6a92e94b273cdfc9663feff1d6717f9abf3ac6b
macmangr <106015597+macmangr@users.noreply.github.com>
2026-05-07 10:27:37 +0200

    Update lgcalibrationstudio.sh
    Removed packageID line

--- fragments/labels/lgcalibrationstudio.sh
+++ fragments/labels/lgcalibrationstudio.sh
@@ -1,7 +1,6 @@
 lgcalibrationstudio)
     name="LG Calibration Studio"
     type="pkgInZip"
-    packageID="LGSI.TrueColorPro"
     releaseURL="https://www.lg.com/us/support/product/lg-34WK95U-W.AUS"
     appNewVersion=$(curl -sf "$releaseURL" | grep -m 1 "\[Mac\] LG Calibration Studio - version " | sed -E 's/.*\[Mac\] LG Calibration Studio - version ([^<]+)<\/p>.*/\1/')
     fileID=$(curl -sf "$releaseURL" | grep "Mac_LCS_" | sed -n '2p' | grep -o '"originalFileName":"Mac_LCS_[^"]*\.zip","fileName":"[^"]*"' | head -n1 | sed -E 's/.*"fileName":"([^"]*)".*/\1/')
