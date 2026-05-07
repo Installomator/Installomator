cc39b1be91adb1c1a6b716d77910729a96348524
hwlans <175405805+hwlans@users.noreply.github.com>
2026-05-07 21:08:48 +0200

    Update duckduckgo.sh

--- fragments/labels/duckduckgo.sh
+++ fragments/labels/duckduckgo.sh
@@ -1,7 +1,8 @@
 duckduckgo)
     name="DuckDuckGo"
     type="dmg"
-    appNewVersion=$(curl -fsL "https://staticcdn.duckduckgo.com/macos-desktop-browser/appcast2.xml" | xpath '(//rss/channel/item/sparkle:shortVersionString/text())' | head -1)
-    downloadURL=$(curl -fsL "https://staticcdn.duckduckgo.com/macos-desktop-browser/appcast2.xml" | xpath '(//rss/channel/item/enclosure/@url' 2>/dev/null | cut -d '"' -f 2 | head -1)
+    ddgXML=$(curl -fsL "https://staticcdn.duckduckgo.com/macos-desktop-browser/appcast2.xml")
+    appNewVersion=$(echo "$ddgXML" | xpath '(//rss/channel/item/sparkle:shortVersionString/text())' | head -1)
+    downloadURL=$(echo "$ddgXML" | xpath '(//rss/channel/item/enclosure/@url' 2>/dev/null | cut -d '"' -f 2 | head -1)
     expectedTeamID="HKE973VLUW"
     ;;
