65af8ce75d4de46404f7c3983079636ee2f21720
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-05-07 15:51:47 +0200

    Removed old comment

--- fragments/labels/rodecentral.sh
+++ fragments/labels/rodecentral.sh
@@ -1,7 +1,6 @@
 rodecentral)
     name="RODE Central"
     type="pkgInZip"
-    #packageID="com.rodecentral.installer"
     downloadURL="https://update.rode.com/central/RODE_Central_MACOS.zip"
     appNewVersion=$(curl -fLs https://rode.com/en/release-notes/rode-central | grep -oE "Version [0-9].[0-9].[0-9.]*" | awk '{print$2}' | sort --version-sort | tail -1)
     expectedTeamID="Z9T72PWTJA"
