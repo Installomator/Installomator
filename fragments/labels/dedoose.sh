862cc143d677ee2e0919cc78793d3b5f8134bf99
rougegoat <rougegoat@gmail.com>
2026-05-08 14:20:02 -0500

    Update dedoose.sh'
    Updated AppNewVersion and downloadURL to match vendor changes

--- fragments/labels/dedoose.sh
+++ fragments/labels/dedoose.sh
@@ -1,7 +1,7 @@
 dedoose)
     name="Dedoose"
     type="dmg"
-    downloadURL=$(curl -fsL "https://www.dedoose.com/download-the-app" | grep -oE 'https://[^"]+\.dmg' | head -1)
-    appNewVersion=$(echo "$downloadURL" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
+    appNewVersion=$(curl -I https://downloads.dedoose.com/dedoose-app-releases/Dedoose-Mac.dmg | grep -o "Dedoose-Mac-v[0-9.].*[0-9.].*[0-9.].dmg" | sed -e 's/Dedoose-Mac-v//g' -e 's/.dmg//g')
+    downloadURL="https://downloads.dedoose.com/dedoose-app-releases/Dedoose-Mac.dmg"
     expectedTeamID="9U74Q6K62X"
     ;;
