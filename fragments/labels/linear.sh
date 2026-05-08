59c43072185ffe589c86202ce24bf77ecba87c5e
Lisa Lungershausen <lisa.lungershausen@tomorrow.one>
2026-05-08 07:26:38 +0200

    Remove redundant appName and update blockingProcesses

--- fragments/labels/linear.sh
+++ fragments/labels/linear.sh
@@ -5,6 +5,5 @@ linear)
     appNewVersion=$(curl -sIkL $downloadURL | sed -r '/filename=/!d;s/.*filename=(.*)$/\1/' | awk -F'-' '{print $2}')
     expectedTeamID="7VZ2S3V9RV"
     versionKey="CFBundleShortVersionString"
-    appName="Linear.app"
-    blockingProcesses=( "Linear" )
+    blockingProcesses=( "Linear" "Linear Helper" )
     ;;
