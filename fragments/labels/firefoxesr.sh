dc692830839892d55d531eb2a3ebd70a45709628
Markus Kachel <33146694+allandanton@users.noreply.github.com>
2026-05-08 13:24:30 +0200

    Update firefoxesr.sh to get correct version
    Remove esr suffix from the website version because Firefox.app does not have it in its version string.

--- fragments/labels/firefoxesr.sh
+++ fragments/labels/firefoxesr.sh
@@ -5,6 +5,7 @@ firefoxesrpkg)
     downloadURL="https://download.mozilla.org/?product=firefox-esr-pkg-latest-ssl&os=osx"
     firefoxVersions=$(curl -fs "https://product-details.mozilla.org/1.0/firefox_versions.json")
     appNewVersion=$(getJSONValue "$firefoxVersions" "FIREFOX_ESR")
+    appNewVersion=${appNewVersion:0:-3}
     expectedTeamID="43AQ936H96"
     blockingProcesses=( firefox )
     ;;
