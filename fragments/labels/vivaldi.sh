b0479d5437b592a8bf22817cfa882ab5f34d98b8
hwlans <175405805+hwlans@users.noreply.github.com>
2026-05-10 01:42:32 +0200

    Update vivaldi.sh

--- fragments/labels/vivaldi.sh
+++ fragments/labels/vivaldi.sh
@@ -1,7 +1,8 @@
 vivaldi)
     name="Vivaldi"
     type="tbz"
-    downloadURL=$(curl -fsL "https://update.vivaldi.com/update/1.0/public/mac/appcast.xml" | xpath '//rss/channel/item[1]/enclosure/@url' 2>/dev/null  | cut -d '"' -f 2)
-    appNewVersion=$(curl -fs "https://update.vivaldi.com/update/1.0/public/mac/appcast.xml" | grep sparkle:version | sed 's/[^0-9\.]//g')
+    vivaldiXML=$(curl -fsL "https://update.vivaldi.com/update/1.0/public/mac/appcast.xml")
+    appNewVersion=$(echo "$vivaldiXML" | xpath 'string(//rss/channel/item/sparkle:shortVersionString)')
+    downloadURL=$(echo "$vivaldiXML" | xpath 'string(//rss/channel/item/enclosure/@url)')
     expectedTeamID="4XF3XNRN6Y"
     ;;
