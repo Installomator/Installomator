f26bc726545e5365ac7c5acf825a61ed5a5316a6
Gil Burns <gil@gilburns.com>
2026-05-09 16:22:16 -0500

    Update elgatocamerahub.sh

--- fragments/labels/elgatocamerahub.sh
+++ fragments/labels/elgatocamerahub.sh
@@ -4,7 +4,7 @@ elgatocamerahub)
     elgatoJSON=$(curl -fsSL "https://gc-updates.elgato.com/mac/echm-update/final/app-version-check.json")
     appNewVersion=$(getJSONValue "$elgatoJSON" "Automatic.Version")
     downloadURL=$(getJSONValue "$elgatoJSON" "Automatic.fileURL")
-    appCustomVersion() { version=$(defaults read "/Applications/Camera Hub.app/Contents/Info.plist" CFBundleShortVersionString 2>/dev/null); build=$(defaults read "/Applications/Camera Hub.app/Contents/Info.plist" CFBundleVersion 2>/dev/null); echo "${version}.${build}"; }
+    appCustomVersion() { version=$(defaults read "/Applications/${name}.app/Contents/Info.plist" CFBundleShortVersionString 2>/dev/null); build=$(defaults read "/Applications/${name}.app/Contents/Info.plist" CFBundleVersion 2>/dev/null); echo "${version}.${build}"; }
     expectedTeamID="Y93VXCB8Q5"
     blockingProcesses=( "Camera Hub" )
     ;;
