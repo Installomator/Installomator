fc1fc553b27d5660776d4462d168d869156aba5b
hwlans <175405805+hwlans@users.noreply.github.com>
2026-05-10 23:45:33 +0200

    Update krita.sh

--- fragments/labels/krita.sh
+++ fragments/labels/krita.sh
@@ -1,8 +1,8 @@
 krita)
-    # credit: Søren Theilgaard (@theilgaard)
     name="krita"
     type="dmg"
-    downloadURL=$( curl -fsL "https://krita.org/en/download" | grep ".*https.*stable.*dmg.*" | head -1 | sed -E 's/.*(https.*dmg).*/\1/g' )
-    appNewVersion=$( echo "${downloadURL}" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)-.*/\1/g' )
+    kritaXML=$(curl -fsL "https://apps.kde.org/en-gb/krita/index.xml")
+    downloadURL=$(echo "$kritaXML" | xpath "string(//a[contains(@href, 'signed.dmg')]/@href")
+    appNewVersion=$(echo "${downloadURL}" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)-.*/\1/g')
     expectedTeamID="DL93766A3G"
     ;;
