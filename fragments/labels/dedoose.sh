3ac7fcc44f79ce5c58a4c78682e62d0e024f66c2
rougegoat <rougegoat@gmail.com>
2026-05-11 13:47:15 -0500

    Update dedoose.sh
    Revised based on feedback from maintainers

--- fragments/labels/dedoose.sh
+++ fragments/labels/dedoose.sh
@@ -1,7 +1,7 @@
 dedoose)
     name="Dedoose"
     type="dmg"
-    appNewVersion=$(curl -I https://downloads.dedoose.com/dedoose-app-releases/Dedoose-Mac.dmg | grep -o "Dedoose-Mac-v[0-9.].*[0-9.].*[0-9.].dmg" | sed -e 's/Dedoose-Mac-v//g' -e 's/.dmg//g')
     downloadURL="https://downloads.dedoose.com/dedoose-app-releases/Dedoose-Mac.dmg"
+    appNewVersion=$(curl -fsI https://downloads.dedoose.com/dedoose-app-releases/Dedoose-Mac.dmg | grep -i "^content-disposition:" | grep -o "v[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}" | sed 's/^v//')
     expectedTeamID="9U74Q6K62X"
     ;;
