5ce643f7ad3044f4d077d4907cadd5f0ebce6ac1
Maarten Wijnants <wijnants.maarten@gmail.com>
2026-05-12 08:40:17 +0200

    Use Github release for Brave

--- fragments/labels/brave.sh
+++ fragments/labels/brave.sh
@@ -1,15 +1,13 @@
 brave)
     name="Brave Browser"
     type="dmg"
-    if [[ $(arch) != "i386" ]]; then
-        printlog "Architecture: arm64 (not i386)"
-        downloadURL=$(curl -fsIL https://laptop-updates.brave.com/latest/osxarm64/release | grep -i "^location" | sed -E 's/.*(https.*\.dmg).*/\1/g')
-        appNewVersion="$(curl -fsL "https://updates.bravesoftware.com/sparkle/Brave-Browser/stable-arm64/appcast.xml" | xpath '//rss/channel/item[1]/enclosure/@sparkle:version' 2>/dev/null  | cut -d '"' -f 2)"
+    if [[ $(arch) == "arm64" ]]; then
+        archiveName="Brave-Browser-arm64.dmg"
     else
-        printlog "Architecture: i386"
-        downloadURL=$(curl -fsIL https://laptop-updates.brave.com/latest/osx/release | grep -i "^location" | sed -E 's/.*(https.*\.dmg).*/\1/g')
-        appNewVersion="$(curl -fsL "https://updates.bravesoftware.com/sparkle/Brave-Browser/stable/appcast.xml" | xpath '//rss/channel/item[1]/enclosure/@sparkle:version' 2>/dev/null  | cut -d '"' -f 2)"
+        archiveName="Brave-Browser-x64.dmg"
     fi
+    downloadURL=$(downloadURLFromGit brave brave-browser)
+    appNewVersion=$(versionFromGit brave brave-browser | sed 's/\.//')
     versionKey="CFBundleVersion"
     expectedTeamID="KL8N8XSYF4"
     ;;
