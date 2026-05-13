353b905ad08b3ecb873f763509749455c084f719
RIchard Glaser <richard.glaser@utah.edu>
2026-05-13 13:54:50 -0600

    Update yubikeymanagerqt.sh

--- fragments/labels/yubikeymanagerqt.sh
+++ fragments/labels/yubikeymanagerqt.sh
@@ -1,5 +1,5 @@
 yubikeymanagerqt)
-    name="YubiKey Manager GUI"
+    name="YubiKey Manager"
     type="pkg"
     downloadURL="https://developers.yubico.com/yubikey-manager-qt/Releases/yubikey-manager-qt-latest-mac.pkg"
     appNewVersion=$(curl -fsIL "${downloadURL}" | grep -i "^location" | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")
