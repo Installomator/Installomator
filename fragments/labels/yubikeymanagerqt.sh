1e4ca5dccdf3c9ee003ae8ae6c22a918cd596353
RIchard Glaser <richard.glaser@utah.edu>
2026-05-13 14:01:30 -0600

    Update yubikeymanagerqt.sh

--- fragments/labels/yubikeymanagerqt.sh
+++ fragments/labels/yubikeymanagerqt.sh
@@ -3,6 +3,5 @@ yubikeymanagerqt)
     type="pkg"
     downloadURL="https://developers.yubico.com/yubikey-manager-qt/Releases/yubikey-manager-qt-latest-mac.pkg"
     appNewVersion=$(curl -fsIL "${downloadURL}" | grep -i "^location" | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")
-    blockingProcesses=("YubiKey Manager")
     expectedTeamID="LQA3CS5MM7"
     ;;
