e018cece38a44e6c07caa555910ecd1fc03f70fc
RIchard Glaser <richard.glaser@utah.edu>
2026-05-13 10:14:14 -0600

    Update zulujdk23.sh

--- fragments/labels/zulujdk23.sh
+++ fragments/labels/zulujdk23.sh
@@ -1,13 +1,13 @@
-zulujdk23)
-    name="Zulu JDK 23"
+zulujdk18)
+    name="Zulu JDK 18"
     type="pkgInDmg"
-    packageID="com.azulsystems.zulu.23"
+    packageID="com.azulsystems.zulu.18"
     if [[ $(arch) == i386 ]]; then
-        downloadURL=https://cdn.azul.com/zulu/bin/$(curl -fs "https://cdn.azul.com/zulu/bin/" | grep -Eio '">zulu23.*ca-jdk23.*x64.dmg(.*)' | cut -c3- | sed 's/<\/a><\/td>//' | sed -E 's/([0-9.]*)M//' | awk '{print $2 $1}' | sort -V | tail -1)
+        downloadURL=https://cdn.azul.com/zulu/bin/$(curl -fs "https://cdn.azul.com/zulu/bin/" | grep -Eio 'href="/zulu/bin/(zulu18[^"]*ca-jdk18[^"]*x64\.dmg)"' | sed -E 's|href="/zulu/bin/(.*)\"|\1|' | sort | tail -1)
     elif [[ $(arch) == arm64 ]]; then
-        downloadURL=https://cdn.azul.com/zulu/bin/$(curl -fs "https://cdn.azul.com/zulu/bin/" | grep -Eio '">zulu23.*ca-jdk23.*aarch64.dmg(.*)' | cut -c3- | sed 's/<\/a><\/td>//' | sed -E 's/([0-9.]*)M//' | awk '{print $2 $1}' | sort -V | tail -1)
+        downloadURL=https://cdn.azul.com/zulu/bin/$(curl -fs "https://cdn.azul.com/zulu/bin/" | grep -Eio 'href="/zulu/bin/(zulu18[^"]*ca-jdk18[^"]*aarch64\.dmg)"' | sed -E 's|href="/zulu/bin/(.*)\"|\1|' | sort | tail -1)
     fi
     expectedTeamID="TDTHCUPYFR"
-    appCustomVersion(){ if [ -f "/Library/Java/JavaVirtualMachines/zulu-23.jdk/Contents/Info.plist" ]; then /usr/bin/defaults read "/Library/Java/JavaVirtualMachines/zulu-23.jdk/Contents/Info.plist" "CFBundleName" | sed 's/Zulu //'; fi }
-    appNewVersion=$(echo "$downloadURL" | cut -d "-" -f 1 | sed -e "s/.*zulu//" | sed 's/jdk//') 
+    appCustomVersion(){ if [ -f "/Library/Java/JavaVirtualMachines/zulu-18.jdk/Contents/Info.plist" ]; then /usr/bin/defaults read "/Library/Java/JavaVirtualMachines/zulu-18.jdk/Contents/Info.plist" "CFBundleShortVersionString"; fi; }
+    appNewVersion=$(echo "$downloadURL" | grep -Eo 'zulu[0-9]+\.[0-9]+\.[0-9]+' | head -1 | sed 's/zulu//')
     ;;
