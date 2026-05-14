6f18f2e6d81573af3b6e9afc83289bd1af000c4d
RIchard Glaser <richard.glaser@utah.edu>
2026-05-14 11:47:03 -0600

    Output
    sudo Installomator/utils/assemble.sh logitune DEBUG=0
    2026-05-14 11:46:16 : INFO  : logitune : setting variable from argument DEBUG=0
    2026-05-14 11:46:16 : INFO  : logitune : Total items in argumentsArray: 1
    2026-05-14 11:46:16 : INFO  : logitune : argumentsArray: DEBUG=0
    2026-05-14 11:46:16 : REQ   : logitune : ################## Start Installomator v. 10.9beta, date 2026-05-14
    2026-05-14 11:46:16 : INFO  : logitune : ################## Version: 10.9beta
    2026-05-14 11:46:16 : INFO  : logitune : ################## Date: 2026-05-14
    2026-05-14 11:46:16 : INFO  : logitune : ################## logitune
    2026-05-14 11:46:17 : INFO  : logitune : Reading arguments again: DEBUG=0
    2026-05-14 11:46:17 : INFO  : logitune : BLOCKING_PROCESS_ACTION=tell_user
    2026-05-14 11:46:17 : INFO  : logitune : NOTIFY=success
    2026-05-14 11:46:17 : INFO  : logitune : LOGGING=INFO
    2026-05-14 11:46:17 : INFO  : logitune : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2026-05-14 11:46:17 : INFO  : logitune : Label type: pkg
    2026-05-14 11:46:17 : INFO  : logitune : archiveName: Logi Tune.pkg
    2026-05-14 11:46:17 : INFO  : logitune : no blocking processes defined, using Logi Tune as default
    2026-05-14 11:46:17 : INFO  : logitune : name: Logi Tune, appName: Logi Tune.app
    2026-05-14 11:46:18 : WARN  : logitune : No previous app found
    2026-05-14 11:46:18 : WARN  : logitune : could not find Logi Tune.app
    2026-05-14 11:46:18 : INFO  : logitune : appversion:
    2026-05-14 11:46:18 : INFO  : logitune : Latest version of Logi Tune is 3.11.89
    2026-05-14 11:46:18 : REQ   : logitune : Downloading https://software.vc.logitech.com/downloads/tune/LogiTuneInstaller.pkg to Logi Tune.pkg
    2026-05-14 11:46:25 : INFO  : logitune : Downloaded Logi Tune.pkg – Type is  xar archive compressed TOC – SHA is b840b2d67877817a1b9302b9e5b802fb1290cd32 – Size is 278768 kB
    2026-05-14 11:46:25 : REQ   : logitune : no more blocking processes, continue with update
    2026-05-14 11:46:25 : REQ   : logitune : Installing Logi Tune
    2026-05-14 11:46:25 : INFO  : logitune : Verifying: Logi Tune.pkg
    2026-05-14 11:46:25 : INFO  : logitune : Team ID: QED4VVPZWA (expected: QED4VVPZWA )
    2026-05-14 11:46:25 : INFO  : logitune : Installing Logi Tune.pkg to /
    2026-05-14 11:46:35 : INFO  : logitune : Finishing...
    2026-05-14 11:46:38 : INFO  : logitune : App(s) found: /Applications/Logi Tune.app
    2026-05-14 11:46:38 : INFO  : logitune : found app at /Applications/Logi Tune.app, version 3.13.138, on versionKey CFBundleShortVersionString
    2026-05-14 11:46:38 : REQ   : logitune : Installed Logi Tune, version 3.11.89
    2026-05-14 11:46:38 : INFO  : logitune : notifying
    2026-05-14 11:46:38 : INFO  : logitune : Installomator did not close any apps, so no need to reopen any apps.
    2026-05-14 11:46:38 : REQ   : logitune : All done!
    2026-05-14 11:46:38 : REQ   : logitune : ################## End Installomator, exit code 0

--- fragments/labels/logitune.sh
+++ fragments/labels/logitune.sh
@@ -1,10 +1,7 @@
 logitune)
     name="Logi Tune"
-    type="dmg"
-    downloadURL="https://software.vc.logitech.com/downloads/tune/LogiTuneInstaller.dmg"
-    appNewVersion=$(curl -fs "https://sw-update.vcc.logitech.com/downloads/tune/mac/logitune_mac_version.txt" | grep -oE "[0-9.]+" | head -1)
+    type="pkg"
+    downloadURL="https://software.vc.logitech.com/downloads/tune/LogiTuneInstaller.pkg"
+    appNewVersion=$(curl -fs "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,webos=mac-macos-x-11.0" | tr '}' '\n' | grep "Logi Tune" | grep -o "Software Version: <\/span><\/b>[0-9.]*" | grep -oE "[0-9.]+" | head -1)
     expectedTeamID="QED4VVPZWA"
-    appName="LogiTuneInstaller.app"
-    CLIInstaller="LogiTuneInstaller.app/Contents/MacOS/LogiTuneInstaller"
-    CLIArguments=(--silent)
     ;;
