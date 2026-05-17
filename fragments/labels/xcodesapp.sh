0e8ac980de0c70542c6872a4ac5671318cad6738
Gil Burns <gil@gilburns.com>
2026-05-17 09:29:01 -0500

    Update xcodesapp.sh

--- fragments/labels/xcodesapp.sh
+++ fragments/labels/xcodesapp.sh
@@ -4,4 +4,5 @@ xcodesapp)
     downloadURL="$(downloadURLFromGit XcodesOrg XcodesApp)"
     appNewVersion="$(versionFromGit XcodesOrg XcodesApp)"
     expectedTeamID="ZU6GR6B2FY"
+    appCustomVersion(){appSVS=$(defaults read "/Applications/${name}.app/Contents/Info.plist" CFBundleShortVersionString); appBV=$(defaults read "/Applications/${name}.app/Contents/Info.plist" CFBundleVersion); echo "${appSVS}${appBV}"}
     ;;
