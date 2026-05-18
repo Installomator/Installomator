c7698a61677617a65acc3c827ee4f11931193abb
DanielRossEUPE <daniel.ross@teladochealth.com>
2026-05-18 14:24:39 -0600

    Update teladocproviderinstaller.sh
    2026-05-18 14:23:17 : INFO  : teladocproviderinstaller : setting variable from argument DEBUG=0
    2026-05-18 14:23:17 : INFO  : teladocproviderinstaller : Total items in argumentsArray: 1
    2026-05-18 14:23:17 : INFO  : teladocproviderinstaller : argumentsArray: DEBUG=0
    2026-05-18 14:23:17 : REQ   : teladocproviderinstaller : ################## Start Installomator v. 10.9beta, date 2026-05-18
    2026-05-18 14:23:17 : INFO  : teladocproviderinstaller : ################## Version: 10.9beta
    2026-05-18 14:23:17 : INFO  : teladocproviderinstaller : ################## Date: 2026-05-18
    2026-05-18 14:23:17 : INFO  : teladocproviderinstaller : ################## teladocproviderinstaller
    2026-05-18 14:23:17 : INFO  : teladocproviderinstaller : Reading arguments again: DEBUG=0
    2026-05-18 14:23:18 : INFO  : teladocproviderinstaller : BLOCKING_PROCESS_ACTION=tell_user
    2026-05-18 14:23:18 : INFO  : teladocproviderinstaller : NOTIFY=success
    2026-05-18 14:23:18 : INFO  : teladocproviderinstaller : LOGGING=INFO
    2026-05-18 14:23:18 : INFO  : teladocproviderinstaller : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2026-05-18 14:23:18 : INFO  : teladocproviderinstaller : Label type: pkg
    2026-05-18 14:23:18 : INFO  : teladocproviderinstaller : archiveName: TeladocProviderInstaller.pkg
    2026-05-18 14:23:18 : INFO  : teladocproviderinstaller : no blocking processes defined, using TeladocProviderInstaller as default
    2026-05-18 14:23:18 : INFO  : teladocproviderinstaller : No version found using packageID com.intouchhealth.ith-provider
    2026-05-18 14:23:18 : INFO  : teladocproviderinstaller : name: TeladocProviderInstaller, appName: TeladocProviderInstaller.app
    2026-05-18 14:23:18 : WARN  : teladocproviderinstaller : No previous app found
    2026-05-18 14:23:18 : WARN  : teladocproviderinstaller : could not find TeladocProviderInstaller.app
    2026-05-18 14:23:18 : INFO  : teladocproviderinstaller : appversion:
    2026-05-18 14:23:18 : INFO  : teladocproviderinstaller : Latest version of TeladocProviderInstaller is 1.7.14
    2026-05-18 14:23:18 : REQ   : teladocproviderinstaller : Downloading https://update.intouchreports.com/cdn/PROD/ida/provider/mac/universal/manual/TeladocProviderInstaller_1.7.14_mac_universal_manual.pkg to TeladocProviderInstaller.pkg
    2026-05-18 14:23:29 : INFO  : teladocproviderinstaller : Downloaded TeladocProviderInstaller.pkg – Type is  xar archive compressed TOC – SHA is 2474f46161417bb1b0635f660ef9b6f8fa2a216d – Size is 213492 kB
    2026-05-18 14:23:29 : REQ   : teladocproviderinstaller : no more blocking processes, continue with update
    2026-05-18 14:23:29 : REQ   : teladocproviderinstaller : Installing TeladocProviderInstaller
    2026-05-18 14:23:29 : INFO  : teladocproviderinstaller : Verifying: TeladocProviderInstaller.pkg
    2026-05-18 14:23:30 : INFO  : teladocproviderinstaller : Team ID: TXR7NRCG98 (expected: TXR7NRCG98 )
    2026-05-18 14:23:30 : INFO  : teladocproviderinstaller : Installing TeladocProviderInstaller.pkg to /
    2026-05-18 14:23:37 : INFO  : teladocproviderinstaller : Finishing...
    2026-05-18 14:23:40 : INFO  : teladocproviderinstaller : found packageID com.intouchhealth.ith-provider installed, version 0
    2026-05-18 14:23:41 : REQ   : teladocproviderinstaller : Installed TeladocProviderInstaller, version 1.7.14
    2026-05-18 14:23:41 : INFO  : teladocproviderinstaller : notifying
    2026-05-18 14:23:41 : INFO  : teladocproviderinstaller : Installomator did not close any apps, so no need to reopen any apps.
    2026-05-18 14:23:41 : REQ   : teladocproviderinstaller : All done!
    2026-05-18 14:23:41 : REQ   : teladocproviderinstaller : ################## End Installomator, exit code 0

--- fragments/labels/teladocproviderinstaller.sh
+++ fragments/labels/teladocproviderinstaller.sh
@@ -1,8 +1,8 @@
 teladocproviderinstaller)
     name="TeladocProviderInstaller"
     type="pkg"
-    downloadURL="https://update.intouchreports.com/cdn/prod/ida/provider/mac/x64/manual/TeladocProviderInstaller.pkg"
-    appNewVersion=$(curl -fsL "https://update.intouchreports.com/cdn/prod/ida/provider/mac/x64/manual/latest-mac.yml" | grep -E 'version: ' | sed -E 's/version: \"?([0-9.]*)\"?/\1/')
+    appNewVersion=$(curl -fsL "https://update.intouchreports.com/cdn/PROD/ida/provider/mac/universal/manual/latest-mac.yml" | grep -E 'version: ' | sed -E 's/version: \"?([0-9.]*)\"?/\1/')
+    downloadURL="https://update.intouchreports.com/cdn/PROD/ida/provider/mac/universal/manual/TeladocProviderInstaller_${appNewVersion}_mac_universal_manual.pkg"
     packageID="com.intouchhealth.ith-provider"
     expectedTeamID="TXR7NRCG98"
     ;;
