0d032165872ae4042b4391b4fba917734d942d5b
Maarten Wijnants <wijnants.maarten@gmail.com>
2026-05-20 09:16:47 +0200

    Fix gitkraken appNewVersion

--- fragments/labels/gitkraken.sh
+++ fragments/labels/gitkraken.sh
@@ -1,7 +1,7 @@
 gitkraken)
     name="gitkraken"
     type="dmg"
-    appNewVersion=$( curl -sfL https://www.gitkraken.com/download | grep -o 'Latest release: [0-9.]*' | grep -o '[0-9.]*' )
+    appNewVersion=$( curl -sfL https://www.gitkraken.com/download | grep -oi 'Latest release: [0-9.]*' | grep -o '[0-9.]*' )
     if [[ $(arch) == "arm64" ]]; then
         downloadURL="https://release.gitkraken.com/darwin-arm64/installGitKraken.dmg"
     elif [[ $(arch) == "i386" ]]; then
