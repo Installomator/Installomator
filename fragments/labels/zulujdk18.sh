4d963bb348210e632288b22aac012c82197a6f01
RIchard Glaser <richard.glaser@utah.edu>
2026-05-20 09:55:44 -0600

    Update

--- fragments/labels/zulujdk18.sh
+++ fragments/labels/zulujdk18.sh
@@ -8,6 +8,6 @@ zulujdk18)
         downloadURL="https://cdn.azul.com/zulu/bin/$(curl -fsL 'https://cdn.azul.com/zulu/bin/' | grep -Eo 'zulu18[^"<]*ca-jdk18[^"<]*x64[.]dmg' | sort -Vu | tail -n 1)"
     fi
     appCustomVersion(){ java -version 2>&1 | grep Runtime | awk '{print $4}' | sed -e "s/.*Zulu//" | cut -d '-' -f 1 | sed -e "s/+/\./" }
-    appNewVersion=$(echo "$downloadURL" | cut -d "-" -f 1 | sed -e "s/.*zulu//") # Cannot be compared to anything
+    appNewVersion=$(echo "$downloadURL" | cut -d "-" -f 1 | sed -e "s/.*zulu//")
     expectedTeamID="TDTHCUPYFR"
     ;;
