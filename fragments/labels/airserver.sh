25bb2adc79d111e0168c796fc964a8da3085a8ee
Gil Burns <gil@gilburns.com>
2026-05-25 16:40:29 -0500

    Update airserver.sh

--- fragments/labels/airserver.sh
+++ fragments/labels/airserver.sh
@@ -1,8 +1,7 @@
 airserver)
-    # credit: AP Orlebeke (@apizz)
     name="AirServer"
     type="dmg"
     downloadURL="https://www.airserver.com/download/mac/latest"
-    appNewVersion=$(curl -fsIL "${downloadURL}" | grep -i "location" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g')
+    appNewVersion=$(curl -fsIL "${downloadURL}" | grep -i "location" | sed -E 's/.*\/[a-zA-Z]*-([0-9.]*)\..*/\1/g' | head -1)
     expectedTeamID="6C755KS5W3"
     ;;
