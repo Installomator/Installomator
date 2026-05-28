bf13f9668e2cd8118e28ff684f87b93379de086f
Adam Codega <acodega@users.noreply.github.com>
2026-05-27 21:06:14 -0400

    Remove packageID from TeladocProviderInstaller script

--- fragments/labels/teladocproviderinstaller.sh
+++ fragments/labels/teladocproviderinstaller.sh
@@ -3,6 +3,5 @@ teladocproviderinstaller)
     type="pkg"
     appNewVersion=$(curl -fsL "https://update.intouchreports.com/cdn/PROD/ida/provider/mac/universal/manual/latest-mac.yml" | grep -E 'version: ' | sed -E 's/version: \"?([0-9.]*)\"?/\1/')
     downloadURL="https://update.intouchreports.com/cdn/PROD/ida/provider/mac/universal/manual/TeladocProviderInstaller_${appNewVersion}_mac_universal_manual.pkg"
-    packageID="com.intouchhealth.ith-provider"
     expectedTeamID="TXR7NRCG98"
     ;;
