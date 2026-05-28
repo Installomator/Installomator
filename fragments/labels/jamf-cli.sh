64e3c33418d6786acce6ed0a922747714575d3f7
Richard <xplora@wak.co.nz>
2026-05-28 15:38:36 +1200

    Create jamf-cli.sh
    Add new label for jamf-cli

---
+++ fragments/labels/jamf-cli.sh
@@ -0,0 +1,9 @@
+jamf-cli)
+    name="jamf-cli"
+    type="pkg"
+    downloadURL="$( downloadURLFromGit Jamf-Concepts jamf-cli )"
+    appNewVersion="$( versionFromGit Jamf-Concept jamf-cli )"
+    expectedTeamID="483DWKW443"
+    appName="jamf-cli"
+    appCustomVersion() { /usr/local/bin/jamf-cli --version | head -n1 | awk '{ print \$2 }' }
+    ;;
