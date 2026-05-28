a13c13bbc7f27f82fa48dd3bbec198c314da5ad5
RIchard Glaser <richard.glaser@utah.edu>
2026-05-28 12:55:55 -0600

    Removed space

--- fragments/labels/rstudio.sh
+++ fragments/labels/rstudio.sh
@@ -2,7 +2,7 @@ rstudio)
     name="RStudio"
     type="dmg"
     downloadURL="https://rstudio.org/download/latest/stable/desktop/mac/RStudio-latest.dmg"
-     finalURL=$(curl -fsL -o /dev/null -w "%{url_effective}" "$downloadURL")
+    finalURL=$(curl -fsL -o /dev/null -w "%{url_effective}" "$downloadURL")
     appNewVersion=$(echo "$finalURL" | grep -oE '[0-9]{4}\.[0-9]{2}\.[0-9]{1,2}\-[0-9]+' | sed 's/-/+/')
     expectedTeamID="FYF2F5GFX4"
     ;;
