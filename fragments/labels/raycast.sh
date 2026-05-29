b69d6e5268b4db6b79cdfb9f85b925420357d2e9
RIchard Glaser <richard.glaser@utah.edu>
2026-05-29 08:35:21 -0600

    update

--- fragments/labels/raycast.sh
+++ fragments/labels/raycast.sh
@@ -1,7 +1,11 @@
 raycast)
     name="Raycast"
     type="dmg"
-    downloadURL="https://releases.raycast.com/download?build=release"
-    appNewVersion=$(curl -fsIL -X GET "$downloadURL" | awk 'BEGIN{IGNORECASE=1}/^location:/{gsub("\r",""); print $2}' | tail -n 1 | sed -E 's|.*/Raycast_v?([0-9]+\.[0-9]+\.[0-9]+)_.*|\1|')
+    appNewVersion=$(curl -fsIL "https://releases.raycast.com/download?build=arm" | awk 'BEGIN{IGNORECASE=1}/^location:/{print $2}' | sed -E 's|.*/releases/([0-9]+\.[0-9]+\.[0-9]+)/.*|\1|')
+    if [[ "$(arch)" == "arm64" ]]; then
+        downloadURL="https://releases.raycast.com/releases/${appNewVersion}/download?build=arm"
+    else
+        downloadURL="https://releases.raycast.com/releases/${appNewVersion}/download?build=x86_64"
+    fi
     expectedTeamID="SY64MV22J9"
     ;;
