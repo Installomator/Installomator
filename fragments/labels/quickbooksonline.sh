d92c8fdeafb80480ce3c2c891fa18f84b99eadc0
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-06-01 09:59:08 -0600

    Remove appNewVersion from quickbooksonline.sh
    Remove the appNewVersion variable from QuickBooks Online script. I couldn't find a remote method to get appNewVersion so best to just remove it and use Installomtor methods locally.

--- fragments/labels/quickbooksonline.sh
+++ fragments/labels/quickbooksonline.sh
@@ -2,7 +2,6 @@ quickbooksonline)
     name="QuickBooks Online"
     type="dmg"
     downloadURL="https://http-download.intuit.com/http.intuit/CMO/tango/static/latest/QuickBooks%20Online.dmg"
-    appNewVersion="99.99.99"
     versionKey="CFBundleShortVersionString"
     expectedTeamID="G4SSPX3CBL"
     ;;
