24ab29a1a84301918aac07a7cece1a656af0f627
Cody Keats <cocopuff2u@yahoo.com>
2026-06-02 15:39:19 -0400

    Create mace.sh

---
+++ fragments/labels/mace.sh
@@ -0,0 +1,8 @@
+mace)
+    name="MACE"
+    type="dmg"
+    appName="MACE.app"
+    downloadURL=$(downloadURLFromGit "MACE-App" "MACE")
+    appNewVersion=$(versionFromGit "MACE-App" "MACE")
+    expectedTeamID="7U624389H9"
+    ;;
