109f5a6490a05d229fcf0fc2371b9feddc908143
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-06-01 19:23:21 -0600

    Remove versionKey from quickbooksonline.sh
    Remove versionKey assignment from QuickBooks Online script.

--- fragments/labels/quickbooksonline.sh
+++ fragments/labels/quickbooksonline.sh
@@ -2,6 +2,5 @@ quickbooksonline)
     name="QuickBooks Online"
     type="dmg"
     downloadURL="https://http-download.intuit.com/http.intuit/CMO/tango/static/latest/QuickBooks%20Online.dmg"
-    versionKey="CFBundleShortVersionString"
     expectedTeamID="G4SSPX3CBL"
     ;;
