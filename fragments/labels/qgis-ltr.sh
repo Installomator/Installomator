a2c7b5623104a607354faeeaca4fc4db83d1c782
Fredrik Larsson <33235315+kryptonit1@users.noreply.github.com>
2026-06-02 08:54:34 +0200

    Changed label name + curl optimization

--- fragments/labels/qgis-ltr.sh
+++
@@ -1,7 +0,0 @@
-qgis-ltr)
-    name="QGIS"
-    type="dmg"
-    downloadURL="$(getJSONValue "$(curl -fs "https://raw.githubusercontent.com/qgis/QGIS-Website/refs/heads/main/data/conf.json")" "ltr_dmg")"
-    appNewVersion="$(getJSONValue "$(curl -fs "https://raw.githubusercontent.com/qgis/QGIS-Website/refs/heads/main/data/conf.json")" "ltrrelease")"
-    expectedTeamID="4F7N4UDA22"
-    ;;
