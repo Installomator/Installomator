21937950f0a1bbec3f9bdd201ca337f8ab817976
Mirko Steinbrecher <mirko.steinbrecher@hypoport.de>
2026-06-02 08:24:44 +0200

    Create new label techsmithaudiate

---
+++ fragments/labels/techsmithaudiate.sh
@@ -0,0 +1,8 @@
+techsmithaudiate)
+    #
+    name="TechSmith Audiate"
+    type="dmg"
+    downloadURL="https://cdn-audiate.cloud.techsmith.com/audiate/latest/Audiate.dmg"
+    expectedTeamID="7TQL462TU8"
+    appName="Audiate.app"
+    ;;
