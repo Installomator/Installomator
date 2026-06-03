ac5d96a33513bc2514b62be79752fe9e5cd1e001
Cody Keats <cocopuff2u@yahoo.com>
2026-06-03 11:29:52 -0400

    Update mace.sh

--- fragments/labels/mace.sh
+++ fragments/labels/mace.sh
@@ -1,8 +1,7 @@
 mace)
     name="MACE"
     type="dmg"
-    appName="MACE.app"
-    downloadURL=$(downloadURLFromGit "MACE-App" "MACE")
+downloadURL=$(downloadURLFromGit "MACE-App" "MACE")
     appNewVersion=$(versionFromGit "MACE-App" "MACE")
     expectedTeamID="7U624389H9"
     ;;
