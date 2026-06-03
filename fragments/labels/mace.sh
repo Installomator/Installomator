c19df11b5b0117e7e799dba441e8cd42ecf4d08e
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-06-03 09:56:23 -0600

    Update mace.sh

--- fragments/labels/mace.sh
+++ fragments/labels/mace.sh
@@ -1,7 +1,7 @@
 mace)
     name="MACE"
     type="dmg"
-downloadURL=$(downloadURLFromGit "MACE-App" "MACE")
+    downloadURL=$(downloadURLFromGit "MACE-App" "MACE")
     appNewVersion=$(versionFromGit "MACE-App" "MACE")
     expectedTeamID="7U624389H9"
     ;;
