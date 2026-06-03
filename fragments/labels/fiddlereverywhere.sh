58548d02751d4444bb15638c3ff7543dd7fa9ace
RIchard Glaser <richard.glaser@utah.edu>
2026-06-03 14:40:50 -0600

    Update fiddlereverywhere.sh

--- fragments/labels/fiddlereverywhere.sh
+++ fragments/labels/fiddlereverywhere.sh
@@ -1,7 +1,11 @@
 fiddlereverywhere)
     name="Fiddler Everywhere"
     type="dmg"
-    downloadURL="https://api.getfiddler.com/mac/latest-mac"
+    if [[ "$(arch)" == "arm64" ]]; then
+        downloadURL="https://api.getfiddler.com/mac-arm64/latest-mac"
+    else
+        downloadURL="https://api.getfiddler.com/mac/latest-mac"
+    fi
     appNewVersion=$(curl -fsL -w "%{url_effective}" -o /dev/null "$downloadURL" | sed -E 's|.*Fiddler%20Everywhere%20([0-9.]+)\.dmg|\1|')
     expectedTeamID="CHSQ3M3P37"
     ;;
