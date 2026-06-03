f10cf55d8fd66507afa07178dce22a6c356286e9
Ben Figlin <ben.figlin@qnergy.com>
2026-06-03 16:45:12 -0600

    Revert "Use $arch, correctly this time."
    This reverts commit e93e25b87f8184fd3c235e980ad1ef2ef272850d.

--- fragments/labels/devin.sh
+++ fragments/labels/devin.sh
@@ -2,7 +2,7 @@ devin|\
 windsurf)
     name="Devin"
     type="dmg"
-    [[ $arch == "arm64" ]] && cpu_arch="arm64" || cpu_arch="x64"
+    [[ $(arch) == "arm64" ]] && cpu_arch="arm64" || cpu_arch="x64"
     downloadURL="$( curl -s https://docs.devin.ai/desktop/releases | tr '"\\' "\n" | grep -m1 "darwin-${cpu_arch}.*\.dmg" )"
     appNewVersion="$( echo "$downloadURL" | awk -F '-' '{ print $NF }' | cut -d '.' -f 1-3 )"
     expectedTeamID="83Z2LHX6XW"
