96cd450cb18842e3d8af2dab8951195f02850dc9
RIchard Glaser <richard.glaser@utah.edu>
2026-06-03 11:42:54 -0600

    1password8-update-label

--- fragments/labels/1password8.sh
+++ fragments/labels/1password8.sh
@@ -1,8 +1,8 @@
 1password8)
     name="1Password"
     type="pkg"
-    downloadURL="https://downloads.1password.com/mac/1Password.pkg"
-    appNewVersion=$(curl -s https://releases.1password.com/mac/stable/index.xml | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | tail -n1)
+    appNewVersion=$(curl -fsL "https://app-updates.agilebits.com/product_history/OPM8" | grep -Eo '1Password-[0-9]+([.][0-9]+)+[.]pkg' | head -n 1 | sed -E 's/1Password-([0-9.]+)[.]pkg/\1/')
+    downloadURL="https://cache.agilebits.com/dist/1P/mac8/1Password-${appNewVersion}.pkg"
     expectedTeamID="2BUA8C4S2C"
     blockingProcesses=( "1Password Extension Helper" "1Password 7" "1Password 8" "1Password" "1PasswordNativeMessageHost" "1PasswordSafariAppExtension" )
     ;;
