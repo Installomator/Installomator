add4f9231518b73e2561bb71ae72b4f77a724859
Mirko Steinbrecher <mirko.steinbrecher@hypoport.de>
2026-06-04 12:00:47 +0200

    Update techsmithaudiate.sh
    Script result: 2026-06-04 11:56:53 : REQ   :  : shifting arguments for Jamf
    2026-06-04 11:56:53 : INFO  : techsmithaudiate : setting variable from argument NOTIFY=silent
    2026-06-04 11:56:53 : INFO  : techsmithaudiate : Total items in argumentsArray: 1
    2026-06-04 11:56:53 : INFO  : techsmithaudiate : argumentsArray: NOTIFY=silent
    2026-06-04 11:56:53 : REQ   : techsmithaudiate : ################## Start Installomator v. 10.8beta, date 2025-03-28
    2026-06-04 11:56:53 : INFO  : techsmithaudiate : ################## Version: 10.8beta
    2026-06-04 11:56:53 : INFO  : techsmithaudiate : ################## Date: 2025-03-28
    2026-06-04 11:56:53 : INFO  : techsmithaudiate : ################## techsmithaudiate
    2026-06-04 11:56:54 : INFO  : techsmithaudiate : Reading arguments again: NOTIFY=silent
    2026-06-04 11:56:54 : INFO  : techsmithaudiate : BLOCKING_PROCESS_ACTION=tell_user
    2026-06-04 11:56:54 : INFO  : techsmithaudiate : NOTIFY=silent
    2026-06-04 11:56:54 : INFO  : techsmithaudiate : LOGGING=INFO
    2026-06-04 11:56:54 : INFO  : techsmithaudiate : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2026-06-04 11:56:54 : INFO  : techsmithaudiate : Label type: zip
    2026-06-04 11:56:54 : INFO  : techsmithaudiate : archiveName: TechSmith Audiate.zip
    2026-06-04 11:56:54 : INFO  : techsmithaudiate : no blocking processes defined, using TechSmith Audiate as default
    2026-06-04 11:56:54 : INFO  : techsmithaudiate : App(s) found: /Applications/Audiate.app
    2026-06-04 11:56:54 : INFO  : techsmithaudiate : found app at /Applications/Audiate.app, version 2026.2.3, on versionKey CFBundleShortVersionString
    2026-06-04 11:56:54 : INFO  : techsmithaudiate : appversion: 2026.2.3
    2026-06-04 11:56:54 : INFO  : techsmithaudiate : Latest version of TechSmith Audiate is 2026.2.3
    2026-06-04 11:56:54 : INFO  : techsmithaudiate : There is no newer version available.
    2026-06-04 11:56:55 : INFO  : techsmithaudiate : Installomator did not close any apps, so no need to reopen any apps.
    2026-06-04 11:56:55 : REQ   : techsmithaudiate : No newer version.
    2026-06-04 11:56:55 : REQ   : techsmithaudiate : ################## End Installomator, exit code 0

--- fragments/labels/techsmithaudiate.sh
+++ fragments/labels/techsmithaudiate.sh
@@ -1,7 +1,10 @@
 techsmithaudiate)
-    name="TechSmith Audiate"
-    type="dmg"
-    downloadURL="https://cdn-audiate.cloud.techsmith.com/audiate/latest/Audiate.dmg"
-    expectedTeamID="7TQL462TU8"
-    appName="Audiate.app"
-    ;;
+	name="TechSmith Audiate"
+	type="zip"
+	feedURL="https://cdn-audiate.cloud.techsmith.com/audiate/latest-mac.yml"
+	baseURL="https://cdn-audiate.cloud.techsmith.com/audiate/"
+	appNewVersion="$(curl -fsL "$feedURL" | awk -F': ' '/^version: /{print $2}')"
+	downloadURL="${baseURL}$(curl -fsL "$feedURL" | awk -F': ' '/^path: /{print $2}')"
+	appName="Audiate.app"
+	expectedTeamID="7TQL462TU8"
+	;;
