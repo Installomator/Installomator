c9033495dac9b81582aa9b3a56ec489c86efede2
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-06-04 10:03:55 +0200

    Add appCustomVersion to redshift

--- fragments/labels/redshift.sh
+++ fragments/labels/redshift.sh
@@ -3,10 +3,10 @@ redshift)
     appName="Maxon Redshift Installer.app"
     blockingProcesses=( "Cinema 4D" )
     type="dmg"
-    packageID="com.redshift3d.redshift"
     expectedTeamID="4ZY22YGXQG"
     downloadURL=$(curl -fsL https://www.maxon.net/en/downloads | grep -oE '[^"]*redshift[^"]*macos\.dmg' | head -1)
     appNewVersion=$(sed -n 's/.*redshift_\([^_]*\).*/\1/p' <<< "${downloadURL}")
+    appCustomVersion() {/usr/bin/defaults read "/Applications/Maxon Redshift 2026/uninstall.app/Contents/Info.plist" CFBundleVersion }
     installerTool="Maxon Redshift Installer.app"
     CLIInstaller="Maxon Redshift Installer.app/Contents/MacOS/installbuilder.sh"
     # Customize --enable-components for other DCC integrations (Maya, Vectorworks, ZBrush)
