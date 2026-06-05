60dd70c9ad36896c5b0f851aad3c3d5de60ad7a5
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-06-05 08:56:28 -0600

    Update 1Password 8 script to fetch new version

--- fragments/labels/1password8.sh
+++ fragments/labels/1password8.sh
@@ -1,8 +1,10 @@
 1password8)
     name="1Password"
     type="pkg"
-    appNewVersion=$(curl -fsL "https://app-updates.agilebits.com/product_history/OPM8" | grep -Eo '1Password-[0-9]+([.][0-9]+)+[.]pkg' | head -n 1 | sed -E 's/1Password-([0-9.]+)[.]pkg/\1/')
+    onepassDetails=$(curl -fs "https://app-updates.agilebits.com/check/2/14.6.1/arm64/OPM8/en/80000000/A1/N")
+    appNewVersion=$(getJSONValue "${onepassDetails}" "version")
     downloadURL="https://cache.agilebits.com/dist/1P/mac8/1Password-${appNewVersion}.pkg"
     expectedTeamID="2BUA8C4S2C"
-    blockingProcesses=( "1Password Extension Helper" "1Password 7" "1Password 8" "1Password" "1PasswordNativeMessageHost" "1PasswordSafariAppExtension" )
+    appName="1Password.app"
     ;;
+    
