5ffee39bb3eaf71d7dd7d2a1395bd4a7fd2e61a4
RIchard Glaser <richard.glaser@utah.edu>
2026-06-05 16:35:06 -0600

    arcbrowser-update-label

--- fragments/labels/arcbrowser.sh
+++ fragments/labels/arcbrowser.sh
@@ -1,7 +1,7 @@
 arcbrowser)
-name="Arc"
-type="dmg"
-downloadURL="https://releases.arc.net/release/Arc-latest.dmg"
-appNewVersion="$(curl -fsIL https://releases.arc.net/release/Arc-latest.dmg | grep -i ^location | sed -E 's/.*-([0-9]+\.[0-9]+\.[0-9]+-[0-9]+).*/\1/')"
-expectedTeamID="S6N382Y83G"
+    name="Arc"
+    type="dmg"
+    downloadURL="https://releases.arc.net/release/Arc-latest.dmg"
+    appNewVersion=$(curl -fsIL "$downloadURL" | awk 'BEGIN{IGNORECASE=1}/^location:/{gsub("\r",""); print $2}' | tail -n 1 | sed -E 's|.*/Arc-([0-9]+(\.[0-9]+)+)-[0-9]+\.dmg|\1|')
+    expectedTeamID="S6N382Y83G"
     ;;
