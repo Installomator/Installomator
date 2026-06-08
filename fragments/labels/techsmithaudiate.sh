66e89af556ef2797ea2fd9c6ca652e7622863c66
Mirko Steinbrecher <mirko.steinbrecher@hypoport.de>
2026-06-08 13:11:14 +0200

    change to universal app

--- fragments/labels/techsmithaudiate.sh
+++ fragments/labels/techsmithaudiate.sh
@@ -1,10 +1,10 @@
 techsmithaudiate)
 	name="TechSmith Audiate"
-	type="zip"
+	type="dmg"
 	feedURL="https://cdn-audiate.cloud.techsmith.com/audiate/latest-mac.yml"
 	baseURL="https://cdn-audiate.cloud.techsmith.com/audiate/"
 	appNewVersion="$(curl -fsL "$feedURL" | awk -F': ' '/^version: /{print $2}')"
-	downloadURL="${baseURL}$(curl -fsL "$feedURL" | awk -F': ' '/^path: /{print $2}')"
+	downloadURL="${baseURL}$(curl -fsL "$feedURL" | awk '/universal.*\.dmg/{print $3}')"
 	appName="Audiate.app"
 	expectedTeamID="7TQL462TU8"
 	;;
