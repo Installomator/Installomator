8cc0afb2039c597d01db1835214557f4c698bd4c
Mirko Steinbrecher <mirko.steinbrecher@hypoport.de>
2026-06-11 09:18:04 +0200

    Change to one curl and one baseURL

--- fragments/labels/techsmithaudiate.sh
+++ fragments/labels/techsmithaudiate.sh
@@ -1,10 +1,10 @@
 techsmithaudiate)
 	name="TechSmith Audiate"
 	type="dmg"
-	feedURL="https://cdn-audiate.cloud.techsmith.com/audiate/latest-mac.yml"
-	baseURL="https://cdn-audiate.cloud.techsmith.com/audiate/"
-	appNewVersion="$(curl -fsL "$feedURL" | awk -F': ' '/^version: /{print $2}')"
-	downloadURL="${baseURL}$(curl -fsL "$feedURL" | awk '/universal.*\.dmg/{print $3}')"
+	baseURL="https://cdn-audiate.cloud.techsmith.com/audiate"
+	feedURL="$(curl -fsL "$baseURL/latest-mac.yml")"
+	appNewVersion="$(echo "$feedURL" | awk -F': ' '/^version: /{print $2}')"
+	downloadURL="${baseURL}/$(echo "$feedURL" | awk '/universal.*\.dmg/{print $3}')"
 	appName="Audiate.app"
 	expectedTeamID="7TQL462TU8"
 	;;
