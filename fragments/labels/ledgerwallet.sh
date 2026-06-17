703da16c06bb1032f0a6fc22794f8ec642203484
Michaël De Bona <debona.michael@gmail.com>
2026-06-17 09:50:12 +0200

    feat: adding label for the Ledger Wallet app

---
+++ fragments/labels/ledgerwallet.sh
@@ -0,0 +1,9 @@
+ledgerwallet)
+    name="Ledger Wallet"
+    type="dmg"
+    downloadURL="https://download.live.ledger.com/latest/mac?c=1"
+    appNewVersion=$(curl -fs "https://download.live.ledger.com/latest-mac.yml" \
+        | grep "^version:" | sed 's/version: //')
+    appName="Ledger Wallet.app"
+    expectedTeamID="X6LFS5BQKN"
+    ;;
