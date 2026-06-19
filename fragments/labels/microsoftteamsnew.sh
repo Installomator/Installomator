8ad3c0ff6eab407cf2b1add6ac07c9c2dc6e0cf4
Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>
2026-06-19 04:45:15 -0600

    Refine microsoftteamsnew XPath

--- fragments/labels/microsoftteamsnew.sh
+++ fragments/labels/microsoftteamsnew.sh
@@ -3,12 +3,8 @@ microsoftteamsnew)
     type="pkg"
     MAUSource="https://res.public.onecdn.static.microsoft/mro1cdnstorage/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409TEAMS21.xml"
     mauXML=$(curl -fsL "$MAUSource")
-    appNewVersion=$(printf '%s' "$mauXML" | xmllint --xpath 'string((//key[text()="Update Version"]/following-sibling::string[1])[1])' - 2>/dev/null)
-    downloadURL=$(printf '%s' "$mauXML" | xmllint --xpath 'string((//key[text()="FullUpdaterLocation"]/following-sibling::string[1])[1])' - 2>/dev/null)
-    if [[ -z "$downloadURL" ]]; then
-        downloadURL=$(printf '%s' "$mauXML" | xmllint --xpath 'string((//key[text()="Location"]/following-sibling::string[contains(text(),"/MicrosoftTeams.pkg")][1])[1])' - 2>/dev/null)
-    fi
-    [[ "$downloadURL" == *BinaryDelta* ]] && downloadURL=""
+    appNewVersion=$(printf '%s' "$mauXML" | xmllint --xpath 'string((//dict[key[text()="Application ID"]/following-sibling::string[1]="TEAMS21" and key[text()="Location"]/following-sibling::string[1][contains(.,"/MicrosoftTeams.pkg")]]/key[text()="Update Version"]/following-sibling::string[1])[1])' - 2>/dev/null)
+    downloadURL=$(printf '%s' "$mauXML" | xmllint --xpath 'string((//dict[key[text()="Application ID"]/following-sibling::string[1]="TEAMS21" and key[text()="Location"]/following-sibling::string[1][contains(.,"/MicrosoftTeams.pkg")]]/key[text()="Location"]/following-sibling::string[1])[1])' - 2>/dev/null)
     expectedTeamID="UBF8T346G9"
     blockingProcesses=( MSTeams "Microsoft Teams" "Microsoft Teams WebView" "Microsoft Teams Launcher" "Microsoft Teams (work preview)")
     if [[ -x "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" && $INSTALL != "force" && $DEBUG -eq 0 ]]; then
