612cb575789afd7d896b307df5e0412cd5f95ac9
Michaël De Bona <debona.michael@gmail.com>
2026-06-22 10:51:42 +0200

    fix: remove AppName variable and multiline code

--- fragments/labels/ledgerwallet.sh
+++ fragments/labels/ledgerwallet.sh
@@ -2,8 +2,6 @@ ledgerwallet)
     name="Ledger Wallet"
     type="dmg"
     downloadURL="https://download.live.ledger.com/latest/mac?c=1"
-    appNewVersion=$(curl -fs "https://download.live.ledger.com/latest-mac.yml" \
-        | grep "^version:" | sed 's/version: //')
-    appName="Ledger Wallet.app"
+    appNewVersion=$(curl -fs "https://download.live.ledger.com/latest-mac.yml" | grep "^version:" | sed 's/version: //')
     expectedTeamID="X6LFS5BQKN"
     ;;
