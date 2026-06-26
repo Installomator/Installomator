3749fe283554b76345471913038e6617cc51e95e
vassgard <havard.vassgard@icloud.com>
2026-06-26 08:22:50 +0200

    Update gitcredentialmanager.sh

--- fragments/labels/gitcredentialmanager.sh
+++ fragments/labels/gitcredentialmanager.sh
@@ -3,7 +3,7 @@ gitcredentialmanager)
     type="pkg"
     packageID="com.microsoft.gitcredentialmanager"
     appNewVersion=$(versionFromGit git-ecosystem git-credential-manager)
-    if [[ "$arch" == "arm64" ]]; then
+    if [[ "$(arch)" == "arm64" ]]; then
         downloadURL="https://github.com/git-ecosystem/git-credential-manager/releases/download/v${appNewVersion}/gcm-osx-arm64-${appNewVersion}.pkg"
     else
         downloadURL="https://github.com/git-ecosystem/git-credential-manager/releases/download/v${appNewVersion}/gcm-osx-x64-${appNewVersion}.pkg"
