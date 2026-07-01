1eed4210f6a3ef9b598fa4cd85b0c3efcf8dff52
Angelos-V <a.veziridis@apfelwerk.de>
2026-07-01 14:22:41 +0200

    Add fierysoftwaremanager label
    Installs the Fiery Software Manager app, which is used to select and
    install Fiery Command WorkStation 7.2 and other Fiery software. Tested
    successfully on macOS 26.5.1 (download, Team ID verification, and
    install all confirmed working).

---
+++ fragments/labels/fierysoftwaremanager.sh
@@ -0,0 +1,7 @@
+fierysoftwaremanager)
+    name="Fiery Software Manager"
+    type="dmg"
+    downloadURL="https://solutions.fiery.com/cws72-mac-fsm"
+    appNewVersion="7.2.0.13"
+    expectedTeamID="5N677U7DA8"
+    ;;
