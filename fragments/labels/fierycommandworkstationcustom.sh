dbc28bd600dff8a658ab9a341e157ce998e60f1e
Angelos-V <a.veziridis@apfelwerk.de>
2026-07-01 16:41:49 +0200

    delete a random albel

--- fragments/labels/fierycommandworkstationcustom.sh
+++
@@ -1,8 +0,0 @@
-fierycommandworkstationcustom)
-    name="FieryCommandWorkStation"
-    pkgName="/OSX/CWS Wrapper.pkg"
-    type="pkgInDmg"
-    downloadURL="https://d4ad3f5ztqcgt.cloudfront.net/Products/CWSP/71/DCenter/Aug_604/FieryCommandWorkStation7_1.dmg"
-    appNewVersion="7.1"
-    expectedTeamID="5N677U7DA8"
-    ;;
