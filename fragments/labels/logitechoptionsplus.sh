5d2f8b2c4adbea2e1ba974cc1f515664b3d15890
iDvL-dracea <58770969+iDvL-dracea@users.noreply.github.com>
2026-07-04 15:36:07 +0100

    Fix logitechoptionsplus macOS label fallback

--- fragments/labels/logitechoptionsplus.sh
+++ fragments/labels/logitechoptionsplus.sh
@@ -5,8 +5,13 @@ logitechoptionsplus)
     installerTool="logioptionsplus_installer.app"
     type="zip"
     osMajorVersion=$(sw_vers -productVersion | awk -F "." '{print$1}')
-    downloadURL="$(curl -fs "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,webos=mac-macos-x-${osMajorVersion}.0" | tr "," "\n"  | grep  -o "https://.*logioptionsplus.*zip" | head -1)"
-    appNewVersion=$(curl -fs "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,webos=mac-macos-x-${osMajorVersion}.0" | tr "," "\n" | grep -A 10 "macOS" | grep -B 5 -ie "https.*/.*/optionsplus/.*\.zip" | grep "Software Version" | sed 's/\\u[0-9a-z][0-9a-z][0-9a-z][0-9a-z]//g' | grep -ioe "Software Version.*[0-9.]*" | tr "/" "\n" | grep -oe "[0-9.]*" | head -1)
+    logitechOptionsPlusJSON="$(curl -fs "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,webos=mac-macos-x-${osMajorVersion}.0")"
+    if ! printf "%s\n" "$logitechOptionsPlusJSON" | grep -q "https://.*logioptionsplus.*zip"; then
+        # Logitech sometimes publishes app updates before adding labels for new macOS releases.
+        logitechOptionsPlusJSON="$(curl -fs "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload")"
+    fi
+    downloadURL="$(printf "%s\n" "$logitechOptionsPlusJSON" | tr "," "\n"  | grep  -o "https://.*logioptionsplus.*zip" | head -1)"
+    appNewVersion=$(printf "%s\n" "$logitechOptionsPlusJSON" | tr "," "\n" | grep -A 10 "macOS" | grep -B 5 -ie "https.*/.*/optionsplus/.*\.zip" | grep "Software Version" | sed 's/\\u[0-9a-z][0-9a-z][0-9a-z][0-9a-z]//g' | grep -ioe "Software Version.*[0-9.]*" | tr "/" "\n" | grep -oe "[0-9.]*" | head -1)
     CLIInstaller="logioptionsplus_installer.app/Contents/MacOS/logioptionsplus_installer"
     CLIArguments=(--quiet)
     expectedTeamID="QED4VVPZWA"
