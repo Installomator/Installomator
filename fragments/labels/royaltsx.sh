baa59b400bf88b75806bfe94129eec7828ad0c4f
RIchard Glaser <richard.glaser@utah.edu>
2026-07-06 13:44:40 -0600

    royaltsx-label-update

--- fragments/labels/royaltsx.sh
+++ fragments/labels/royaltsx.sh
@@ -1,7 +1,8 @@
 royaltsx)
     name="Royal TSX"
     type="dmg"
-    downloadURL=$(curl -fs https://royaltsx-v6.royalapps.com/updates_stable | xpath '//rss/channel/item[1]/enclosure/@url'  2>/dev/null | cut -d '"' -f 2)
-    appNewVersion=$(curl -fs https://royaltsx-v6.royalapps.com/updates_stable | xpath '//rss/channel/item[1]/enclosure/@sparkle:shortVersionString'  2>/dev/null | cut -d '"' -f 2)
+    sparkleData=$(curl -fsL "https://royaltsx-v6.royalapps.com/updates_stable")
+    downloadURL=$(echo "$sparkleData" | grep -m1 -oE 'url="[^"]+"' | cut -d '"' -f 2)
+    appNewVersion=$(echo "$sparkleData" | grep -m1 -oE 'sparkle:shortVersionString="[^"]+"' | cut -d '"' -f 2)
     expectedTeamID="VXP8K9EDP6"
     ;;
