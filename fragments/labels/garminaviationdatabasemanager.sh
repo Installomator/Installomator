7fcc88920280824062bd250968b41eb37c6bdfd4
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-07-06 14:07:08 -0600

    Remove comment on download URL parsing method
    Removed comment about preferred download URL method.

--- fragments/labels/garminaviationdatabasemanager.sh
+++ fragments/labels/garminaviationdatabasemanager.sh
@@ -1,7 +1,6 @@
 garminaviationdatabasemanager)
     name="Garmin Aviation Database Manager"
     type="pkg"
-    # Preferred: parse FlyGarmin product page for the Mac download URL
     downloadURL="$(curl -fs "https://fly.garmin.com/fly-garmin/garmin-aviation-database-manager/" | grep -Eo "https://static.garmin.com/apps/fly/files/desktop/mac[^\"']*garminAvnDbManagerSetup_[0-9]+(\\.[0-9]+)*\\.pkg" | head -n1)"
     appNewVersion="$(echo "${downloadURL}" | sed -E 's/.*garminAvnDbManagerSetup_([0-9]+(\.[0-9]+)*)\.pkg/\1/')"
     expectedTeamID="72ES32VZUA"
