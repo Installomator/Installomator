160d65f9145745b712f2a820f3dc4d22f31ba72e
Jonathan Synotte <jsynotte@me.com>
2026-07-06 11:18:09 -0400

    Update downloadURL to dynamically fetch the latest Garmin Aviation Database Manager package

--- fragments/labels/garminaviationdatabasemanager.sh
+++ fragments/labels/garminaviationdatabasemanager.sh
@@ -1,7 +1,8 @@
 garminaviationdatabasemanager)
     name="Garmin Aviation Database Manager"
     type="pkg"
-    downloadURL="https://static.garmin.com/apps/fly/files/desktop/mac/electron/channels/2024/garminAvnDbManagerSetup_26.4.23.pkg"
+    # Preferred: parse FlyGarmin product page for the Mac download URL
+    downloadURL="$(curl -fs "https://fly.garmin.com/fly-garmin/garmin-aviation-database-manager/" | grep -Eo "https://static.garmin.com/apps/fly/files/desktop/mac[^\"']*garminAvnDbManagerSetup_[0-9]+(\\.[0-9]+)*\\.pkg" | head -n1)"
     appNewVersion="$(echo "${downloadURL}" | sed -E 's/.*garminAvnDbManagerSetup_([0-9]+(\.[0-9]+)*)\.pkg/\1/')"
     expectedTeamID="72ES32VZUA"
     ;;
