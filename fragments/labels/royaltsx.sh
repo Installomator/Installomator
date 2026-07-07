20a204d567186609dba5456cafa84bd63a07806b
RIchard Glaser <richard.glaser@utah.edu>
2026-07-07 15:22:19 -0600

    Update to use xpath vs grep

--- fragments/labels/royaltsx.sh
+++ fragments/labels/royaltsx.sh
@@ -2,7 +2,7 @@ royaltsx)
     name="Royal TSX"
     type="dmg"
     sparkleData=$(curl -fsL "https://royaltsx-v6.royalapps.com/updates_stable")
-    downloadURL=$(echo "$sparkleData" | grep -m1 -oE 'url="[^"]+"' | cut -d '"' -f 2)
-    appNewVersion=$(echo "$sparkleData" | grep -m1 -oE 'sparkle:shortVersionString="[^"]+"' | cut -d '"' -f 2)
+    downloadURL=$(echo "$sparkleData" | xpath '//rss/channel/item[1]/enclosure/@url' 2>/dev/null | cut -d '"' -f 2)
+    appNewVersion=$(echo "$sparkleData" | xpath '//rss/channel/item[1]/enclosure/@sparkle:shortVersionString' 2>/dev/null | cut -d '"' -f 2)
     expectedTeamID="VXP8K9EDP6"
     ;;
