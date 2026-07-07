ed99cb731b8a90b3755af6e531b5c0164a974c33
RIchard Glaser <richard.glaser@utah.edu>
2026-07-07 15:31:03 -0600

    More concise label version

--- fragments/labels/dbvisualizer.sh
+++ fragments/labels/dbvisualizer.sh
@@ -2,10 +2,7 @@ dbvisualizer)
     name="DbVisualizer"
     type="dmg"
     dbvisVersion=$(curl -fsL "https://www.dbvis.com/download/" | grep -oE 'Latest version [0-9]+(\.[0-9]+)+' | head -n 1 | awk '{print $3}')
-    appNewVersion="$dbvisVersion"
-    if [[ "$appNewVersion" =~ ^[0-9]+\.[0-9]+$ ]]; then
-        appNewVersion="${appNewVersion}.0"
-    fi
+    appNewVersion=$(echo "$dbvisVersion" | awk -F. '{for (i = NF+1; i <= 3; i++) $i = 0; print $1"."$2"."$3}')
     downloadVersion="${dbvisVersion//./_}"
     if [[ $(arch) == "arm64" ]]; then
         downloadURL="https://www.dbvis.com/product_download/dbvis-${dbvisVersion}/media/dbvis_macos-aarch64_${downloadVersion}.dmg"
