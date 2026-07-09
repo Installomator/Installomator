a07043cff9e112cf4e0c7668ba878218400b78d8
Maqs <m.switon@apfelwerk.de>
2026-07-09 13:43:38 +0200

    Update label for Blender

--- fragments/labels/blender.sh
+++ fragments/labels/blender.sh
@@ -8,9 +8,8 @@ blender)
         archiveName=$(curl -sf "https://ftp.nluug.nl/pub/graphics/blender/release/Blender$baseVersion/"| grep -o 'blender-[0-9]\+\.[0-9]\+\.[0-9]\+-macos-arm64\.dmg' | sort -V | tail -1)
         downloadURL="https://ftp.nluug.nl/pub/graphics/blender/release/Blender$baseVersion/$archiveName"
     elif [[ $(arch) == "i386" ]]; then
-        appNewVersion=$(curl -sf https://ftp.nluug.nl/pub/graphics/blender/release/Blender$baseVersion/ | grep -o 'blender-[0-9]\+\.[0-9]\+\.[0-9]\+-macos-x64\.dmg' | sort -V | tail -1 | sed -E 's/[^0-9]*([0-9]+\.[0-9]+\.[0-9]+).*/\1/' )
-        archiveName=$(curl -sf "https://ftp.nluug.nl/pub/graphics/blender/release/Blender$baseVersion/" | grep -o 'blender-[0-9]\+\.[0-9]\+\.[0-9]\+-macos-x64\.dmg' | sort -V | tail -1)
-        downloadURL="https://ftp.nluug.nl/pub/graphics/blender/release/Blender$baseVersion/$archiveName"
+        downloadURL="https://www.blender.org/download/release/Blender4.5/blender-4.5.11-macos-x64.dmg"
+        printlog "Latest version of Blender for Intel is 4.5.11, which is no longer updated. Please consider upgrading to Apple Silicon."
     fi
     expectedTeamID="68UA947AUU"
     ;;
