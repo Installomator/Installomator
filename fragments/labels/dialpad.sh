fd4dfa7057f6d126c8bed3e7d0498681efd03081
Fraser Hess <fraser@renewedheart.org>
2026-07-10 16:03:41 -0600

    Add appNewVersion to dialpad

--- fragments/labels/dialpad.sh
+++ fragments/labels/dialpad.sh
@@ -3,8 +3,11 @@ dialpad)
     type="dmg"
     if [[ $(arch) == "arm64" ]]; then
         downloadURL="https://download.dialpad.com/osx/arm64/Dialpad.dmg"
+        dialpadVersionURL="https://dialpad.com/native/minsupportedversion/darwin/arm64/?channel=stable"
     elif [[ $(arch) == "i386" ]]; then
         downloadURL="https://download.dialpad.com/osx/x64/Dialpad.dmg"
+        dialpadVersionURL="https://dialpad.com/native/minsupportedversion/darwin/x64/?channel=stable"
     fi
+    appNewVersion=$(getJSONValue "$(curl -fsL "$dialpadVersionURL")" latest_version)
     expectedTeamID="9V29MQSZ9M"
     ;;
