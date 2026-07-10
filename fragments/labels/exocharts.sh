8da1dbd6f68998844cf7ab05d73ee5ed03da6d49
Tuttu <debona.michael@gmail.com>
2026-07-10 09:18:36 +0200

    fix: incorrect downloadURL
    This fix introduce a check for the computer's architecture (the app runs only on Apple Silicon) and changes how the downloadURL and appNewVersion are found by using a JSON metadata file rather than scrapping the HTML page.
    
    Thanks to @uurazzle for the suggested label

--- fragments/labels/exocharts.sh
+++ fragments/labels/exocharts.sh
@@ -1,7 +1,13 @@
 exocharts)
     name="Exocharts"
     type="dmg"
-    appNewVersion=$(curl -fs "https://help.exocharts.com/hc/en-us/articles/5466518923281-Exocharts-downloads" | grep -o "Exocharts_[0-9.]*_M1_ARM.dmg" | head -1 | sed -E 's/Exocharts_([0-9.]+)_M1_ARM.dmg/\1/')
-    downloadURL="https://exocharts.com/downloads/Exocharts_${appNewVersion}_M1_ARM.dmg"
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL=$(curl -fsL "https://help.exocharts.com/api/v2/help_center/en-us/articles/5466518923281.json" | plutil -extract article.body raw -o - - | grep -o 'https://exocharts.com/downloads/Exocharts_[0-9.]*_M1_ARM\.dmg' | head -1)
+    else
+        printlog "Exocharts is only compatible with Apple Silicon (arm64) Macs." ERROR
+        cleanupAndExit 95 "Exocharts requires Apple Silicon" ERROR
+    fi
+    appNewVersion=$(echo "$downloadURL" | sed -E 's|.*/Exocharts_([0-9.]+)_M1_ARM\.dmg|\1|')
+    versionKey="CFBundleVersion"
     expectedTeamID="7HSW4JKZM2"
     ;;
