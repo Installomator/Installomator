23a60f25ec5b74078a8a28edc550ce7a2374d5eb
Maqs <m.switon@apfelwerk.de>
2026-07-13 14:59:17 +0200

    –Update label snagit2025 –Create label snagit2026

--- fragments/labels/snagit2025.sh
+++ fragments/labels/snagit2025.sh
@@ -1,15 +1,7 @@
-snagit|\
 snagit2025)
-    name="Snagit"
+    name="Snagit 2025"
     type="dmg"
-    sparkleData=$(curl -fsL -H 'User-Agent: Snagit/2025.0.0' 'https://www.techsmith.com/redirect.asp?target=sparkleappcast&product=snagitmac&ver=2025.0.0&lang=enu&os=mac')
-    appNewVersion=$(
-        echo "$sparkleData" | \
-        xmllint -xpath 'string(//*[local-name()="item"][last()]/*[local-name()="shortVersionString"]/text())' -
-    )
-    downloadURL=$(
-        echo "$sparkleData" | \
-        xmllint -xpath 'string(//*[local-name()="item"][last()]/enclosure/@url)' -
-    )
+    appNewVersion="25.4.0"
+    downloadURL="https://download.techsmith.com/snagitmac/releases/2025.4.0/snagit.dmg"
     expectedTeamID="7TQL462TU8"
     ;;
