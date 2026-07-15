b09383eb6192c321106476d75af848a0b94c2b27
Lennart Duncker <lennart.duncker@yoummday.com>
2026-07-15 07:22:03 +0200

    yubikeymanagerqt: adopt maintainer's suggested label (atom.xml source, ykman-gui blocking process)

--- fragments/labels/yubikeymanagerqt.sh
+++ fragments/labels/yubikeymanagerqt.sh
@@ -1,9 +1,9 @@
 yubikeymanagerqt)
-    name="YubiKey Manager GUI"
-    appName="YubiKey Manager.app"
+    name="YubiKey Manager"
     type="pkg"
-    downloadURL=$(curl -fsL "https://developers.yubico.com/yubikey-manager-qt/Releases/" | grep -oE "yubikey-manager-qt-[0-9][^\"]*-mac\.pkg" | head -1 | sed 's|^|https://developers.yubico.com/yubikey-manager-qt/Releases/|')
+    releaseData=$(curl -fsL "https://developers.yubico.com/yubikey-manager-qt/Releases/atom.xml")
+    downloadURL=$(echo "$releaseData" | grep -oE 'https://developers\.yubico\.com/yubikey-manager-qt/Releases/yubikey-manager-qt-[0-9][^"]*-mac\.pkg' | head -1)
     appNewVersion=$(echo "$downloadURL" | sed -E 's|.*/yubikey-manager-qt-([^-]+)-mac\.pkg$|\1|')
     expectedTeamID="LQA3CS5MM7"
-    blockingProcesses=( "YubiKey Manager" )
+    blockingProcesses=( "ykman-gui" )
     ;;
