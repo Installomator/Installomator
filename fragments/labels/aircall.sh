fd3f91810af695c3fc09ecfcada4b4a99bfecd40
Gianluca Varisco <gianluca@netsec.it>
2026-07-15 10:58:55 +0200

    Refactor Aircall download URL logic for architecture

--- fragments/labels/aircall.sh
+++ fragments/labels/aircall.sh
@@ -2,12 +2,11 @@ aircall)
     name="Aircall Workspace"
     type="pkg"
     packageID="io.aircall.workspace"
-    if [[ $(arch) == "arm64" ]]; then
-        cpu_arch="arm64"
-    elif [[ $(arch) == "i386" ]]; then
-        cpu_arch="x64"
+    if [[ "$(arch)" == "arm64" ]]; then
+        downloadURL=$(curl -fsL -r 0-0 -o /dev/null -w "%{url_effective}" "https://electron.aircall.io/download/osx?appType=aircall-workspace&arch=arm64&platform=macPkg")
+    else
+        downloadURL=$(curl -fsL -r 0-0 -o /dev/null -w "%{url_effective}" "https://electron.aircall.io/download/osx?appType=aircall-workspace&arch=x64&platform=macPkg")
     fi
-    downloadURL=$(curl -fsL -r 0-0 -o /dev/null -w "%{url_effective}" "https://electron.aircall.io/download/osx?appType=aircall-workspace&arch=${cpu_arch}&platform=macPkg")
     appNewVersion=$(echo "${downloadURL}" | sed -E 's/.*-([0-9.]*)-.*/\1/')
     expectedTeamID="3ML357Q795"
     ;;
