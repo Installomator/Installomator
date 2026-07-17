c6b4372a2413b5ac273950421c14b3c191c769fd
Corey Oliphant <corey.oliphant@icloud.com>
2026-07-17 09:50:41 -0500

    Update label to check client arch

--- fragments/labels/dia.sh
+++ fragments/labels/dia.sh
@@ -1,7 +1,12 @@
 dia)
     name="Dia"
     type="dmg"
-    downloadURL="https://releases.diabrowser.com/release/Dia-latest.dmg"
-    appNewVersion=$(curl -sIL "$downloadURL" | grep -i "^content-disposition:" | sed 's/.*Dia-\([0-9.]*\)-.*\.dmg.*/\1/')
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL="https://releases.diabrowser.com/release/Dia-latest.dmg"
+    else
+        printlog "Dia is only compatible with Apple Silicon (arm64) Macs." ERROR
+        cleanupAndExit 95 "Dia requires Apple Silicon" ERROR
+    fi
+    appNewVersion=$(curl -sIL "$downloadURL" | sed -nE 's/.*Dia-([0-9]+([.][0-9]+)+)-[0-9]+[.]dmg.*/\1/p')
     expectedTeamID="S6N382Y83G"
     ;;
