fe348a52fe844b5b110858db4bb20450976d42b9
Tully <tjagoe@systima.com.au>
2026-07-17 13:34:49 +1000

    New label: Radix

---
+++ fragments/labels/radix.sh
@@ -0,0 +1,9 @@
+radix)
+    name="Radix"
+    type="zip"
+    packageID="com.colinkim.Radix"
+    versionKey="CFBundleShortVersionString"
+    appNewVersion=$(versionFromGit colinvkim Radix)
+    downloadURL=$(downloadURLFromGit colinvkim Radix)
+    expectedTeamID="42MBX5D86L"
+    ;;
