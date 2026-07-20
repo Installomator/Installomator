4142f961841882394fbacca1b1f2dc03f070ea64
Maqs <m.switon@apfelwerk.de>
2026-07-20 16:06:36 +0200

    Fix download URL and version source for Mist package

--- fragments/labels/mist.sh
+++ fragments/labels/mist.sh
@@ -2,8 +2,8 @@ mist)
     name="Mist"
     type="pkg"
     packageID="com.ninxsoft.pkg.mist"
-    downloadURL=$(downloadURLFromGit "ninxsoft" "mist")
-    appNewVersion=$(versionFromGit "ninxsoft" "mist")
+    downloadURL=$(downloadURLFromGit "minxsoft" "mist")
+    appNewVersion=$(versionFromGit "minxsoft" "mist")
     expectedTeamID="7K3HVCLV7Z"
     blockingProcesses=( NONE )
     ;;
