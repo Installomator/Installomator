3311a26db49200ef43008a3085dd9bb5c44d47c0
Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>
2026-07-20 13:16:20 -0400

    fix(codex): use Sparkle ZIP

--- fragments/labels/codex.sh
+++ fragments/labels/codex.sh
@@ -1,18 +1,19 @@
 codex)
     name="Codex"
-    type="dmg"
+    type="zip"
     appName="ChatGPT.app"
     targetDir="/Applications/ChatGPT.localized"
     appCustomVersion() {
         /usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$targetDir/$appName/Contents/Info.plist" 2>/dev/null
     }
     if [[ $(arch) == "arm64" ]]; then
-        downloadURL="https://persistent.oaistatic.com/codex-app-prod/Codex.dmg"
+        sparkleData=$(curl -fsL "https://persistent.oaistatic.com/codex-app-prod/appcast.xml")
+        appNewVersion=$(echo "$sparkleData" | awk -F "[<>]" '/<sparkle:shortVersionString>/{print $3; exit}')
+        downloadURL=$(echo "$sparkleData" | awk -F 'url="' '/<enclosure url=/{split($2,a,"\""); print a[1]; exit}')
     else
         printlog "Codex is only compatible with Apple Silicon (arm64) Macs." ERROR
         cleanupAndExit 95 "Codex requires Apple Silicon" ERROR
     fi
-    appNewVersion="$(curl -fs "https://persistent.oaistatic.com/codex-app-prod/appcast.xml" | grep -o '<sparkle:shortVersionString>[^<]*' | head -1 | cut -d '>' -f 2)"
     expectedTeamID="2DC432GLL2"
     blockingProcesses=( "ChatGPT" )
     ;;
