b39182aea5ee69388a615b6352557717c72a26fe
Gil Burns <gil@gilburns.com>
2026-07-21 10:24:41 -0500

    Update mailtooutlook.sh (#3005)

--- fragments/labels/mailtooutlook.sh
+++ fragments/labels/mailtooutlook.sh
@@ -1,6 +1,6 @@
 mailtooutlook)
     name="MailToOutlook"
     type="pkg"
-    downloadURL=https://macadmins.software/tools/MailToOutlook_2.1.pkg
+    downloadURL="https://raw.githubusercontent.com/cocopuff2u/MOFA/refs/heads/main/office_reset_tools/office_reset_archived/packages/Office-Reset.com_MailToOutlook_2.1.pkg"
     expectedTeamID="QGS93ZLCU7"
     ;;
