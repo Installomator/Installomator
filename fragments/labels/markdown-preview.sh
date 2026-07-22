dc28198286f396613ca897bfc39d4bc1dc2c513e
Henk <hbokhoven+github@gmail.com>
2026-07-22 17:39:36 +0200

    chore: delete fragments/labels/markdown-preview.sh

--- fragments/labels/markdown-preview.sh
+++
@@ -1,8 +0,0 @@
-markdown-preview)
-    name="Markdown Preview"
-    type="dmg"
-    downloadURL=$(downloadURLFromGit "pluk-inc" "markdown-preview")
-    appNewVersion=$(versionFromGit "pluk-inc" "markdown-preview")
-    archiveName="Markdown-Preview.dmg"
-    expectedTeamID="5P3TSMNV42"
-    ;;
