63f49b0fc2823a37348da5f55900d6f1ac961a12
Henk <hbokhoven+github@gmail.com>
2026-07-22 08:38:51 +0200

    chore: use downloadURLFromGit

--- fragments/labels/markdown-preview.sh
+++ fragments/labels/markdown-preview.sh
@@ -1,7 +1,7 @@
 markdown-preview)
     name="Markdown Preview"
     type="dmg"
-    downloadURL="https://github.com/pluk-inc/markdown-preview/releases/latest/download/Markdown-Preview.dmg"
+    downloadURL=$(downloadURLFromGit "pluk-inc" "markdown-preview")
     appNewVersion=$(versionFromGit "pluk-inc" "markdown-preview")
     expectedTeamID="5P3TSMNV42"
     ;;
