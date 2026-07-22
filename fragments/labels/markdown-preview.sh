cb74068a0ab2957168c1bd2170c4b25525f101ad
Henk <hbokhoven+github@gmail.com>
2026-07-22 08:56:04 +0200

    chore: use archiveName w/o space

--- fragments/labels/markdown-preview.sh
+++ fragments/labels/markdown-preview.sh
@@ -3,5 +3,6 @@ markdown-preview)
     type="dmg"
     downloadURL=$(downloadURLFromGit "pluk-inc" "markdown-preview")
     appNewVersion=$(versionFromGit "pluk-inc" "markdown-preview")
+    archiveName="Markdown-Preview.dmg"
     expectedTeamID="5P3TSMNV42"
     ;;
