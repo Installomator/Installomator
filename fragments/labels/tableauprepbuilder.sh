2cdec08022aab993774506f60c7dc1134c3958d9
Adam Martin <adam.martin@smartsheet.com>
2026-07-23 15:38:58 -0700

    Update tableauprepbuilder.sh
    Modified architecture logic and cleaned up line formatting

--- fragments/labels/tableauprepbuilder.sh
+++ fragments/labels/tableauprepbuilder.sh
@@ -2,15 +2,12 @@ tableauprepbuilder)
     name="Tableau Prep Builder"
     type="pkgInDmg"
     pkgName="Tableau Prep Builder.pkg"
-    appNewVersion=$(curl -fsL "https://www.tableau.com/support/releases/prep" \
-        | grep -oE 'releases/prep/202[0-9]\.[0-9]+(\.[0-9]+)?' \
-        | sed -E 's|.*prep/||' \
-        | head -1)
+    appNewVersion=$(curl -fsL "https://www.tableau.com/support/releases/prep" | grep -oE 'releases/prep/202[0-9]\.[0-9]+(\.[0-9]+)?' | sed -E 's|.*prep/||' | head -1)
     if [[ $(echo "$appNewVersion" | grep -o '\.' | wc -l) -lt 2 ]]; then
         appNewVersion="${appNewVersion}.0"
     fi
     urlVersion=$(echo "$appNewVersion" | sed -E 's/\./-/g')
-    if [[ $(/usr/bin/arch) == "arm64" ]]; then
+    if [[ "$(arch)" == "arm64" ]]; then
         downloadURL="https://downloads.tableau.com/esdalt/tableau_prep/${appNewVersion}/TableauPrep-${urlVersion}-arm64.dmg"
     else
         downloadURL="https://downloads.tableau.com/esdalt/tableau_prep/${appNewVersion}/TableauPrep-${urlVersion}.dmg"
