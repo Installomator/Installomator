e7a7095adf833f77dd9fe81ecda3c2ed2eeccbd5
Adam Martin <adam.martin@smartsheet.com>
2026-07-23 12:49:21 -0700

    Update zoapi.sh
    Updated with arch logic and removal of file checksum logic.

--- fragments/labels/zoapi.sh
+++ fragments/labels/zoapi.sh
@@ -2,9 +2,12 @@ zoapiclient)
 	name="Zoapi Client"
 	type="dmg"
 	appName="Zoapi.app"
-	ymlContent=$(curl -fsL "https://share.zoapi.com/zoapi/v0/api/fetch/client/mac/latest-mac.yml")
-	appNewVersion=$(echo "$ymlContent" | grep "^version:" | sed 's/^version:[[:space:]]*\([0-9.]*\).*/\1/')
-	sha512=$(echo "$ymlContent" | grep -A2 "url: ZoapiClient-Setup.dmg" | grep "sha512:" | sed 's/^[[:space:]]*sha512:[[:space:]]*//')
-	downloadURL="https://share.zoapi.com/zoapi/client/ZoapiClient-Setup.dmg"
+	if [[ $(arch) == "arm64" ]]; then
+		downloadURL="https://share.zoapi.com/zoapi/client/ZoapiClient-Setup.dmg"
+	else
+		printlog "Zoapi Client is only compatible with Apple Silicon (arm64) Macs." ERROR
+		cleanupAndExit 95 "Zoapi Client requires Apple Silicon" ERROR
+	fi
+	appNewVersion=$(curl -fsL "https://share.zoapi.com/zoapi/v0/api/fetch/client/mac/latest-mac.yml" | grep "^version:" | sed 's/^version:[[:space:]]*\([0-9.]*\).*/\1/')
 	expectedTeamID="WFY2HJH6B3"
 	;;
