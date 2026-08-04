2286244b9b8e493445efe933acebc2651eeef49a
MacMike077 <104210875+MacMike077@users.noreply.github.com>
2026-08-04 12:16:57 +0200

    Update monotypeconnect.sh
    Better AppNewVersion control. Thanks @uurazzle for the suggestion!

--- fragments/labels/monotypeconnect.sh
+++ fragments/labels/monotypeconnect.sh
@@ -2,6 +2,6 @@ monotypeconnect)
     name="Monotype Connect"
     type="dmg"
     downloadURL="https://links.extensis.com/extensis_connect/ec_latest?language=en&platform=mac"
-    appNewVersion=$( curl -is "$downloadURL" | grep "location:" | grep -o "[[:digit:]]\+-[[:digit:]]\+-[[:digit:]]\+" | sed -e 's/-/./g' )
+    appNewVersion=$(curl -fsS -D - -o /dev/null "$downloadURL" | awk 'BEGIN{IGNORECASE=1}/^location:/{gsub("\r",""); print $2}' | tail -n 1 | sed -E 's|.*/ExtensisConnect-M-([0-9]+)-([0-9]+)-([0-9]+)\.dmg$|\1.\2.\3|')
     expectedTeamID="J6MMHGD9D6"
     ;;
