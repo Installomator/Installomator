3aae111dc3caf2edeeb638bbfe59b7b5acf96675
Gil Burns <gil@gilburns.com>
2026-08-03 20:18:03 -0500

    Create intunepppcutility.sh

---
+++ fragments/labels/intunepppcutility.sh
@@ -0,0 +1,7 @@
+intunepppcutility)
+    name="Intune PPPC Utility"
+    type="zip"
+    downloadURL="$(downloadURLFromGit gilburns Intune-PPPC-Utility)"
+    appNewVersion="$(versionFromGit gilburns Intune-PPPC-Utility)"
+    expectedTeamID="G4MQ57TVLE"
+    ;;
