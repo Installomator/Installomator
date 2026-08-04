b04aebc467ff649216cff0706de2df5f915b7841
Gil Burns <gil@gilburns.com>
2026-08-03 20:06:41 -0500

    Create thirdpartypatcher.sh

---
+++ fragments/labels/thirdpartypatcher.sh
@@ -0,0 +1,8 @@
+thirdpartypatcher)
+    name="Third Party Patcher"
+    appName="Available Software.app"
+    type="pkg"
+    downloadURL="$(downloadURLFromGit gilburns Third-Party-Patcher)"
+    appNewVersion="$(versionFromGit gilburns Third-Party-Patcher)"
+    expectedTeamID="G4MQ57TVLE"
+    ;;
