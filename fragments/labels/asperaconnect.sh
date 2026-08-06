2adc9aa8a27da019117984e5095c24a38e4c0bc5
RIchard Glaser <richard.glaser@utah.edu>
2026-08-06 10:20:28 -0600

    Update asperaconnect.sh

--- fragments/labels/asperaconnect.sh
+++ fragments/labels/asperaconnect.sh
@@ -1,10 +1,9 @@
-asperaconnect)
-    name="Aspera Connect"
+asperaconnect|\
+ibmasperaconnect)
+    name="IBM Aspera Connect"
     type="pkg"
-    indexFilePath=$(curl -fsL https://ibmaspera.com/help/downloads/desktop | grep 'script type="module"' | grep -o "/.*.js")
-    downloadBaseUrl="https:$(curl -fsL https://ibmaspera.com${indexFilePath} | grep versions.js | grep -o "var i=\"//.*downloads/connect" | sed -E 's/var i="//g')"
-    appInfo=$(curl -fs ${downloadBaseUrl}/latest/versions.js | grep -o "{.*}")
-    appNewVersion=$(echo ${appInfo} | jq -r '.entries.[] | select(.title == "Aspera Connect for macOS") | .version' | awk -F "." '{print$1"."$2"."$3}')
-    downloadURL="${downloadBaseUrl}/latest/$(echo ${appInfo} | jq -r '.entries.[] | select(.title == "Aspera Connect for macOS").links.[] | select(.rel == "enclosure-one-click").href')"
+    appInfo=$(curl -fsL "https://d3gcli72yxqn2z.cloudfront.net/downloads/connect/latest/versions.js"|sed 's/^window.connectVersions = //')
+    appNewVersion=$(getJSONValue "$appInfo" "entries[1].version"|awk -F. '{print $1"."$2"."$3}')
+    downloadURL="https://d3gcli72yxqn2z.cloudfront.net/downloads/connect/latest/$(getJSONValue "$appInfo" "entries[1].links[1].href")"
     expectedTeamID="PETKK2G752"
     ;;
