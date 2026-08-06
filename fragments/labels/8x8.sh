a820f39a58c6c0a170296f095b77e614c323d8b6
fradamer-ops <fradamer@gmail.com>
2026-08-06 10:34:07 -0400

    Update 8x8.sh

--- fragments/labels/8x8.sh
+++ fragments/labels/8x8.sh
@@ -1,12 +1,16 @@
 8x8)
     name="8x8 Work"
     type="dmg"
+
+    pageContent=$(curl -fs -L 'https://help.8x8.com/docs/download-8x8-work-for-desktop' | sed 's/&quot;/"/g')
+
     if [[ $(arch) == "arm64" ]]; then
-        downloadURL=$(curl -fs -L 'https://support-portal.8x8.com/helpcenter/viewArticle.html?d=8bff4970-6fbf-4daf-842d-8ae9b533153d' | grep -o "https.*arm64-dmg-v[0-9.-]*.dmg" | head -n 1 | sed 's/\"//' | awk '{print $1}')
+        downloadURL=$(echo "$pageContent" | grep -o "https[^\" ]*work-arm64-dmg-v[0-9.-]*\.dmg" | head -n 1)
     else
-        downloadURL=$(curl -fs -L 'https://support-portal.8x8.com/helpcenter/viewArticle.html?d=8bff4970-6fbf-4daf-842d-8ae9b533153d' | grep -m 1 -o "https.*dmg" | sed 's/\"//' | awk '{print $1}')
+        downloadURL=$(echo "$pageContent" | grep -o "https[^\" ]*work-dmg-v[0-9.-]*\.dmg" | head -n 1)
     fi
-    # Check newer version than 7.2.4
-    appNewVersion=$(curl -fs -L 'https://support-portal.8x8.com/helpcenter/viewArticle.html?d=8bff4970-6fbf-4daf-842d-8ae9b533153d' | grep -m 1 -o "https.*dmg" | sed 's/\"//' | awk '{print $1}' | sed -E 's/.*-v([0-9\.]*)[-\.]*.*/\1/' )
+
+    appNewVersion=$(echo "$downloadURL" | sed -E 's/.*-v([0-9]+\.[0-9]+\.[0-9]+)-.*/\1/')
+
     expectedTeamID="FC967L3QRG"
     ;;
