76ab982f38f9b175ef6be9b9d9c07f621cf9cd34
Daniel.Ebeck <D.N.Ebeck@gmail.com>
2026-08-11 15:08:11 +0100

    working fragment

--- fragments/labels/timelymemory.sh
+++ fragments/labels/timelymemory.sh
@@ -3,6 +3,6 @@ timelymemory)
     type="zip"
     archiveName="memory.app.zip"
     downloadURL="https://memory.timelyapp.com/download/mac"
-    appNewVersion="$(curl -fsIL https://memory.timelyapp.com/download/mac | grep -i ^location | cut -d "/" -f6)"
+    appNewVersion="$(curl -fsIL ${downloadURL} | grep -i ^location | cut -d "/" -f6)"
     expectedTeamID="NGR7G7F269"
     ;;
