ea229fa20ffe6250199afd61d5f597bcfb2a2815
Daniel.Ebeck <D.N.Ebeck@gmail.com>
2026-08-11 13:59:54 +0100

    defined URLs

--- fragments/labels/timelymemory.sh
+++ fragments/labels/timelymemory.sh
@@ -2,6 +2,6 @@ timelymemory)
     name="Memory"
     type="zip"
     downloadURL="https://memory.timelyapp.com/download/mac/memory.app.zip"
-   # appNewVersion="$(curl -s https://memory.timelyapp.com/updates/memory-mac/stable/releases | grep -m1 -o '<h2>[^<]*</h2>' | sed 's/<h2>\(.*\)<\/h2>/\1/' | awk '{print $1}')"
+    appNewVersion="$(curl -fsIL https://memory.timelyapp.com/download/mac | grep -i ^location | cut -d "/" -f6)"
     expectedTeamID="NGR7G7F269"
     ;;
