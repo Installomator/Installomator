41e0391238cc4faaa0dac9e97d9322305044529f
Daniel.Ebeck <D.N.Ebeck@gmail.com>
2026-08-11 11:36:48 +0100

    basic label build

---
+++ fragments/labels/timelymemory.sh
@@ -0,0 +1,7 @@
+timelymemory)
+    name="Memory"
+    type="zip"
+    downloadURL="https://memory.timelyapp.com/download/mac/memory.app.zip"
+   # appNewVersion="$(curl -s https://memory.timelyapp.com/updates/memory-mac/stable/releases | grep -m1 -o '<h2>[^<]*</h2>' | sed 's/<h2>\(.*\)<\/h2>/\1/' | awk '{print $1}')"
+    expectedTeamID="NGR7G7F269"
+    ;;
