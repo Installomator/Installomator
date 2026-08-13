677439a355f9092f7ddce14aa5b9ea111612da8a
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-08-13 14:47:50 +0200

    Removed unnecessary "appname" variable

--- fragments/labels/diskdrill.sh
+++ fragments/labels/diskdrill.sh
@@ -1,7 +1,6 @@
 diskdrill)
     name="Disk Drill"
     type="dmg"
-    appname="Disk Drill.app"
     downloadURL="https://dl.cleverfiles.com/diskdrill.dmg"
     appNewVersion=$( curl -fsL "https://www.cleverfiles.com/releases/auto-update/dd5-newestr.xml" | xpath 'string(//rss/channel/item/enclosure/@sparkle:version)' 2>/dev/null)
     versionKey="CFBundleVersion"
