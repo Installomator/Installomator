33f5ac01b54d34d8eafcf23f2afbb472966d357e
schwartzb-wra <schwartzb@wra.net>
2026-08-14 12:39:10 -0400

    Add label for AudioPen

---
+++ fragments/labels/audiopen.sh
@@ -0,0 +1,7 @@
+audiopen)
+    name="AudioPen"
+    type="dmg"
+    downloadURL=$(downloadURLFromGit louispereira23 AudioPen-Releases)
+    appNewVersion=$(versionFromGit louispereira23 AudioPen-Releases)
+    expectedTeamID="WBFQ68C53U"
+    ;;
