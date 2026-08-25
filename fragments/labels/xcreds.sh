97eea5c4f62df5d392b96db88189e46d84434bbe
RIchard Glaser <richard.glaser@utah.edu>
2026-08-25 13:22:26 -0600

    Add `version_info` feed to label

--- fragments/labels/xcreds.sh
+++ fragments/labels/xcreds.sh
@@ -2,8 +2,8 @@ xcreds)
     name="XCreds"
     type="pkg"
     packageID="com.twocanoes.pkg.secureremoteaccess"
-    downloadURL=$(curl -fsL "https://twocanoes.com/products/mac/xcreds/history/" | awk '/<h3>Change Log<\/h3>/{found=1; next} found && /https:\/\/twocanoes-software-updates\.s3\.amazonaws\.com\/XCreds_Build-[0-9]+_Version-[0-9.]+\.pkg/{print; exit}' | sed -E 's|.*href="([^"]+)".*|\1|')
-    appNewVersion=$(echo "$downloadURL" | sed -E 's|.*_Version-([0-9.]+)\.pkg|\1|')
+    downloadURL="https://twocanoes-software-updates.s3.amazonaws.com/XCreds.pkg"
+    appNewVersion=$(getJSONValue "$(curl -fsL "https://data.twocanoes.com/api/version_info")" '["com.twocanoes.xcreds"].version')
     expectedTeamID="UXP6YEHSPW"
     blockingProcesses=( NONE )
     ;;
