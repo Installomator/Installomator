745c315bbc335913b64a60f23cff30562388eb2f
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-08-25 11:30:23 -0600

    dbvisualizer-update-label (#3061)
    * dbvisualizer-update-label
    
    * More concise label version

--- fragments/labels/dbvisualizer.sh
+++ fragments/labels/dbvisualizer.sh
@@ -1,9 +1,13 @@
 dbvisualizer)
     name="DbVisualizer"
     type="dmg"
-    appNewVersion=$(curl -fsL "https://www.dbvis.com/download/" | sed -n 's/.*Latest version \([0-9.]*\).*/\1/p' | head -n1)
-    versionUnderscore="${appNewVersion//./_}"
-    downloadURL="https://www.dbvis.com/product_download/dbvis-${appNewVersion}/media/dbvis_macos-aarch64_${versionUnderscore}.dmg"
+    dbvisVersion=$(curl -fsL "https://www.dbvis.com/download/" | grep -oE 'Latest version [0-9]+(\.[0-9]+)+' | head -n 1 | awk '{print $3}')
+    appNewVersion=$(echo "$dbvisVersion" | awk -F. '{for (i = NF+1; i <= 3; i++) $i = 0; print $1"."$2"."$3}')
+    downloadVersion="${dbvisVersion//./_}"
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL="https://www.dbvis.com/product_download/dbvis-${dbvisVersion}/media/dbvis_macos-aarch64_${downloadVersion}.dmg"
+    else
+        downloadURL="https://www.dbvis.com/product_download/dbvis-${dbvisVersion}/media/dbvis_macos-x64_${downloadVersion}.dmg"
+    fi
     expectedTeamID="U9TP5KYV49"
-    blockingProcesses=( "DbVisualizer" )
     ;;
