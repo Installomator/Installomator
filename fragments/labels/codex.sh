06fd53c2d254427ba323b8ea9190a00457fecea8
Gil Burns <gil@gilburns.com>
2026-08-25 20:44:10 -0500

    Rename Codex to ChatGPT in codex.sh

--- fragments/labels/codex.sh
+++ fragments/labels/codex.sh
@@ -1,16 +1,14 @@
-codex)
-    name="Codex"
+chatgpt|codex)
+    name="ChatGPT"
     type="zip"
-    appName="ChatGPT.app"
-    targetDir="/Applications/ChatGPT.localized"
     if [[ $(arch) == "arm64" ]]; then
         sparkleData=$(curl -fsL "https://persistent.oaistatic.com/codex-app-prod/appcast.xml")
-        appNewVersion=$(echo "$sparkleData" | awk -F "[<>]" '/<sparkle:shortVersionString>/{print $3; exit}')
-        downloadURL=$(echo "$sparkleData" | awk -F 'url="' '/<enclosure url=/{split($2,a,"\""); print a[1]; exit}')
+        appNewVersion=$(echo "$sparkleData" | xpath 'string(//rss/channel/item[1]/sparkle:shortVersionString)')
+        downloadURL=$(echo "$sparkleData" | xpath 'string(//rss/channel/item[1]/enclosure/@url)')
     else
-        printlog "Codex is only compatible with Apple Silicon (arm64) Macs." ERROR
-        cleanupAndExit 95 "Codex requires Apple Silicon" ERROR
+        printlog "ChatGPT is only compatible with Apple Silicon (arm64) Macs." ERROR
+        cleanupAndExit 95 "ChatGPT requires Apple Silicon" ERROR
     fi
-    expectedTeamID="2DC432GLL2"
     blockingProcesses=( "ChatGPT" )
+    expectedTeamID="2DC432GLL2"
     ;;
