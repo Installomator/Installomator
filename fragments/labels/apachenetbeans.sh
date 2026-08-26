f14799408b596ed5afd1ed39027378501707ec1d
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-08-26 04:30:49 -0600

    Update apachenetbeans.sh (#2996)
    * Update apachenetbeans.sh
    
    * Fix indentation in apachenetbeans.sh

--- fragments/labels/apachenetbeans.sh
+++ fragments/labels/apachenetbeans.sh
@@ -1,12 +1,12 @@
 apachenetbeans)
     name="Apache NetBeans"
     type="pkg"
-    if [[ $(arch) = "arm64" ]]; then
-        archiveName="Apache-NetBeans-[0-9]*-arm64.pkg"
+    if [[ "$arch" == "arm64" ]]; then
+        archiveName="arm64.pkg"
     else
-        archiveName="Apache-NetBeans-[0-9]*-x86_64.pkg"
+        archiveName="x86_64.pkg"
     fi
-    downloadURL="$(downloadURLFromGit Friends-of-Apache-NetBeans netbeans-installers)"
-    appNewVersion=$(curl -sLI "https://github.com/Friends-of-Apache-NetBeans/netbeans-installers/releases/latest" | grep -i "^location" | tr "/" "\n" | tail -1 | sed -E 's/v([0-9]+).*/\1/')
+    downloadURL=$(downloadURLFromGit Friends-of-Apache-NetBeans netbeans-installers)
+    appNewVersion=$(versionFromGit Friends-of-Apache-NetBeans netbeans-installers | sed -E 's/^v?([0-9]+).*/\1/')
     expectedTeamID="44YNN9Q525"
     ;;
