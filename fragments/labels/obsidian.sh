b679e861d060ab588ac7debf6ec28d42dda89a98
Adam Martin <adam.martin@smartsheet.com>
2026-08-26 12:30:16 -0700

    Update obsidian.sh
    Obsidian recently changed their releases to be mobile only (as of 08-20-26) which threw off how the current label is looking at the latest version available within the releases URL in GitHub since the latest only has a .apk file and not a .dmg. The added logic here fixes this

--- fragments/labels/obsidian.sh
+++ fragments/labels/obsidian.sh
@@ -2,7 +2,7 @@ obsidian)
     # credit: Søren Theilgaard (@theilgaard)
     name="Obsidian"
     type="dmg"
-    downloadURL=$( downloadURLFromGit obsidianmd obsidian-releases )
-    appNewVersion=$(versionFromGit obsidianmd obsidian-releases)
+    downloadURL=$(curl -sfL --connect-timeout 10 --max-time 30 "https://api.github.com/repos/obsidianmd/obsidian-releases/releases?per_page=10" | awk -F '"' '/browser_download_url/ && /\.dmg"/ { print $4; exit }')
+    appNewVersion=$(echo "$downloadURL" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | head -1)
     expectedTeamID="6JSW4SJWN9"
     ;;
