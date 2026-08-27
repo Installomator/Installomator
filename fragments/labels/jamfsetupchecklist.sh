74dc6c00bc0d03641e672bb3f28792222c0720b0
Mischa van der Bent <mvdbent@gmail.com>
2026-08-27 15:03:44 +0200

    Add Jamf Setup Checklist label
    Adds a new Installomator label for Jamf Setup Checklist. It downloads the package from the jamf-concepts/setup-checklist GitHub repository, detects the app version from the repo, and sets the expected Team ID for installation.

---
+++ fragments/labels/jamfsetupchecklist.sh
@@ -0,0 +1,7 @@
+jamfsetupchecklist)
+    name="Setup Setup Checklist"
+    type="pkg"
+    downloadURL=$(downloadURLFromGit jamf-concepts setup-checklist)
+    appNewVersion=$(versionFromGit jamf-concepts setup-checklist)
+    expectedTeamID="483DWKW443"
+    ;;
