47f3b2c7f67459416b248bb986b29178be75a0e2
Adam Martin <adam.martin@smartsheet.com>
2026-08-27 10:03:13 -0700

    Revised update for obsidian.sh
    Revised latest update for Obsidian to be cleaner

--- fragments/labels/obsidian.sh
+++ fragments/labels/obsidian.sh
@@ -1,7 +1,8 @@
 obsidian)
-name="Obsidian"
+    name="Obsidian"
     type="dmg"
-    appNewVersion=$(curl -sfL --connect-timeout 10 --max-time 30 "https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/desktop-releases.json" | awk -F'"' '/"beta"/{exit} /"latestVersion"/{print $4; exit}')
+    obsidianData=$(curl -fsL "https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/desktop-releases.json")
+    appNewVersion=$(getJSONValue "$obsidianData" "latestVersion")
     downloadURL="https://github.com/obsidianmd/obsidian-releases/releases/download/v${appNewVersion}/Obsidian-${appNewVersion}.dmg"
     expectedTeamID="6JSW4SJWN9"
     ;;
