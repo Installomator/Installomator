75b83b1b7bc81062d152fe074133cc9290b79039
northalpha <northalpha@c3pb.de>
2026-08-28 13:42:01 +0200

    Create jamfsetupchecklist.sh (#3141)

---
+++ fragments/labels/jamfsetupchecklist.sh
@@ -0,0 +1,8 @@
+jamfsetupchecklist)
+    name="JAMF Setup Checklist"
+    type="pkg"
+    downloadURL=$(downloadURLFromGit Jamf-Concepts Setup-Checklist)
+    appNewVersion=$(versionFromGit Jamf-Concepts Setup-Checklist)
+    expectedTeamID="483DWKW443"
+    ;;
+  
