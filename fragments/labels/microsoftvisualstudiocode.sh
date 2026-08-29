89a58f9171b8f294980f84767624c63fc52b1aed
RIchard Glaser <richard.glaser@utah.edu>
2026-08-29 15:43:39 -0600

    Update microsoftvisualstudiocode.sh

--- fragments/labels/microsoftvisualstudiocode.sh
+++ fragments/labels/microsoftvisualstudiocode.sh
@@ -2,9 +2,8 @@ microsoftvisualstudiocode|\
 visualstudiocode)
     name="Visual Studio Code"
     type="zip"
-    appNewVersion=$(curl -fsL "https://update.code.visualstudio.com/api/releases/stable" | tr ',' '\n' | head -1 | tr -d '["')
+    appNewVersion=$(curl -fsL "https://update.code.visualstudio.com/api/releases/stable" | plutil -extract 0 raw -o - -)
     downloadURL="https://update.code.visualstudio.com/latest/darwin-universal/stable"
     expectedTeamID="UBF8T346G9"
-    appName="Visual Studio Code.app"
     blockingProcesses=( Code )
     ;;
