22ceb3a19a5b25026da965c05923edc05a77ba5e
RIchard Glaser <richard.glaser@utah.edu>
2026-08-29 14:03:04 -0600

    Create starface91x.sh

---
+++ fragments/labels/starface91x.sh
@@ -0,0 +1,9 @@
+starface91x)
+    name="STARFACE"
+    type="dmg"
+    sparkleData=$(curl -fsL "https://www.starface-cdn.de/starface/clients/mac/appcast.xml")
+    downloadURL=$(echo "$sparkleData" | xmllint --xpath 'string((//*[local-name()="enclosure" and starts-with(@*[local-name()="shortVersionString"], "9.1.")])[1]/@url)' -)
+    appNewVersion=$(echo "$sparkleData" | xmllint --xpath 'string((//*[local-name()="enclosure" and starts-with(@*[local-name()="shortVersionString"], "9.1.")])[1]/@*[local-name()="version"])' -)
+    expectedTeamID="Q965D3UXEW"
+    versionKey="CFBundleVersion"
+    ;;
