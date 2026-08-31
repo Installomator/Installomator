05e1eac35072cd402a87387cb89f097a90c7d580
RIchard Glaser <richard.glaser@utah.edu>
2026-08-31 17:04:37 -0600

    Update tableaupublic.sh

--- fragments/labels/tableaupublic.sh
+++ fragments/labels/tableaupublic.sh
@@ -1,8 +1,7 @@
 tableaupublic)
     name="Tableau Public"
     type="pkgInDmg"
-    packageID="com.tableausoftware.tableaudesktop"
-    downloadURL=$(curl -fs https://www.tableau.com/downloads/public/mac | awk '/TableauPublic/' | xmllint --recover --html --xpath "//a/text()" -)
-    appNewVersion=$( echo $downloadURL | sed -E 's/.*TableauPublic-([-0-9]*)\.dmg/\1/g' | tr "-" "." )
+    downloadURL=$(curl -fsIL -o /dev/null -w "%{url_effective}" "https://www.tableau.com/downloads/public/mac")
+    appNewVersion=$(echo "$downloadURL" | sed -E 's#.*TableauPublic-([0-9]+)-([0-9]+)-([0-9]+)\.dmg#\1.\2.\3#')
     expectedTeamID="QJ4XPRK37C"
     ;;
