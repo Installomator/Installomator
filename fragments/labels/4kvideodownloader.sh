8b1a061cafa07671774b7728e750b2e3c47a429c
RIchard Glaser <richard.glaser@utah.edu>
2026-08-31 12:00:30 -0600

    Update 4kvideodownloader.sh

--- fragments/labels/4kvideodownloader.sh
+++ fragments/labels/4kvideodownloader.sh
@@ -1,8 +1,9 @@
 4kvideodownloader)
     name="4K Video Downloader"
     type="dmg"
-    downloadURL="$(curl -fsL "https://www.4kdownload.com/products/product-videodownloader" | grep -E -o "https:\/\/dl\.4kdownload\.com\/app\/4kvideodownloader_.*?.dmg\?source=website" | head -1)"
-    appNewVersion=$(echo "${downloadURL}" | sed -E 's/.*\/[0-9a-zA-Z]*_([0-9.]*)\.dmg.*/\1/g')
-	versionKey="CFBundleVersion"
+    sparkleData=$(curl -fsL "https://dl.4kdownload.com/app/appcast/videodownloader.xml")
+    appNewVersion=$(xmllint --xpath 'normalize-space((//*[local-name()="enclosure" and @*[local-name()="os"]="mac-64"])[1]/@*[local-name()="version"])' - <<< "$sparkleData" | cut -d "." -f1-3)
+    downloadURL="https://dl.4kdownload.com/app/4kvideodownloader_${appNewVersion}_x64.dmg?source=website"
+    versionKey="CFBundleVersion"
     expectedTeamID="GHQ37VJF83"
     ;;
