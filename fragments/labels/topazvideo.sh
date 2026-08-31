4bea322f3ae1a978b074d08700c5806455d9a1cf
Richard Glaser <6226458+uurazzle@users.noreply.github.com>
2026-08-31 08:42:36 -0600

    Fix missing newline at end of topazvideo.sh
    Added a newline at the end of the topazvideo.sh file.

--- fragments/labels/topazvideo.sh
+++ fragments/labels/topazvideo.sh
@@ -6,3 +6,4 @@ topazvideoai)
     appNewVersion=$(curl -fsIL "$downloadURL" | awk 'BEGIN{IGNORECASE=1}/^location:/{gsub("\r",""); print $2}' | tail -n 1 | sed -E 's#.*/TopazVideoAI-([0-9]+(\.[0-9]+)+)\.dmg$#\1#')
     expectedTeamID="3G3JE37ZHF"
     ;;
+    
