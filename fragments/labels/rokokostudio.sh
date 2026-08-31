72e1222d4f87da3210043d223bbfb801b0a71b8b
RIchard Glaser <richard.glaser@utah.edu>
2026-08-31 12:36:13 -0600

    Create rokokostudio.sh

---
+++ fragments/labels/rokokostudio.sh
@@ -0,0 +1,7 @@
+rokokostudio)
+    name="Rokoko Studio"
+    type="dmg"
+    downloadURL="https://cdn.rokoko.com/software/studio2-autoupdater/RokokoStudio.dmg"
+    blockingProcesses=( "Studio2Updater" )
+    expectedTeamID="5K4RZM8SUS"
+    ;;
