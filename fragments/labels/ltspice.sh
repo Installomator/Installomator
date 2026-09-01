f0f02d79631dbed5253ac60f90808856b1f23ca2
RIchard Glaser <richard.glaser@utah.edu>
2026-09-01 16:44:02 -0600

    Update ltspice.sh

--- fragments/labels/ltspice.sh
+++ fragments/labels/ltspice.sh
@@ -1,8 +1,8 @@
 ltspice)
     name="LTspice"
     type="pkg"
-    packageID="com.analog.ltspice"
     downloadURL="https://ltspice.analog.com/software/LTspice_26.pkg"
-    appNewVersion=$(curl -fsL "https://formulae.brew.sh/api/cask/ltspice.json" | sed -nE 's/.*"version":"([^"]+)".*/\1/p' | head -n 1)
+    appNewVersion=$(curl -fsL "https://formulae.brew.sh/api/cask/ltspice.json" | sed -nE 's/.*"version":"([^"]+)".*/\1.1/p' | head -n 1)
+    versionKey="CFBundleVersion"
     expectedTeamID="6ZM4J3A422"
     ;;
