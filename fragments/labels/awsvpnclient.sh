0922ec71c4be586facd90a4307d4e47a98bd7411
Mark Robertson <mark.robertson@addepar.com>
2026-09-02 15:13:57 +0100

    Update awsvpnclient.sh
    Updated downloadURL to use "latest" tag instead of the calculated version whilst still utilising the RSS feed for getting the matching version. If distribution endpoint stays consistent for newer versions of the AWS VPN client, then versioning could be re-added and baseURL changed.

--- fragments/labels/awsvpnclient.sh
+++ fragments/labels/awsvpnclient.sh
@@ -5,9 +5,9 @@ awsvpnclient)
     appNewVersion=$(curl -s "https://docs.aws.amazon.com/vpn/latest/clientvpn-user/client-vpn-user-guide.rss" | grep -o 'AWS provided client ([0-9]*\.[0-9]*\.[0-9]*) for macOS' | head -1 | grep -o '[0-9]*\.[0-9]*\.[0-9]*')
     if [[ $(arch) == "arm64" ]]
     then
-        downloadURL="${baseURL}_ARM64/${appNewVersion}/AWS_VPN_Client_ARM64.pkg"
+        downloadURL="${baseURL}_ARM64/latest/AWS_VPN_Client_ARM64.pkg"
     else
-        downloadURL="${baseURL}/${appNewVersion}/AWS_VPN_Client.pkg"
+        downloadURL="${baseURL}/latest/AWS_VPN_Client.pkg"
     fi
     expectedTeamID="94KV3E626L"
     ;;
