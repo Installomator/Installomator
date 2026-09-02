584909afe6663c179c2961a206e8d3bcdb2b5756
RIchard Glaser <richard.glaser@utah.edu>
2026-09-02 14:16:56 -0600

    anaconda-label-update

--- fragments/labels/anaconda.sh
+++ fragments/labels/anaconda.sh
@@ -1,22 +1,15 @@
 anaconda)
     name="Anaconda-Navigator"
-    packageID="com.anaconda.io"
     type="pkg"
     if [[ "$(arch)" == "arm64" ]]; then
-        archiveName=$( curl -sf https://repo.anaconda.com/archive/ | awk '/href=".*Anaconda.*MacOSX.*arm64.*\.pkg"/{gsub(/.*href="|".*/, ""); gsub(/.*\//, ""); print; exit}' )
+        archiveArch="arm64"
+        packageID="com.anaconda.pkg.prepare_installation"
     else
-        archiveName=$( curl -sf https://repo.anaconda.com/archive/ | awk '/href=".*Anaconda.*MacOSX.*x86_64.*\.pkg"/{gsub(/.*href="|".*/, ""); gsub(/.*\//, ""); print; exit}' )
+        archiveArch="x86_64"
+        packageID="io.continuum.pkg.prepare_installation"
     fi
+    archiveName=$(curl -fsL "https://repo.anaconda.com/archive/" | awk -v archiveArch="$archiveArch" '/href="Anaconda3-.*MacOSX-.*[.]pkg"/ && $0 ~ "MacOSX-" archiveArch "[.]pkg" {gsub(/.*href="|".*/, ""); print; exit}')
+    appNewVersion=$(awk -F'-' '{print $2"-"$3}' <<< "$archiveName")
     downloadURL="https://repo.anaconda.com/archive/$archiveName"
-    appNewVersion=$( awk -F'-' '{print $2}' <<< "$archiveName" )
     expectedTeamID="Z5788K4JT7"
-    blockingProcesses=( "Anaconda-Navigator.app" )
-    appCustomVersion() {
-        if [ -e "/Users/$currentUser/opt/anaconda3/bin/conda" ]; then
-            "/Users/$currentUser/opt/anaconda3/bin/conda" list -f ^anaconda$ | awk '/anaconda /{print $2}'
-        fi
-    }
-    updateTool="/Users/$currentUser/opt/anaconda3/bin/conda"
-    updateToolArguments=( install -y anaconda=$appNewVersion )
-    updateToolRunAsCurrentUser=1
     ;;
