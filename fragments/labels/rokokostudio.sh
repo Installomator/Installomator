255a5709d848f015e7aa1688642fb00e37339cac
RIchard Glaser <richard.glaser@utah.edu>
2026-09-09 16:25:13 -0600

    Update rokokostudio.sh

--- fragments/labels/rokokostudio.sh
+++ fragments/labels/rokokostudio.sh
@@ -1,9 +1,7 @@
 rokokostudio)
-    name="RokokoStudio"
+    name="Rokoko Studio"
     type="pkg"
     downloadURL="https://downloads.rokoko.com/studio-mac"
-    appNewVersion=$(curl -fsL -o /dev/null -w "%{url_effective}" "$downloadURL" | grep -oE '[0-9]+(\.[0-9]+)+')
-    versionKey="CFBundleVersion"
+    appNewVersion=$(curl -fsL -r 0-0 -o /dev/null -w "%{url_effective}" "$downloadURL" | grep -oE '[0-9]+(\.[0-9]+)+')
     expectedTeamID="5K4RZM8SUS"
     ;;
-    
