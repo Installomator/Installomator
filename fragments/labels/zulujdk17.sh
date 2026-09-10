736cd518fbca1e283cd8ed170b9eacec24936057
RIchard Glaser <richard.glaser@utah.edu>
2026-09-10 09:06:50 -0600

    zulujdk17-label-update

--- fragments/labels/zulujdk17.sh
+++ fragments/labels/zulujdk17.sh
@@ -2,13 +2,12 @@ zulujdk17)
     name="Zulu JDK 17"
     type="pkgInDmg"
     packageID="com.azulsystems.zulu.17"
-    curlOptions=( -H "Referer: https://www.azul.com/downloads/" )
-    if [[ "$arch" == "arm64" ]]; then
-        downloadURL=$(curl -fsL "https://api.azul.com/metadata/v1/zulu/packages/?java_version=17&os=macos&arch=arm&java_package_type=jdk&archive_type=dmg&javafx_bundled=false&release_status=ga&availability_types=CA&latest=true" | grep -oE '"download_url":"[^"]*"' | head -1 | sed 's/"download_url":"//' | tr -d '"')
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL=$(curl -fsL "https://api.azul.com/metadata/v1/zulu/packages/?java_version=17&os=macos&arch=arm&archive_type=dmg&java_package_type=jdk&javafx_bundled=false&release_status=ga&availability_type=CA&page_size=1" | plutil -extract 0.download_url raw -)
     else
-        downloadURL=$(curl -fsL "https://api.azul.com/metadata/v1/zulu/packages/?java_version=17&os=macos&arch=x64&java_package_type=jdk&archive_type=dmg&javafx_bundled=false&release_status=ga&availability_types=CA&latest=true" | grep -oE '"download_url":"[^"]*"' | head -1 | sed 's/"download_url":"//' | tr -d '"')
+        downloadURL=$(curl -fsL "https://api.azul.com/metadata/v1/zulu/packages/?java_version=17&os=macos&arch=x86&archive_type=dmg&java_package_type=jdk&javafx_bundled=false&release_status=ga&availability_type=CA&page_size=1" | plutil -extract 0.download_url raw -)
     fi
-    appNewVersion=$(echo "$downloadURL" | grep -oE 'jdk17\.[0-9]+\.[0-9]+' | sed 's/jdk//')
-    appCustomVersion() { [ -f "/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Info.plist" ] && /usr/bin/defaults read "/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Info.plist" "CFBundleName" | sed 's/Zulu //'; }
+    appNewVersion=$(echo "$downloadURL" | sed -E 's|.*/zulu([0-9]+\.[0-9]+)\.([0-9]+)-ca-jdk17.*|\1+\2|')
     expectedTeamID="TDTHCUPYFR"
+    blockingProcesses=( NONE )
     ;;
