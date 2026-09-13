795ba63a6026cd94d8b2533979e1019a53ef3206
macmangr <106015597+macmangr@users.noreply.github.com>
2026-09-13 12:56:28 +0200

    Update synologyactiveprotectagent.sh

--- fragments/labels/synologyactiveprotectagent.sh
+++ fragments/labels/synologyactiveprotectagent.sh
@@ -1,12 +1,10 @@
 synologyactiveprotectagent)
-    name="Synology ActiveProtect Agent"
+    name="ActiveProtect Agent"
     type="pkg"
-    packageID="com.synology.activeprotect.pkg"
+    releaseURL="https://archive.synology.com/download/Utility/ActiveProtectAgent"
+    releaseVersion=$(curl -fsL "$releaseURL/" | xmllint --html --xpath 'string((//a[contains(@href, "/download/Utility/ActiveProtectAgent/")])[1])' - 2>/dev/null)
+    appNewVersion="${releaseVersion##*-}"
+    downloadURL=$(curl -fsL "$releaseURL/$releaseVersion" | xmllint --html --xpath 'string((//a[contains(@href, "/Mac/") and substring(@href, string-length(@href) - 3) = ".pkg"])[1]/@href)' - 2>/dev/null)
     versionKey="CFBundleVersion"
-    appCustomVersion(){ pkgutil --pkg-info-plist com.synology.activeprotect.pkg 2>/dev/null | grep -A1 pkg-version | tail -1 | sed -E 's/.*<string>.*-([0-9]+)<\/string>.*/\1/'; }
-    downloadURL=$(appVersion=`curl -sf https://archive.synology.com/download/Utility/ActiveProtectAgent | grep -m 1 /download/Utility/ActiveProtectAgent/ | sed "s|.*>\(.*\)<.*|\\1|"` && appShortVersion=`sed 's#.*-\(\)#\1#' <<< $appVersion` && echo https://global.download.synology.com/download/Utility/ActiveProtectAgent/"$appVersion"/Mac/x86_64/Synology%20ActiveProtect%20Agent-"$appVersion".pkg)
-    # downloadURL=$(appVersion=`curl -sf https://archive.synology.com/download/Utility/ActiveProtectAgent | grep -m 1 /download/Utility/ActiveProtectAgent/ | sed "s|.*>\(.*\)<.*|\\1|"` && appShortVersion=`sed 's#.*-\(\)#\1#' <<< $appVersion` && echo https://global.synologydownload.com/download/Utility/ActiveProtectAgent/"$appVersion"/Mac/x86_64/Synology%20ActiveProtect%20Agent-"$appVersion".pkg)
-    appNewVersion=$(curl -sf https://archive.synology.com/download/Utility/ActiveProtectAgent | grep -m 1 /download/Utility/ActiveProtectAgent/ | sed "s|.*>\(.*\)<.*|\\1|" | sed "s#.*-\(\)#\1#")
     expectedTeamID="X85BAK35Y4"
-    blockingProcesses=( ActiveProtect Agent Service )
     ;;
