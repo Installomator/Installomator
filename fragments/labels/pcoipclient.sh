a4fd1def6ffdda33bbcd468c217eda3114253375
RIchard Glaser <richard.glaser@utah.edu>
2026-09-14 09:47:52 -0600

    Update pcoipclient.sh

--- fragments/labels/pcoipclient.sh
+++ fragments/labels/pcoipclient.sh
@@ -1,6 +1,9 @@
 pcoipclient)
     name="PCoIPClient"
     type="dmg"
-    downloadURL="https://dl.anyware.hp.com/DeAdBCiUYInHcSTy/pcoip-client/raw/names/pcoip-client-dmg/versions/latest/pcoip-client_latest.dmg"
+    pcoipClientInfo=$(curl -fs "https://dl.anyware.hp.com/ztqM7i47Dt06ETYM/pcoip-client/raw/names/pcoip-client-info/versions/dmg/pcoip-client-dmg-info.json")
+    appNewVersion=$(getJSONValue "$pcoipClientInfo" "[0].currentVersion")
+    downloadURL="https://dl.anyware.hp.com/pcoip-client/raw/names/pcoip-client-dmg/versions/${appNewVersion}/pcoip-client_${appNewVersion}.dmg"
     expectedTeamID="RU4LW7W32C"
+    blockingProcesses=( "Teradici PCoIP Client" )
     ;;
