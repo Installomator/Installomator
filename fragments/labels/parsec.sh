e740da1b5ead73fe788dc425f9b5760750152872
RIchard Glaser <richard.glaser@utah.edu>
2026-09-14 09:55:44 -0600

    Update parsec.sh

--- fragments/labels/parsec.sh
+++ fragments/labels/parsec.sh
@@ -2,6 +2,7 @@ parsec)
     name="Parsec"
     type="pkg"
     downloadURL="https://builds.parsec.app/package/parsec-macos.pkg"
+    appNewVersion=$(parsecTmpDir=$(mktemp -d); trap 'rm -rf "$parsecTmpDir"' EXIT; curl -fsL "$downloadURL" -o "$parsecTmpDir/parsec.pkg" && xar -xf "$parsecTmpDir/parsec.pkg" -C "$parsecTmpDir" PackageInfo && sed -nE 's/.*CFBundleShortVersionString="([^"]+)".*/\1/p' "$parsecTmpDir/PackageInfo" | head -1)
     expectedTeamID="Y9MY52XZDB"
     blockingProcesses=( NONE )
     ;;
