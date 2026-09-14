79d6ec304c4ed2f16df62d3c40d72c948ec69fe9
RIchard Glaser <richard.glaser@utah.edu>
2026-09-14 17:04:44 -0600

    Update microsoft365copilot.sh

--- fragments/labels/microsoft365copilot.sh
+++ fragments/labels/microsoft365copilot.sh
@@ -2,7 +2,7 @@ microsoft365copilot)
     name="Microsoft 365 Copilot"
     type="pkg"
     downloadURL="https://go.microsoft.com/fwlink/?linkid=2325438"
-    appNewVersion=$(curl -fsLw "%{url_effective}" -o /dev/null "$downloadURL" | awk -F '_' '{print $(NF-1)}')
+    appNewVersion=$(curl -fsILw "%{url_effective}" -o /dev/null "$downloadURL" | awk -F '_' '{print $(NF-1)}')
     versionKey="CFBundleVersion"
     expectedTeamID="UBF8T346G9"
     updateTool="/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate"
