3e6df7906cfe3fc54040e52ddc3ac6998ad732cc
RIchard Glaser <richard.glaser@utah.edu>
2026-09-14 09:24:31 -0600

    boxtools-label-update

--- fragments/labels/swiftpkgcombined.sh
+++ fragments/labels/swiftpkgcombined.sh
@@ -1,7 +1,6 @@
 swiftpkgcombined)
     name="Swiftpkgr"
     type="pkg"
-    packageID="com.codecarton.swiftpkg.installer"
     archiveName="swiftpkg-[0-9.]*-combined.pkg"
     downloadURL=$(downloadURLFromGit codecarton swiftpkg)
     appNewVersion=$(versionFromGit codecarton swiftpkg)
