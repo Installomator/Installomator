8a315557dd332f3e2e877a43894883e2f31e17bb
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-09-16 13:38:51 +0200

    Removed backslash and changed jq to getJSON

--- fragments/labels/davinciresolvestudio.sh
+++ fragments/labels/davinciresolvestudio.sh
@@ -3,10 +3,10 @@ blackmagicdavinciresolvestudio)
     name="DaVinci Resolve Studio"
     appName="DaVinci Resolve/DaVinci Resolve.app"
     type="pkgInDmgInZip"
-    downloadID=$(curl -fs https://www.blackmagicdesign.com/api/support/us/downloads.json | jq -r '[.downloads.[]|select(.name | startswith ("DaVinci Resolve Studio"))][0].urls|."Mac OS X".[].downloadId')
-    downloadURL=$(curl --compressed --location --header "Content-Type: application/json;charset=UTF-8" --header "User-Agent: Mozilla/5.0" --data '{"country": "us", "platform": "Mac OS X", "product": "DaVinci Resolve Studio"}' \
-        "https://www.blackmagicdesign.com/api/register/us/download/${downloadID}")
-    appNewVersion=$(curl -fs https://www.blackmagicdesign.com/api/support/us/downloads.json | jq -r '[.downloads.[]|select(.name | startswith ("DaVinci Resolve Studio"))][0].urls|."Mac OS X".[]|"\(.major).\(.minor).\(.releaseNum)"')
+    versionFeed=$(curl -fs https://www.blackmagicdesign.com/api/support/us/downloads.json)
+    downloadID=$(getJSONValue "${versionFeed}" ".downloads.find(item => item.name.includes('DaVinci Resolve Studio')).urls['Mac OS X'][0].downloadId")
+    downloadURL=$(curl --compressed --location --header "Content-Type: application/json;charset=UTF-8" --header "User-Agent: Mozilla/5.0" --data '{"country": "us", "platform": "Mac OS X", "product": "DaVinci Resolve Studio"}' "https://www.blackmagicdesign.com/api/register/us/download/${downloadID}")
+    appNewVersion=$(getJSONValue "${versionFeed}" ".downloads.find(item => item.name.includes('DaVinci Resolve Studio')).urls['Mac OS X'].map(version => [version.major, version.minor, version.releaseNum].join('.'))[0]")
     blockingProcesses=( Resolve "DaVinci Control Panels Setup" "DaVinci Remote Monitor" "Fairlight Studio Utility" )
     expectedTeamID="9ZGFBWLSYP"
     ;;
