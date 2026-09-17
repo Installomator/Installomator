471b2ce28d1df6102eb80ee146966e250765fad4
Henk <hbokhoven+github@gmail.com>
2026-09-17 10:51:21 +0200

    pique-label-update

--- fragments/labels/pique.sh
+++ fragments/labels/pique.sh
@@ -3,6 +3,6 @@ pique)
     type="pkg"
     packageID="io.macadmins.Pique"
     downloadURL=$(downloadURLFromGit macadmins pique )
-    appNewVersion=$(versionFromGit macadmins pique )
+    appNewVersion=$(printf '%s\n' "$downloadURL" | sed -E 's#.*\/releases\/download\/([^/]+)\/.*#\1#; s/^v//')
     expectedTeamID="T4SK8ZXCXG"
     ;;
