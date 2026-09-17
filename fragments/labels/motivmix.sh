8e15dfde41e4a5e5bafd49db97bced0b814c62e7
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-09-17 16:07:49 +0200

    Renamed label and switched to version based on installer

--- fragments/labels/motivmix.sh
+++ fragments/labels/motivmix.sh
@@ -1,9 +1,10 @@
-motiv_mix)
+motivmix)
     name="MOTIV Mix"
     type="zip"
     downloadURL=$(curl -fsLI "https://www.shure.com/en-US/sw/motiv-mix-mac" | grep -i ^location | sed -E 's/.*(https.*\.zip).*/\1/g')
     archiveName=$(basename ${downloadURL})
-    appNewVersion="${${${archiveName#ShureMOTIVMixInstaller_mac.}%.*}%.*}-release.${${archiveName%.zip}##*.}"
+    appNewVersion="$(grep -oE "[0-9].*[0-9]" <<< ${archiveName})"
+    appCustomVersion(){ echo "$(defaults read /Applications/Shure/MOTIV\ Mix/MOTIV\ Mix.app/Contents/Info.plist CFBundleShortVersionString | sed 's/-release//')" }
     installerTool="$(echo ${archiveName} | sed 's/zip/app/')"
     CLIInstaller="${installerTool}/Contents/MacOS/installbuilder.sh"
     CLIArguments=(--mode unattended --unattendedmodeui none)
