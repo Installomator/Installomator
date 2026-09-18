14cf0bde76e79ee3e1173c9d0c5437da2b983d54
Henk <hbokhoven+github@gmail.com>
2026-09-18 09:04:35 +0200

    pique-label-update - suggested label

--- fragments/labels/pique.sh
+++ fragments/labels/pique.sh
@@ -1,8 +1,16 @@
 pique)
     name="Pique"
     type="pkg"
-    packageID="io.macadmins.Pique"
-    downloadURL=$(downloadURLFromGit macadmins pique )
-    appNewVersion=$(printf '%s\n' "$downloadURL" | sed -E 's#.*\/releases\/download\/([^/]+)\/.*#\1#; s/^v//')
+    if ! is-at-least 26 "$installedOSversion"; then
+        printlog "Pique requires macOS 26 or later." ERROR
+        cleanupAndExit 15 "Pique requires macOS 26 or later" ERROR
+    fi
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL=$(downloadURLFromGit macadmins pique)
+    else
+        printlog "Pique is only compatible with Apple Silicon (arm64) Macs." ERROR
+        cleanupAndExit 95 "Pique requires Apple Silicon" ERROR
+    fi
+    appNewVersion=$(printf '%s\n' "$downloadURL" | sed -nE 's#.*/releases/download/v?([^/]+)/.*#\1#p')
     expectedTeamID="T4SK8ZXCXG"
     ;;
