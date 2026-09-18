22467e7f1c7a2689f35ed7aa8ad2716a32d21602
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-09-18 08:51:48 +0200

    Silenced curl

--- fragments/labels/davinciresolvestudio.sh
+++ fragments/labels/davinciresolvestudio.sh
@@ -5,7 +5,7 @@ blackmagicdavinciresolvestudio)
     type="pkgInDmgInZip"
     versionFeed=$(curl -fs https://www.blackmagicdesign.com/api/support/us/downloads.json)
     downloadID=$(getJSONValue "${versionFeed}" ".downloads.find(item => item.name.includes('DaVinci Resolve Studio')).urls['Mac OS X'][0].downloadId")
-    downloadURL=$(curl --compressed --location --header "Content-Type: application/json;charset=UTF-8" --header "User-Agent: Mozilla/5.0" --data '{"country": "us", "platform": "Mac OS X", "product": "DaVinci Resolve Studio"}' "https://www.blackmagicdesign.com/api/register/us/download/${downloadID}")
+    downloadURL=$(curl --compressed -fsL --header "Content-Type: application/json;charset=UTF-8" --header "User-Agent: Mozilla/5.0" --data '{"country": "us", "platform": "Mac OS X", "product": "DaVinci Resolve Studio"}' "https://www.blackmagicdesign.com/api/register/us/download/${downloadID}")
     appNewVersion=$(getJSONValue "${versionFeed}" ".downloads.find(item => item.name.includes('DaVinci Resolve Studio')).urls['Mac OS X'].map(version => [version.major, version.minor, version.releaseNum].join('.'))[0]")
     blockingProcesses=( Resolve "DaVinci Control Panels Setup" "DaVinci Remote Monitor" "Fairlight Studio Utility" )
     expectedTeamID="9ZGFBWLSYP"
