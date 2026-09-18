a47a6942f835657abee99b6842226b1008ef04eb
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-09-18 08:56:40 +0200

    Silenced curl

--- fragments/labels/blackmagicdesktopvideo.sh
+++ fragments/labels/blackmagicdesktopvideo.sh
@@ -4,7 +4,7 @@ blackmagicdesktopvideo)
     type="pkgInDmgInZip"
     versionFeed=$(curl -fs https://www.blackmagicdesign.com/api/support/us/downloads.json)
     downloadID=$(getJSONValue "${versionFeed}" ".downloads.find(item => item.name.includes('Desktop Video')).urls['Mac OS X'][0].downloadId")
-    downloadURL=$(curl --compressed --location --header "Content-Type: application/json;charset=UTF-8" --header "User-Agent: Mozilla/5.0" --data '{"country": "us", "platform": "Mac OS X", "product": "Desktop Video"}' "https://www.blackmagicdesign.com/api/register/us/download/${downloadID}")
+    downloadURL=$(curl --compressed -fsL --header "Content-Type: application/json;charset=UTF-8" --header "User-Agent: Mozilla/5.0" --data '{"country": "us", "platform": "Mac OS X", "product": "Desktop Video"}' "https://www.blackmagicdesign.com/api/register/us/download/${downloadID}")
     appNewVersion=$(echo ${downloadURL} | grep -oE '/v([0-9.]+)' | cut -d'v' -f2)
     blockingProcesses=( BlackmagicDesktopVideoSetup LiveKey )
     expectedTeamID="9ZGFBWLSYP"
