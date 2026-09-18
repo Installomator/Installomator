b152bac798d7d5aec92b1e3c9042ac10dd2c2de1
RIchard Glaser <richard.glaser@utah.edu>
2026-09-18 15:45:48 -0600

    firefoxesr-label-update

--- fragments/labels/firefoxesr.sh
+++ fragments/labels/firefoxesr.sh
@@ -2,9 +2,8 @@ firefoxesr|\
 firefoxesrpkg)
     name="Firefox"
     type="pkg"
-    downloadURL="https://download.mozilla.org/?product=firefox-esr-pkg-latest-ssl&os=osx"
-    firefoxVersions=$(curl -fs "https://product-details.mozilla.org/1.0/firefox_versions.json")
-    appNewVersion=$(getJSONValue "$firefoxVersions" "FIREFOX_ESR")
-    appNewVersion=${appNewVersion:0:-3}
+    downloadURL="https://download.mozilla.org/?product=firefox-esr-pkg-latest-ssl&os=osx&lang=en-US"
+    appNewVersion=$(getJSONValue "$(curl -fsL "https://product-details.mozilla.org/1.0/firefox_versions.json")" "FIREFOX_ESR" | sed 's/esr$//')
     expectedTeamID="43AQ936H96"
+    blockingProcesses=( firefox )
     ;;
