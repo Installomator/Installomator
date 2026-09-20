317bc04f457adb0d81aa25660d51ac4203b970b6
Gil Burns <gil@gilburns.com>
2026-09-20 15:58:55 -0500

    Update lastpassdesktop.sh
    Simply and logic and use the json update feed.

--- fragments/labels/lastpassdesktop.sh
+++ fragments/labels/lastpassdesktop.sh
@@ -2,7 +2,7 @@ lastpassdesktop)
     name="LastPass for Desktop"
     type="dmg"
     downloadURL="https://download.cloud.lastpass.com/lastpass-for-desktop-macos/universal/LastPassForDesktopMac.dmg"
-    appNewVersion=$(curl -fsL "https://lastpass.com/misc_download2.php" | tr "<>" "\n\n" | awk '/id="macos-app"/{found=1} found && /Version [0-9]+\.[0-9]+\.[0-9]+/{print $2; exit}')
-    appCustomVersion() { local plist="/Applications/$name.app/Contents/Info.plist"; [[ -f "$plist" ]] || return; /usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$plist" 2>/dev/null | sed -E 's/-[0-9]+$//' }
+    lastpassJSON=$(curl -fsL "https://download.cloud.lastpass.com/lastpass-for-desktop-macos/universal/releases.json")
+    appNewVersion=$(getJSONValue "$lastpassJSON" "currentRelease")
     expectedTeamID="RNDLY9ZML8"
     ;;
