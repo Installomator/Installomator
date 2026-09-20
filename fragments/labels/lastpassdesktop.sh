6d958d474fa6e898ae059e9b5db3745b29c36aed
Gil Burns <gil@gilburns.com>
2026-09-20 15:34:40 -0500

    Update lastpassdesktop.sh
    When the Info.plist file is missing, PlistBuddy writes "File Doesn't Exist, Will Create: …" to stdout

--- fragments/labels/lastpassdesktop.sh
+++ fragments/labels/lastpassdesktop.sh
@@ -3,6 +3,6 @@ lastpassdesktop)
     type="dmg"
     downloadURL="https://download.cloud.lastpass.com/lastpass-for-desktop-macos/universal/LastPassForDesktopMac.dmg"
     appNewVersion=$(curl -fsL "https://lastpass.com/misc_download2.php" | tr "<>" "\n\n" | awk '/id="macos-app"/{found=1} found && /Version [0-9]+\.[0-9]+\.[0-9]+/{print $2; exit}')
-    appCustomVersion() { /usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "/Applications/LastPass for Desktop.app/Contents/Info.plist" | sed -E 's/-[0-9]+$//'; }
+    appCustomVersion() { local plist="/Applications/$name.app/Contents/Info.plist"; [[ -f "$plist" ]] || return; /usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$plist" 2>/dev/null | sed -E 's/-[0-9]+$//' }
     expectedTeamID="RNDLY9ZML8"
     ;;
