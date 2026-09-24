feec707a2420e71147f4d718d586cd84b47b0cef
Aurelien Arino <aurelien@withthegrid.com>
2026-09-24 10:01:54 +0200

    add cloudtalk

---
+++ fragments/labels/cloudtalk.sh
@@ -0,0 +1,8 @@
+cloudtalk)
+    name="CloudTalk"
+    type="dmg"
+    downloadURL="https://www.cloudtalk.io/download/mac"
+    appNewVersion="$(curl -fsL 'https://www.cloudtalk.io/api/app-version/?os=mac')"
+    appName="CloudTalk Phone.app"
+    expectedTeamID="7SQQD5UJAM"
+    ;;
