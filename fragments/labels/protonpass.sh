302d16d48afed0dbccc334b56346f9690ff8fe7c
Aurelien Arino <aurelien@withthegrid.com>
2026-09-24 18:40:11 +0200

    address feedback

--- fragments/labels/protonpass.sh
+++ fragments/labels/protonpass.sh
@@ -1,7 +1,17 @@
 protonpass)
-  name="Proton Pass"
-  type="dmg"
-  downloadURL=$(getJSONValue "$(curl -fsL 'https://proton.me/download/PassDesktop/darwin/universal/version.json')" 'Releases[0].File[0].Url')
-  appNewVersion=$( grep -oE '\d+\.[0-9.]*\d' <<< $downloadURL )
-  expectedTeamID="2SB5Z68H26"
-  ;;
+    name="Proton Pass"
+    type="dmg"
+    protonPassData=$(curl -fsL 'https://proton.me/download/PassDesktop/darwin/universal/version.json')
+    protonPassReleaseCount=$(getJSONValue "$protonPassData" 'Releases.length')
+    for (( protonPassReleaseIndex=0; protonPassReleaseIndex<protonPassReleaseCount; protonPassReleaseIndex++ )); do
+        if [[ $(getJSONValue "$protonPassData" "Releases[$protonPassReleaseIndex].CategoryName") == "Stable" ]]; then
+            appNewVersion=$(getJSONValue "$protonPassData" "Releases[$protonPassReleaseIndex].Version")
+            downloadURL=$(getJSONValue "$protonPassData" "Releases[$protonPassReleaseIndex].File[0].Url")
+            break
+        fi
+    done
+    if [[ -z $appNewVersion || -z $downloadURL ]]; then
+        cleanupAndExit 2 "Unable to resolve the latest stable Proton Pass release." ERROR
+    fi
+    expectedTeamID="2SB5Z68H26"
+    ;;
