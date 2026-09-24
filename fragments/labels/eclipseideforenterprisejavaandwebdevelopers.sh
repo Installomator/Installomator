d7bc4dd6c4af8cf6cb0c49778903173889c355b0
RIchard Glaser <richard.glaser@utah.edu>
2026-09-24 09:20:41 -0600

    Create eclipseideforenterprisejavaandwebdevelopers.sh

---
+++ fragments/labels/eclipseideforenterprisejavaandwebdevelopers.sh
@@ -0,0 +1,23 @@
+eclipseideforenterprisejavaandwebdevelopers)
+    name="Eclipse"
+    type="dmg"
+    eclipseRelease=$(curl -fsL "https://download.eclipse.org/technology/epp/downloads/release/release.xml" | xpath 'string(/packages/present)' 2>/dev/null)
+    eclipseReleaseMonth="${eclipseRelease%%/*}"
+    eclipseReleaseMetadata=$(curl -fsL "https://projects.eclipse.org/api/projects/technology.packaging/releases?pagesize=10&order_by=DESC")
+    for eclipseReleaseIndex in {0..9}; do
+        eclipseReleaseDate=$(getJSONValue "$eclipseReleaseMetadata" "releases[$eclipseReleaseIndex].date")
+        if [[ "${eclipseReleaseDate%-*}" == "$eclipseReleaseMonth" ]]; then
+            appNewVersion=$(getJSONValue "$eclipseReleaseMetadata" "releases[$eclipseReleaseIndex].name")
+            break
+        fi
+    done
+    if [[ $(arch) == "arm64" ]]; then
+        eclipseArchitecture="aarch64"
+    elif [[ $(arch) == "i386" ]]; then
+        eclipseArchitecture="x86_64"
+    fi
+    downloadURL="https://download.eclipse.org/technology/epp/downloads/release/${eclipseRelease}/eclipse-jee-${eclipseRelease/\//-}-macosx-cocoa-${eclipseArchitecture}.dmg"
+    appCustomVersion() { if [[ $(defaults read "$targetDir/$appName/Contents/Info.plist" CFBundleIdentifier 2>/dev/null) == "epp.package.jee" ]]; then defaults read "$targetDir/$appName/Contents/Info.plist" CFBundleShortVersionString; fi }
+    expectedTeamID="JCDTMS22B4"
+    blockingProcesses=( eclipse )
+    ;;
