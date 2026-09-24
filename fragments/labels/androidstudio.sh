5688449e8d9c22de43b711f1e3ceec141554e560
Gil Burns <gil@gilburns.com>
2026-09-23 19:43:24 -0500

    Update androidstudio.sh

--- fragments/labels/androidstudio.sh
+++ fragments/labels/androidstudio.sh
@@ -5,7 +5,6 @@ androidstudio)
         downloadURL=$(curl -fsL "https://developer.android.com/studio#downloads" | grep -i arm.dmg | head -2 | grep -o -i -E "https.*" | cut -d '"' -f1)
     elif [[ $(arch) == i386 ]]; then
         downloadURL=$(curl -fsL "https://developer.android.com/studio#downloads" | grep -i mac.dmg | head -2 | grep -o -i -E "https.*" | cut -d '"' -f1)
-        appNewVersion=$()
     fi
     androidXML="https://dl.google.com/android/studio/patches/updates.xml"
     appNewVersion=$(curl -fsSL "$androidXML" | xmllint --xpath 'string((//channel[@status="release"]/build)[1]/@number)' - 2>/dev/null)
